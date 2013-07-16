a3bbaa1a2479902a890926beef1c059cb85d6917
Daniel Rotter <daniel.rotter@massiveart.com>
2013-07-16 17:46:10 +0200

    initialised first version of bundle

---
+++ src/Sulu/Bundle/TranslateBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sulu\TranslateBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('SuluTranslateBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Sulu/Bundle/TranslateBundle/SuluTranslateBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sulu\TranslateBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SuluTranslateBundle extends Bundle
+{
+}
