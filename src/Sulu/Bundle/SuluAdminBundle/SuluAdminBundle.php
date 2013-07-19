31164057346c061ccda64c64515dfe99f31389da
Daniel Rotter <daniel.rotter@massiveart.com>
2013-07-19 14:55:02 +0200

    scaffolded AddAdmin-CompilerPass

--- src/Sulu/Bundle/SuluAdminBundle/SuluAdminBundle.php
+++ src/Sulu/Bundle/SuluAdminBundle/SuluAdminBundle.php
@@ -3,7 +3,15 @@
 namespace Sulu\Bundle\AdminBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Sulu\Bundle\AdminBundle\DependencyInjection\Compiler\AddAdminPass;
 
 class SuluAdminBundle extends Bundle
 {
+    public function build(ContainerBuilder $container) {
+        parent::build($container);
+
+        $container->addCompilerPass(new AddAdminPass);
+    }
 }
