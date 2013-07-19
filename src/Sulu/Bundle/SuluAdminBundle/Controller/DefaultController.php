1461a1e62762c5e3e0cd7f8fedda2bb3d2da5dae
Daniel Rotter <daniel.rotter@massiveart.com>
2013-07-19 16:06:34 +0200

    registered something in the AdminPool

--- src/Sulu/Bundle/SuluAdminBundle/Controller/DefaultController.php
+++ src/Sulu/Bundle/SuluAdminBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('SuluAdminBundle:Default:index.html.twig', array('name' => 'SULU'));
+        return $this->render('SuluAdminBundle:Default:index.html.twig', array('name' => var_dump($this->get('sulu_admin.admin_pool'))));
     }
 }
