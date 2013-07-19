edd2cc1a7da1cf47d21c933aae4df334ebe60df2
Daniel Rotter <daniel.rotter@massiveart.com>
2013-07-19 15:00:32 +0200

    added basics for AdminPool

---
+++ src/Sulu/Bundle/AdminBundle/Admin/Admin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: danielrotter
+ * Date: 19.07.13
+ * Time: 14:55
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Sulu\Bundle\AdminBundle\Admin;
+
+
+abstract class Admin {
+
+}
---
+++ src/Sulu/Bundle/AdminBundle/Admin/AdminPool.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: danielrotter
+ * Date: 19.07.13
+ * Time: 14:55
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Sulu\Bundle\AdminBundle\Admin;
+
+
+/**
+ * The AdminPool is a container for all the registrated Admin-objects.
+ *
+ * @package Sulu\Bundle\AdminBundle\Admin
+ */
+class AdminPool {
+    /**
+     * The array for all the admin-objects
+     * @var array
+     */
+    private $pool;
+}
