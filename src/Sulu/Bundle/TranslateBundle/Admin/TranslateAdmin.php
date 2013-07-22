b4565ddbbe08f04aba16fc3511908b51591cb60b
Daniel Rotter <daniel.rotter@massiveart.com>
2013-07-22 16:33:20 +0200

    added licensing information and head comments

---
+++ src/Sulu/Bundle/TranslateBundle/Admin/TranslateAdmin.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART Webservices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\TranslateBundle\Admin;
+
+use Sulu\Bundle\AdminBundle\Admin\Admin;
+use Sulu\Bundle\AdminBundle\Navigation\Navigation;
+use Sulu\Bundle\AdminBundle\Navigation\NavigationItem;
+
+class TranslateAdmin extends Admin {
+
+    function __construct()
+    {
+        $rootNavigationItem = new NavigationItem('Settings');
+        $rootNavigationItem->addChild(new NavigationItem('Translate'));
+        $this->navigation = new Navigation($rootNavigationItem);
+    }
+}
