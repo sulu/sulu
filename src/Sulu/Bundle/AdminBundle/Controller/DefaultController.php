817dde556881d64831f019bd0811a429073b738c
Daniel Rotter <daniel.rotter@massiveart.com>
2013-07-24 07:51:44 +0200

    removed debugging information from controller

--- src/Sulu/Bundle/AdminBundle/Controller/DefaultController.php
+++ src/Sulu/Bundle/AdminBundle/Controller/DefaultController.php
@@ -16,9 +16,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $pool = $this->get('sulu_admin.admin_pool');
-        echo var_dump($pool);
-
-        return $this->render('SuluAdminBundle:Default:index.html.twig', array('name' => 'SULU'));
+        return $this->render('SuluAdminBundle:Default:index.html.twig', array());
     }
 }
