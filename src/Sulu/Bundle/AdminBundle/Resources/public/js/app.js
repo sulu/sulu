b3adf01c940689d2b5c6da715877ce63a758e73c
Daniel Rotter <daniel.rotter@massiveart.com>
2013-07-25 09:19:58 +0200

    set up requirejs

---
+++ src/Sulu/Bundle/AdminBundle/Resources/public/js/app.js
@@ -0,0 +1,9 @@
+define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
+    initialize = function() {
+        alert($('#navigation'));
+    };
+
+    return {
+        initialize: initialize
+    }
+})
