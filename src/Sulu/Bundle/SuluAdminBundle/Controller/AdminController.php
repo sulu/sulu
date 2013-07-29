5d6bfc4b3a3cb00a5d871b62e52c24340d6ccfae
Daniel Rotter <daniel.rotter@massiveart.com>
2013-07-29 09:26:55 +0200

    instantiated backbone router

---
+++ src/Sulu/Bundle/SuluAdminBundle/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART Webservices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class AdminController extends Controller {
+    public function routesAction() {
+
+    }
+}
