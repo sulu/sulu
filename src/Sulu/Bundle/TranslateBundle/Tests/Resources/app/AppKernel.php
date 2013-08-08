849ca15732ccf62607ff7b2a673eeb6d22cd9197
Thomas Schedler <thomas@chirimoya.at>
2013-08-08 15:34:45 +0200

    test kernel

---
+++ src/Sulu/Bundle/TranslateBundle/Tests/Resources/app/AppKernel.php
@@ -0,0 +1,30 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            // Dependencies
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+
+            // Sulu
+            new \Sulu\Bundle\TranslateBundle\SuluTranslateBundle(),
+        );
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__ . '/config/config.yml');
+    }
+}
