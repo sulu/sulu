9fe81d5ba2139ea545526081dff47462c2ae0d9c
Daniel Rotter <daniel.rotter@massiveart.com>
2013-08-12 10:58:58 +0200

    added command to admin

--- src/Sulu/Bundle/TranslateBundle/Admin/SuluTranslateAdmin.php
+++ src/Sulu/Bundle/TranslateBundle/Admin/SuluTranslateAdmin.php
@@ -13,11 +13,12 @@ namespace Sulu\Bundle\TranslateBundle\Admin;
 use Sulu\Bundle\AdminBundle\Admin\Admin;
 use Sulu\Bundle\AdminBundle\Navigation\Navigation;
 use Sulu\Bundle\AdminBundle\Navigation\NavigationItem;
+use Sulu\Bundle\TranslateBundle\Command\ImportCommand;
 
 class SuluTranslateAdmin extends Admin
 {
 
-    function __construct()
+    public function __construct()
     {
         $rootNavigationItem = new NavigationItem('Root');
         $settings = new NavigationItem('Settings');
@@ -28,5 +29,15 @@ class SuluTranslateAdmin extends Admin
         $translate->setType('content');
         $settings->addChild($translate);
         $this->setNavigation(new Navigation($rootNavigationItem));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCommands()
+    {
+        return array(
+            new ImportCommand()
+        );
     }
 }
---
+++ src/Sulu/Bundle/TranslateBundle/Command/ImportCommand.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART Webservices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\TranslateBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Sulu\Bundle\TranslateBundle\Translate\Import;
+
+/**
+ * The command to execute an import on the console
+ * @package Sulu\Bundle\TranslateBundle\Command
+ */
+class ImportCommand extends ContainerAwareCommand {
+    protected function configure()
+    {
+        $this->setName('sulu:translate:import')
+            ->setDescription('Import a xliff catalogue in Sulu')
+            ->addArgument(
+                'file',
+                InputArgument::REQUIRED,
+                'The file to import'
+            )
+            ->addArgument(
+                'name',
+                InputArgument::REQUIRED,
+                'The name of the new translate package in Sulu'
+            )
+            ->addArgument(
+                'locale',
+                InputArgument::REQUIRED,
+                'The locale of the first catalogue in the translate package'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $file = $input->getArgument('file');
+        $name = $input->getArgument('name');
+        $locale = $input->getArgument('locale');
+
+        $import = $this->getContainer()->get('sulu_translate.import');
+
+        $import->setFile($file);
+        $import->setName($name);
+        $import->setFormat(Import::XLIFF); //FIXME design configurable, if there will be more supported formats
+        $import->setLocale($locale);
+        $import->execute();
+        $output->writeln('Successfully imported file to Sulu Database!');
+    }
+}
