8efa8bfa2a4a0b246274f8a7f87c1637a373e200
Daniel Rotter <daniel.rotter@massiveart.com>
2013-08-14 14:44:13 +0200

    implemented own repository

--- src/Sulu/Bundle/TranslateBundle/Entity/TranslationRepository.php
+++ src/Sulu/Bundle/TranslateBundle/Entity/TranslationRepository.php
@@ -10,22 +10,59 @@ use Doctrine\ORM\EntityRepository;
  */
 class TranslationRepository extends EntityRepository
 {
-    public function findFiltered($packageId, $locale)
+    public function findFiltered($packageId, $locale, $backend = null, $frontend = null, $location = null)
     {
-        return $this->getEntityManager()
-            ->createQuery('
+        $dql = '
                 SELECT t
                 FROM SuluTranslateBundle:Translation t
                 JOIN t.catalogue ca
                 JOIN ca.package p
                 JOIN t.code co
+                JOIN co.location l
                 WHERE ca.locale = :locale
                     AND p.id = :packageId
-            ')
+            ';
+
+        // add additional conditions if backend or frontend is set
+        if ($backend != null) {
+            $dql .= '
+                AND co.backend = :backend
+            ';
+        }
+
+        if( $frontend != null) {
+            $dql .= '
+                AND co.frontend = :frontend
+            ';
+        }
+
+        if ($location != null) {
+            $dql .= '
+                AND l.name = :location
+            ';
+        }
+
+        $query = $this->getEntityManager()
+            ->createQuery($dql)
             ->setParameters(array(
                     'packageId' => $packageId,
                     'locale' => $locale
-                ))
-            ->getResult();
+                )
+            );
+
+        // set the additional parameter, if they are set
+        if ($backend != null) {
+            $query->setParameter('backend', $backend);
+        }
+
+        if ($frontend != null) {
+            $query->setParameter('frontend', $frontend);
+        }
+
+        if ($location != null) {
+            $query->setParameter('location', $location);
+        }
+
+        return $query->getResult();
     }
 }
