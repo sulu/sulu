cdd3c58da0f6bb49975feb71b03ef0646cbecac8
Johannes Wachter <johannes.wachter@massiveart.com>
2013-08-20 17:04:54 +0200

    initiated controller and test class

---
+++ src/Sulu/Bundle/TranslateBundle/Controller/CodesController.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\TranslateBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+
+/**
+ * Makes the translation codes accessible trough an REST-API
+ * @package Sulu\Bundle\TranslateBundle\Controller
+ */
+class CodesController extends FOSRestController
+{
+
+}
---
+++ src/Sulu/Bundle/TranslateBundle/Tests/Controller/CodesControllerTest.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\TranslateBundle\Tests\Controller;
+
+use Doctrine\ORM\Tools\SchemaTool;
+use Sulu\Bundle\CoreBundle\Tests\DatabaseTestCase;
+
+class CodesControllerTest extends DatabaseTestCase
+{
+    /**
+     * @var array
+     */
+    protected static $entities;
+
+    /**
+     * @var SchemaTool
+     */
+    protected static $tool;
+
+    public function setUp()
+    {
+        $this->setUpSchema();
+
+        // TODO create entities
+
+        self::$em->flush();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        self::$tool->dropSchema(self::$entities);
+    }
+
+    public function setUpSchema() {
+        self::$tool = new SchemaTool(self::$em);
+
+        self::$entities = array(
+            self::$em->getClassMetadata('Sulu\Bundle\TranslateBundle\Entity\Catalogue'),
+            self::$em->getClassMetadata('Sulu\Bundle\TranslateBundle\Entity\Code'),
+            self::$em->getClassMetadata('Sulu\Bundle\TranslateBundle\Entity\Location'),
+            self::$em->getClassMetadata('Sulu\Bundle\TranslateBundle\Entity\Package'),
+            self::$em->getClassMetadata('Sulu\Bundle\TranslateBundle\Entity\Translation'),
+        );
+
+        self::$tool->createSchema(self::$entities);
+    }
+}
