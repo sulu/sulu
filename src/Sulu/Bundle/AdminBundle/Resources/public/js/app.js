133671a8498e6ec173944fe70439bbeaf99c6290
Thomas Schedler <thomas@chirimoya.at>
2013-08-20 10:39:11 +0200

    code review & cleanup

--- src/Sulu/Bundle/AdminBundle/Resources/public/js/app.js
+++ src/Sulu/Bundle/AdminBundle/Resources/public/js/app.js
@@ -1,7 +1,7 @@
 /*
  * This file is part of the Sulu CMS.
  *
- * (c) MASSIVE ART Webservices GmbH
+ * (c) MASSIVE ART WebServices GmbH
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
@@ -11,29 +11,28 @@ define(['jquery', 'husky', 'router'], function($, Husky, Router) {
 
     'use strict';
 
-    var $navigation = $('#navigation');
+    var $navigation = $('#navigation'),
+        $content = $('#content'),
 
-    var $content = $('#content');
+        initialize = function() {
+            var App = window.App || {};
 
-    var initialize = function() {
-        var App = window.App || {};
+            Router.initialize(App);
 
-        Router.initialize(App);
+            $navigation.huskyNavigation({
+                url: 'navigation'
+            });
 
-        $navigation.huskyNavigation({
-            url: 'navigation'
-        });
+            $navigation.data('Husky.Ui.Navigation').on('navigation:item:content:show', function(item) {
+                Router.navigate(item.get('action'));
+            });
 
-        $navigation.data('Husky.Ui.Navigation').on('navigation:item:content:show', function(item){
-            Router.navigate(item.get('action'));
-        });
+            // Make some Shortcuts globally available
+            App.$content = $content;
+            App.Navigation = $navigation.data('Husky.Ui.Navigation');
 
-        // Make some Shortcuts globally available
-        App.$content = $content;
-        App.$navigation = $navigation.data('Husky.Ui.Navigation');
-
-        window.App = App;
-    };
+            window.App = App;
+        };
 
     return {
         initialize: initialize
--- src/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky.min.css
+++ src/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky.min.css
@@ -1,7 +1,7 @@
 /* 
  * husky v0.1.0
  *  
- * (c) MASSIVE ART Webservices GmbH
+ * (c) MASSIVE ART WebServices GmbH
  * 
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
