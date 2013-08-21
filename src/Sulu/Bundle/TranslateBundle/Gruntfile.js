c44b4edc5c7945e26f5e696df288a93a0e000e6c
Michael Zangerle <michael.zangerle@massiveart.com>
2013-08-21 16:43:05 +0200

    package.json and gruntfile.js update for sass

--- src/Sulu/Bundle/TranslateBundle/Gruntfile.js
+++ src/Sulu/Bundle/TranslateBundle/Gruntfile.js
@@ -1,10 +1,14 @@
 module.exports = function (grunt) {
+
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         copy: {
             public: {
                 files: [
-                    {expand: true, cwd: 'Resources/public', src: ['**'], dest: '../../../../../../web/bundles/sulutranslate/'}
+                    {expand: true, cwd: 'Resources/public', src: ['**', '!**/scss/**'], dest: '../../../../../../web/bundles/sulutranslate/'}
                 ]
             }
         },
@@ -19,19 +23,45 @@ module.exports = function (grunt) {
             }
         },
         watch: {
-            scripts: {
-                files: ['Resources/public/**'],
-                tasks: ['publish']
+            options: {
+                nospawn: true
+            },
+            compass: {
+                files: ['Resources/public/scss/{,*/}*.{scss,sass}'],
+                tasks: ['compass:dev']
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+        cssmin: {
+            // TODO: options: { banner: '<%= meta.banner %>' },
+            compress: {
+                files: {
+                       'dist/main.min.css': ['Resources/public/css/']
+                }
+            }
+        },
+        compass: {
+            dev: {
+                options: {
+                    sassDir: 'Resources/public/scss/',
+                    specify: ['Resources/public/scss/main.scss'],
+                    cssDir: 'Resources/public/css/',
+                    relativeAssets: false
+                }
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-
     grunt.registerTask('publish', [
         'clean:public',
         'copy:public'
+    ]);
+
+    grunt.registerTask('default', [
+        'watch'
     ]);
 };
