866e6dc0fd3eb0df814b2f6ada080f5e4d404bfc
Johannes Wachter <johannes.wachter@massiveart.com>
2013-08-22 12:42:24 +0200

    add comments

--- src/Sulu/Bundle/TranslateBundle/Entity/CodeRepository.php
+++ src/Sulu/Bundle/TranslateBundle/Entity/CodeRepository.php
@@ -18,8 +18,17 @@ use Doctrine\ORM\EntityRepository;
  */
 class CodeRepository extends EntityRepository
 {
+    /**
+     * Searches Entities by where clauses, pagination and sorted
+     * @param integer|null $limit Page size for Pagination
+     * @param integer|null $offset Offset for Pagination
+     * @param array|null $sorting Columns to sort
+     * @param array|null $where Where clauses
+     * @return array Results
+     */
     public function findGetAll($limit = null, $offset = null, $sorting = null, $where = array())
     {
+        // create basic query
         $qb = $this->createQueryBuilder('u')
             ->join('u.package', 'p')
             ->join('u.translations', 't')
@@ -28,12 +37,15 @@ class CodeRepository extends EntityRepository
             ->addSelect('p')
             ->addSelect('t')
             ->addSelect('c');
+        // add order by
         foreach ($sorting as $k => $d) {
             $qb->addOrderBy('u.' . $k, $d);
         }
+        // add pagination
         $qb->setFirstResult($offset);
         $qb->setMaxResults($limit);
 
+        // if needed add where statements
         if (sizeof($where) > 0) {
             $and = $qb->expr()->andX();
             foreach ($where as $k => $v) {
