b19a7455bd193566acd699279aab29a9f337059f
Johannes Wachter <johannes.wachter@massiveart.com>
2013-08-22 15:56:58 +0200

    init with sensio generation bundle files

---
+++ src/Sulu/Bundle/GeneratorBundle/Generator/Generator.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\GeneratorBundle\Generator;
+
+/**
+ * Generator is the base class for all generators.
+ */
+class Generator
+{
+    private $skeletonDirs;
+
+    /**
+     * Sets an array of directories to look for templates.
+     *
+     * The directories must be sorted from the most specific to the most
+     * directory.
+     *
+     * @param array $skeletonDirs An array of skeleton dirs
+     */
+    public function setSkeletonDirs($skeletonDirs)
+    {
+        $this->skeletonDirs = is_array($skeletonDirs) ? $skeletonDirs : array($skeletonDirs);
+    }
+
+    protected function render($template, $parameters)
+    {
+        $twig = new \Twig_Environment(new \Twig_Loader_Filesystem($this->skeletonDirs), array(
+            'debug' => true,
+            'cache' => false,
+            'strict_variables' => true,
+            'autoescape' => false,
+        ));
+
+        return $twig->render($template, $parameters);
+    }
+
+    protected function renderFile($template, $target, $parameters)
+    {
+        if (!is_dir(dirname($target))) {
+            mkdir(dirname($target), 0777, true);
+        }
+
+        return file_put_contents($target, $this->render($template, $parameters));
+    }
+}
