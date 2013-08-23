10e8dc9e15d6b76a9f8d75c361bc93c3af0349b8
Johannes Wachter <johannes.wachter@massiveart.com>
2013-08-23 15:05:09 +0200

    added Template for Contact Form

---
+++ src/Sulu/Bundle/SuluContactBundle/Resources/public/js/bundle.js
@@ -0,0 +1,28 @@
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+define(['router'], function (Router) {
+
+    'use strict';
+
+    var initialize = function () {
+        // show form for new translation package
+        Router.route('contact/add', 'contact:form', function() {
+            require(['sulucontact/controller/contact/form'], function(Form) {
+                new Form({
+                    el: App.$content
+                });
+            });
+        });
+    };
+
+    return {
+        initialize: initialize
+    }
+});
---
+++ src/Sulu/Bundle/SuluContactBundle/Resources/public/js/main.js
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+require.config({
+    paths: {
+        sulucontact: '../../sulucontact/js'
+    }
+});
+
+define(['sulucontact/bundle'], function (Bundle) {
+
+    'use strict';
+
+    Bundle.initialize();
+
+});
