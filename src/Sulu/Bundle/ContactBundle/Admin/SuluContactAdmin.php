698672e9ad61a27b7cb032d67b93bd0fff7db7fd
Daniel Rotter <daniel.rotter@massiveart.com>
2013-08-23 09:01:27 +0200

    initialized bundle

---
+++ src/Sulu/Bundle/ContactBundle/Admin/SuluContactAdmin.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\ContactBundle\Admin;
+
+use Sulu\Bundle\AdminBundle\Admin\Admin;
+use Sulu\Bundle\AdminBundle\Navigation\NavigationItem;
+
+class SuluContactAdmin extends Admin
+{
+
+    public function __construct()
+    {
+        $rootNavigationItem = new NavigationItem('Root');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCommands()
+    {
+        return array();
+    }
+
+}
