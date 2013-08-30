a0b43089c59dde392e442275330edbbe32f4949c
Johannes Wachter <johannes.wachter@massiveart.com>
2013-08-30 15:46:29 +0200

    added drop down for types, added remove email

--- src/Sulu/Bundle/SuluContactBundle/Resources/public/js/controller/contact/form.js
+++ src/Sulu/Bundle/SuluContactBundle/Resources/public/js/controller/contact/form.js
@@ -16,11 +16,14 @@ define([
     'sulucontact/model/country',
     'sulucontact/model/email',
     'sulucontact/model/emailType',
+    'sulucontact/collections/emailTypes',
     'sulucontact/model/phone',
     'sulucontact/model/phoneType',
+    'sulucontact/collections/phoneTypes',
     'sulucontact/model/address',
-    'sulucontact/model/addressType'
-], function ($, Backbone, Router, Account, Contact, Country, Email, EmailType, Phone, PhoneType, Address, AddressType) {
+    'sulucontact/model/addressType',
+    'sulucontact/collections/addressTypes'
+], function($, Backbone, Router, Account, Contact, Country, Email, EmailType, EmailTypes, Phone, PhoneType, PhoneTypes, Address, AddressType, AddressTypes) {
 
     'use strict';
 
@@ -32,63 +35,99 @@ define([
             'submit #contact-form': 'submitForm',
             'click #addEmail': 'addEmail',
             'click #addPhone': 'addPhone',
-            'click #addAddress': 'addAddress'
+            'click #addAddress': 'addAddress',
+            'click .remove-email': 'removeEmail',
+            'click .remove-phone': 'removePhone',
+            'click .remove-address': 'removeAddress'
         },
 
-        initialize: function () {
+        initialize: function() {
             this.render();
         },
 
-        getTabs: function (id) {
+        getTabs: function(id) {
             // TODO Tabs contact form
             return null;
         },
 
-        render: function () {
+        render: function() {
             Backbone.Relational.store.reset(); //FIXME really necessary?
-            require(['text!/contact/template/contact/form'], function (Template) {
+            require(['text!/contact/template/contact/form'], function(Template) {
                 var template;
 
                 var contactJson = _.clone(Contact.prototype.defaults);
-                var emailJson = _.clone(Email.prototype.defaults);
-                var phoneJson = _.clone(Phone.prototype.defaults);
-                var addressJson = _.clone(Address.prototype.defaults);
 
                 if (!this.options.id) {
                     contact = new Contact();
-
-                    this.fillFields(contactJson.emails, 2, emailJson);
-                    this.fillFields(contactJson.phones, 2, phoneJson);
-                    this.fillFields(contactJson.addresses, 1, addressJson);
-
-                    template = _.template(Template, contactJson);
-                    this.$el.html(template);
+                    this.initTemplate(contactJson, template, Template);
                 } else {
                     contact = new Contact({id: this.options.id});
                     contact.fetch({
-                        success: function (contact) {
+                        success: function(contact) {
                             var contactJson = contact.toJSON();
-
-                            this.fillFields(contactJson.emails, 2, emailJson);
-                            this.fillFields(contactJson.phones, 2, phoneJson);
-                            this.fillFields(contactJson.addresses, 1, addressJson);
-
-                            template = _.template(Template, contactJson);
-                            this.$el.html(template);
+                            this.initTemplate(contactJson, template, Template);
                         }.bind(this)
                     });
                 }
             }.bind(this));
         },
 
-        submitForm: function (event) {
+        initTemplate: function(contactJson, template, Template) {
+            var emailJson = _.clone(Email.prototype.defaults);
+            var phoneJson = _.clone(Phone.prototype.defaults);
+            var addressJson = _.clone(Address.prototype.defaults);
+
+            this.fillFields(contactJson.emails, 2, emailJson);
+            this.fillFields(contactJson.phones, 2, phoneJson);
+            this.fillFields(contactJson.addresses, 1, addressJson);
+
+            template = _.template(Template, contactJson);
+            this.$el.html(template);
+
+            this.initFields();
+        },
+
+        initFields: function() {
+            this.$('#company').huskyAutoComplete({
+                url: '/contacts/api/accounts'
+            });
+
+            var that = this;
+            this.$('.type-value').each(function(event) {
+                if ($(this).parent().hasClass('email-item')) {
+                    that.initDropDown($(this).parent(), emailTypes);
+                } else if ($(this).parent().hasClass('phone-item')) {
+                    that.initDropDown($(this).parent(), phoneTypes);
+                } else if ($(this).parent().parent().parent().hasClass('address-item')) {
+                    that.initDropDown($(this).parent(), addressTypes);
+                }
+            });
+        },
+
+        initDropDown: function(that, types) {
+            var $element = $(that);
+            var dd = $element.huskyDropDown({
+                data: types,
+                trigger: '.drop-down-trigger',
+                setParentDropDown: true
+            });
+
+            dd.data('Husky.Ui.DropDown').on('drop-down:click-item', function(item) {
+                console.log("click item: " + item);
+                $element.find('.type-value').data('id', item.id);
+                $element.find('.type-name').text(item.name);
+            }.bind(this));
+        },
+
+        submitForm: function(event) {
             Backbone.Relational.store.reset(); //FIXME really necessary?
             event.preventDefault();
             contact.set({
                 firstName: this.$('#firstName').val(),
                 lastName: this.$('#lastName').val(),
                 title: this.$('#title').val(),
-                position: this.$('#position').val()
+                position: this.$('#position').val(),
+                account: new Account({id: this.$('#company').data('id')})
             });
 
             // FIXME remove
@@ -96,7 +135,7 @@ define([
                 id: 1
             });
 
-            $('#emails .email-item').each(function () {
+            $('#emails .email-item').each(function() {
                 var email = contact.get('emails').get($(this).data('id'));
                 if (!email) {
                     email = new Email();
@@ -105,7 +144,7 @@ define([
                 if (emailValue) {
                     email.set({
                         email: emailValue,
-                        emailType: emailType
+                        emailType: {id: $(this).find('.type-value').data('id')}
                     });
                     contact.get('emails').add(email);
                 }
@@ -116,7 +155,7 @@ define([
                 id: 1
             });
 
-            $('#phones .phone-item').each(function () {
+            $('#phones .phone-item').each(function() {
                 var phone = contact.get('phones').get($(this).data('id'));
                 if (!phone) {
                     phone = new Phone();
@@ -125,7 +164,7 @@ define([
                 if (phoneValue) {
                     phone.set({
                         phone: phoneValue,
-                        phoneType: phoneType
+                        phoneType: {id: $(this).find('.type-value').data('id')}
                     });
 
                     contact.get('phones').add(phone);
@@ -140,7 +179,7 @@ define([
                 id: 1
             });
 
-            $('#addresses .address-item').each(function () {
+            $('#addresses .address-item').each(function() {
                 var address = contact.get('addresses').get($(this).data('id'));
                 if (!address) {
                     address = new Address();
@@ -161,7 +200,7 @@ define([
                         city: city,
                         state: state,
                         country: country,
-                        addressType: addressType
+                        addressType: {id: $(this).find('.type-value').data('id')}
                     });
 
                     contact.get('addresses').add(address);
@@ -169,36 +208,75 @@ define([
             });
 
             contact.save(null, {
-                success: function () {
+                success: function() {
                     Router.navigate('contacts/people');
                 }
             });
         },
 
-        addEmail: function (event) {
+        addEmail: function(event) {
             var $element = $(event.currentTarget);
             var id = $element.data("target-id");
             var $div = $('#' + id);
 
-            $div.append(_.template(this.staticTemplates.emailRow(), {email: ''}));
+            var $email = $(_.template(this.staticTemplates.emailRow(), {email: ''}));
+            $div.append($email);
+            //$(window).scrollTop($email.offset().top);
+
+            this.initDropDown($email.find('.type-value').parent(), emailTypes);
         },
 
-        addPhone: function (event) {
+        addPhone: function(event) {
             var $element = $(event.currentTarget);
             var id = $element.data("target-id");
             var $div = $('#' + id);
 
-            $div.append(_.template(this.staticTemplates.phoneRow(), {phone: ''}));
+            var $phone = $(_.template(this.staticTemplates.phoneRow(), {phone: ''}));
+            $div.append($phone);
+            //$(window).scrollTop($phone.offset().top);
+
+            this.initDropDown($phone.find('.type-value').parent(), phoneTypes);
         },
 
-        addAddress: function (event) {
+        addAddress: function(event) {
             var $element = $(event.currentTarget);
             var id = $element.data("target-id");
             var $div = $('#' + id);
 
-            require(['text!sulucontact/templates/contact/address.html'], function (Template) {
-                $div.append(_.template(Template, {id: null, street: '', number: '', additional: '', zip: '', city: '', state: '', country: ''}));
-            });
+            require(['text!sulucontact/templates/contact/address.html'], function(Template) {
+                var $address = $(_.template(Template, {address: {
+                    id: null,
+                    street: '',
+                    number: '',
+                    additional: '',
+                    zip: '',
+                    city: '',
+                    state: '',
+                    country: ''
+                }}));
+                $div.append($address);
+                $(window).scrollTop($address.offset().top);
+
+                this.initDropDown($address.find('.type-value').parent(), addressTypes);
+            }.bind(this));
+        },
+
+        removeEmail: function(event) {
+            var $element = $(event.currentTarget).parent();
+            var id = $element.data('id');
+            if (id != null && id != '') {
+                var email = contact.get('emails').get(id);
+                contact.get('emails').remove(email);
+            }
+            $element.remove();
+        },
+
+        removePhone: function($element) {
+
+        },
+
+        removeAddress: function($element) {
+
         },
 
         fillFields: function(field, minAmount, value) {
@@ -208,19 +286,27 @@ define([
         },
 
         staticTemplates: {
-            emailRow: function () {
+            emailRow: function() {
                 return [
-                    '<div class="grid-col-6 email-item">',
-                    '<label>[Email Type]</label>',
-                    '<input class="form-element emailValue" type="text" value="<%= email %>"/>',
+                    '<div class="grid-col-6 email-item" data-id="<%= email.id %>">',
+                    '<label class="bold drop-down-trigger type-value pull-left" data-id="<%= (!!email.emailType)?email.emailType.id :\'[Email Type ID]\' %>">',
+                    '<span class="type-name"><%= (!!email.emailType)?email.emailType.name :\'[Email Type]\' %></span>',
+                    '<span class="dropdown-toggle inline"></span>',
+                    '</label>',
+                    '<div class="remove-email"><span class="icon-remove pull-right"></span></div>',
+                    '<input class="form-element emailValue" type="text" value="<%= email.email %>"/>',
                     '</div>'
                 ].join('')
             },
-            phoneRow: function () {
+            phoneRow: function() {
                 return [
-                    '<div class="grid-col-6 phone-item">',
-                    '<label>[Phone Type]</label>',
-                    '<input class="form-element phoneValue" type="text" value="<%= phone %>"/>',
+                    '<div class="grid-col-6 phone-item" data-id="<%= phone.id %>">',
+                    '<label class="bold drop-down-trigger type-value pull-left" data-id="<%= (!!phone.phoneType)? phone.phoneType.id:\'[Phone Type ID]\' %>">',
+                    '<span class="type-name"><%= (!!phone.phoneType)? phone.phoneType.name:\'[Phone Type]\' %></span>',
+                    '<span class="dropdown-toggle inline"></span>',
+                    '</label>',
+                    '<div class="remove-phone"><span class="icon-remove pull-right"></span></div>',
+                    '<input class="form-element phoneValue" type="text" value="<%= phone.phone %>"/>',
                     '</div>'
                 ].join('')
             }
