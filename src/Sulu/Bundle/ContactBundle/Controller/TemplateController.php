a4d1caa287b925ba66f272bdc1557ea02a3cf365
Johannes Wachter <johannes.wachter@massiveart.com>
2013-08-30 15:46:29 +0200

    added drop down for types, added remove email

--- src/Sulu/Bundle/ContactBundle/Controller/TemplateController.php
+++ src/Sulu/Bundle/ContactBundle/Controller/TemplateController.php
@@ -6,9 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TemplateController extends Controller
 {
-    public function contactFormAction()
-    {
-        return $this->render('SuluContactBundle:Template:contact.form.html.twig', array());
-    }
+	public function contactFormAction()
+	{
+		$emailTypes = $this->getDoctrine()
+			->getRepository('SuluContactBundle:EmailType')
+			->findAll();
+
+		$phoneTypes = $this->getDoctrine()
+			->getRepository('SuluContactBundle:PhoneType')
+			->findAll();
+
+		$addressTypes = $this->getDoctrine()
+			->getRepository('SuluContactBundle:AddressType')
+			->findAll();
+
+		$countries = $this->getDoctrine()
+			->getRepository('SuluContactBundle:Country')
+			->findAll();
+
+		return $this->render('SuluContactBundle:Template:contact.form.html.twig', array(
+			'addressTypes' => $addressTypes,
+			'phoneTypes' => $phoneTypes,
+			'emailTypes' => $emailTypes,
+			'countries' => $countries
+		));
+	}
 
 }
