0fc96a032e2325c12dc93b8e374f4b0048d9d202
Johannes Wachter <johannes.wachter@massiveart.com>
2013-09-06 15:52:41 +0200

    added grunt task update, added vendor files

--- src/Sulu/Bundle/SuluAdminBundle/Gruntfile.js
+++ src/Sulu/Bundle/SuluAdminBundle/Gruntfile.js
@@ -24,6 +24,51 @@ module.exports = function (grunt) {
                 files: [
                     {src: ['Resources/public/index.html.twig'], dest: 'Resources/views/Admin/index.html.dist.twig'}
                 ]
+            },
+            bower: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/backbone/backbone.js', 'bower_components/backbone/backbone-min.js', 'bower_components/backbone/backbone-min.map'], dest: 'Resources/public/js/vendor/backbone'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/backbone-relational/backbone-relational.js'], dest: 'Resources/public/js/vendor/backbone-relational'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/husky/dist/',
+                        src: ['**'],
+                        dest: 'Resources/public/js/vendor/husky/'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/jquery/jquery.js', 'bower_components/jquery/jquery.min.map', 'bower_components/jquery/jquery.min.js'], dest: 'Resources/public/js/vendor/jquery'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/requirejs/require.js'], dest: 'Resources/public/js/vendor/requirejs'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/requirejs-text/text.js'], dest: 'Resources/public/js/vendor/requirejs-text'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/parsleyjs/parsley.js'], dest: 'Resources/public/js/vendor/parsleyjs'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/underscore/underscore.js', 'bower_components/underscore/underscore-min.js', 'bower_components/underscore/underscore-min.map'], dest: 'Resources/public/js/vendor/underscore'
+                    }
+                ]
             }
         },
         clean: {
@@ -46,6 +91,20 @@ module.exports = function (grunt) {
                     {src: ['Resources/public/index.html.twig']},
                     {src: ['Resources/public/bundles']}
                 ]
+            },
+            bower_after: {
+                files: {
+                    src: [
+                        'bower_components'
+                    ]
+                }
+            },
+            bower_before: {
+                files: {
+                    src: [
+                        'Resources/public/js/vendor'
+                    ]
+                }
             }
         },
         replace: {  //FIXME: remove as soon as usemin bugs are fixed (leading slashes, basedir)
@@ -128,6 +187,18 @@ module.exports = function (grunt) {
                     relativeAssets: false
                 }
             }
+        },
+        bower: {
+            install: {
+                options: {
+                    copy: false,
+                    layout: 'byComponent',
+                    install: true,
+                    verbose: false,
+                    cleanTargetDir: false,
+                    cleanBowerDir: false
+                }
+            }
         }
     });
 
@@ -150,6 +221,13 @@ module.exports = function (grunt) {
         'replace:buildResult',
         'clean:build',
         'publish'
+    ]);
+
+    grunt.registerTask('update', [
+        'clean:bower_before',
+        'bower:install',
+        'copy:bower',
+        'clean:bower_after'
     ]);
 
     grunt.registerTask('default', [
