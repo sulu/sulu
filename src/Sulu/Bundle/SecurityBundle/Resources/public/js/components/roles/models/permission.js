baae6ff0fb5e42df883ccfea9588a490bc3009ce
Daniel Rotter <daniel.rotter@massiveart.com>
2013-09-18 14:04:49 +0200

    extended javascript models for usage with permissions

---
+++ src/Sulu/Bundle/SecurityBundle/Resources/public/js/components/roles/models/permission.js
@@ -0,0 +1,20 @@
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART Webservices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+define(['mvc/relationalmodel'], function(RelationalModel) {
+    return RelationalModel({
+        urlRoot: '/security/api/roles',
+
+        defaults: function() {
+            return {
+                context: '',
+                permissions: ''
+            }
+        }
+    });
+});
