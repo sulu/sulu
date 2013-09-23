085b6fea16fee7130cda091c09df174262548ea1
Johannes Wachter <johannes.wachter@massiveart.com>
2013-09-23 18:08:22 +0200

    added jsonserializeable

--- src/Sulu/Bundle/ContactBundle/Entity/UrlType.php
+++ src/Sulu/Bundle/ContactBundle/Entity/UrlType.php
@@ -15,7 +15,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * UrlType
  */
-class UrlType
+class UrlType implements \JsonSerializable
 {
     /**
      * @var string
@@ -104,5 +104,20 @@ class UrlType
     public function getUrls()
     {
         return $this->urls;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.4.0)<br/>
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     */
+    public function jsonSerialize()
+    {
+        return array(
+            'id' => $this->getId(),
+            'name' => $this->getName()
+        );
     }
 }
