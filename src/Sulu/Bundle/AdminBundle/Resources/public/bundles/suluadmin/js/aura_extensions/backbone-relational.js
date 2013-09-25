f2578befdb5a34260e9d6a3b6eb7cd7cc5141cf3
Daniel Rotter <daniel.rotter@massiveart.com>
2013-09-25 10:43:17 +0200

    huskyfied login formular

---
+++ src/Sulu/Bundle/AdminBundle/Resources/public/bundles/suluadmin/js/aura_extensions/backbone-relational.js
@@ -0,0 +1,46 @@
+(function() {
+
+    define(['vendor/backbone-relational/backbone-relational'], function(RelationalModel) {
+
+        return {
+
+            name: 'relationalmodel',
+
+            initialize: function(app) {
+                var core = app.core,
+                    sandbox = app.sandbox,
+                    _ = app.sandbox.util._;
+
+                core.mvc.relationalModel = Backbone.RelationalModel;
+
+                sandbox.mvc.relationalModel = function(options) {
+                    return core.mvc.relationalModel.extend(options);
+                };
+
+                define('mvc/relationalmodel', function() {
+                    return sandbox.mvc.relationalModel;
+                });
+
+                sandbox.mvc.HasMany  = Backbone.HasMany;
+                sandbox.mvc.HasOne   = Backbone.HasOne;
+
+                define('mvc/hasmany', function() {
+                    return sandbox.mvc.HasMany;
+                });
+
+
+                define('mvc/hasone', function() {
+                    return sandbox.mvc.HasOne;
+                });
+
+
+                sandbox.mvc.Store = Backbone.Relational.store;
+
+                define('mvc/relationalstore', function() {
+                    return sandbox.mvc.Store;
+                });
+
+            }
+        }
+    });
+})();
