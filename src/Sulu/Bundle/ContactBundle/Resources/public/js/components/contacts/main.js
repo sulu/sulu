e35034d5f1dfdf8be02d1c60e8b4bdcbf63d4a2e
Johannes Wachter <johannes.wachter@massiveart.com>
2013-09-30 17:41:56 +0200

    fixed a few bugs

--- src/Sulu/Bundle/ContactBundle/Resources/public/js/components/contacts/main.js
+++ src/Sulu/Bundle/ContactBundle/Resources/public/js/components/contacts/main.js
@@ -11,12 +11,9 @@ define(['sulucontact/model/contact'], function(Contact) {
 
     'use strict';
 
-    var sandbox;
-
     return {
 
         initialize: function() {
-            sandbox = this.sandbox;
             if (this.options.display === 'list') {
                 this.renderList();
             } else if (this.options.display === 'form') {
@@ -28,43 +25,43 @@ define(['sulucontact/model/contact'], function(Contact) {
 
         renderList: function() {
 
-            sandbox.start([
+           this.sandbox.start([
                 {name: 'contacts/components/list@sulucontact', options: { el: this.$el}}
             ]);
 
             // wait for navigation events
-            sandbox.on('sulu.contacts.contacts.load', function(id) {
-                sandbox.emit('husky.header.button-state', 'loading-add-button');
-                sandbox.emit('sulu.router.navigate', 'contacts/contacts/edit:' + id);
+           this.sandbox.on('sulu.contacts.contacts.load', function(id) {
+               this.sandbox.emit('husky.header.button-state', 'loading-add-button');
+               this.sandbox.emit('sulu.router.navigate', 'contacts/contacts/edit:' + id);
             }, this);
 
             // add new contact
-            sandbox.on('sulu.contacts.contacts.new', function() {
-                sandbox.emit('husky.header.button-state', 'loading-add-button');
-                sandbox.emit('sulu.router.navigate', 'contacts/contacts/add');
+           this.sandbox.on('sulu.contacts.contacts.new', function() {
+               this.sandbox.emit('husky.header.button-state', 'loading-add-button');
+               this.sandbox.emit('sulu.router.navigate', 'contacts/contacts/add');
             }, this);
 
             // delete selected contacts
-            sandbox.on('sulu.contacts.contacts.delete', function(ids) {
+           this.sandbox.on('sulu.contacts.contacts.delete', function(ids) {
                 if (ids.length<1) {
-                    sandbox.emit('sulu.dialog.error.show','No contacts selected for Deletion');
+                   this.sandbox.emit('sulu.dialog.error.show','No contacts selected for Deletion');
                     return;
                 }
                 this.confirmDeleteDialog(function(wasConfirmed) {
                     if (wasConfirmed) {
-                        sandbox.emit('husky.header.button-state', 'loading-add-button');
+                       this.sandbox.emit('husky.header.button-state', 'loading-add-button');
                         ids.forEach(function(id) {
                             var contact = new Contact({id: id});
                             contact.destroy({
                                 success: function() {
-                                    sandbox.emit('husky.datagrid.row.remove', id);
-                                }
+                                   this.sandbox.emit('husky.datagrid.row.remove', id);
+                                }.bind(this)
                             });
                         });
-                        sandbox.emit('husky.header.button-state', 'standard');
+                       this.sandbox.emit('husky.header.button-state', 'standard');
                     }
                 });
-            });
+           }, this);
 
         },
 
@@ -72,7 +69,7 @@ define(['sulucontact/model/contact'], function(Contact) {
         renderForm: function() {
 
             // show navigation submenu
-            sandbox.emit('navigation.item.column.show', {
+           this.sandbox.emit('navigation.item.column.show', {
                 data: this.getTabs(this.options.id)
             });
 
@@ -82,49 +79,49 @@ define(['sulucontact/model/contact'], function(Contact) {
                 contact.set({id: this.options.id});
                 contact.fetch({
                     success: function(model) {
-                        sandbox.start([
+                       this.sandbox.start([
                             {name: 'contacts/components/form@sulucontact', options: { el: this.$el, data: model.toJSON()}}
                         ]);
                     }.bind(this),
                     error: function() {
-                        sandbox.logger.log("error while fetching contact");
-                    }
+                       this.sandbox.logger.log("error while fetching contact");
+                    }.bind(this)
                 });
             } else {
-                sandbox.start([
+               this.sandbox.start([
                     {name: 'contacts/components/form@sulucontact', options: { el: this.$el, data: contact.toJSON()}}
                 ]);
             }
 
             // delete contact
-            sandbox.on('sulu.contacts.contacts.delete',function(){
+           this.sandbox.on('sulu.contacts.contacts.delete',function(){
                 this.confirmDeleteDialog(function(wasConfirmed){
                     if(wasConfirmed) {
-                        sandbox.emit('husky.header.button-state', 'loading-delete-button');
+                       this.sandbox.emit('husky.header.button-state', 'loading-delete-button');
                         contact.destroy({
                             success: function() {
-                                sandbox.emit('sulu.router.navigate', 'contacts/contacts');
-                            }
+                               this.sandbox.emit('sulu.router.navigate', 'contacts/contacts');
+                            }.bind(this)
                         });
                     }
                 });
             }, this);
 
             // save contact
-            sandbox.on('sulu.contacts.contacts.save',function(data) {
-                sandbox.emit('husky.header.button-state', 'loading-save-button');
+           this.sandbox.on('sulu.contacts.contacts.save',function(data) {
+               this.sandbox.emit('husky.header.button-state', 'loading-save-button');
                 contact.set(data);
                 contact.save(null, {
                     // on success save contacts id
                     success: function(response) {
-                        sandbox.emit('husky.header.button-state', 'standard');
-                        sandbox.emit('sulu.contacts.contacts.saved', response.id);
+                       this.sandbox.emit('husky.header.button-state', 'standard');
+                       this.sandbox.emit('sulu.contacts.contacts.saved', response.id);
                     }.bind(this),
                     error: function() {
-                        sandbox.logger.log("error while saving profile");
-                    }
-                });
-            });
+                       this.sandbox.logger.log("error while saving profile");
+                    }.bind(this)
+                }.bind(this));
+            }, this);
         },
 
 
@@ -137,7 +134,7 @@ define(['sulucontact/model/contact'], function(Contact) {
             if ( !!callbackFunction && typeof(callbackFunction)!=='function') { throw 'callback is not a function'; }
 
             // show dialog
-            sandbox.emit('sulu.dialog.confirmation.show', {
+           this.sandbox.emit('sulu.dialog.confirmation.show', {
                 content: {
                     title: "Be careful!",
                     content: "<p>The operation you are about to do will delete data.<br/>This is not undoable!</p><p>Please think about it and accept or decline.</p>"
@@ -149,16 +146,16 @@ define(['sulucontact/model/contact'], function(Contact) {
             });
 
             // submit -> delete
-            sandbox.once('husky.dialog.submit', function() {
-                sandbox.emit('husky.dialog.hide');
+           this.sandbox.once('husky.dialog.submit', function() {
+               this.sandbox.emit('husky.dialog.hide');
                 if (!!callbackFunction) { callbackFunction(true); }
-            });
+           }.bind(this));
 
             // cancel
-            sandbox.once('husky.dialog.cancel', function() {
-                sandbox.emit('husky.dialog.hide');
+           this.sandbox.once('husky.dialog.cancel', function() {
+               this.sandbox.emit('husky.dialog.hide');
                 if (!!callbackFunction) { callbackFunction(false); }
-            });
+           }.bind(this));
         },
 
 
