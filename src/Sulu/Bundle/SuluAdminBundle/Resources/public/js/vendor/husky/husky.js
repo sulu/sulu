65b9b8a54eb4024b339ee534ed718d8292900508
Johannes Wachter <johannes.wachter@massiveart.com>
2013-09-30 17:40:56 +0200

    added new husky version, update app main

--- src/Sulu/Bundle/SuluAdminBundle/Resources/public/js/vendor/husky/husky.js
+++ src/Sulu/Bundle/SuluAdminBundle/Resources/public/js/vendor/husky/husky.js
@@ -18912,6 +18912,7 @@ define('__component__$navigation@husky',['jquery'], function($) {
                     columnItemClasses = [];
 
                     !!itemModel.get('class') && columnItemClasses.push(itemModel.get('class'));
+                    !!itemModel.get('selected') && columnItemClasses.push('selected');
                     columnItemClasses.push('navigation-column-item');
 
                     columnItemClass = ' class="' + columnItemClasses.join(' ') + '"';
@@ -19214,7 +19215,7 @@ define('__component__$navigation@husky',['jquery'], function($) {
             params = params || {};
 
             if (!!params.data) {
-                if (params.data.displayOption === 'content') {
+                if (!params.data.displayOption || params.data.displayOption === 'content') {
                     this.removeContentColumn();
                 }
                 $column0 = $('#column-0');
@@ -19245,7 +19246,7 @@ define('__component__$navigation@husky',['jquery'], function($) {
 
                 this.addedColumn = this.currentColumnIdx;
                 $showedColumn = $('#column-' + this.addedColumn);
-                $showedColumn.find('ul.navigation-items li:first').addClass('selected');
+                //$showedColumn.find('ul.navigation-items li:first').addClass('selected');
 
                 sandbox.emit('navigation.size.changed', {
                     data: this.getNavigationData()
@@ -19850,6 +19851,7 @@ define('__component__$button@husky',[], function() {
  *      - selectItem.type: typ of select [checkbox, radio]
  *      - selectItem.width: typ of select [checkbox, radio]
  *      - url: url to fetch content
+ *      - appendTBody: add TBODY to table
  *
  *    Provided Events:
  *       - husky.datagrid.item.deselect - raised when item is deselected
@@ -19900,7 +19902,8 @@ define('__component__$datagrid@husky',[],function() {
             //clickable: false   // defines if background is clickable TODO do not use until fixed
         },
         tableHead: [],
-        url: null
+        url: null,
+        appendTBody: true   // add TBODY to table
     };
 
 
@@ -20031,7 +20034,9 @@ define('__component__$datagrid@husky',[],function() {
             }
 
             if (!!this.data.items) {
-                $tbody = this.sandbox.dom.$('<tbody/>');
+                if (!!this.options.appendTBody) {
+                    $tbody = this.sandbox.dom.$('<tbody/>');
+                }
                 $tbody.append(this.prepareTableRows());
                 $table.append($tbody);
             }
@@ -20492,8 +20497,12 @@ define('__component__$datagrid@husky',[],function() {
         },
 
         // trigger selected items
-        getSelectedItemsIds: function() {
-            this.sandbox.emit('husky.datagrid.items.selected', this.selectedItemIds);
+        getSelectedItemsIds: function(callback) {
+            if (typeof callback === 'function') {
+                callback(this.selectedItemIds);
+            } else {
+                this.sandbox.emit('husky.datagrid.items.selected', this.selectedItemIds);
+            }
         },
 
         templates: {
@@ -20538,8 +20547,8 @@ define('__component__$datagrid@husky',[],function() {
 
                 return [
                     '<ul>',
-                        '<li class="pagination-first page" data-page="1"></li>',
-                        '<li class="pagination-prev page" data-page="', selectedPage - 1, '">', 'Previous', '</li>',
+                    '<li class="pagination-first page" data-page="1"></li>',
+                    '<li class="pagination-prev page" data-page="', selectedPage - 1, '">', 'Previous', '</li>',
                     '</ul>'
                 ].join('');
             },
@@ -20555,8 +20564,8 @@ define('__component__$datagrid@husky',[],function() {
 
                 return [
                     '<ul>',
-                        '<li class="pagination-next page" data-page="', selectedPage + 1, '">', next, '</li>',
-                        '<li class="pagination-last page" data-page="', pageSize, '"></li>',
+                    '<li class="pagination-next page" data-page="', selectedPage + 1, '">', next, '</li>',
+                    '<li class="pagination-last page" data-page="', pageSize, '"></li>',
                     '</ul>'
                 ].join('');
             },
@@ -21705,6 +21714,259 @@ define('__component__$auto-complete@husky',[], function() {
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  *
+ * Name: dropdown multiple select
+ * Options:
+ *      data: [],  array of data [string, object]
+ *      valueName: name of property which should be used
+ *      instanceName: instance name of this component
+ *      defaultLabel: default label which gets displayed
+ *      checkedAllLabel: Label if all checked
+ *
+ * Provided Events:
+ * husky.dropdown.multiple.select.<<instanceName>>.selected.item   - triggered when item selected
+ * husky.dropdown.multiple.select.<<instanceName>>.deselected.item   - triggered when item deselected
+
+ *
+ * Use Events
+ * husky.dropdown.multiple.select.<<instanceName>>.toggle     - toggles (show/hide) dropdown menu
+ * husky.dropdown.multiple.select.<<instanceName>>.show       - show dropdown menu
+ * husky.dropdown.multiple.select.<<instanceName>>.hide       - hide dropdown menu
+ */
+
+define('__component__$dropdown-multiple-select@husky',[], function() {
+
+    
+
+    var defaults = {
+        data: [],                         // data array
+        valueName: 'name',                // name of text property
+        instanceName: 'undefined',        // instance name
+        defaultLabel: 'Please choose',    // default label which gets displayed
+        checkedAllLabel: 'All Languages'  // Label if all checked
+    };
+
+
+    return {
+
+        initialize: function() {
+
+            this.sandbox.logger.log('initialize', this);
+            this.options = this.sandbox.util.extend({}, defaults, this.options);
+
+            this.selectedElements = {};
+
+            this.labelId = 'husky-dropdown-multiple-select-' + this.options.instanceName + '-label';
+            this.listId = 'husky-dropdown-multiple-select-' + this.options.instanceName + '-list';
+            this.dropdownContainerId = 'husky-dropdown-multiple-select-' + this.options.instanceName + '-menu';
+
+            this.render();
+        },
+
+        render: function() {
+
+            var $originalElement = this.sandbox.dom.$(this.options.el);
+            this.sandbox.dom.append($originalElement, this.template.basicStructure.call(this, this.options.defaultLabel));
+            this.$list = this.sandbox.dom.$('#' + this.listId);
+            this.$dropdownContainer = this.sandbox.dom.$('#' + this.dropdownContainerId);
+            this.prepareData();
+
+            // bind dom elements
+            this.bindDOMEvents();
+            this.bindCustomEvents();
+        },
+
+        // prepares data for dropDown, if options.data not set load with ajax
+        prepareData: function() {
+            if (this.options.data.length > 0) {
+                this.generateDropDown(this.options.data);
+            } else {
+                this.sandbox.logger.log('error: data not set');
+            }
+            // TODO load data from url
+        },
+
+        // generate dropDown with given items
+        generateDropDown: function(items) {
+            // remove all elements
+            this.sandbox.dom.remove(this.$list, 'li');
+
+            if (items.length > 0) {
+                this.sandbox.util.each(items, function(index, value) {
+                    this.sandbox.dom.append(this.$list, this.template.menuElement.call(this, value, this.options.valueName));
+                }.bind(this));
+            } else {
+                this.$dropDownList.append('<li>No data received</li>');
+            }
+        },
+
+        // bind dom elements
+        bindDOMEvents: function() {
+
+            this.sandbox.dom.on(this.sandbox.dom.window, 'click', this.hideDropDown.bind(this));
+
+            // toggle drop-down
+            this.sandbox.dom.on('#' + this.options.instanceName, 'click', this.toggleDropDown.bind(this), '.dropdown-label');
+
+            // click on single item
+            this.sandbox.dom.on('#' + this.listId, 'click', this.clickItem.bind(this), 'li');
+
+        },
+
+        bindCustomEvents: function() {
+            this.sandbox.on(this.getEventName('toggle'), this.toggleDropDown.bind(this));
+            this.sandbox.on(this.getEventName('show'), this.showDropDown.bind(this));
+            this.sandbox.on(this.getEventName('hide'), this.hideDropDown.bind(this));
+            this.sandbox.on(this.getEventName('getChecked'), this.getChecked.bind(this));
+        },
+
+        // trigger event with clicked item
+        clickItem: function(event) {
+
+            this.sandbox.dom.stopPropagation(event);
+
+            var key = this.sandbox.dom.attr(event.currentTarget, 'data-key'),
+                value = this.sandbox.dom.text(this.sandbox.dom.find('.item-value', event.currentTarget)),
+                $checkbox = this.sandbox.dom.find('input[type=checkbox]', event.currentTarget);
+
+            if (key in this.selectedElements) {
+
+                this.sandbox.dom.removeClass($checkbox, 'is-selected');
+                this.sandbox.dom.prop($checkbox, 'checked', false);
+                delete this.selectedElements[key];
+                this.sandbox.emit(this.getEventName('deselected.item'), key);
+
+            } else {
+
+                this.sandbox.dom.addClass($checkbox, 'is-selected');
+                this.sandbox.dom.prop($checkbox, 'checked', true);
+                this.selectedElements[key] = value;
+                this.sandbox.emit(this.getEventName('selected.item'), key);
+            }
+
+            this.changeLabel();
+
+        },
+
+        getEventName: function(suffix) {
+            return 'husky.dropdown.multiple.select.' + this.options.instanceName + '.' + suffix;
+        },
+
+        changeLabel: function() {
+
+            if (this.selectedElements.length === this.options.data.length) {
+                this.sandbox.dom.text('#' + this.labelId, this.options.checkedAllLabel);
+            } else if (this.selectedElements.length === 0) {
+                this.sandbox.dom.text('#' + this.labelId, this.options.defaultLabel);
+            } else {
+
+                var text = "";
+                this.sandbox.util.each(this.selectedElements, function(index, value) {
+                    text += ' ' + value + ',';
+                });
+                this.sandbox.dom.text('#' + this.labelId, text.substring(1, text.length - 1));
+            }
+        },
+
+        // toggle dropDown visible
+        toggleDropDown: function(event) {
+            this.sandbox.logger.log('toggle dropdown ' + this.options.instanceName);
+            this.sandbox.dom.toggleClass(this.$dropdownContainer, 'hidden');
+
+            this.sandbox.dom.stopPropagation(event);
+        },
+
+        // make dropDown visible
+        showDropDown: function(event) {
+            this.sandbox.logger.log('show dropdown ' + this.options.instanceName);
+            this.sandbox.dom.removeClass(this.$dropdownContainer, 'hidden');
+
+            this.sandbox.dom.stopPropagation(event);
+        },
+
+        // hide dropDown
+        hideDropDown: function(event) {
+            this.sandbox.logger.log('hide dropdown ' + this.options.instanceName);
+            this.sandbox.dom.addClass(this.$dropdownContainer, 'hidden');
+
+            this.sandbox.dom.stopPropagation(event);
+        },
+
+        // return checked values
+        getChecked: function(callback) {
+            if (typeof callback === 'function') {
+                callback(this.selectedElements);
+            } else {
+                throw 'error: callback is not a function';
+            }
+        },
+
+
+        template: {
+
+            basicStructure: function(defaultLabel) {
+                return [
+                    '<div class="husky-dropdown-multiple-select">',
+                    '    <div class="grid-row dropdown-label dropdown-open">',
+                    '       <div class="grid-col-11 checkbox">',
+                    '           <span id="', this.labelId, '">', defaultLabel, '</span>',
+                    '       </div>',
+                    '       <div class="grid-col-1 align-right">',
+                    '           <span class="dropdown-toggle inline-block"></span>',
+                    '       </div>',
+                    '   </div>',
+                    '   <div class="grid-row dropdown-list dropdown-align-right hidden" id="', this.dropdownContainerId, '">',
+                    '       <ul id="', this.listId, '"></ul>',
+                    '   </div>',
+                    '</div>'
+                ].join('');
+            },
+            menuElement: function(value, property) {
+
+                if (typeof value === 'string') {
+
+                    return [
+                        '<li data-key="', value, '">',
+                        '    <div class="grid-row">',
+                        '        <div class="grid-col-1">',
+                        '            <input type="checkbox" class="form-element custom-checkbox"/>',
+                        '            <span class="custom-checkbox-icon"></span>',
+                        '        </div>',
+                        '        <div class="grid-col-11 m-top-10 item-value">', value, '</div>',
+                        '    </div>',
+                        '</li>'
+                    ].join('');
+
+                } else {
+
+                    return [
+                        '<li data-key="', value.id, '">',
+                        '    <div class="grid-row">',
+                        '        <div class="grid-col-1">',
+                        '            <input type="checkbox" class="form-element custom-checkbox"/>',
+                        '            <span class="custom-checkbox-icon"></span>',
+                        '        </div>',
+                        '        <div class="grid-col-11 m-top-10 item-value">', value[property], '</div>',
+                        '    </div>',
+                        '</li>'
+                    ].join('');
+                }
+
+            }
+
+        }
+
+    };
+
+});
+
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ *
  * Name: button
  * Options:
  *  labels.inputPassword1: text for the label for the first password field
@@ -21783,7 +22045,7 @@ define('__component__$password-fields@husky',[], function() {
                 '                <div class="grid-col-6">',
                 '                    <label>', this.options.labels.inputPassword1, '</label>',
                 '                </div>',
-                '                <div class="grid-col-6 align-right" id="', this.options.ids.generateLabel, '">',
+                '                <div class="grid-col-6 align-right hidden" id="', this.options.ids.generateLabel, '">',
                 '                    <span class="icon-keys m-right-10"></span><span class="pointer">', this.options.labels.generateLabel, '</span>',
                 '                </div>',
                 '            </div>',
@@ -22176,8 +22438,8 @@ define('husky_extensions/collection',[],function() {
                 return $(selector).height();
             };
 
-            app.core.dom.remove = function(selector) {
-                return $(selector).remove();
+            app.core.dom.remove = function(context, selector) {
+                return $(context).remove(selector);
             };
 
             app.core.dom.attr = function(selector, attributes) {
@@ -22242,7 +22504,23 @@ define('husky_extensions/collection',[],function() {
             };
 
             app.core.dom.text = function(selector, value) {
-                return $(selector).text(value);
+                if (!!value) {
+                    $(selector).text(value);
+                } else {
+                    return $(selector).text();
+                }
+            };
+
+            app.core.dom.prop = function(selector, propertyName, value) {
+                if (!!value) {
+                    return $(selector).prop(propertyName, value);
+                } else {
+                    return $(selector).prop(propertyName);
+                }
+            };
+
+            app.core.dom.stopPropagation = function(event) {
+                event.stopPropagation();
             };
 
 
