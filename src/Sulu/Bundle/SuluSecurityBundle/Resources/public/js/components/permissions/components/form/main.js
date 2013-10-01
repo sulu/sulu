2284dd163353f8c91197dcb56deb9d5f73db5c6d
Michael Zangerle <michael.zangerle@massiveart.com>
2013-10-01 16:08:39 +0200

    added new headerbar handling, basic tempaltes for permissions grid

--- src/Sulu/Bundle/SuluSecurityBundle/Resources/public/js/components/permissions/components/form/main.js
+++ src/Sulu/Bundle/SuluSecurityBundle/Resources/public/js/components/permissions/components/form/main.js
@@ -19,58 +19,157 @@ define(['text!/security/template/permission/form'], function(Template) {
 
         initialize: function() {
 
-            this.data = this.options.data;
+            this.formId = '#permissions-form';
 
-            this.initializeHeader();
+            if(!!this.options.data) {
+                this.user = this.options.data.user;
+                this.contact = this.options.data.contact;
+                this.roles = this.options.data.roles;
+            } else {
+                // TODO error message
+                this.sandbox.logger.log("no data given");
+            }
+
             this.render();
             this.initializePasswordFields();
             this.initializeRoles();
 
             this.bindDOMEvents();
             this.bindCustomEvents();
+
+            this.initializeHeaderbar();
         },
 
-        initializeHeader: function() {
+        // Headerbar
 
-            this.sandbox.emit('husky.header.button-type', 'saveDelete');
+        initializeHeaderbar: function(){
+            this.currentType = '';
+            this.currentState = '';
 
-            this.sandbox.on('husky.button.save.click', function() {
-                this.save();
-            }.bind(this));
-
-            this.sandbox.on('husky.button.delete.click', function() {
-                this.sandbox.emit('sulu.permissions.delete', this.options.id);
-            }.bind(this));
+            this.setHeaderBar(true);
+            this.listenForChange();
         },
+
+        setHeaderBar: function(saved) {
+
+            var changeType,
+                changeState,
+                ending = (!!this.contact.id) ? 'Delete' : '';
+
+            changeType = 'save' + ending;
+
+            if (saved) {
+                if (ending === '') {
+                    changeState = 'hide';
+                } else {
+                    changeState = 'standard';
+                }
+            } else {
+                changeState = 'dirty';
+            }
+
+            if (this.currentType !== changeType) {
+                this.sandbox.emit('husky.header.button-type', changeType);
+                this.currentType = changeType;
+            }
+            if (this.currentState !== changeState) {
+                this.sandbox.emit('husky.header.button-state', changeState);
+                this.currentState = changeState;
+            }
+
+        },
+
+        listenForChange: function() {
+
+            this.sandbox.dom.on(this.formId, 'change', function() {
+                this.setHeaderBar(false);
+            }.bind(this), "select, input");
+            this.sandbox.dom.on(this.formId, 'keyup', function() {
+                this.setHeaderBar(false);
+            }.bind(this), "input");
+
+            //TODO ddms
+        },
+
+
+
+        
 
         render: function() {
             this.sandbox.dom.html(this.$el, this.sandbox.template.parse(Template, {data: this.options.data}));
         },
 
-        initializePasswordFields: function(){
-            this.sandbox.start([{
-                name: 'password-fields@husky',
-                options: {
-                    instanceName: "instance1",
-                    el: '#password-component'
+        initializePasswordFields: function() {
+            this.sandbox.start([
+                {
+                    name: 'password-fields@husky',
+                    options: {
+                        instanceName: "instance1",
+                        el: '#password-component'
+                    }
                 }
-            }]);
+            ]);
         },
 
-        initializeRoles: function(){
+        initializeRoles: function() {
 
-            // #permissions-grid
+            var $permissionsContainer = this.sandbox.dom.$('#permissions-grid'),
+                $table = this.sandbox.dom.createElement('<table/>', {class: 'table'}),
+                $tableHeader = this.prepareTableHeader(),
+                //$tableRows = this.prepareTableContent(),
+                $tmp;
+
+            //this.sandbox.dom.empty($permissionsContainer);
+            // TODO clear div before - add empty() to jquery extension when everything merged
+
+
+            $tmp = this.sandbox.dom.append($table, $tableHeader);
+
+            // TODO test - multiple permissions?
+            //$tmp = this.sandbox.dom.append($tmp, $tableRows);
+            this.sandbox.dom.html($permissionsContainer,$tmp);
+
+            // TODO start ddms
+        },
+
+        prepareTableHeader: function() {
+            return this.template.tableHead('Title', 'Languages', 'Permissions');
+        },
+
+        prepareTableContent: function() {
+            var $tableBody = this.sandbox.dom.createElement('<tbody/>'),
+                tableContent = [];
+
+            this.sandbox.util.each(this.roles, function(index, value){
+                tableContent.push(this.template.tableRow(value.id, value.name, value.permissions));
+            }.bind(this));
+
+            return this.sandbox.dom.append($tableBody, tableContent.join(''));
 
         },
 
         bindDOMEvents: function() {
-            //TODO
+
         },
 
         bindCustomEvents: function() {
-            this.sandbox.on('husky.matrix.changed', function(data) {
-                this.changePermission(data);
-            }.bind(this));
+            // delete contact
+            this.sandbox.on('husky.button.delete.click', function() {
+                //this.sandbox.emit('sulu.user.permissions.delete', this.contact.id);
+            }, this);
+
+            // contact saved
+            this.sandbox.on('sulu.user.permissions.save', function(id, data) {
+                if (!this.options.data.id) {
+                    this.options.data = data;
+                }
+                this.setHeaderBar(true);
+            }, this);
+
+            // contact saved
+            this.sandbox.on('husky.button.save.click', function() {
+                this.save();
+            }, this);
         },
 
         save: function() {
@@ -82,7 +181,52 @@ define(['text!/security/template/permission/form'], function(Template) {
 //                permissions: permissionData
             };
 
-            this.sandbox.emit('sulu.roles.save', data);
+            this.sandbox.emit('sulu.user.permissions.save', data);
+        },
+
+        template: {
+            tableHead: function(thLabel1, thLabel2, thLabel3) {
+                return [
+                    '<thead>',
+                        '<tr>' +
+                            '<th><input id=\'selectAll\' type=\'checkbox\'/></th>',
+                            '<th>',thLabel1,'</th>',
+                            '<th>',thLabel2,'</th>',
+                            '<th colspan=\'7\'>',thLabel3,'</th>',
+                        '</tr>',
+                    '</thead>'
+                ].join('');
+
+            },
+            tableRow: function(id, title, permissions) {
+
+                this.sandbox.util.each(permissions, function(index,value){
+                    // TODO
+                });
+
+                var $row = [
+                    '<tr data-id=\"',id,'\">',
+                        '<td><input type="checkbox" /></td>',
+                        '<td>',title,'</td>',
+                        '<td class=\"languageSelector\"></td>',
+                        '<td><span class="icon-add"></span></td>',
+                        '<td><span class="icon-edit"></span></td>',
+                        '<td><span class="icon-search is-active"></span></td>',
+                        '<td><span class="icon-remove"></span></td>',
+                        '<td><span class="icon-settings"></span></td>',
+                        '<td><span class="icon-circle-ok is-active"></span></td>',
+                        '<td><span class="icon-building"></span></td>',
+                    '</tr>'
+                ].join('');
+
+
+                return $row;
+            }
         }
+
+
     };
 });
+
+
+
