a234bc43b70795321f26227e9406a4774146c7dd
Johannes Wachter <johannes.wachter@massiveart.com>
2013-10-05 17:05:19 +0200

    added translation keys for account

--- src/Sulu/Bundle/AdminBundle/Resources/public/js/language/sulu_translate.sql
+++ src/Sulu/Bundle/AdminBundle/Resources/public/js/language/sulu_translate.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 04. Okt 2013 um 17:40
+-- Erstellungszeit: 05. Okt 2013 um 17:04
 -- Server Version: 5.5.32
--- PHP-Version: 5.4.19-1+debphp.org~quantal+3
+-- PHP-Version: 5.4.20-1+debphp.org~quantal+1
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
@@ -63,16 +63,16 @@ INSERT INTO `tr_codes` (`id`, `code`, `backend`, `frontend`, `length`, `idLocati
 (29, 'contact.contacts.lastName', 1, 0, 20, NULL, 1),
 (30, 'contact.contacts.position', 1, 0, 20, NULL, 1),
 (31, 'contact.contacts.company', 1, 0, 20, NULL, 1),
-(32, 'contact.contacts.details.addEmail', 1, 0, 20, NULL, 1),
-(33, 'contact.contacts.details.addPhone', 1, 0, 20, NULL, 1),
-(34, 'contact.contacts.details.addAddress', 1, 0, 20, NULL, 1),
-(35, 'contact.contacts.details.street', 1, 0, 20, NULL, 1),
-(36, 'contact.contacts.details.number', 1, 0, 20, NULL, 1),
-(37, 'contact.contacts.details.additional', 1, 0, 20, NULL, 1),
-(38, 'contact.contacts.details.zip', 1, 0, 20, NULL, 1),
-(39, 'contact.contacts.details.city', 1, 0, 20, NULL, 1),
-(40, 'contact.contacts.details.state', 1, 0, 20, NULL, 1),
-(41, 'contact.contacts.details.country', 1, 0, 20, NULL, 1),
+(32, 'contact.form.addEmail', 1, 0, 20, NULL, 1),
+(33, 'contact.form.addPhone', 1, 0, 20, NULL, 1),
+(34, 'contact.form.addAddress', 1, 0, 20, NULL, 1),
+(35, 'contact.address.street', 1, 0, 20, NULL, 1),
+(36, 'contact.address.number', 1, 0, 20, NULL, 1),
+(37, 'contact.address.additional', 1, 0, 20, NULL, 1),
+(38, 'contact.address.zip', 1, 0, 20, NULL, 1),
+(39, 'contact.address.city', 1, 0, 20, NULL, 1),
+(40, 'contact.address.state', 1, 0, 20, NULL, 1),
+(41, 'contact.address.country', 1, 0, 20, NULL, 1),
 (42, 'public.email', 1, 0, 20, NULL, 1),
 (43, 'public.phone', 1, 0, 20, NULL, 1),
 (44, 'public.address', 1, 0, 20, NULL, 1),
@@ -83,7 +83,11 @@ INSERT INTO `tr_codes` (`id`, `code`, `backend`, `frontend`, `length`, `idLocati
 (49, 'address.home', 0, 0, 20, NULL, 1),
 (50, 'address.work', 0, 0, 20, NULL, 1),
 (51, 'country.austria', 1, 0, 20, NULL, 1),
-(52, 'country.germany', 1, 0, 20, NULL, 1);
+(52, 'country.germany', 1, 0, 20, NULL, 1),
+(53, 'contact.accounts.title', 1, 0, 20, NULL, 1),
+(54, 'contact.accounts.name', 1, 0, 20, NULL, 1),
+(55, 'contact.accounts.website', 1, 0, 20, NULL, 1),
+(56, 'contact.accounts.company', 1, 0, 40, NULL, 1);
 
 --
 -- Daten für Tabelle `tr_packages`
@@ -145,6 +149,10 @@ INSERT INTO `tr_translations` (`value`, `idCatalogues`, `idCodes`) VALUES
 ('Geschäftlich', 3, 50),
 ('Österreich', 3, 51),
 ('Deutschland', 3, 52),
+('Account', 3, 53),
+('Name', 3, 54),
+('Internetseite', 3, 55),
+('Übergeordnetes Unternehmen', 3, 56),
 ('Name', 4, 4),
 ('Edit', 4, 6),
 ('Add', 4, 7),
