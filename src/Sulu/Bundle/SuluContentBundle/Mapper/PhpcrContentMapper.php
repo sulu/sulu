e1c27248b8e5d6405478a86421e990babb513200
Daniel Rotter <daniel.rotter@massiveart.com>
2013-10-14 12:55:29 +0200

    added simple test without evaluation

---
+++ src/Sulu/Bundle/SuluContentBundle/Mapper/PhpcrContentMapper.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: danielrotter
+ * Date: 14.10.13
+ * Time: 11:27
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Sulu\Bundle\ContentBundle\Mapper;
+
+
+class PhpcrContentMapper implements ContentMapperInterface {
+    public function save($data)
+    {
+    }
+}
