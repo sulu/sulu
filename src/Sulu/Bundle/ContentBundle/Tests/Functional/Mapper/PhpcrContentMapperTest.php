a857ee2188659e93b842fcb30d1f5198ee3b0ae4
Daniel Rotter <daniel.rotter@massiveart.com>
2013-10-14 15:03:17 +0200

    implemented first test

--- src/Sulu/Bundle/ContentBundle/Tests/Functional/Mapper/PhpcrContentMapperTest.php
+++ src/Sulu/Bundle/ContentBundle/Tests/Functional/Mapper/PhpcrContentMapperTest.php
@@ -9,21 +9,41 @@
 
 namespace Sulu\Bundle\ContentBundle\Tests\Functional\Mapper;
 
+use Jackalope\Session;
+use PHPCR\Util\NodeHelper;
 use Sulu\Bundle\ContentBundle\Mapper\PhpcrContentMapper;
 use Sulu\Bundle\CoreBundle\Tests\DatabaseTestCase;
 
-class PhpcrContentMapperTest extends \PHPUnit_TestCase
+class PhpcrContentMapperTest extends \PHPUnit_Framework_TestCase
 {
     protected $mapper;
 
-    public function __construct()
+    /**
+     * @var Session
+     */
+    protected $session;
+
+    public function setUp()
     {
         $this->mapper = new PhpcrContentMapper();
+
+        $factoryclass = '\Jackalope\RepositoryFactoryJackrabbit';
+        $parameters = array('jackalope.jackrabbit_uri' => 'http://localhost:8080/server');
+        $factory = new $factoryclass();
+        $repository = $factory->getRepository($parameters);
+        $credentials = new \PHPCR\SimpleCredentials('admin','admin');
+        $this->session = $repository->login($credentials, 'default');
+    }
+
+    public function tearDown()
+    {
+        NodeHelper::purgeWorkspace($this->session);
     }
 
     public function testSave()
     {
         $data = array(
+            'language' => 'de',
             'title' => 'Testtitle',
             'url' => '/de/test',
             'article' => 'Test'
@@ -31,6 +51,12 @@ class PhpcrContentMapperTest extends \PHPUnit_TestCase
 
         $this->mapper->save($data);
 
+        $root = $this->session->getRootNode();
+        $route = $root->getNode('cmf/routes/de/test');
 
+        $content = $route->getProperty('content')->getNode();
+
+        $this->assertEquals($content->getProperty('title')->getString(), 'Testtitle');
+        $this->assertEquals($content->getProperty('article')->getString(), 'Test');
     }
 }
