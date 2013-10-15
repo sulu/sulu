cfdea92c230727cfe9e872f561b65f751660bd13
Daniel Rotter <daniel.rotter@massiveart.com>
2013-10-15 07:59:23 +0200

    updated file headers

--- src/Sulu/Bundle/ContentBundle/Mapper/PhpcrContentMapper.php
+++ src/Sulu/Bundle/ContentBundle/Mapper/PhpcrContentMapper.php
@@ -1,10 +1,11 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: danielrotter
- * Date: 14.10.13
- * Time: 11:27
- * To change this template use File | Settings | File Templates.
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Sulu\Bundle\ContentBundle\Mapper;
