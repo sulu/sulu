ab4b35a5e68c0ff62762015a949bc67dd0d9d16f
Michael Zangerle <michael.zangerle@massiveart.com>
2013-10-21 13:54:19 +0200

    added custom get account by id

--- src/Sulu/Bundle/SuluContactBundle/Entity/AccountRepository.php
+++ src/Sulu/Bundle/SuluContactBundle/Entity/AccountRepository.php
@@ -13,7 +13,8 @@ namespace Sulu\Bundle\ContactBundle\Entity;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\NoResultException;
 use Doctrine\ORM\QueryBuilder;
-use Sulu\Bundle\CoreBundle\Controller\Exception\EntityNotFoundException;
+use Sulu\Component\Rest\Exception\EntityNotFoundException;
+use Doctrine\ORM\Query;
 
 /**
  * Repository for the Codes, implementing some additional functions
@@ -33,5 +34,50 @@ class AccountRepository extends EntityRepository
         $query = $qb->getQuery();
 
         return $query->getSingleResult();
+    }
+
+    /**
+     * Get account by id
+     * @param $id
+     * @return mixed
+     */
+    public function findAccountById($id)
+    {
+        try {
+            $qb = $this->createQueryBuilder('account')
+                ->leftJoin('account.addresses', 'addresses')
+                ->leftJoin('addresses.country', 'country')
+                ->leftJoin('addresses.addressType', 'addressType')
+                ->leftJoin('account.parent', 'parent')
+                ->leftJoin('account.urls', 'urls')
+                ->leftJoin('urls.urlType', 'urlType')
+                ->leftJoin('account.phones', 'phones')
+                ->leftJoin('phones.phoneType', 'phoneType')
+                ->leftJoin('account.emails', 'emails')
+                ->leftJoin('emails.emailType', 'emailType')
+                ->leftJoin('account.notes', 'notes')
+                ->addSelect('addresses')
+                ->addSelect('addressType')
+                ->addSelect('country')
+                ->addSelect('parent')
+                ->addSelect('urls')
+                ->addSelect('urlType')
+                ->addSelect('phones')
+                ->addSelect('phoneType')
+                ->addSelect('emails')
+                ->addSelect('emailType')
+                ->addSelect('notes')
+                ->where('account.id=:accountId');
+
+            $query = $qb->getQuery();
+            $query->setHint(Query::HINT_FORCE_PARTIAL_LOAD, true);
+            $query->setParameter('accountId', $id);
+
+            return $query->getSingleResult();
+
+
+        } catch (NoResultException $ex) {
+            return null;
+        }
     }
 }
