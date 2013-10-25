9461f03f31de96964226a390a5397bfcb2318753
Johannes Wachter <johannes.wachter@massiveart.com>
2013-10-25 21:51:05 +0200

    added template array

--- src/Sulu/Bundle/SuluContentBundle/Controller/TemplateController.php
+++ src/Sulu/Bundle/SuluContentBundle/Controller/TemplateController.php
@@ -14,9 +14,43 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TemplateController extends Controller
 {
-    public function contentAction()
+    public function contentAction($key)
     {
-        return $this->render('SuluContentBundle:Template:content.html.twig');
+        $template = $this->getTemplate($key);
+
+        return $this->render('SuluContentBundle:Template:content.html.twig', array('template' => $template));
+    }
+
+    private function getTemplate($key)
+    {
+        // TODO get Template
+        // perhaps? $this->get('template.parser')->get($key);
+        return array(
+            'key' => 'overview',
+            'view' => 'page.html.twig',
+            'controller' => 'SuluContentBundle:Default:index',
+            'cacheLifeTime' => 2400,
+            'properties' => array(
+                'title' => array(
+                    'name' => 'title',
+                    'type' => 'textLine',
+                    'mandatory' => true,
+                    'multilingual' => true
+                ),
+                'url' => array(
+                    'name' => 'url',
+                    'type' => 'resourceLocator',
+                    'mandatory' => true,
+                    'multilingual' => true
+                ),
+                'article' => array(
+                    'name' => 'article',
+                    'type' => 'textArea',
+                    'mandatory' => false,
+                    'multilingual' => true
+                )
+            )
+        );
     }
 
 }
