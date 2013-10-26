e9878d5451e60525a32aac70d34d9b094ec4eaa0
Johannes Wachter <johannes.wachter@massiveart.com>
2013-10-26 10:45:07 +0200

    fixed js setup

---
+++ src/Sulu/Bundle/SuluContentBundle/Controller/NavigationController.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\ContentBundle\Controller;
+
+use Sulu\Bundle\AdminBundle\Admin\ContentNavigation;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class NavigationController extends Controller
+{
+
+    const SERVICE_NAME = 'sulu_content.admin.content_navigation';
+
+    /**
+     * Lists all the contacts or filters the contacts by parameters
+     * Special function for lists
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function contentAction()
+    {
+
+        // TODO: get type of content (contact / account ...)
+
+        /** @var ContentNavigation $contentNavigation */
+        if ($this->has(self::SERVICE_NAME)) {
+            $contentNavigation = $this->get(self::SERVICE_NAME);
+        }
+
+        return new Response(json_encode($contentNavigation->toArray('contact')));
+    }
+}
---
+++ src/Sulu/Bundle/SuluContentBundle/Resources/public/js/components/content/components/list/main.js
@@ -0,0 +1,97 @@
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+// TODO change to content (this is contact)
+
+define(function() {
+
+    'use strict';
+
+    return {
+
+        view: true,
+
+        templates: ['/admin/contact/template/contact/list'],
+
+        initialize: function() {
+            this.render();
+        },
+
+        render: function() {
+            this.sandbox.dom.html(this.$el, this.renderTemplate('/admin/contact/template/contact/list'));
+
+            // dropdown - showing options
+            this.sandbox.start([{
+                name: 'dropdown@husky',
+                options: {
+                    el: '#options-dropdown',
+                    trigger: '.dropdown-toggle',
+                    setParentDropDown: true,
+                    instanceName: 'options',
+                    alignment: 'right',
+                    data: [
+                        {
+                            'id': 1,
+                            'type':'delete',
+                            'name': this.sandbox.translate('public.delete')
+                        }
+                    ]
+                }
+            }]);
+
+            // datagrid
+            this.sandbox.start([{
+                name: 'datagrid@husky',
+                options: {
+                    el: this.sandbox.dom.find('#people-list', this.$el),
+                    url: '/admin/api/contact/contacts/list?fields=id,title,firstName,lastName,position',
+                    pagination: false,
+                    selectItem: {
+                        type: 'checkbox'
+                    },
+                    removeRow: false,
+                    tableHead: [
+
+                        {content: this.sandbox.translate('contact.contacts.contactTitle')},
+                        {content: this.sandbox.translate('contact.contacts.firstName')},
+                        {content: this.sandbox.translate('contact.contacts.lastName')},
+                        {content: this.sandbox.translate('contact.contacts.position')}
+                    ],
+                    excludeFields: ['id']
+                }
+            }]);
+
+            // navigate to edit contact
+            this.sandbox.on('husky.datagrid.item.click', function(item) {
+                this.sandbox.emit('sulu.contacts.contacts.load', item);
+            }, this);
+
+
+            this.sandbox.on('husky.dropdown.options.clicked',  function() {
+                this.sandbox.emit('husky.dropdown.options.toggle');
+            }, this);
+
+            // optionsmenu clicked
+            this.sandbox.on('husky.dropdown.options.item.click', function(event) {
+                if (event.type === "delete") {
+                    this.sandbox.emit('husky.datagrid.items.get-selected', function(ids) {
+                        this.sandbox.emit('sulu.contacts.contacts.delete', ids);
+                    }.bind(this));
+                }
+            },this);
+
+            // add button in headerbar
+            this.sandbox.emit('husky.header.button-type', 'add');
+
+            this.sandbox.on('husky.button.add.click', function() {
+                this.sandbox.emit('sulu.contacts.contacts.new');
+            }, this);
+        }
+    };
+});
