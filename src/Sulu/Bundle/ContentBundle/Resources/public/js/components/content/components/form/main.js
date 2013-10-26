a28705d73636d3ac7feccbe04402dd1d1eb154ee
Johannes Wachter <johannes.wachter@massiveart.com>
2013-10-26 10:45:07 +0200

    fixed js setup

---
+++ src/Sulu/Bundle/ContentBundle/Resources/public/js/components/content/components/form/main.js
@@ -0,0 +1,129 @@
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+define([], function() {
+
+    'use strict';
+
+    return {
+
+        view: true,
+
+        templates: ['/admin/content/template/form/overview'],
+
+        initialize: function() {
+            this.currentType = this.currentState = '';
+
+            this.formId = '#content-form';
+            this.render();
+
+            this.setHeaderBar(true);
+            this.listenForChange();
+        },
+
+        render: function() {
+            this.$el.html(this.renderTemplate('/admin/content/template/form/overview'));
+
+            var data = this.initData();
+            this.createForm(data);
+
+            this.bindDomEvents();
+            this.bindCustomEvents();
+        },
+
+        createForm: function(data) {
+            var formObject = this.sandbox.form.create(this.formId);
+            formObject.initialized.then(function() {
+
+            }.bind(this));
+        },
+
+        bindDomEvents: function() {
+            this.sandbox.dom.keypress(this.formId, function(event) {
+                if (event.which === 13) {
+                    event.preventDefault();
+                    this.submit();
+                }
+            }.bind(this));
+        },
+
+        bindCustomEvents: function() {
+            // delete contact
+            this.sandbox.on('husky.button.delete.click', function() {
+                this.sandbox.emit('sulu.content.content.delete', this.options.data.id);
+            }, this);
+
+            // contact saved
+            this.sandbox.on('sulu.content.content.saved', function(id) {
+                this.setHeaderBar(true);
+            }, this);
+
+            // contact saved
+            this.sandbox.on('husky.button.save.click', function() {
+                this.submit();
+            }, this);
+        },
+
+        initData: function() {
+            var contentJson = this.options.data;
+            return contentJson;
+        },
+
+        submit: function() {
+            this.sandbox.logger.log('save Model');
+
+            if (this.sandbox.form.validate(this.formId)) {
+                var data = this.sandbox.form.getData(this.formId);
+
+                this.sandbox.logger.log('data', data);
+
+                this.sandbox.emit('sulu.content.content.save', data);
+            }
+        },
+
+        // @var Bool saved - defines if saved state should be shown
+        setHeaderBar: function(saved) {
+
+            var changeType, changeState,
+                ending = (!!this.options.data && !!this.options.data.id) ? 'Delete' : '';
+
+            changeType = 'save' + ending;
+
+            if (saved) {
+                if (ending === '') {
+                    changeState = 'hide';
+                } else {
+                    changeState = 'standard';
+                }
+            } else {
+                changeState = 'dirty';
+            }
+
+            if (this.currentType !== changeType) {
+                this.sandbox.emit('husky.header.button-type', changeType);
+                this.currentType = changeType;
+            }
+            if (this.currentState !== changeState) {
+                this.sandbox.emit('husky.header.button-state', changeState);
+                this.currentState = changeState;
+            }
+        },
+
+
+        listenForChange: function() {
+            this.sandbox.dom.on(this.formId, 'change', function() {
+                this.setHeaderBar(false);
+            }.bind(this), "select, input");
+            this.sandbox.dom.on(this.formId, 'keyup', function() {
+                this.setHeaderBar(false);
+            }.bind(this), "input");
+        }
+
+    };
+});
