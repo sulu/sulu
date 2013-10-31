a52abb1e2ad8c7678089cd127bf12a420b608dd2
turbo-ele <elias.hiller@gmx.at>
2013-10-31 17:17:37 +0100

    made gui adaption and included content navigation

---
+++ src/Sulu/Bundle/SecurityBundle/Controller/NavigationController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Sulu\Bundle\SecurityBundle\Controller;
+
+use Sulu\Bundle\AdminBundle\Admin\ContentNavigation;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class NavigationController extends Controller
+{
+    // has to be the same as defined in service.yml
+    const SERVICE_NAME = 'sulu_security.admin.roles_navigation';
+
+    /**
+     * Lists all the contacts or filters the contacts by parameters
+     * Special function for lists
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function rolesAction()
+    {
+
+        /** @var ContentNavigation $contentNavigation */
+        if ($this->has(self::SERVICE_NAME)) {
+            $contentNavigation = $this->get(self::SERVICE_NAME);
+        }
+
+        return new Response(json_encode($contentNavigation->toArray('roles')));
+    }
+}
