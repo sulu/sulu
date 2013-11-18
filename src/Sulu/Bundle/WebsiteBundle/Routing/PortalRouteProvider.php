e2ab411c1ce3f2bc138303a2571550672e9c7640
Daniel Rotter <daniel.rotter@massiveart.com>
2013-11-18 08:50:24 +0100

    created basic route provider

---
+++ src/Sulu/Bundle/WebsiteBundle/Routing/PortalRouteProvider.php
@@ -0,0 +1,104 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\WebsiteBundle\Routing;
+
+
+use Symfony\Cmf\Component\Routing\RouteProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * The PortalRouteProvider should load the dynamic routes created by Sulu
+ * @package Sulu\Bundle\WebsiteBundle\Routing
+ */
+class PortalRouteProvider implements RouteProviderInterface
+{
+
+    /**
+     * Finds routes that may potentially match the request.
+     *
+     * This may return a mixed list of class instances, but all routes returned
+     * must extend the core symfony route. The classes may also implement
+     * RouteObjectInterface to link to a content document.
+     *
+     * This method may not throw an exception based on implementation specific
+     * restrictions on the url. That case is considered a not found - returning
+     * an empty array. Exceptions are only used to abort the whole request in
+     * case something is seriously broken, like the storage backend being down.
+     *
+     * Note that implementations may not implement an optimal matching
+     * algorithm, simply a reasonable first pass.  That allows for potentially
+     * very large route sets to be filtered down to likely candidates, which
+     * may then be filtered in memory more completely.
+     *
+     * @param Request $request A request against which to match.
+     *
+     * @return \Symfony\Component\Routing\RouteCollection with all Routes that
+     *      could potentially match $request. Empty collection if nothing can
+     *      match.
+     */
+    public function getRouteCollectionForRequest(Request $request)
+    {
+        $path = $request->getRequestUri();
+
+        $collection = new RouteCollection();
+
+        $route = new Route($path, array(
+            '_controller' => 'SuluWebsiteBundle:DefaultController:index'
+        ));
+
+        $collection->add('dynamic_route_' . uniqid(), $route);
+
+        return $collection;
+    }
+
+    /**
+     * Find the route using the provided route name.
+     *
+     * @param string $name       the route name to fetch
+     * @param array $parameters DEPRECATED the parameters as they are passed
+     *      to the UrlGeneratorInterface::generate call
+     *
+     * @return \Symfony\Component\Routing\Route
+     *
+     * @throws \Symfony\Component\Routing\Exception\RouteNotFoundException if
+     *      there is no route with that name in this repository
+     */
+    public function getRouteByName($name, $parameters = array())
+    {
+        // TODO: Implement getRouteByName() method.
+    }
+
+    /**
+     * Find many routes by their names using the provided list of names.
+     *
+     * Note that this method may not throw an exception if some of the routes
+     * are not found or are not actually Route instances. It will just return the
+     * list of those Route instances it found.
+     *
+     * This method exists in order to allow performance optimizations. The
+     * simple implementation could be to just repeatedly call
+     * $this->getRouteByName() while catching and ignoring eventual exceptions.
+     *
+     * @param array $names      the list of names to retrieve
+     * @param array $parameters DEPRECATED the parameters as they are passed to
+     *      the UrlGeneratorInterface::generate call. (Only one array, not one
+     *      for each entry in $names.
+     *
+     * @return \Symfony\Component\Routing\Route[] iterable thing with the keys
+     *      the names of the $names argument.
+     */
+    public function getRoutesByNames($names, $parameters = array())
+    {
+        // TODO: Implement getRoutesByNames() method.
+    }
+}
