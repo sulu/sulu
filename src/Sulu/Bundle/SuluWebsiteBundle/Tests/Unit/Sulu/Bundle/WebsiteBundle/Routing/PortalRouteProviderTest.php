112ce905900f4497d58c3f9809ce988f2b66da61
Daniel Rotter <daniel.rotter@massiveart.com>
2013-11-19 15:31:14 +0100

    added test for route not found

--- src/Sulu/Bundle/SuluWebsiteBundle/Tests/Unit/Sulu/Bundle/WebsiteBundle/Routing/PortalRouteProviderTest.php
+++ src/Sulu/Bundle/SuluWebsiteBundle/Tests/Unit/Sulu/Bundle/WebsiteBundle/Routing/PortalRouteProviderTest.php
@@ -10,6 +10,7 @@
 
 namespace Sulu\Bundle\WebsiteBundle\Routing;
 
+use Sulu\Component\Content\Exception\ResourceLocatorNotFoundException;
 use Sulu\Component\Portal\Portal;
 use Sulu\Component\Portal\Theme;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,6 +28,60 @@ class PortalRouteProviderTest extends \PHPUnit_Framework_TestCase
         $theme->setKey('theme');
         $portal->setTheme($theme);
 
+        $structure = $this->getStructureMock($uuid);
+        $portalManager = $this->getPortalManagerMock($portal);
+        $activeTheme = $this->getActiveThemeMock();
+
+        $contentMapper = $this->getContentMapperMock($structure);
+        $contentMapper->expects($this->any())->method('loadByResourceLocator')->will($this->returnValue($structure));
+
+        $portalRouteProvider = new PortalRouteProvider($contentMapper, $portalManager, $activeTheme);
+
+        $request = $this->getRequestMock($path);
+
+        // Test the route provider
+        $routes = $portalRouteProvider->getRouteCollectionForRequest($request);
+
+        $this->assertCount(1, $routes);
+        $this->assertEquals(1, $routes->getIterator()->current()->getDefaults()['content']->getUuid());
+    }
+
+    public function testGetCollectionForNotExistingRequest()
+    {
+        // Set up test
+        $path = '/';
+        $uuid = 1;
+        $portal = new Portal();
+        $portal->setKey('portal');
+        $theme = new Theme();
+        $theme->setKey('theme');
+        $portal->setTheme($theme);
+
+        $structure = $this->getStructureMock($uuid);
+        $portalManager = $this->getPortalManagerMock($portal);
+        $activeTheme = $this->getActiveThemeMock();
+
+        $contentMapper = $this->getContentMapperMock($structure);
+        $contentMapper->expects($this->any())->method('loadByResourceLocator')->will($this->throwException(new ResourceLocatorNotFoundException()));
+
+        $portalRouteProvider = new PortalRouteProvider($contentMapper, $portalManager, $activeTheme);
+
+        $request = $this->getRequestMock($path);
+
+        // Test the route provider
+        $routes = $portalRouteProvider->getRouteCollectionForRequest($request);
+
+        $this->assertCount(1, $routes);
+        $route = $routes->getIterator()->current();
+        $this->assertEquals('SuluWebsiteBundle:Default:error404', $route->getDefaults()['_controller']);
+        $this->assertEquals('/', $route->getDefaults()['path']);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getStructureMock($uuid)
+    {
         $structure = $this->getMockForAbstractClass(
             '\Sulu\Component\Content\StructureInterface',
             array(),
@@ -39,8 +94,14 @@ class PortalRouteProviderTest extends \PHPUnit_Framework_TestCase
 
         $structure->expects($this->any())->method('getUuid')->will($this->returnValue($uuid));
 
-        $structure->expects($this->any())->method('getUuid')->will($this->returnValue($structure));
+        return $structure;
+    }
 
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getPortalManagerMock($portal)
+    {
         $portalManager = $this->getMockForAbstractClass(
             '\Sulu\Component\Portal\PortalManagerInterface',
             array(),
@@ -50,9 +111,18 @@ class PortalRouteProviderTest extends \PHPUnit_Framework_TestCase
             true,
             array('findByUrl', 'getCurrentPortal')
         );
+
         $portalManager->expects($this->any())->method('findByUrl')->will($this->returnValue($portal));
         $portalManager->expects($this->any())->method('getCurrentPortal')->will($this->returnValue($portal));
 
+        return $portalManager;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getContentMapperMock()
+    {
         $contentMapper = $this->getMockForAbstractClass(
             '\Sulu\Component\Content\Mapper\ContentMapperInterface',
             array(),
@@ -62,18 +132,26 @@ class PortalRouteProviderTest extends \PHPUnit_Framework_TestCase
             true,
             array('loadByResourceLocator')
         );
-        $contentMapper->expects($this->any())->method('loadByResourceLocator')->will($this->returnValue($structure));
 
-        $activeTheme = $this->getMock('\Liip\ThemeBundle\ActiveTheme', array(), array(), '', false);
+        return $contentMapper;
+    }
 
-        $portalRouteProvider = new PortalRouteProvider($contentMapper, $portalManager, $activeTheme);
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getActiveThemeMock()
+    {
+        return $this->getMock('\Liip\ThemeBundle\ActiveTheme', array(), array(), '', false);
+    }
 
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getRequestMock($path)
+    {
         $request = $this->getMock('\Symfony\Component\HttpFoundation\Request', array('getRequestUri'));
         $request->expects($this->any())->method('getRequestUri')->will($this->returnValue($path));
 
-        // Test the route provider
-        $routes = $portalRouteProvider->getRouteCollectionForRequest($request);
-
-        $this->assertEquals(1, $routes->getIterator()->current()->getDefaults()['content']->getUuid());
+        return $request;
     }
 }
