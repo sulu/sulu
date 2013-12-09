bbeb2625bd5966025177e9f11e670a233e2d79d5
Daniel Rotter <daniel.rotter@massiveart.com>
2013-12-09 09:48:12 +0100

    added entities missing for the group concept

---
+++ src/Sulu/Bundle/SecurityBundle/Entity/UserGroup.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Sulu\Bundle\SecurityBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UserGroup
+ */
+class UserGroup
+{
+    /**
+     * @var string
+     */
+    private $locale;
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var \Sulu\Bundle\SecurityBundle\Entity\User
+     */
+    private $user;
+
+    /**
+     * @var \Sulu\Bundle\SecurityBundle\Entity\Group
+     */
+    private $group;
+
+
+    /**
+     * Set locale
+     *
+     * @param string $locale
+     * @return UserGroup
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    
+        return $this;
+    }
+
+    /**
+     * Get locale
+     *
+     * @return string 
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \Sulu\Bundle\SecurityBundle\Entity\User $user
+     * @return UserGroup
+     */
+    public function setUser(\Sulu\Bundle\SecurityBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+    
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Sulu\Bundle\SecurityBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set group
+     *
+     * @param \Sulu\Bundle\SecurityBundle\Entity\Group $group
+     * @return UserGroup
+     */
+    public function setGroup(\Sulu\Bundle\SecurityBundle\Entity\Group $group = null)
+    {
+        $this->group = $group;
+    
+        return $this;
+    }
+
+    /**
+     * Get group
+     *
+     * @return \Sulu\Bundle\SecurityBundle\Entity\Group
+     */
+    public function getGroup()
+    {
+        return $this->group;
+    }
+}
