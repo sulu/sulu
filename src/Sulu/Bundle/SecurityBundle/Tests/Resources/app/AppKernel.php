83b5bd8aaaf6f0abe3f828663547351d2d0ac306
Daniel Rotter <daniel.rotter@massiveart.com>
2013-12-17 11:25:16 +0100

    updated test structure

--- src/Sulu/Bundle/SecurityBundle/Tests/Resources/app/AppKernel.php
+++ src/Sulu/Bundle/SecurityBundle/Tests/Resources/app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
 
             // Sulu
             new \Sulu\Bundle\CoreBundle\SuluCoreBundle(),
+            new \Sulu\Bundle\AdminBundle\SuluAdminBundle(),
             new \Sulu\Bundle\SecurityBundle\SuluSecurityBundle(),
             new \Sulu\Bundle\ContactBundle\SuluContactBundle(),
         );
