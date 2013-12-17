0c61392311c6656def6bc102b03721341c7c4da1
Daniel Rotter <daniel.rotter@massiveart.com>
2013-12-17 09:43:33 +0100

    corrected root navigation title

--- src/Sulu/Bundle/ContactBundle/Admin/SuluContactAdmin.php
+++ src/Sulu/Bundle/ContactBundle/Admin/SuluContactAdmin.php
@@ -17,9 +17,9 @@ use Sulu\Bundle\AdminBundle\Navigation\NavigationItem;
 class SuluContactAdmin extends Admin
 {
 
-    public function __construct()
+    public function __construct($title)
     {
-        $rootNavigationItem = new NavigationItem('Root');
+        $rootNavigationItem = new NavigationItem($title);
 
         $section = new NavigationItem('Tools');
 
