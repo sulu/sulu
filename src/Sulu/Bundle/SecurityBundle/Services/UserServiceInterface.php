7d4d648d22610ef8bffca555361ff60ba20ba787
Johannes Wachter <johannes.wachter@massiveart.com>
2014-01-09 16:19:40 +0100

    added userservice interface

---
+++ src/Sulu/Bundle/SecurityBundle/Services/UserServiceInterface.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\SecurityBundle\Services;
+
+use Sulu\Bundle\SecurityBundle\Entity\User;
+
+interface UserServiceInterface
+{
+    /**
+     * returns user for given id
+     * @param integer $id userId
+     * @return User
+     */
+    public function getUserByUserId($id);
+
+    /**
+     * returns username by id
+     * @param integer $id userId
+     * @return string
+     */
+    public function getUsernameByUserId($id);
+
+    /**
+     * returns fullName for userId
+     * @param integer $id userId
+     * @return string
+     */
+    public function getFullNameByUserId($id);
+} 
