5a11dc8f467fdf2b438e2611e63132fe82ac64ef
Daniel Rotter <daniel.rotter@massiveart.com>
2014-01-13 08:56:49 +0100

    implemented tests for TagController

---
+++ src/Sulu/Bundle/TagBundle/Controller/TagController.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\TagBundle\Controller;
+
+/**
+ * Makes tag available through
+ * @package Sulu\Bundle\TagBundle\Controller
+ */
+class TagController
+{
+
+} 
