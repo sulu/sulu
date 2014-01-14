eec10d87a5cc72f79e25c1852054cf67b7b6b884
Johannes Wachter <johannes.wachter@massiveart.com>
2014-01-14 15:32:49 +0100

    refactored preview interface

--- src/Sulu/Bundle/SuluContentBundle/Preview/PreviewInterface.php
+++ src/Sulu/Bundle/SuluContentBundle/Preview/PreviewInterface.php
@@ -46,9 +46,18 @@ interface PreviewInterface
      * @param string $contentUuid
      * @param string $property propertyName which was changed
      * @param mixed $data new data
-     * @return string
+     * @return \Sulu\Component\Content\StructureInterface
+     * @throws PreviewNotFoundException
      */
     public function update($userId, $contentUuid, $property, $data);
+
+    /**
+     * returns pending changes for given user and content
+     * @param $userId
+     * @param $contentUuid
+     * @return array
+     */
+    public function getChanges($userId, $contentUuid);
 
     /**
      * renders a content for given user
