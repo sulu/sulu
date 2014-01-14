16b1fce69e4930f2c98e27c620d4ea0eeade43ac
Johannes Wachter <johannes.wachter@massiveart.com>
2014-01-14 17:33:05 +0100

    implemented simple preview controller

--- src/Sulu/Bundle/ContentBundle/Controller/PreviewController.php
+++ src/Sulu/Bundle/ContentBundle/Controller/PreviewController.php
@@ -10,9 +10,12 @@
 
 namespace Sulu\Bundle\ContentBundle\Controller;
 
+use DateTime;
 use Sulu\Bundle\ContentBundle\Preview\PreviewInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 class PreviewController extends Controller
 {
@@ -35,6 +38,25 @@ class PreviewController extends Controller
         $content = $this->getPreview()->render($uid, $contentUuid);
 
         return $this->render('SuluContentBundle:Preview:render.html.twig', array('content' => $content));
+    }
+
+    public function updateAction($contentUuid, Request $request)
+    {
+        $uid = $this->getUserId();
+        $property = $request->get('property');
+        $value = $request->get('value');
+
+        $this->getPreview()->update($uid, $contentUuid, $property, $value);
+
+        return new Response();
+    }
+
+    public function changesAction($contentUuid)
+    {
+        $uid = $this->getUserId();
+        $changes = $this->getPreview()->getChanges($uid, $contentUuid);
+
+        return new Response(json_encode($changes));
     }
 
     /**
