4a8c9009f9c35bf61fb4fdd273301061fbc9a110
Daniel Rotter <daniel.rotter@massiveart.com>
2014-01-20 11:59:20 +0100

    implemented list view of tags

---
+++ src/Sulu/Bundle/TagBundle/Resources/public/js/components/tags/main.js
@@ -0,0 +1,40 @@
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+define([
+    'sulutag/model/tag'
+], function(Tag) {
+
+    'use strict';
+
+    return {
+
+        initialize: function() {
+            this.bindCustomEvents();
+
+            if (this.options.display === 'list') {
+                this.renderList();
+            } else {
+                throw 'display type wrong';
+            }
+        },
+
+        bindCustomEvents: function() {
+
+        },
+
+        renderList: function() {
+            var $list = this.sandbox.dom.createElement('<div id="contacts-list-container"/>');
+            this.html($list);
+            this.sandbox.start([
+                {name: 'tags/components/list@sulutag', options: { el: $list}}
+            ]);
+        }
+    };
+});
