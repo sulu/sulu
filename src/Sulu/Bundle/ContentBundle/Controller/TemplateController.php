33dbafb6392d9522f14965ad6a968051f120a582
Johannes Wachter <johannes.wachter@massiveart.com>
2014-02-03 15:08:25 +0100

    added base template

--- src/Sulu/Bundle/ContentBundle/Controller/TemplateController.php
+++ src/Sulu/Bundle/ContentBundle/Controller/TemplateController.php
@@ -10,6 +10,7 @@
 
 namespace Sulu\Bundle\ContentBundle\Controller;
 
+use Sulu\Bundle\AdminBundle\UserManager\UserManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TemplateController extends Controller
@@ -21,6 +22,62 @@ class TemplateController extends Controller
         return $this->render(
             'SuluContentBundle:Template:content.html.twig',
             array('template' => $template)
+        );
+    }
+
+    public function splitScreenAction($contentUuid)
+    {
+        return $this->render(
+            'SuluContentBundle:Template:split-screen.html.twig',
+            $this->getTemplateVarsSplitScreen($contentUuid)
+        );
+    }
+
+    /**
+     * FIXME remove after change ui (from new window to inline)
+     * DEEP COPY from AdminController:indexAction
+     * @param $contentUuid
+     * @return array
+     */
+    private function getTemplateVarsSplitScreen($contentUuid)
+    {
+        // get user data
+        $serviceId = $this->container->getParameter('sulu_admin.user_data_service');
+
+        $user = array();
+        if ($this->has($serviceId)) {
+            /** @var UserManagerInterface $userManager */
+            $userManager = $this->get($serviceId);
+            $userData = $userManager->getCurrentUserData();
+
+            // user settings
+            $userSettings = $userData->getUserSettings();
+            $first = true;
+            $userSettingsString = '{';
+            foreach ($userSettings as $settings) {
+                if (!$first) {
+                    $userSettingsString .= ',';
+                } else {
+                    $first = !$first;
+                }
+                $userSettingsString .= '"' . $settings->getKey() . '"' . ':' . $settings->getValue();
+            }
+            $userSettingsString .= '}';
+
+            if ($userData->isLoggedIn()) {
+                $user['id'] = $userData->getId();
+                $user['username'] = $userData->getFullName();
+                $user['logout'] = $userData->getLogoutLink();
+                $user['locale'] = $userData->getLocale();
+                $user['settings'] = $userSettingsString;
+            }
+        }
+
+        // render template
+        return array(
+            'name' => $this->container->getParameter('sulu_admin.name'),
+            'user' => $user,
+            'contentUuid' => $contentUuid
         );
     }
 
