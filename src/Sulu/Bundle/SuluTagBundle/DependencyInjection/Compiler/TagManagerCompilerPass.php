c4cf604db079ab982c5bc39891ddb46d6ce3f8e4
Daniel Rotter <daniel.rotter@massiveart.com>
2014-02-10 10:04:41 +0100

    made SecurityContext dependency optional

---
+++ src/Sulu/Bundle/SuluTagBundle/DependencyInjection/Compiler/TagManagerCompilerPass.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\TagBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class TagManagerCompilerPass implements CompilerPassInterface
+{
+
+    /**
+     * You can modify the container here before it is dumped to PHP code.
+     *
+     * @param ContainerBuilder $container
+     *
+     * @api
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if ($container->hasDefinition('security.context')) {
+            // add security context to tag manager only if there is a security context
+            $container->getDefinition('sulu_tag.tag_manager')->addMethodCall(
+                'setSecurityContext',
+                array(new Reference('security.context'))
+            );
+        }
+    }
+}
