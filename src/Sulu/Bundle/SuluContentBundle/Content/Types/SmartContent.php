bc5762df586180266a09962d8ddf7982da7787ed
Daniel Rotter <daniel.rotter@massiveart.com>
2014-02-10 16:35:40 +0100

    stored data of smart content field

--- src/Sulu/Bundle/SuluContentBundle/Content/Types/SmartContent.php
+++ src/Sulu/Bundle/SuluContentBundle/Content/Types/SmartContent.php
@@ -10,19 +10,19 @@
 
 namespace Sulu\Bundle\ContentBundle\Content\Types;
 
-use Sulu\Component\Content\SimpleContentType;
+use PHPCR\NodeInterface;
+use Sulu\Component\Content\ComplexContentType;
+use Sulu\Component\Content\PropertyInterface;
 
 /**
  * ContentType for TextEditor
  */
-class SmartContent extends SimpleContentType
+class SmartContent extends ComplexContentType
 {
     private $template;
 
     function __construct($template)
     {
-        parent::__construct('SmartContent', '');
-
         $this->template = $template;
     }
 
@@ -33,5 +33,49 @@ class SmartContent extends SimpleContentType
     public function getTemplate()
     {
         return $this->template;
+    }
+
+    /**
+     * returns type of ContentType
+     * PRE_SAVE or POST_SAVE
+     * @return int
+     */
+    public function getType()
+    {
+        return self::PRE_SAVE;
+    }
+
+    /**
+     * reads the value for given property from the node + sets the value of the property
+     * @param NodeInterface $node
+     * @param PropertyInterface $property
+     * @param string $webspaceKey
+     * @return mixed
+     */
+    public function get(NodeInterface $node, PropertyInterface $property, $webspaceKey)
+    {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * save the value from given property
+     * @param NodeInterface $node
+     * @param PropertyInterface $property
+     * @param string $webspaceKey
+     * @return mixed
+     */
+    public function set(NodeInterface $node, PropertyInterface $property, $webspaceKey)
+    {
+        $node->setProperty($property->getName(), json_encode($property->getValue()));
+    }
+
+    /**
+     * remove property from given node
+     * @param NodeInterface $node
+     * @param PropertyInterface $property
+     */
+    public function remove(NodeInterface $node, PropertyInterface $property)
+    {
+        // TODO: Implement remove() method.
     }
 }
