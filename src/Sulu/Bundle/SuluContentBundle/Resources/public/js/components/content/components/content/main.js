1f4657f2e2ea4c9b0ee4bb3923ec8c2183e84a06
Johannes Wachter <johannes.wachter@massiveart.com>
2014-02-13 09:10:28 +0100

    implemented template as function

--- src/Sulu/Bundle/SuluContentBundle/Resources/public/js/components/content/components/content/main.js
+++ src/Sulu/Bundle/SuluContentBundle/Resources/public/js/components/content/components/content/main.js
@@ -12,53 +12,59 @@ define(function() {
     'use strict';
 
     return {
-        content: {
-            url: '/admin/content/navigation/content',
-            title: 'content.contents.title',
-            extendTemplate: 'default',
-            template: function() {
-                return [
-                    {
-                        icon: 'eye-open',
-                        iconSize: 'large',
-                        group: 'right',
-                        position: 1,
-                        items: [
-                            {
-                                title: this.sandbox.translate('sulu.edit-toolbar.new-window'),
-                                callback: function() {
-                                    this.sandbox.emit('sulu.edit-toolbar.preview.new-window');
-                                }.bind(this)
-                            },
-                            {
-                                title: this.sandbox.translate('sulu.edit-toolbar.split-screen'),
-                                callback: function() {
-                                    this.sandbox.emit('sulu.edit-toolbar.preview.split-screen');
+
+        content: function() {
+            return {
+                url: '/admin/content/navigation/content',
+                title: 'content.contents.title',
+                extendTemplate: 'default',
+                template: function() {
+                    if (!!result.options.id) {
+                        return [];
+                    }
+                    return [
+                        {
+                            icon: 'eye-open',
+                            iconSize: 'large',
+                            group: 'right',
+                            position: 1,
+                            items: [
+                                {
+                                    title: this.sandbox.translate('sulu.edit-toolbar.new-window'),
+                                    callback: function() {
+                                        result.sandbox.emit('sulu.edit-toolbar.preview.new-window');
+                                    }.bind(this)
+                                },
+                                {
+                                    title: this.sandbox.translate('sulu.edit-toolbar.split-screen'),
+                                    callback: function() {
+                                        this.sandbox.emit('sulu.edit-toolbar.preview.split-screen');
+                                    }.bind(this)
+                                }
+                            ]
+                        },
+                        {
+                            id: 'template',
+                            icon: 'tag',
+                            iconSize: 'large',
+                            group: 'right',
+                            position: 1,
+                            type: 'select',
+                            title: '',
+                            itemsOption: {
+                                url: '/admin/content/template',
+                                titleAttribute: 'template',
+                                idAttribute: 'template',
+                                translate: true,
+                                languageNamespace: 'template.',
+                                callback: function(item) {
+                                    result.sandbox.emit('sulu.edit-toolbar.dropdown.template.item-clicked', item);
                                 }.bind(this)
                             }
-                        ]
-                    },
-                    {
-                        id: 'template',
-                        icon: 'tag',
-                        iconSize: 'large',
-                        group: 'right',
-                        position: 1,
-                        type: 'select',
-                        title: '',
-                        itemsOption: {
-                            url: '/admin/content/template',
-                            titleAttribute: 'template',
-                            idAttribute: 'template',
-                            translate: true,
-                            languageNamespace: 'template.',
-                            callback: function(item) {
-                                this.sandbox.emit('sulu.edit-toolbar.dropdown.template.item-clicked', item);
-                            }.bind(this)
                         }
-                    }
-                ];
-            }
-        }
+                    ];
+                }.bind(this)
+            };
+        }.bind(this)
     };
 });
