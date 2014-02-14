46c5dca334d8f6508b7439ac261bfb75bd0582bd
marcelmoosbrugger <marcel.moosbrugger@massiveart.com>
2014-02-14 14:08:59 +0100

    basic file strucutre

---
+++ src/Sulu/Bundle/AdminBundle/Resources/public/js/components/labels/main.js
@@ -0,0 +1,42 @@
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ * provides:
+ *  - sulu.edittoolbar.setState();
+ *  - sulu.edittoolbar.setButton(id);
+ *
+ * triggers:
+ *  - sulu.edittoolbar.submit - when most left button was clicked
+ *
+ * options:
+ *  - heading - string
+ *  - tabsData - dataArray needed for building tabs
+ *  -
+ *
+ *
+ */
+
+define([], function() {
+
+    'use strict';
+
+    var defaults = {
+
+    }
+
+
+    return {
+        view: true,
+
+        initialize: function() {
+            // merge defaults
+            this.options = this.sandbox.util.extend(true, {}, defaults, this.options);
+
+        }
+    };
+});
