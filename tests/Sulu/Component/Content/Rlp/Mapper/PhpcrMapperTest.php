27a7f3e46d11dc14ab4a01ccd3d08a0e64cce2dd
Daniel Rotter <daniel.rotter@massiveart.com>
2014-02-17 15:08:56 +0100

    corrected tests

--- tests/Sulu/Component/Content/Rlp/Mapper/PhpcrMapperTest.php
+++ tests/Sulu/Component/Content/Rlp/Mapper/PhpcrMapperTest.php
@@ -10,20 +10,11 @@
 
 namespace Sulu\Component\Content\Rlp\Strategy;
 
-use Jackalope\RepositoryFactoryJackrabbit;
 use PHPCR\NodeInterface;
-use PHPCR\SessionInterface;
-use PHPCR\SimpleCredentials;
-use PHPCR\Util\NodeHelper;
-use \PHPUnit_Framework_TestCase;
 use Sulu\Bundle\TestBundle\Testing\PhpcrTestCase;
 use Sulu\Component\Content\Exception\ResourceLocatorMovedException;
 use Sulu\Component\Content\Types\Rlp\Mapper\PhpcrMapper;
 use Sulu\Component\Content\Types\Rlp\Mapper\RlpMapperInterface;
-use Sulu\Component\PHPCR\NodeTypes\Base\SuluNodeType;
-use Sulu\Component\PHPCR\NodeTypes\Path\PathNodeType;
-use Sulu\Component\PHPCR\SessionManager\SessionManager;
-use Sulu\Component\PHPCR\SessionManager\SessionManagerInterface;
 
 class PhpcrMapperTest extends PhpcrTestCase
 {
@@ -32,70 +23,98 @@ class PhpcrMapperTest extends PhpcrTestCase
      */
     private $content1;
 
+    /**
+     * @var RlpMapperInterface
+     */
+    private $rlpMapper;
+
     public function setUp()
     {
         $this->prepareMapper();
+        $this->prepareTestData();
+
+        $this->rlpMapper = new PhpcrMapper($this->sessionManager, '/cmf/routes');
+    }
+
+    private function prepareTestData()
+    {
+        $products = $this->routes->addNode('products');
+        $products->addMixin('mix:referenceable');
+
+        $machines = $products->addNode('machines');
+        $machines->addMixin('mix:referenceable');
+
+        $machines1 = $products->addNode('machines-1');
+        $machines1->addMixin('mix:referenceable');
+
+        $drill = $machines->addNode('drill');
+        $drill->addMixin('mix:referenceable');
+
+        $drill1 = $machines->addNode('drill-1');
+        $drill1->addMixin('mix:referenceable');
 
         $this->content1 = $this->contents->addNode('content1');
         $this->content1->addMixin('mix:referenceable');
-    }
+
+        $this->session->save();
+      }
 
     public function testUnique()
     {
         // exists in phpcr
-        $result = $this->mapper->unique('/products/machines', 'default');
+        $result = $this->rlpMapper->unique('/products/machines', 'default');
         $this->assertFalse($result);
 
         // exists in phpcr
-        $result = $this->mapper->unique('/products/machines/drill', 'default');
+        $result = $this->rlpMapper->unique('/products/machines/drill', 'default');
         $this->assertFalse($result);
 
         // not exists in phpcr
-        $result = $this->mapper->unique('/products/machines-2', 'default');
+        $result = $this->rlpMapper->unique('/products/machines-2', 'default');
         $this->assertTrue($result);
 
         // not exists in phpcr
-        $result = $this->mapper->unique('/products/machines/drill-2', 'default');
+        $result = $this->rlpMapper->unique('/products/machines/drill-2', 'default');
         $this->assertTrue($result);
 
         // not exists in phpcr
-        $result = $this->mapper->unique('/news', 'default');
+        $result = $this->rlpMapper->unique('/news', 'default');
         $this->assertTrue($result);
     }
 
     public function testGetUniquePath()
     {
         // machines & machines-1 exists
-        $result = $this->mapper->getUniquePath('/products/machines', 'default');
+        $result = $this->rlpMapper->getUniquePath('/products/machines', 'default');
         $this->assertEquals('/products/machines-2', $result);
-        $this->assertTrue($this->mapper->unique($result, 'default'));
+        $this->assertTrue($this->rlpMapper->unique($result, 'default'));
 
         // drill & drill-1 exists
-        $result = $this->mapper->getUniquePath('/products/machines/drill', 'default');
+        $result = $this->rlpMapper->getUniquePath('/products/machines/drill', 'default');
         $this->assertEquals('/products/machines/drill-2', $result);
-        $this->assertTrue($this->mapper->unique($result, 'default'));
+        $this->assertTrue($this->rlpMapper->unique($result, 'default'));
 
         // products exists
-        $result = $this->mapper->getUniquePath('/products', 'default');
+        $result = $this->rlpMapper->getUniquePath('/products', 'default');
         $this->assertEquals('/products-1', $result);
-        $this->assertTrue($this->mapper->unique($result, 'default'));
+        $this->assertTrue($this->rlpMapper->unique($result, 'default'));
 
         // news not exists
-        $result = $this->mapper->getUniquePath('/news', 'default');
+        $result = $this->rlpMapper->getUniquePath('/news', 'default');
         $this->assertEquals('/news', $result);
-        $this->assertTrue($this->mapper->unique($result, 'default'));
+        $this->assertTrue($this->rlpMapper->unique($result, 'default'));
     }
 
     public function testSaveFailure()
     {
         $this->setExpectedException('Sulu\Component\Content\Exception\ResourceLocatorAlreadyExistsException');
-        $this->mapper->save($this->content1, '/products/machines/drill', 'default');
+        $this->rlpMapper->save($this->content1, '/products/machines/drill', 'default');
     }
 
     public function testSave()
     {
-        $this->mapper->save($this->content1, '/products/news/content1-news', 'default');
-        $this->sessionService->getSession()->save();
+        $this->rlpMapper->save($this->content1, '/products/news/content1-news', 'default');
+        $this->sessionManager->getSession()->save();
 
         $route = '/cmf/default/routes/products/news/content1-news';
 
@@ -107,58 +126,58 @@ class PhpcrMapperTest extends PhpcrTestCase
     public function testReadFailure()
     {
         $this->setExpectedException('Sulu\Component\Content\Exception\ResourceLocatorNotFoundException');
-        $this->mapper->loadByContent($this->content1, 'default');
+        $this->rlpMapper->loadByContent($this->content1, 'default');
     }
 
     public function testReadFailureUuid()
     {
         $this->setExpectedException('Sulu\Component\Content\Exception\ResourceLocatorNotFoundException');
-        $this->mapper->loadByContentUuid($this->content1->getIdentifier(), 'default');
+        $this->rlpMapper->loadByContentUuid($this->content1->getIdentifier(), 'default');
     }
 
     public function testRead()
     {
-        $this->mapper->save($this->content1, '/products/news/content1-news', 'default');
-        $this->sessionService->getSession()->save();
+        $this->rlpMapper->save($this->content1, '/products/news/content1-news', 'default');
+        $this->sessionManager->getSession()->save();
 
-        $result = $this->mapper->loadByContent($this->content1, 'default');
+        $result = $this->rlpMapper->loadByContent($this->content1, 'default');
         $this->assertEquals('/products/news/content1-news', $result);
     }
 
     public function testReadUuid()
     {
-        $this->mapper->save($this->content1, '/products/news/content1-news', 'default');
-        $this->sessionService->getSession()->save();
+        $this->rlpMapper->save($this->content1, '/products/news/content1-news', 'default');
+        $this->sessionManager->getSession()->save();
 
-        $result = $this->mapper->loadByContentUuid($this->content1->getIdentifier(), 'default');
+        $result = $this->rlpMapper->loadByContentUuid($this->content1->getIdentifier(), 'default');
         $this->assertEquals('/products/news/content1-news', $result);
     }
 
     public function testLoadFailure()
     {
         $this->setExpectedException('Sulu\Component\Content\Exception\ResourceLocatorNotFoundException');
-        $this->mapper->loadByResourceLocator('/test/test-1', 'default');
+        $this->rlpMapper->loadByResourceLocator('/test/test-1', 'default');
     }
 
     public function testLoad()
     {
         // create route for content
-        $this->mapper->save($this->content1, '/products/news/content1-news', 'default');
-        $this->sessionService->getSession()->save();
+        $this->rlpMapper->save($this->content1, '/products/news/content1-news', 'default');
+        $this->sessionManager->getSession()->save();
 
-        $result = $this->mapper->loadByResourceLocator('/products/news/content1-news', 'default');
+        $result = $this->rlpMapper->loadByResourceLocator('/products/news/content1-news', 'default');
         $this->assertEquals($this->content1->getIdentifier(), $result);
     }
 
     public function testMove()
     {
         // create route for content
-        $this->mapper->save($this->content1, '/products/news/content1-news', 'default');
-        $this->sessionService->getSession()->save();
+        $this->rlpMapper->save($this->content1, '/products/news/content1-news', 'default');
+        $this->sessionManager->getSession()->save();
 
         // move
-        $this->mapper->move('/products/news/content1-news', '/products/asdf/content2-news', 'default');
-        $this->sessionService->getSession()->save();
+        $this->rlpMapper->move('/products/news/content1-news', '/products/asdf/content2-news', 'default');
+        $this->sessionManager->getSession()->save();
 
         $oldNode = $this->session->getNode('/cmf/default/routes/products/news/content1-news');
         $newNode = $this->session->getNode('/cmf/default/routes/products/asdf/content2-news');
@@ -174,27 +193,27 @@ class PhpcrMapperTest extends PhpcrTestCase
         $this->assertEquals($this->content1, $newNode->getPropertyValue('sulu:content'));
 
         // get content from new path
-        $result = $this->mapper->loadByResourceLocator('/products/asdf/content2-news', 'default');
+        $result = $this->rlpMapper->loadByResourceLocator('/products/asdf/content2-news', 'default');
         $this->assertEquals($this->content1->getIdentifier(), $result);
 
         // get content from history should throw an exception
         $this->setExpectedException('Sulu\Component\Content\Exception\ResourceLocatorMovedException');
-        $result = $this->mapper->loadByResourceLocator('/products/news/content1-news', 'default');
+        $result = $this->rlpMapper->loadByResourceLocator('/products/news/content1-news', 'default');
     }
 
     public function testMoveTwice()
     {
         // create route for content
-        $this->mapper->save($this->content1, '/products/news/content1-news', 'default');
-        $this->sessionService->getSession()->save();
+        $this->rlpMapper->save($this->content1, '/products/news/content1-news', 'default');
+        $this->sessionManager->getSession()->save();
 
         // first move
-        $this->mapper->move('/products/news/content1-news', '/products/news/content2-news', 'default');
-        $this->sessionService->getSession()->save();
+        $this->rlpMapper->move('/products/news/content1-news', '/products/news/content2-news', 'default');
+        $this->sessionManager->getSession()->save();
 
         // second move
-        $this->mapper->move('/products/news/content2-news', '/products/asdf/content2-news', 'default');
-        $this->sessionService->getSession()->save();
+        $this->rlpMapper->move('/products/news/content2-news', '/products/asdf/content2-news', 'default');
+        $this->sessionManager->getSession()->save();
 
         $oldNode = $this->session->getNode('/cmf/default/routes/products/news/content1-news');
         $newNode = $this->session->getNode('/cmf/default/routes/products/asdf/content2-news');
@@ -209,73 +228,73 @@ class PhpcrMapperTest extends PhpcrTestCase
         $this->assertEquals($this->content1, $newNode->getPropertyValue('sulu:content'));
 
         // get content from new path
-        $result = $this->mapper->loadByResourceLocator('/products/asdf/content2-news', 'default');
+        $result = $this->rlpMapper->loadByResourceLocator('/products/asdf/content2-news', 'default');
         $this->assertEquals($this->content1->getIdentifier(), $result);
 
         // get content from history should throw an exception
         $this->setExpectedException('Sulu\Component\Content\Exception\ResourceLocatorMovedException');
-        $result = $this->mapper->loadByResourceLocator('/products/news/content1-news', 'default');
+        $result = $this->rlpMapper->loadByResourceLocator('/products/news/content1-news', 'default');
     }
 
     public function testMoveNotExist()
     {
         // create routes for content
-        $this->mapper->save($this->content1, '/news/news-1', 'default');
-        $this->sessionService->getSession()->save();
+        $this->rlpMapper->save($this->content1, '/news/news-1', 'default');
+        $this->sessionManager->getSession()->save();
 
         $this->setExpectedException('Sulu\Component\Content\Exception\ResourceLocatorNotFoundException');
-        $this->mapper->move('/news', '/neuigkeiten', 'default');
+        $this->rlpMapper->move('/news', '/neuigkeiten', 'default');
     }
 
     public function testMoveTree()
     {
-        $session = $this->sessionService->getSession();
+        $session = $this->sessionManager->getSession();
 
         // create routes for content
-        $this->mapper->save($this->content1, '/news', 'default');
-        $this->mapper->save($this->content1, '/news/news-1', 'default');
-        $this->mapper->save($this->content1, '/news/news-1/sub-1', 'default');
-        $this->mapper->save($this->content1, '/news/news-1/sub-2', 'default');
+        $this->rlpMapper->save($this->content1, '/news', 'default');
+        $this->rlpMapper->save($this->content1, '/news/news-1', 'default');
+        $this->rlpMapper->save($this->content1, '/news/news-1/sub-1', 'default');
+        $this->rlpMapper->save($this->content1, '/news/news-1/sub-2', 'default');
 
-        $this->mapper->save($this->content1, '/news/news-2', 'default');
-        $this->mapper->save($this->content1, '/news/news-2/sub-1', 'default');
-        $this->mapper->save($this->content1, '/news/news-2/sub-2', 'default');
+        $this->rlpMapper->save($this->content1, '/news/news-2', 'default');
+        $this->rlpMapper->save($this->content1, '/news/news-2/sub-1', 'default');
+        $this->rlpMapper->save($this->content1, '/news/news-2/sub-2', 'default');
         $session->save();
 
         // move route
-        $this->mapper->move('/news', '/test', 'default');
+        $this->rlpMapper->move('/news', '/test', 'default');
         $session->save();
         $session->refresh(false);
 
         // check exist new routes
         $this->assertEquals(
             $this->content1->getIdentifier(),
-            $this->mapper->loadByResourceLocator('/test', 'default')
+            $this->rlpMapper->loadByResourceLocator('/test', 'default')
         );
         $this->assertEquals(
             $this->content1->getIdentifier(),
-            $this->mapper->loadByResourceLocator('/test/news-1', 'default')
+            $this->rlpMapper->loadByResourceLocator('/test/news-1', 'default')
         );
         $this->assertEquals(
             $this->content1->getIdentifier(),
-            $this->mapper->loadByResourceLocator('/test/news-1/sub-1', 'default')
+            $this->rlpMapper->loadByResourceLocator('/test/news-1/sub-1', 'default')
         );
         $this->assertEquals(
             $this->content1->getIdentifier(),
-            $this->mapper->loadByResourceLocator('/test/news-1/sub-2', 'default')
+            $this->rlpMapper->loadByResourceLocator('/test/news-1/sub-2', 'default')
         );
 
         $this->assertEquals(
             $this->content1->getIdentifier(),
-            $this->mapper->loadByResourceLocator('/test/news-2', 'default')
+            $this->rlpMapper->loadByResourceLocator('/test/news-2', 'default')
         );
         $this->assertEquals(
             $this->content1->getIdentifier(),
-            $this->mapper->loadByResourceLocator('/test/news-2/sub-1', 'default')
+            $this->rlpMapper->loadByResourceLocator('/test/news-2/sub-1', 'default')
         );
         $this->assertEquals(
             $this->content1->getIdentifier(),
-            $this->mapper->loadByResourceLocator('/test/news-2/sub-2', 'default')
+            $this->rlpMapper->loadByResourceLocator('/test/news-2/sub-2', 'default')
         );
 
         // check history
@@ -292,7 +311,7 @@ class PhpcrMapperTest extends PhpcrTestCase
     private function getRlForHistory($rl)
     {
         try {
-            $this->mapper->loadByResourceLocator($rl, 'default');
+            $this->rlpMapper->loadByResourceLocator($rl, 'default');
 
             return false;
         } catch (ResourceLocatorMovedException $ex) {
