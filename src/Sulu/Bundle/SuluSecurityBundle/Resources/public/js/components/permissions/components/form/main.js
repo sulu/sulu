1b3900e0dbca1cdc1fd00906c6a1faa7f45cf1c6
turbo-ele <elias.hiller@gmx.at>
2014-02-28 10:23:09 +0100

    form changes in permissions template

--- src/Sulu/Bundle/SuluSecurityBundle/Resources/public/js/components/permissions/components/form/main.js
+++ src/Sulu/Bundle/SuluSecurityBundle/Resources/public/js/components/permissions/components/form/main.js
@@ -441,7 +441,7 @@ define([], function() {
                         '<tr data-id=\"', id, '\">',
                             '<td><input type="checkbox" class="custom-checkbox is-selected" checked/><span class="custom-checkbox-icon"></span></td>',
                             '<td>', title, '</td>',
-                            '<td id="languageSelector', id, '"></td>',
+                            '<td class="m-top-15" id="languageSelector', id, '"></td>',
                             '<td></td>',
                         '</tr>'
                     ].join('');
@@ -450,7 +450,7 @@ define([], function() {
                         '<tr data-id=\"', id, '\">',
                             '<td><input type="checkbox" class="custom-checkbox"/><span class="custom-checkbox-icon"></span></td>',
                             '<td>', title, '</td>',
-                            '<td id="languageSelector', id, '"></td>',
+                            '<td class="m-top-15" id="languageSelector', id, '"></td>',
                             '<td></td>',
                         '</tr>'
                     ].join('');
