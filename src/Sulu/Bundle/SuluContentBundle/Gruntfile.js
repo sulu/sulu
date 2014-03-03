ab021261b5099a3790f55352b941a413544de32a
Johannes Wachter <johannes.wachter@massiveart.com>
2014-03-03 08:36:00 +0100

    added requirejs:complete grunt task

--- src/Sulu/Bundle/SuluContentBundle/Gruntfile.js
+++ src/Sulu/Bundle/SuluContentBundle/Gruntfile.js
@@ -58,6 +58,53 @@ module.exports = function (grunt) {
                     relativeAssets: false
                 }
             }
+        },
+        requirejs: {
+            // TODO doesn´t work (combine anonymous module, same url)
+            complete: {
+                options: {
+                    out: 'Resources/public/dist/content.js',
+                    baseUrl: 'Resources/public/js/',
+                    preserveLicenseComments: false,
+                    paths: {
+                        'sulucontent': './',
+                        'type/default': '../../../vendor/sulu/admin-bundle/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky',
+                        'form/util': '../../../vendor/sulu/admin-bundle/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky',
+                        'app-config': '../../../vendor/sulu/admin-bundle/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky',
+                        'mvc/relationalmodel': '../../../vendor/sulu/admin-bundle/Sulu/Bundle/AdminBundle/Resources/public/js/components/app-config/main',
+
+                        'sulucontent/main': 'main',
+                        'type/resourceLocator': 'validation/types/resourceLocator',
+                        'type/textEditor': 'validation/types/textEditor',
+                        'type/smartContent': 'validation/types/smartContent',
+
+                        '__component__$content@sulucontent': 'components/content/main',
+                        '__component__$content/components/form@sulucontent': 'components/content/components/form/main',
+                        '__component__$content/components/content@sulucontent': 'components/content/components/content/main',
+                        '__component__$content/components/column@sulucontent': 'components/content/components/column/main',
+                        '__component__$content/components/list@sulucontent': 'components/content/components/list/main',
+                        '__component__$content/components/split-screen@sulucontent': 'components/content/components/split-screen/main'
+                    },
+                    exclude: [
+                        'type/default',
+                        'form/util',
+                        'app-config',
+                        'mvc/relationalmodel'
+                    ],
+                    include: [
+                        'sulucontent/main',
+                        'type/resourceLocator',
+                        'type/textEditor',
+                        'type/smartContent',
+                        '__component__$content@sulucontent',
+                        '__component__$content/components/form@sulucontent',
+                        '__component__$content/components/content@sulucontent',
+                        '__component__$content/components/column@sulucontent',
+                        '__component__$content/components/list@sulucontent',
+                        '__component__$content/components/split-screen@sulucontent'
+                    ]
+                }
+            }
         }
     });
 
