833ce9ece7b4cc4d273c9b1cc76e7d8be6d73ea9
Johannes Wachter <johannes.wachter@massiveart.com>
2014-03-04 16:17:43 +0100

    updated husky

--- src/Sulu/Bundle/SuluAdminBundle/Resources/public/js/vendor/husky/husky.js
+++ src/Sulu/Bundle/SuluAdminBundle/Resources/public/js/vendor/husky/husky.js
@@ -28366,9 +28366,21 @@ define('__component__$matrix@husky',[],function() {
         },
 
         toggleIcon: function(event) {
-            var $target = event.currentTarget;
+            var $target = event.currentTarget,
+                $tr = sandbox.dom.parent($target),
+                $allTargets = sandbox.dom.find('span[class^="icon-"]', $tr),
+                $activeTargets,
+                $link = sandbox.dom.find('td:last-child span', $tr);
+
             $target = sandbox.dom.find('span[class^="icon-"]', $target);
             sandbox.dom.toggleClass($target, activeClass);
+
+            $activeTargets = sandbox.dom.find('span[class^="icon-"].' + activeClass, $tr);
+            if ($activeTargets.length < $allTargets.length) {
+                sandbox.dom.html($link, this.options.captions.all);
+            } else {
+                sandbox.dom.html($link, this.options.captions.none);
+            }
 
             // emit events for communication with the outside
             sandbox.emit('husky.matrix.changed', {
@@ -28380,8 +28392,17 @@ define('__component__$matrix@husky',[],function() {
 
         setRowActive: function(event) {
             var $tr = sandbox.dom.parent(event.currentTarget),
-                $targets = sandbox.dom.find('span[class^="icon-"]', $tr);
-            sandbox.dom.addClass($targets, activeClass);
+                $targets = sandbox.dom.find('span[class^="icon-"]', $tr),
+                $activeTargets = sandbox.dom.find('span[class^="icon-"].' + activeClass, $tr),
+                $link = sandbox.dom.find('td:last-child span', $tr);
+
+            if ($activeTargets.length < $targets.length) {
+                sandbox.dom.addClass($targets, activeClass);
+                sandbox.dom.html($link, this.options.captions.none);
+            } else {
+                sandbox.dom.removeClass($targets, activeClass);
+                sandbox.dom.html($link, this.options.captions.all);
+            }
 
             // emit events for communication with the outside
             sandbox.emit('husky.matrix.changed', {
@@ -35413,7 +35434,7 @@ define('husky_extensions/collection',[],function() {
                         if ($(selector).length === 0) {
                             return false;
                         }
-                        
+
                         if (!force) {
                             force = false;
                         }
