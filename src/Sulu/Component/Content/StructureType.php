f5d2b071a43b3deac630d168c0b53c13cef5f92d
Johannes Wachter <johannes.wachter@massiveart.com>
2014-03-11 11:28:49 +0100

    added basic structure type

---
+++ src/Sulu/Component/Content/StructureType.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Component\Content;
+
+
+class StructureType
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $value;
+
+    function __construct($name, $value)
+    {
+        $this->name = $name;
+        $this->value = $value;
+    }
+
+    /**
+     * return name of type
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * return value of type
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * returns a array representation
+     * @return array
+     */
+    public function toArray()
+    {
+    }
+}
