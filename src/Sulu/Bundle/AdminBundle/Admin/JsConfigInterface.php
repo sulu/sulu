857e0063fe45310574be47aeef917a78cdcee44c
Thomas Schedler <thomas@chirimoya.at>
2014-03-14 09:17:47 +0100

    cleanup

--- src/Sulu/Bundle/AdminBundle/Admin/JsConfigInterface.php
+++ src/Sulu/Bundle/AdminBundle/Admin/JsConfigInterface.php
@@ -7,6 +7,7 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
+
 namespace Sulu\Bundle\AdminBundle\Admin;
 
 /**
@@ -18,8 +19,13 @@ interface JsConfigInterface
 {
     /**
      * returns array of parameters
+     * @return array
      */
     public function getParameters();
 
+    /**
+     * returns the identifying name
+     * @return string
+     */
     public function getName();
 }
