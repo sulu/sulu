85676c7dc86a0a463cc36d586faac5ffc5fa02e5
Thomas Schedler <thomas@chirimoya.at>
2014-04-02 21:54:39 +0200

    code review & cleanup

--- src/Sulu/Bundle/SuluWebsiteBundle/DependencyInjection/SuluWebsiteExtension.php
+++ src/Sulu/Bundle/SuluWebsiteBundle/DependencyInjection/SuluWebsiteExtension.php
@@ -30,7 +30,7 @@ class SuluWebsiteExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
     }
 }
