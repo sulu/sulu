a5a3cffdb38a89efa1ac29320ba503148e7b6b5c
Johannes Wachter <johannes.wachter@massiveart.com>
2014-04-08 13:31:33 +0200

    added request parameter and missing parameter exception

---
+++ src/Sulu/Component/Rest/Exception/MissingParameterException.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Component\Rest\Exception;
+
+use Sulu\Component\Rest\Exception\RestException;
+
+/**
+ * missing parameter in api
+ */
+class MissingParameterException extends RestException
+{
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var string
+     */
+    private $controller;
+
+    function __construct($controller, $name)
+    {
+        $this->controller = $controller;
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getController()
+    {
+        return $this->controller;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+} 
