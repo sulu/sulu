0262269d063a9711a8d0372261d462c41c1c98c7
Alexander Schranz <alexander.schranz@massiveart.com>
2014-04-10 13:42:37 +0200

    setup collection test

---
+++ src/Sulu/Bundle/MediaBundle/Tests/Functional/Controller/CollectionControllerTest.php
@@ -0,0 +1,136 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\MediaBundle\Tests\Functional\Controller;
+
+use DateTime;
+use Doctrine\ORM\Tools\SchemaTool;
+use Sulu\Bundle\MediaBundle\Entity\Collection;
+use Sulu\Bundle\MediaBundle\Entity\CollectionMeta;
+use Sulu\Bundle\MediaBundle\Entity\CollectionType;
+use Sulu\Bundle\TestBundle\Testing\DatabaseTestCase;
+
+class CollectionControllerTest extends DatabaseTestCase
+{
+    /**
+     * @var array
+     */
+    protected static $entities;
+
+    /**
+     * @var Collection
+     */
+    protected static $collection;
+
+    public function setUp()
+    {
+        $this->setUpSchema();
+        $this->setUpCollection(self::$collection);
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        self::$tool->dropSchema(self::$entities);
+    }
+
+    public function setUpSchema()
+    {
+        self::$tool = new SchemaTool(self::$em);
+
+        self::$entities = array(
+            self::$em->getClassMetadata('Sulu\Bundle\TestBundle\Entity\TestUser'),
+            self::$em->getClassMetadata('Sulu\Bundle\MediaBundle\Entity\Collection'),
+            self::$em->getClassMetadata('Sulu\Bundle\MediaBundle\Entity\CollectionType'),
+            self::$em->getClassMetadata('Sulu\Bundle\MediaBundle\Entity\CollectionMeta'),
+            self::$em->getClassMetadata('Sulu\Bundle\TagBundle\Entity\Tag')
+        );
+
+        self::$tool->dropSchema(self::$entities);
+        self::$tool->createSchema(self::$entities);
+    }
+
+    protected function setUpCollection(&$collection)
+    {
+        // Collection
+        $collection = new Collection();
+
+        $style = array(
+            'type' => 'circle',
+            'color' => '#ffcc00'
+        );
+
+        $collection->setStyle(json_encode($style));
+
+        $collection->setCreated(new DateTime());
+        $collection->setChanged(new DateTime());
+
+        // Create Collection Type
+        $collectionType = new CollectionType();
+        $collectionType->setName('Default Collection Type');
+        $collectionType->setDescription('Default Collection Type');
+
+        $collection->setCollectionType($collectionType);
+
+        // Collection Meta 1
+        $collectionMeta = new CollectionMeta();
+        $collectionMeta->setTitle('Test Collection');
+        $collectionMeta->setDescription('This Description is only for testing');
+        $collectionMeta->setLocale('en-gb');
+        $collectionMeta->setCollection($collection);
+
+        $collection->addMeta($collectionMeta);
+
+        // Collection Meta 2
+        $collectionMeta2 = new CollectionMeta();
+        $collectionMeta2->setTitle('Test Kollektion');
+        $collectionMeta2->setDescription('Dies ist eine Test Beschreibung');
+        $collectionMeta2->setLocale('de');
+        $collectionMeta2->setCollection($collection);
+
+        $collection->addMeta($collectionMeta2);
+
+
+        self::$em->persist($collection);
+        self::$em->persist($collectionType);
+        self::$em->persist($collectionMeta);
+        self::$em->persist($collectionMeta2);
+
+        self::$em->flush();
+    }
+
+    private function createTestClient() {
+        return $this->createClient(
+            array(),
+            array(
+                'PHP_AUTH_USER' => 'test',
+                'PHP_AUTH_PW' => 'test',
+            )
+        );
+    }
+
+    /*
+     * Tests
+     */
+
+    public function testGetById() {
+        $client = $this->createTestClient();
+
+        $client->request(
+            'GET',
+            '/api/collections/1'
+        );
+
+        $response = json_decode($client->getResponse()->getContent());
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+}
