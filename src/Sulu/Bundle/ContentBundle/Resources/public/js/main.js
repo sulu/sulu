cd934f22fc13117310d9b68fae4f0790683c4bf7
Michael Zangerle <michael.zangerle@massiveart.com>
2014-04-14 15:44:59 +0200

    added saving of user.settings.contentlanguage - adjustes routing

--- src/Sulu/Bundle/ContentBundle/Resources/public/js/main.js
+++ src/Sulu/Bundle/ContentBundle/Resources/public/js/main.js
@@ -21,13 +21,27 @@ define({
     name: "Sulu Content Bundle",
 
     initialize: function(app) {
+
         'use strict';
 
         var sandbox = app.sandbox;
 
         app.components.addSource('sulucontent', '/bundles/sulucontent/js/components');
 
-        // list all contacts
+        function getContentLanguage(){
+            return sandbox.sulu.getUserSetting('contentLanguage') || sandbox.sulu.user.locale;
+        }
+
+        // redirects to list with specific language
+        sandbox.mvc.routes.push({
+            route: 'content/contents/:webspace',
+            callback: function(webspace) {
+                var language = getContentLanguage();
+                sandbox.emit('sulu.router.navigate', 'content/contents/'+webspace+'/'+language);
+            }
+        });
+
+        // list all contents for a language
         sandbox.mvc.routes.push({
             route: 'content/contents/:webspace/:language',
             callback: function(webspace, language) {
