81527c00650078e76859cdcb5a9cca4353bc4d9a
Michael Zangerle <michael.zangerle@massiveart.com>
2014-04-15 13:06:22 +0200

    added files and code for pre-push-hook

--- src/Sulu/Bundle/TranslateBundle/Gruntfile.js
+++ src/Sulu/Bundle/TranslateBundle/Gruntfile.js
@@ -25,10 +25,30 @@ module.exports = function (grunt) {
                 files: [
                     {expand: true, cwd: 'Resources/public', src: ['**', '!**/scss/**'], dest: '../../../../../../web/bundles/sulutranslate/'}
                 ]
+            },
+            hooks: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            'bin/hooks/*'
+                        ],
+                        dest: '.git/hooks/'
+                    }
+                ]
             }
         },
+
+        exec: {
+            hookrights: {
+                command: 'chmod +x .git/hooks/pre-push'
+            }
+        },
+
         clean: {
             options: { force: true },
+            hooks: ['.git/hooks/*'],
             public: {
                 files: [
                     {
@@ -104,5 +124,11 @@ module.exports = function (grunt) {
 
     grunt.registerTask('default', [
         'watch'
+    ]);
+
+    grunt.registerTask('install:hooks', [
+        'clean:hooks',
+        'copy:hooks',
+        'exec:hookrights'
     ]);
 };
