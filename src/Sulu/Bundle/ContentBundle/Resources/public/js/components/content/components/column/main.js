174e8380edafec26582e98d3f0af27720d28cd78
marcelmoosbrugger <marcel.moosbrugger@massiveart.com>
2014-04-16 16:51:25 +0200

    refactoring new header

--- src/Sulu/Bundle/ContentBundle/Resources/public/js/components/content/components/column/main.js
+++ src/Sulu/Bundle/ContentBundle/Resources/public/js/components/content/components/column/main.js
@@ -11,11 +11,27 @@ define(function() {
 
     'use strict';
 
-    var SHOW_GHOST_PAGES_KEY = 'column-navigation-show-ghost-pages';
+    var SHOW_GHOST_PAGES_KEY = 'column-navigation-show-ghost-pages',
+
+    templates = {
+        toggler: [
+            '<div id="show-ghost-pages"></div>',
+            '<label class="inline spacing-left" for="show-ghost-pages"><%= label %></label>'
+        ].join('')
+    };
 
     return {
 
         view: true,
+
+        header: function() {
+            return {
+                title: this.options.webspace.replace(/_/g, '.'),
+                breadcrumb: [
+                    {title: this.options.webspace.replace(/_/g, '.')}
+                ]
+            };
+        },
 
         initialize: function() {
             this.render();
@@ -50,23 +66,28 @@ define(function() {
             this.sandbox.on('husky.toggler.show-ghost-pages.changed', function(checked) {
                 this.showGhostPages = checked;
                 this.sandbox.sulu.saveUserSetting(SHOW_GHOST_PAGES_KEY, this.showGhostPages);
-                this.restartColumnNavigation();
+                this.startColumnNavigation();
             }, this);
 
             this.sandbox.on('husky.select.language.selected.item', function(localeId) {
                 this.changeLanguage(this.getLocalizationForId(localeId));
             }, this);
+
+            // change language
+            this.sandbox.on('sulu.dropdown.languages.item-clicked', function(item) {
+                this.changeLanguage(item.localization);
+            }, this);
         },
 
-        restartColumnNavigation: function() {
-            this.sandbox.stop('#content-column');
-            this.sandbox.dom.append('#contacts-column-container', '<div id="content-column"></div>');
+        startColumnNavigation: function() {
+            this.sandbox.stop(this.$find('#content-column'));
+            this.sandbox.dom.append(this.$el, '<div id="content-column"></div>');
 
             this.sandbox.start([
                 {
                     name: 'column-navigation@husky',
                     options: {
-                        el: '#content-column',
+                        el: this.$find('#content-column'),
                         url: this.getUrl()
                     }
                 }
@@ -101,36 +122,69 @@ define(function() {
                     context = this.sandbox.util.extend({}, defaults),
                     tpl = this.sandbox.util.template(template, context);
 
-                this.html(tpl);
+                this.sandbox.dom.html(this.$el, tpl);
 
-                // datagrid && tabs
-                this.sandbox.start([
-                    {
-                        name: 'select@husky',
-                        options: {
-                            el: '#language-selector',
-                            instanceName: 'language',
-                            data: this.localizations,
-                            style: 'big',
-                            preSelectedElements: [this.options.language]
-                        }
-                    },
-                    {
-                        name: 'toggler@husky',
-                        options: {
-                            el: '#show-ghost-pages',
-                            checked: this.showGhostPages
-                        }
-                    },
-                    {
-                        name: 'column-navigation@husky',
-                        options: {
-                            el: '#content-column',
-                            url: this.getUrl()
-                        }
-                    }
-                ]);
+                this.addLocaleDropdown();
+                this.addToggler();
+
+                // start column-navigation
+                this.startColumnNavigation();
+
             }.bind(this));
+        },
+
+        /**
+         * Generates the toggler and adds it to the header
+         */
+        addToggler: function() {
+            this.sandbox.emit('sulu.header.set-bottom-content', this.sandbox.util.template(templates.toggler)({
+                label: this.sandbox.translate('content.contents.show-ghost-pages')
+            }));
+
+            this.sandbox.start([{
+                name: 'toggler@husky',
+                options: {
+                    el: '#show-ghost-pages',
+                    checked: this.showGhostPages,
+                    outline: true
+                }
+            }]);
+        },
+
+        /**
+         * Generates the locale-dropdown and adds it to the header
+         * //TODO: abstract in adminbundle
+         */
+        addLocaleDropdown: function() {
+            this.sandbox.on('husky.toolbar.header.items.set', function() {
+                this.sandbox.emit('sulu.header.toolbar.item.change', 'language', this.options.language);
+            }.bind(this));
+
+            var options = {
+                groups: [{
+                    id: 'right',
+                    align: 'right'
+                }],
+                data: [{
+                    id: 'language',
+                    group: 'right',
+                    position: 10,
+                    type: 'select',
+                    title: '',
+                    class: 'highlight-white',
+                    itemsOption: {
+                        url: '/admin/content/languages/' + this.options.webspace,
+                        titleAttribute: 'name',
+                        idAttribute: 'localization',
+                        translate: false,
+                        callback: function(item) {
+                            this.sandbox.emit('sulu.dropdown.languages.item-clicked', item);
+                        }.bind(this)
+                    }
+                }]
+            };
+
+            this.sandbox.emit('sulu.header.set-toolbar', options);
         }
     };
 });
