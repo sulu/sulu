2adef385230f88fd97ddb4dd4ae61d08d75948eb
marcelmoosbrugger <marcel.moosbrugger@massiveart.com>
2014-04-25 14:55:01 +0200

    refactoring for new dialogs

--- src/Sulu/Bundle/SuluContentBundle/Resources/public/js/components/content/main.js
+++ src/Sulu/Bundle/SuluContentBundle/Resources/public/js/components/content/main.js
@@ -206,35 +206,21 @@ define([
                 buttonSubmitText: this.sandbox.translate('content.delete.dialog.submit')
             };
 
-            // show dialog
-            this.sandbox.emit('sulu.dialog.confirmation.show', {
-                content: {
-                    title: params.title,
-                    content: params.content
-                },
-                footer: {
-                    buttonCancelText: params.buttonCancelText,
-                    buttonSubmitText: params.buttonSubmitText
-                },
-                callback: {
-                    submit: function() {
-                        this.sandbox.emit('husky.dialog.hide');
+            // show warning dialog
+            this.sandbox.emit('sulu.overlay.show-warning',
+                params.title,
+                params.content,
 
-                        // call callback function
-                        if (!!callbackFunction) {
-                            callbackFunction(true);
-                        }
-                    }.bind(this),
-                    cancel: function() {
-                        this.sandbox.emit('husky.dialog.hide');
+                function() {
+                    // cancel callback
+                    callbackFunction(false);
+                }.bind(this),
 
-                        // call callback function
-                        if (!!callbackFunction) {
-                            callbackFunction(false);
-                        }
-                    }.bind(this)
-                }
-            }, params.templateType);
+                function() {
+                    // ok callback
+                    callbackFunction(true);
+                }.bind(this)
+            );
         },
 
         changeState: function(state) {
@@ -293,11 +279,6 @@ define([
 
         delContents: function(ids) {
 
-            if (ids.length < 1) {
-                this.sandbox.emit('sulu.dialog.error.show', 'No contents selected for deletion!');
-                return;
-            }
-
             this.confirmDeleteDialog(function(wasConfirmed) {
                 if (wasConfirmed) {
                     // TODO: show loading icon
@@ -327,31 +308,21 @@ define([
                 throw 'callback is not a function';
             }
 
-            // show dialog
-            this.sandbox.emit('sulu.dialog.confirmation.show', {
-                content: {
-                    title: "Be careful!",
-                    content: "<p>The operation you are about to do will delete data.<br/>This is not undoable!</p><p>Please think about it and accept or decline.</p>"
-                },
-                footer: {
-                    buttonCancelText: "Don't do it",
-                    buttonSubmitText: "Do it, I understand"
-                },
-                callback: {
-                    submit: function() {
-                        this.sandbox.emit('husky.dialog.hide');
-                        if (!!callbackFunction) {
-                            callbackFunction(true);
-                        }
-                    }.bind(this),
-                    cancel: function() {
-                        this.sandbox.emit('husky.dialog.hide');
-                        if (!!callbackFunction) {
-                            callbackFunction(false);
-                        }
-                    }.bind(this)
-                }
-            });
+            // show warning dialog
+            this.sandbox.emit('sulu.overlay.show-warning',
+                'Be careful!',
+                'The operation you are about to do will delete data. This is not undoable! Please think about it and accept or decline.',
+
+                function() {
+                    // cancel callback
+                    callbackFunction(false);
+                }.bind(this),
+
+                function() {
+                    // ok callback
+                    callbackFunction(true);
+                }.bind(this)
+            );
         },
 
         renderList: function() {
