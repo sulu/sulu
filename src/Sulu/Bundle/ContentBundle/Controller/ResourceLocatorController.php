034b2a8503ec2c838c27dbc8f3aeff338a9afdc8
Daniel Rotter <daniel.rotter@gmail.com>
2014-04-27 18:09:25 +0200

    fixed tests and cleaned code

--- src/Sulu/Bundle/ContentBundle/Controller/ResourceLocatorController.php
+++ src/Sulu/Bundle/ContentBundle/Controller/ResourceLocatorController.php
@@ -20,7 +20,7 @@ use Symfony\Component\HttpFoundation\Response;
 /**
  * handles resource locator api
  */
-class ResourceLocatorController extends Controller implements ClassResourceInterface
+class ResourceLocatorController extends Controller
 {
     /**
      * return resource-locator for sub-node
