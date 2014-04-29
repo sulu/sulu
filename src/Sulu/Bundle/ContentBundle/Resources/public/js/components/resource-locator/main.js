293231b47f983b0d38eb00c9f7faac555e93aa1d
marcelmoosbrugger <marcel.moosbrugger@massiveart.com>
2014-04-29 09:55:42 +0200

    implemented design

--- src/Sulu/Bundle/ContentBundle/Resources/public/js/components/resource-locator/main.js
+++ src/Sulu/Bundle/ContentBundle/Resources/public/js/components/resource-locator/main.js
@@ -25,13 +25,13 @@ define([], function() {
         skeleton = function(options) {
             return [
                 '<div class="resource-locator">',
-                '<span class="icon-cogwheel pointer" id="', options.ids.edit, '"></span>',
-                (!!options.url) ? '   <span id="' + options.ids.url + '" class="url">' + options.url + '</span>' : '',
-                '   <span id="' + options.ids.tree + '" class="tree"></span>',
-                '   <input type="text" readonly="readonly" id="' + options.ids.input + '" class="form-element"/>',
-                '   <span class="icon-chevron-right pointer" id="', options.ids.toggle, '"></span>',
-                '   <div id="', options.ids.history, '" class="hidden">',
-                '   </div>',
+                '<span id="' + options.ids.url + '" class="grey-font">', (!!options.url) ? options.url : '', '/</span>',
+                '   <input type="text" id="' + options.ids.input + '" class="form-element"/>',
+                '<span class="show pointer small-font" id="', options.ids.toggle, '">',
+                '   <span class="icon-history icon"></span>',
+                '   <span>', options.showHistoryText, '</span>',
+                '</span>',
+                '<div class="loader" id="', options.ids.loader, '"></div>',
                 '</div>'
             ].join('');
         },
@@ -43,12 +43,11 @@ define([], function() {
         render = function() {
             this.options.ids = {
                 url: 'resource-locator-' + this.options.instanceName + '-url',
-                tree: 'resource-locator-' + this.options.instanceName + '-tree',
                 input: 'resource-locator-' + this.options.instanceName + '-input',
-                edit: 'resource-locator-' + this.options.instanceName + '-edit',
                 toggle: 'resource-locator-' + this.options.instanceName + '-toggle',
-                history: 'resource-locator-' + this.options.instanceName + '-history'
+                loader: 'resource-locator-' + this.options.instanceName + '-loader'
             };
+            this.options.showHistoryText = this.sandbox.translate('public.show-history');
             this.sandbox.dom.html(this.$el, skeleton(this.options));
 
             setValue.call(this);
@@ -56,10 +55,32 @@ define([], function() {
             bindDomEvents.call(this);
         },
 
+        startLoader = function() {
+            var $element = this.sandbox.dom.createElement('<div/>');
+            this.sandbox.dom.html(getId.call(this, 'loader'), $element);
+
+            this.sandbox.start([
+                {
+                    name: 'loader@husky',
+                    options: {
+                        el: $element,
+                        size: '16px',
+                        color: '#666666'
+                    }
+                }
+            ]);
+        },
+
+        stopLoader = function() {
+            this.sandbox.stop(getId.call(this, 'loader') + ' > div');
+        },
+
         bindDomEvents = function() {
-            this.sandbox.dom.on(this.$el, 'data-changed', setValue.bind(this));
+            this.sandbox.dom.on(this.$el, 'data-changed', function (e, value) {
+                setValue.call(this, value);
+            }.bind(this));
             this.sandbox.dom.on(getId.call(this, 'edit'), 'click', editClicked.bind(this));
-            this.sandbox.dom.on(getId.call(this, 'toggle'), 'click', toggleClicked.bind(this));
+            this.sandbox.dom.on(getId.call(this, 'toggle'), 'click', loadHistory.bind(this));
             this.sandbox.dom.on(getId.call(this, 'input'), 'change', setDataValue.bind(this));
             this.sandbox.dom.on(getId.call(this, 'input'), 'change', function() {
                 this.$el.trigger('change');
@@ -72,6 +93,9 @@ define([], function() {
         setValue = function(value) {
             if (!value) {
                 value = this.sandbox.dom.data(this.$el, 'value');
+                if (!value) {
+                    value = '';
+                }
             }
             var parts = value.split('/');
             this.sandbox.dom.val(getId.call(this, 'input'), parts.pop());
@@ -82,44 +106,57 @@ define([], function() {
             this.sandbox.dom.removeAttr(getId.call(this, 'input'), 'readonly');
         },
 
-        toggleClicked = function() {
-            var toggleId = getId.call(this, 'toggle');
-            if (this.historyClosed) {
-                this.sandbox.dom.removeClass(toggleId, 'icon-chevron-right');
-                this.sandbox.dom.removeClass(toggleId, 'pointer');
-                this.sandbox.dom.addClass(toggleId, 'icon-chevron-down');
-                this.sandbox.dom.addClass(toggleId, 'pointer');
-                this.historyClosed = false;
-
-                loadHistory.call(this);
-            } else {
-                this.sandbox.dom.removeClass(toggleId, 'icon-chevron-down');
-                this.sandbox.dom.removeClass(toggleId, 'pointer');
-                this.sandbox.dom.addClass(toggleId, 'icon-chevron-right');
-                this.sandbox.dom.addClass(toggleId, 'pointer');
-                this.sandbox.dom.addClass(getId.call(this, 'history'), 'hidden');
-                this.historyClosed = true;
-            }
+        setDataValue = function() {
+            var input = this.sandbox.dom.val(getId.call(this, 'input'));
+            this.sandbox.dom.data(this.$el, 'value', '/' + input);
         },
 
-        setDataValue = function() {
-            var input = this.sandbox.dom.val(getId.call(this, 'input')),
-                tree = this.sandbox.dom.html(getId.call(this, 'tree'));
-            this.sandbox.dom.data(this.$el, 'value', tree + input);
+        /**
+         * Creates the content for the history overlay
+         */
+        renderHistories = function(histories) {
+            var html = ['<ul class="resource-locator-history">'];
+
+            this.sandbox.util.foreach(histories, function(history) {
+                html.push('<li>' +
+                            '<span class="url">' + history.resourceLocator + '</span>' +
+                            '<span class="date">' + this.sandbox.date.format(history.created) + '</span>' +
+                          '</li>');
+            }.bind(this));
+            html.push('</ul>');
+
+            return html.join('');
+        },
+
+        /**
+         * Starts the history-overlay
+         */
+        startOverlay = function(content) {
+            var $element = this.sandbox.dom.createElement('<div/>');
+            this.sandbox.dom.append(this.$el, $element);
+
+            this.sandbox.start([
+                {
+                    name: 'overlay@husky',
+                    options: {
+                        el: $element,
+                        container: $element,
+                        title: this.sandbox.translate('public.url-history'),
+                        openOnStart: true,
+                        removeOnClose: true,
+                        instanceName: 'url-history',
+                        data: content
+                    }
+                }
+            ]);
         },
 
         loadHistory = function() {
+            startLoader.call(this);
             this.sandbox.util.load(this.options.historyApi).then(function(data) {
-                var items = data._embedded,
-                    html = ['<ul>'];
-
-                this.sandbox.util.foreach(items, function(item) {
-                    html.push('<li>' + item.resourceLocator + ' (' + this.sandbox.date.format(item.created) + ')</li>');
-                }.bind(this));
-                html.push('</ul>');
-
-                this.sandbox.dom.html(getId.call(this, 'history'), html.join(''));
-                this.sandbox.dom.removeClass(getId.call(this, 'history'), 'hidden');
+                stopLoader.call(this);
+                var content = renderHistories.call(this, data._embedded);
+                startOverlay.call(this, content);
             }.bind(this));
         };
 
