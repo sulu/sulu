ccf950011dab42bd360b17d20de2c5c3f08c4ce0
turbo-ele <elias.hiller@gmx.at>
2014-04-29 15:01:56 +0200

    included navigation of webspaces from portal bundle

--- src/Sulu/Bundle/SuluContentBundle/Admin/SuluContentAdmin.php
+++ src/Sulu/Bundle/SuluContentBundle/Admin/SuluContentAdmin.php
@@ -17,9 +17,33 @@ use Sulu\Bundle\AdminBundle\Navigation\NavigationItem;
 class SuluContentAdmin extends Admin
 {
 
-    public function __construct($title)
+    public function __construct(WebspaceManagerInterface $webspaceManager, $title, ContainerInterface $container)
     {
         $rootNavigationItem = new NavigationItem($title);
+
+        $section = new NavigationItem('navigation.webspaces');
+
+        $rootNavigationItem->addChild($section);
+
+        /** @var Webspace $webspace */
+        foreach ($webspaceManager->getWebspaceCollection() as $webspace) {
+            $webspaceItem = new NavigationItem($webspace->getName());
+            $webspaceItem->setIcon('globe');
+
+            $contentItem = new NavigationItem('navigation.webspaces.content');
+            $contentItem->setAction('content/contents/' . $webspace->getKey());
+            $webspaceItem->addChild($contentItem);
+
+            $indexPageItem = new NavigationItem('navigation.webspaces.index-page');
+            $indexPageItem->setId('index');
+            $indexPageItem->setAction(
+                'content/contents/' . $webspace->getKey() . '/edit:index/details'
+            );
+            $webspaceItem->addChild($indexPageItem);
+
+            $section->addChild($webspaceItem);
+        }
+
         $this->setNavigation(new Navigation($rootNavigationItem));
     }
 
