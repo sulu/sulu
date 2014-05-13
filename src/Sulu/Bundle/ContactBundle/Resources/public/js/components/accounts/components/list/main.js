2dc5e9d3b9a914db6651526f4ce0b58f57204f77
marcelmoosbrugger <marcel.moosbrugger@massiveart.com>
2014-05-13 15:48:19 +0200

    refactoring for datagrid

--- src/Sulu/Bundle/ContactBundle/Resources/public/js/components/accounts/components/list/main.js
+++ src/Sulu/Bundle/ContactBundle/Resources/public/js/components/accounts/components/list/main.js
@@ -233,10 +233,8 @@ define([
                 {
                     el: this.sandbox.dom.find('#companies-list', this.$el),
                     url: '/admin/api/accounts?flat=true' + dataUrlAddition,
-                    sortable: true,
-                    fullWidth: true,
-                    selectItem: {
-                        type: 'checkbox'
+                    viewOptions: {
+                        fullWidth: true
                     }
                 });
         }
