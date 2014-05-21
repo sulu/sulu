01dba77c7775c7f45121985c26cb35c521a44da5
marcelmoosbrugger <marcel.moosbrugger@massiveart.com>
2014-05-21 16:51:47 +0200

    added file types for media datagrid

--- src/Sulu/Component/Rest/RestController.php
+++ src/Sulu/Component/Rest/RestController.php
@@ -55,7 +55,9 @@ abstract class RestController extends FOSRestController
     protected $fieldTypes = array(
         'created' => 'date',
         'changed' => 'date',
-        'birthday' => 'date'
+        'birthday' => 'date',
+        'title' => 'title',
+        'size' => 'bytes'
     );
 
     /**
