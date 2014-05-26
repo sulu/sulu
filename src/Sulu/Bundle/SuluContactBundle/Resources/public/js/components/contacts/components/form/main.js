34593d4df33a301079a35a5b0af272f2afd2f953
Michael Zangerle <michael.zangerle@massiveart.com>
2014-05-26 15:58:41 +0200

    worked on contact form

--- src/Sulu/Bundle/SuluContactBundle/Resources/public/js/components/contacts/components/form/main.js
+++ src/Sulu/Bundle/SuluContactBundle/Resources/public/js/components/contacts/components/form/main.js
@@ -104,11 +104,14 @@ define([], function() {
             },
 
             setFormData: function(data) {
+
                 // add collection filters to form
                 this.sandbox.emit('sulu.contact-form.add-collectionfilters', form);
                 this.sandbox.form.setData(form, data).then(function() {
                     this.sandbox.start(form);
                     this.sandbox.emit('sulu.contact-form.add-required',['email']);
+                }.bind(this)).fail(function(error){
+                    this.sandbox.logger.error("An error occured when setting data!", error);
                 }.bind(this));
             },
 
