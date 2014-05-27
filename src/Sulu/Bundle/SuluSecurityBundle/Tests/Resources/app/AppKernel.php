0440ae1119d5afad884c12f823a3d9ff68453c0f
Daniel Rotter <daniel.rotter@massiveart.com>
2014-05-27 16:51:18 +0200

    fixed tests

--- src/Sulu/Bundle/SuluSecurityBundle/Tests/Resources/app/AppKernel.php
+++ src/Sulu/Bundle/SuluSecurityBundle/Tests/Resources/app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new \Sulu\Bundle\SecurityBundle\SuluSecurityBundle(),
             new \Sulu\Bundle\ContactBundle\SuluContactBundle(),
             new \Sulu\Bundle\TestBundle\SuluTestBundle(),
+            new \Sulu\Bundle\TagBundle\SuluTagBundle(),
 
             new \Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle()
         );
