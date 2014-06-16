7c71986f26dc4cff60389608ab52b9fd0a227d44
Johannes Wachter <johannes.wachter@massiveart.com>
2014-06-16 10:25:53 +0200

    implemented render medias

--- src/Sulu/Bundle/SuluMediaBundle/Resources/public/js/components/media-selection/main.js
+++ src/Sulu/Bundle/SuluMediaBundle/Resources/public/js/components/media-selection/main.js
@@ -18,14 +18,24 @@ define(['sulumedia/collection/collections'], function(Collections) {
     'use strict';
 
     var defaults = {
+            visibleItems: 999,
             instanceName: null,
-            url: null,
-            preselected: [1],
+            url: '',
+            idsParameter: 'ids',
+            preselected: {ids: [1], displayOption: 'top', config: {}},
+            idKey: 'id',
+            titleKey: 'title',
+            thumbnailKey: 'thumbnails',
+            urlKey: 'url',
+            thumbnailSize: '50x50',
+            resultKey: '_embedded',
             translations: {
                 noMediaSelected: 'media-selection.nomedia-selected',
                 addImages: 'media-selection.add-images',
                 choose: 'media-selection.choose',
-                collections: 'media-selection.collections'
+                collections: 'media-selection.collections',
+                visible: 'media-selection.visible',
+                of: 'media-selection.of'
             }
         },
 
@@ -43,7 +53,33 @@ define(['sulumedia/collection/collections'], function(Collections) {
             return createEventName.call(this, 'input-retrieved');
         },
 
-        /** returns normalized event names */
+        /**
+         * raised when the overlay data has been changed
+         * @event sulu.media-selection.data-changed
+         */
+        DATA_CHANGED = function() {
+            return createEventName.call(this, 'data-changed');
+        },
+
+        /**
+         * raised before data is requested with AJAX
+         * @event sulu.media-selection.data-request
+         */
+        DATA_REQUEST = function() {
+            return createEventName.call(this, 'data-request');
+        },
+
+        /**
+         * raised when data has returned from the ajax request
+         * @event sulu.media-selection.data-retrieved
+         */
+        DATA_RETRIEVED = function() {
+            return createEventName.call(this, 'data-retrieved');
+        },
+
+        /**
+         * returns normalized event names
+         */
         createEventName = function(postFix) {
             return eventNamespace + (this.options.instanceName ? this.options.instanceName + '.' : '') + postFix;
         },
@@ -51,13 +87,19 @@ define(['sulumedia/collection/collections'], function(Collections) {
         templates = {
             skeleton: function(options) {
                 return [
-                    '<div class="media-selection-container form-element">',
+                    '<div class="media-selection-container form-element" id="', options.ids.container, '">',
                     '   <div class="media-selection-header">',
                     '       <a href="#" class="fa-plus-circle add" id="', options.ids.addButton, '"></a>',
-                    '       <div class="position-outer">',
-                    '           <select class="position" id="', options.ids.position, '">',
-                    '               <option value="0">left</option>',
-                    '               <option value="1">right</option>',
+                    '       <div class="display-option-outer">',
+                    '           <select class="display-option" id="', options.ids.displayOption, '">',
+                    '               <option value="left">left</option>',
+                    '               <option value="leftTop">left top</option>',
+                    '               <option value="top">top</option>',
+                    '               <option value="rightTop">right top</option>',
+                    '               <option value="right">right</option>',
+                    '               <option value="rightBottom">right bottom</option>',
+                    '               <option value="bottom">bottom</option>',
+                    '               <option value="leftBottom">left bottom</option>',
                     '           </select>',
                     '       </div>',
                     '       <a href="#" class="fa-cog config" id="', options.ids.configButton, '"></a>',
@@ -83,6 +125,15 @@ define(['sulumedia/collection/collections'], function(Collections) {
                     '   <div id="', options.ids.gridGroup, '"/>',
                     '</div>'
                 ].join('');
+            },
+            contentItem: function(id, num, value, imageUrl) {
+                return [
+                    '<li data-id="', id, '">',
+                    '   <span class="num">', num, '</span>',
+                    '   <img src="', imageUrl, '" class="image"/>',
+                    '   <span class="value">', value, '</span>',
+                    '</li>'
+                ].join('')
             }
         },
 
@@ -95,9 +146,10 @@ define(['sulumedia/collection/collections'], function(Collections) {
             this.collections = new Collections();
 
             this.options.ids = {
+                container: 'media-selection-' + this.options.instanceName + '-container',
                 addButton: 'media-selection-' + this.options.instanceName + '-add',
                 configButton: 'media-selection-' + this.options.instanceName + '-config',
-                position: 'media-selection-' + this.options.instanceName + '-position',
+                displayOption: 'media-selection-' + this.options.instanceName + '-display-option',
                 content: 'media-selection-' + this.options.instanceName + '-content',
                 chooseTab: 'media-selection-' + this.options.instanceName + '-choose-tab',
                 gridGroup: 'media-selection-' + this.options.instanceName + '-grid-group'
@@ -105,9 +157,18 @@ define(['sulumedia/collection/collections'], function(Collections) {
             this.sandbox.dom.html(this.$el, templates.skeleton(this.options));
 
             // init container
+            this.$container = this.sandbox.dom.find(getId.call(this, 'container'), this.$el);
             this.$content = this.sandbox.dom.find(getId.call(this, 'content'), this.$el);
             this.$addButton = this.sandbox.dom.find(getId.call(this, 'addButton'), this.$el);
             this.$configButton = this.sandbox.dom.find(getId.call(this, 'configButton'), this.$el);
+            // TODO footer this.$footer
+
+            // set preselected values
+            if (!!this.sandbox.dom.data(this.$el, 'media-selection')) {
+                setData.call(this, this.sandbox.dom.data(this.$el, 'media-selection'));
+            } else {
+                setData.call(this, this.options.preselected);
+            }
 
             // render no images selected
             renderStartContent.call(this);
@@ -115,12 +176,22 @@ define(['sulumedia/collection/collections'], function(Collections) {
             // sandbox event handling
             bindCustomEvents.call(this);
 
+            // init vars
+            this.itemsVisible = this.options.visibleItems;
+            this.URI = {
+                str: '',
+                hasChanged: false
+            };
+
+            // generate URI for data
+            setURI.call(this);
+
             // init overlays
             // TODO config overlay
             startAddOverlay.call(this);
 
-            // dom event handling
-            bindDomEvents.call(this);
+            // load preselected items
+            loadContent.call(this);
         },
 
         /**
@@ -130,9 +201,6 @@ define(['sulumedia/collection/collections'], function(Collections) {
         renderStartContent = function() {
             var noMedia = this.sandbox.translate(this.options.translations.noMediaSelected);
             this.sandbox.dom.html(this.$content, templates.noContent(noMedia));
-        },
-
-        bindDomEvents = function() {
         },
 
         bindCustomEvents = function() {
@@ -147,7 +215,7 @@ define(['sulumedia/collection/collections'], function(Collections) {
                                     el: this.sandbox.dom.find(getId.call(this, 'gridGroup')),
                                     instanceName: 'collections',
                                     gridUrl: '/admin/api/media?collection=',
-                                    preselected: this.options.preselected,
+                                    preselected: this.data.ids,
                                     dataGridOptions: {
                                         view: 'table',
                                         viewOptions: {table: {excludeFields: ['id']}},
@@ -181,9 +249,60 @@ define(['sulumedia/collection/collections'], function(Collections) {
 
             // data from overlay retrieved
             this.sandbox.on(INPUT_RETRIEVED.call(this), function() {
-                // TODO if data changed load
-                this.sandbox.logger.log(this.ids);
+                setURI.call(this);
+                loadContent.call(this);
             }.bind(this));
+
+            // data from ajax request retrieved
+            this.sandbox.on(DATA_RETRIEVED.call(this), function() {
+                renderContent.call(this);
+            }.bind(this));
+        },
+
+        /**
+         * renders the content decides whether the footer is rendered or not
+         */
+        renderContent = function() {
+            if (this.items.length !== 0) {
+                var ul = this.sandbox.dom.createElement('<ul class="items-list"/>'),
+                    i = -1,
+                    length = this.items.length,
+                    url;
+
+                //loop stops if no more items are left or if number of rendered items matches itemsVisible
+                for (; ++i < length && i < this.itemsVisible;) {
+                    url = this.items[i][this.options.thumbnailKey][this.options.thumbnailSize][this.options.urlKey];
+                    this.sandbox.dom.append(ul, templates.contentItem(this.items[i][this.options.idKey], i + 1, this.items[i][this.options.titleKey], url));
+                }
+
+                this.sandbox.dom.html(this.$content, ul);
+                renderFooter.call(this);
+            } else {
+                renderStartContent.call(this);
+                detachFooter.call(this);
+            }
+        },
+
+        /**
+         * renders the footer and calls a method to bind the events for itself
+         */
+        renderFooter = function() {
+            this.itemsVisible = (this.items.length < this.itemsVisible) ? this.items.length : this.itemsVisible;
+
+            if (this.$footer === null || this.$footer === undefined) {
+                this.$footer = this.sandbox.dom.createElement('<div class="media-selection-footer"/>');
+            }
+
+            this.sandbox.dom.html(this.$footer, [
+                '<span>',
+                    '<strong>' + this.itemsVisible + ' </strong>', this.sandbox.translate(this.options.translations.of) , ' ',
+                    '<strong>' + this.items.length + ' </strong>', this.sandbox.translate(this.options.translations.visible),
+                '</span>'
+            ].join(''));
+
+            // TODO appendViewToggler.call(this);
+            this.sandbox.dom.append(this.$container, this.$footer);
+            // TODO bindFooterEvents.call(this);
         },
 
         /**
@@ -227,13 +346,95 @@ define(['sulumedia/collection/collections'], function(Collections) {
             var idsDef = this.sandbox.data.deferred();
 
             this.sandbox.emit('sulu.grid-group.collections.get-selected-ids', function(ids) {
-                this.ids = ids;
+                setData.call(this, {ids: ids});
                 idsDef.resolve();
             }.bind(this));
 
             idsDef.then(function() {
                 this.sandbox.emit(INPUT_RETRIEVED.call(this));
             }.bind(this));
+        },
+
+        /**
+         * starts the loader component
+         */
+        startLoader = function() {
+            detachFooter.call(this);
+
+            var $loaderContainer = this.sandbox.dom.createElement('<div class="loader"/>');
+            this.sandbox.dom.html(this.$content, $loaderContainer);
+
+            this.sandbox.start([
+                {
+                    name: 'loader@husky',
+                    options: {
+                        el: $loaderContainer,
+                        size: '100px',
+                        color: '#e4e4e4'
+                    }
+                }
+            ]);
+        },
+
+        /**
+         * removes the footer
+         */
+        detachFooter = function() {
+            if (this.$footer !== null) {
+                this.sandbox.dom.remove(this.$footer);
+            }
+        },
+
+        loadContent = function() {
+            //only request if URI has changed
+            if (this.URI.hasChanged === true) {
+                this.sandbox.emit(DATA_REQUEST.call(this));
+                startLoader.call(this);
+
+                // reset item visible
+                this.itemsVisible = this.options.visibleItems;
+
+                this.sandbox.util.load(this.URI.str)
+                    .then(function(data) {
+                        this.items = data[this.options.resultKey];
+
+                        this.sandbox.emit(DATA_RETRIEVED.call(this));
+                    }.bind(this))
+                    .then(function(error) {
+                        this.sandbox.logger.log(error);
+                    }.bind(this));
+            }
+        },
+
+        /**
+         * set data of media-selection
+         */
+        setData = function(data) {
+            data = this.sandbox.util.extend(true, {}, this.data, data);
+            this.sandbox.dom.data(this.$el, 'media-selection', data);
+            this.data = data;
+        },
+
+        /**
+         * generates the URI for the request
+         */
+        setURI = function() {
+            var delimiter = (this.options.url.indexOf('?') === -1) ? '?' : '&',
+                newURI = [
+                    this.options.url,
+                    delimiter, this.options.idsParameter, '=', (this.data.ids || []).join(',')
+                ].join('');
+            // min source must be selected
+            if (this.data.ids.length > 0 && newURI !== this.URI.str) {
+                //emit data changed event only if old URI is not null (not at the startup)
+                if (this.URI.str !== '') {
+                    this.sandbox.emit(DATA_CHANGED.call(this), this.sandbox.dom.data(this.$el, 'media-selection'), this.$el);
+                }
+                this.URI.str = newURI;
+                this.URI.hasChanged = true;
+            } else {
+                this.URI.hasChanged = false;
+            }
         };
 
     return {
