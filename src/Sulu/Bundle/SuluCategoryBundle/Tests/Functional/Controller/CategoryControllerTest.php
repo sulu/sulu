5977e1d02ad3d6e6709437cbb13407db88ee419b
marcelmoosbrugger <marcel.moosbrugger@massiveart.com>
2014-06-17 08:38:31 +0200

    changed travis and phpunit configuration

--- src/Sulu/Bundle/SuluCategoryBundle/Tests/Functional/Controller/CategoryControllerTest.php
+++ src/Sulu/Bundle/SuluCategoryBundle/Tests/Functional/Controller/CategoryControllerTest.php
@@ -12,8 +12,8 @@ namespace Sulu\Bundle\TagBundle\Tests\Functional\Controller;
 
 use Doctrine\ORM\Tools\SchemaTool;
 use Sulu\Bundle\CategoryBundle\Entity\Category;
-use Sulu\Bundle\CategoryBundle\Entity\Meta;
-use Sulu\Bundle\CategoryBundle\Entity\Name;
+use Sulu\Bundle\CategoryBundle\Entity\CategoryMeta;
+use Sulu\Bundle\CategoryBundle\Entity\CategoryName;
 use Sulu\Bundle\TestBundle\Testing\DatabaseTestCase;
 
 class CategoryControllerTest extends DatabaseTestCase
@@ -27,16 +27,58 @@ class CategoryControllerTest extends DatabaseTestCase
     {
         $this->setUpSchema();
 
+        /* First Category
+        -------------------------------------*/
         $category = new Category();
-        $category->setName('Category 1');
         $category->setCreated(new \DateTime());
         $category->setChanged(new \DateTime());
+
+        // name for first category
+        $categoryName = new CategoryName();
+        $categoryName->setLocale('en');
+        $categoryName->setName('First Category');
+        $categoryName->setCategory($category);
+        $category->addName($categoryName);
+
+        // meta for first category
+        $categoryMeta = new CategoryMeta();
+        $categoryMeta->setLocale('en');
+        $categoryMeta->setKey('description');
+        $categoryMeta->setValue('Description of Category');
+        $categoryMeta->setCategory($category);
+        $category->addMeta($categoryMeta);
+
         self::$em->persist($category);
 
-        $category = new Category();
-        $category->setCreated(new \DateTime());
-        $category->setChanged(new \DateTime());
-        self::$em->persist($category);
+        /* Second Category
+        -------------------------------------*/
+        $category2 = new Category();
+        $category2->setCreated(new \DateTime());
+        $category2->setChanged(new \DateTime());
+
+        // name for second category
+        $categoryName2 = new CategoryName();
+        $categoryName2->setLocale('de');
+        $categoryName2->setName('Second Category');
+        $categoryName2->setCategory($category);
+        $category2->addName($categoryName2);
+
+        // meta for second category
+        $categoryMeta2 = new CategoryMeta();
+        $categoryMeta2->setLocale('de');
+        $categoryMeta2->setKey('description');
+        $categoryMeta2->setValue('Description of second Category');
+        $categoryMeta2->setCategory($category2);
+        $category2->addMeta($categoryMeta2);
+
+        // meta without locale for second category
+        $categoryMeta3 = new CategoryMeta();
+        $categoryMeta3->setKey('noLocaleKey');
+        $categoryMeta3->setValue('noLocaleValue');
+        $categoryMeta3->setCategory($category2);
+        $category2->addMeta($categoryMeta3);
+
+        self::$em->persist($category2);
 
         self::$em->flush();
     }
@@ -47,8 +89,8 @@ class CategoryControllerTest extends DatabaseTestCase
 
         self::$entities = array(
             self::$em->getClassMetadata('Sulu\Bundle\CategoryBundle\Entity\Category'),
-            self::$em->getClassMetadata('Sulu\Bundle\CategoryBundle\Entity\Meta'),
-            self::$em->getClassMetadata('Sulu\Bundle\CategoryBundle\Entity\Name'),
+            self::$em->getClassMetadata('Sulu\Bundle\CategoryBundle\Entity\CategoryMeta'),
+            self::$em->getClassMetadata('Sulu\Bundle\CategoryBundle\Entity\CategoryName'),
             self::$em->getClassMetadata('Sulu\Bundle\TestBundle\Entity\TestUser')
         );
 
