7bf617cb122855086ae6a098fea90293e0723de2
Johannes Wachter <johannes.wachter@massiveart.com>
2014-06-23 14:07:18 +0200

    implemented seo structure extension

--- src/Sulu/Bundle/ContentBundle/Content/Structure/SeoStructureExtension.php
+++ src/Sulu/Bundle/ContentBundle/Content/Structure/SeoStructureExtension.php
@@ -20,21 +20,58 @@ use Sulu\Component\Content\StructureExtension\StructureExtension;
 class SeoStructureExtension extends StructureExtension
 {
     /**
-     * {@inheritdoc}
+     * name of structure extension
      */
-    protected $properties = array();
+    const SEO_EXTENSION_NAME = 'seo';
 
     /**
      * {@inheritdoc}
      */
-    protected $name = 'seo';
+    protected $properties = array(
+        'title',
+        'description',
+        'keywords',
+        'canonicalUrl',
+        'noIndex',
+        'noFollow'
+    );
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $name = self::SEO_EXTENSION_NAME;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $additionalPrefix = 'seo';
 
     /**
      * {@inheritdoc}
      */
     public function save(NodeInterface $node, $data, $webspaceKey, $languageCode)
     {
-        // TODO: Implement save() method.
+        // save values
+        $this->saveProperty($node, $data, 'title');
+        $this->saveProperty($node, $data, 'description');
+        $this->saveProperty($node, $data, 'keywords');
+        $this->saveProperty($node, $data, 'canonicalUrl');
+        $this->saveProperty($node, $data, 'noIndex', false);
+        $this->saveProperty($node, $data, 'noFollow', false);
+    }
+
+    /**
+     * save a single property value
+     * @param NodeInterface $node
+     * @param array $data data array
+     * @param string $name name of property in node an data array
+     * @param string $default value if no data exists with given name
+     * @param string $default
+     */
+    private function saveProperty(NodeInterface $node, $data, $name, $default = '')
+    {
+        $value = isset($data[$name]) ? $data[$name] : $default;
+        $node->setProperty($this->getPropertyName($name), $value);
     }
 
     /**
@@ -42,6 +79,25 @@ class SeoStructureExtension extends StructureExtension
      */
     public function load(NodeInterface $node, $webspaceKey, $languageCode)
     {
-        // TODO: Implement load() method.
+        return array(
+            'title' => $this->loadProperty($node, 'title'),
+            'description' => $this->loadProperty($node, 'description'),
+            'keywords' => $this->loadProperty($node, 'keywords'),
+            'canonicalUrl' => $this->loadProperty($node, 'canonicalUrl'),
+            'noIndex' => $this->loadProperty($node, 'noIndex', false),
+            'noFollow' => $this->loadProperty($node, 'noFollow', false)
+        );
+    }
+
+    /**
+     * load a single property value
+     * @param NodeInterface $node
+     * @param string $name name of property in node
+     * @param string $default value if no property exists with given name
+     * @return mixed
+     */
+    private function loadProperty(NodeInterface $node, $name, $default = '')
+    {
+        return $node->getPropertyValueWithDefault($this->getPropertyName($name), $default);
     }
 }
