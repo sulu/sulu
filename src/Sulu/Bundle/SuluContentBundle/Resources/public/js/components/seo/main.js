bda732aae675b2c070bc07d26c39aa9e0e6f8dc5
Johannes Wachter <johannes.wachter@massiveart.com>
2014-06-24 09:57:33 +0200

    seo as sub component of content

--- src/Sulu/Bundle/SuluContentBundle/Resources/public/js/components/seo/main.js
+++ src/Sulu/Bundle/SuluContentBundle/Resources/public/js/components/seo/main.js
@@ -7,7 +7,9 @@
  * with this source code in the file LICENSE.
  */
 
-define([], function() {
+define([
+    'sulucontent/model/seo'
+], function(Seo) {
 
     'use strict';
 
@@ -18,6 +20,39 @@ define([], function() {
             this.sandbox.emit('sulu.app.ui.reset', { navigation: 'small', content: 'auto'});
 
             this.html(this.renderTemplate('/admin/content/template/content/seo'));
+
+            this.loadData();
+        },
+
+        loadData: function() {
+            this.model = new Seo({webspaceKey: this.options.webspace, languageCode: this.options.language});
+            this.model.set({id: this.options.id});
+            this.model.fetch({
+                success: function(model) {
+                    this.data = model.toJSON();
+
+                    this.setTitle()
+                }.bind(this)
+            });
+        },
+
+        /**
+         * hack to simulate form component
+         * copied from form component
+         */
+
+        /**
+         * Sets the title of the page and if in edit mode calls a method to set the breadcrumb
+         */
+        setTitle: function() {
+            var value = this.data['sulu.node.name'];
+
+            if (!!this.options.id && value !== '') {
+                this.sandbox.emit('sulu.header.set-title', value);
+                this.setBreadcrumb();
+            } else {
+                this.sandbox.emit('sulu.header.set-title', this.sandbox.translate('content.contents.title'));
+            }
         }
     };
 });
