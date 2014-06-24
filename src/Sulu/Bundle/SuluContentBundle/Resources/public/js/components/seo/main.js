0514bec9697ba9a292638ce94290893b72fa508d
Johannes Wachter <johannes.wachter@massiveart.com>
2014-06-24 08:38:40 +0200

    added seo tab and fixed bug

---
+++ src/Sulu/Bundle/SuluContentBundle/Resources/public/js/components/seo/main.js
@@ -0,0 +1,21 @@
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+define([], function() {
+
+    'use strict';
+
+    return {
+        initialize: function() {
+            this.sandbox.emit('sulu.app.ui.reset', { navigation: 'small', content: 'auto'});
+
+            this.$el.html('HALLO');
+        }
+    };
+});
