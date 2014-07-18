bfa8e1b38e3ae0dc75c04ae38acc613e5a6bcce1
dantleech <dan.t.leech@gmail.com>
2014-07-18 08:26:34 +0200

    Everything works again

---
+++ src/Sulu/Bundle/LocationBundle/Resources/public/js/validation/types/location.js
@@ -0,0 +1,48 @@
+/*
+ * This file is part of the Husky Validation.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ */
+
+define([
+    'type/default'
+], function(Default) {
+
+    'use strict';
+
+    var dataChangedHandler = function(data, $el) {
+        App.emit('sulu.preview.update', $el, data);
+        App.emit('sulu.content.changed');
+    };
+
+    return function($el, options) {
+        var defaults = {},
+
+            subType = {
+                initializeSub: function() {
+                },
+
+                setValue: function(value) {
+                    App.dom.data($el, 'location', value)
+                },
+
+                getValue: function() {
+                    return App.dom.data($el, 'location')
+                },
+
+                needsValidation: function() {
+                    return false;
+                },
+
+                validate: function() {
+                    return true;
+                }
+            };
+
+        return new Default($el, defaults, options, 'location', subType);
+    };
+});
