4f9c90265e1c9c613a6c3fcdb7976440e4e900f0
marcelmoosbrugger <marcel.moosbrugger@massiveart.com>
2014-07-18 15:08:28 +0200

    refactored widgets

---
+++ src/Sulu/Bundle/SuluAdminBundle/Controller/WidgetGroupsController.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\AdminBundle\Controller;
+
+use Sulu\Bundle\AdminBundle\Widgets\WidgetsHandlerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * renderes the Widgets
+ * @package Sulu\Bundle\AdminBundle\Controller
+ */
+class WidgetGroupsController extends Controller
+{
+    protected $widgetsHandler;
+
+    public function contactInfoAction(Request $request) {
+        $aliases = array(
+            'myWidget1',
+            'myWidget2'
+        );
+        return new Response($this->getWidgetsHandler()->render($aliases, $request->query->all()));
+    }
+
+    /**
+     * Returns the widget handler service
+     * @return WidgetsHandlerInterface
+     */
+    private function getWidgetsHandler() {
+        if ($this->widgetsHandler === null) {
+            $this->widgetsHandler = $this->get('sulu_admin.widgets_handler');
+        }
+        return $this->widgetsHandler;
+    }
+}
