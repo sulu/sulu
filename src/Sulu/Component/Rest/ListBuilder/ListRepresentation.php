9dc4526c66658c9192011c5397855cf81cead61b
Daniel Rotter <daniel.rotter@massiveart.com>
2014-07-18 13:49:57 +0200

    added missing parameters to all-link in ListResponse

--- src/Sulu/Component/Rest/ListBuilder/ListRepresentation.php
+++ src/Sulu/Component/Rest/ListBuilder/ListRepresentation.php
@@ -26,7 +26,7 @@ use Hateoas\Configuration\Annotation\Route;
  *      "all",
  *      href = @Route(
  *          "expr(object.getRoute())",
- *          parameters = "expr({ flat: 'true'})",
+ *          parameters = "expr({ flat: 'true', id:  object.getParameter('id'), fields: object.getParameter('fields'), search: object.getParameter('search'), searchFields: object.getParameter('searchFields') })",
  *          absolute = "expr(object.isAbsolute())",
  *      )
  * )
@@ -94,5 +94,16 @@ class ListRepresentation extends PaginatedRepresentation
         );
 
         $this->total = $total;
+    }
+
+    /**
+     * Returns the parameter with the given key
+     * @param string $key The key of the parameter
+     * @return mixed
+     */
+    public function getParameter($key) {
+        $parameters = $this->getParameters();
+
+        return array_key_exists($key, $parameters) ? $parameters[$key] : '';
     }
 }
