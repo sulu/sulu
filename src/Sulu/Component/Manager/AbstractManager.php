2d015e3a5fa98e92bd45253e62b2d78d3f6b8cdf
turbo-ele <elias.hiller@gmx.at>
2014-07-24 09:27:21 +0200

    added abstract manager

---
+++ src/Sulu/Component/Manager/AbstractManager.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Component\Manager;
+
+
+/**
+ * Abstract Controller for basic Manager functionality
+ * @package Sulu\Component\Manager
+ */
+abstract class AbstractManager
+{
+    /**
+     * Returns the entry from the data with the given key, or the given default value, if the key does not exist
+     * @param array $data
+     * @param string $key
+     * @param string $default
+     * @return mixed
+     */
+    protected function getProperty(array $data, $key, $default = null)
+    {
+        return array_key_exists($key, $data) ? $data[$key] : $default;
+    }
+}
