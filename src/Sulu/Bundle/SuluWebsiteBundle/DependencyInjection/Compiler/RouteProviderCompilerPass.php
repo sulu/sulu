a19ac71411da93c87d25a60c8ddd473bf38a44ea
dantleech <dan.t.leech@gmail.com>
2014-07-25 14:41:27 +0200

    Broken the set theme logic from contnt portal provider
    - Set theme logic is now handled generically in a request
      event listener.
    - And renamed portal provider to content provider

--- src/Sulu/Bundle/SuluWebsiteBundle/DependencyInjection/Compiler/RouteProviderCompilerPass.php
+++ src/Sulu/Bundle/SuluWebsiteBundle/DependencyInjection/Compiler/RouteProviderCompilerPass.php
@@ -28,12 +28,26 @@ class RouteProviderCompilerPass implements CompilerPassInterface
     {
         if ($container->hasDefinition('sulu_core.webspace.request_analyzer')) {
             $container->setDefinition(
-                'sulu_website.provider.portal',
-                new Definition('Sulu\Bundle\WebsiteBundle\Routing\PortalRouteProvider', array(
+                'sulu_website.provider.content',
+                new Definition('Sulu\Bundle\WebsiteBundle\Routing\ContentRouteProvider', array(
                     new Reference('sulu.content.mapper'),
                     new Reference('sulu_core.webspace.request_analyzer'),
-                    new Reference('liip_theme.active_theme')
                 ))
+            );
+
+            $setThemeListenerDefinition = new Definition('Sulu\Bundle\WebsiteBundle\EventListener\SetThemeEventListener', array(
+                new Reference('sulu_core.webspace.request_analyzer'),
+                new Reference('liip_theme.active_theme'),
+            ));
+
+            $setThemeListenerDefinition->addTag('kernel.event_listener', array(
+                'event' => 'kernel.request',
+                'method'=> 'onKernelRequest',
+            ));
+
+            $container->setDefinition(
+                'sulu_website.event_listener.set_theme',
+                $setThemeListenerDefinition
             );
         }
     }
