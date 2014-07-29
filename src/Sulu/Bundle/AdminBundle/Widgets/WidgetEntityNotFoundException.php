0f39242056561b4b9d2df5e61c1cb2f8a979fcbe
Michael Zangerle <michael.zangerle@massiveart.com>
2014-07-29 16:58:15 +0200

    adjusted exception params

--- src/Sulu/Bundle/AdminBundle/Widgets/WidgetEntityNotFoundException.php
+++ src/Sulu/Bundle/AdminBundle/Widgets/WidgetEntityNotFoundException.php
@@ -14,5 +14,21 @@ use Sulu\Bundle\AdminBundle\Widgets\WidgetException;
 
 class WidgetEntityNotFoundException extends WidgetException
 {
+    protected $id;
 
+    function __construct($message, $widget, $id)
+    {
+        parent::__construct($message, $widget);
+        $this->id = $id;
+    }
+
+    public function toArray()
+    {
+        return array(
+            'code' => $this->code,
+            'message' => $this->message,
+            'widget' => $this->widget,
+            'id' => $this->id
+        );
+    }
 }
