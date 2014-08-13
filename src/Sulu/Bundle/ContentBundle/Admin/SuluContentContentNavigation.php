80a1f05d030fae4e352dc41e19bf9b97d0d0669a
Daniel Rotter <daniel.rotter@massiveart.com>
2014-08-13 14:14:03 +0200

    updated ContentNavigation

--- src/Sulu/Bundle/ContentBundle/Admin/SuluContentContentNavigation.php
+++ src/Sulu/Bundle/ContentBundle/Admin/SuluContentContentNavigation.php
@@ -10,13 +10,12 @@
 
 namespace Sulu\Bundle\ContentBundle\Admin;
 
-use Sulu\Bundle\AdminBundle\Admin\ContentNavigation;
-use Sulu\Bundle\AdminBundle\Navigation\NavigationItem;
+use Sulu\Bundle\AdminBundle\Navigation\ContentNavigation;
+use Sulu\Bundle\AdminBundle\Navigation\ContentNavigationItem;
 use Sulu\Component\Content\Structure;
 
 class SuluContentContentNavigation extends ContentNavigation
 {
-
     public function __construct()
     {
         parent::__construct();
@@ -27,69 +26,69 @@ class SuluContentContentNavigation extends ContentNavigation
 
     private function getSeo()
     {
-        $seo = new NavigationItem('content-navigation.contents.seo');
+        $seo = new ContentNavigationItem('content-navigation.contents.seo');
         $seo->setId('tab-seo');
         $seo->setAction('seo');
-        $seo->setContentType('content');
-        $seo->setContentComponent('content/seo@sulucontent');
-        $seo->setContentDisplay(array('edit'));
+        $seo->setGroups(array('content'));
+        $seo->setComponent('content/seo@sulucontent');
+        $seo->setDisplay(array('edit'));
 
         return $seo;
     }
 
     private function getExcerpt()
     {
-        $excerpt = new NavigationItem('content-navigation.contents.excerpt');
+        $excerpt = new ContentNavigationItem('content-navigation.contents.excerpt');
         $excerpt->setId('tab-excerpt');
         $excerpt->setAction('excerpt');
-        $excerpt->setContentType('content');
-        $excerpt->setContentComponent('content/excerpt@sulucontent');
-        $excerpt->setContentDisplay(array('edit'));
+        $excerpt->setGroups(array('content'));
+        $excerpt->setComponent('content/excerpt@sulucontent');
+        $excerpt->setDisplay(array('edit'));
 
         return $excerpt;
     }
 
     private function getSettings()
     {
-        $settings = new NavigationItem('content-navigation.contents.settings');
+        $settings = new ContentNavigationItem('content-navigation.contents.settings');
         $settings->setId('tab-settings');
         $settings->setAction('settings');
-        $settings->setContentType('content');
-        $settings->setContentComponent('content/settings@sulucontent');
-        $settings->setContentDisplay(array('edit'));
+        $settings->setGroups(array('content'));
+        $settings->setComponent('content/settings@sulucontent');
+        $settings->setDisplay(array('edit'));
 
         return $settings;
     }
 
     private function getContent()
     {
-        $content = new NavigationItem('content-navigation.contents.content');
+        $content = new ContentNavigationItem('content-navigation.contents.content');
         $content->setId('tab-content');
         $content->setAction('content');
-        $content->setContentType('content');
-        $content->setContentComponent('content/form@sulucontent');
+        $content->setGroups(array('content'));
+        $content->setComponent('content/form@sulucontent');
 
         return $content;
     }
 
     private function getExternalLink()
     {
-        $tab = new NavigationItem('content-navigation.contents.external-link');
+        $tab = new ContentNavigationItem('content-navigation.contents.external-link');
         $tab->setId('tab-external-link');
         $tab->setAction('content');
-        $tab->setContentType('content');
-        $tab->setContentComponent('content/external@sulucontent');
+        $tab->setGroups(array('content'));
+        $tab->setComponent('content/external@sulucontent');
 
         return $tab;
     }
 
     private function getInternalLink()
     {
-        $tab = new NavigationItem('content-navigation.contents.internal-link');
+        $tab = new ContentNavigationItem('content-navigation.contents.internal-link');
         $tab->setId('tab-internal-link');
         $tab->setAction('content');
-        $tab->setContentType('content');
-        $tab->setContentComponent('content/internal@sulucontent');
+        $tab->setGroups(array('content'));
+        $tab->setComponent('content/internal@sulucontent');
 
         return $tab;
     }
