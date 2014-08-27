c097d06be18fdd4dce3881f3d7624443073d98c0
dantleech <dan.t.leech@gmail.com>
2014-08-27 08:50:27 +0200

    Added unit test

--- src/Sulu/Bundle/WebsiteBundle/DataCollector/SuluCollector.php
+++ src/Sulu/Bundle/WebsiteBundle/DataCollector/SuluCollector.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Sulu\Bundle\WebsiteBundle\DataCollector;
 
@@ -23,18 +31,31 @@ class SuluCollector extends DataCollector
 
     public function collect(Request $request, Response $response, \Exception $exception = null)
     {
-        $ra = $this->requestAnalyzer;
+        $requestAnalyzer = $this->requestAnalyzer;
 
-        $this->data['match_type'] = $ra->getCurrentMatchType();
-        $this->data['redirect'] = $ra->getCurrentRedirect();
-        $this->data['portal_url'] = $ra->getCurrentPortalUrl();
+        $webspace = $requestAnalyzer->getCurrentWebspace();
+        $portal = $requestAnalyzer->getCurrentPortal();
+        $segment = $requestAnalyzer->getCurrentSegment();
 
-        $this->data['webspace'] = $ra->getCurrentWebspace()->toArray();
-        $this->data['portal'] = $ra->getCurrentPortal()->toArray();
-        $this->data['segment'] = $ra->getCurrentSegment() ? $ra->getCurrentSegment()->toArray() : null;
-        $this->data['localization'] = $ra->getCurrentLocalization();
-        $this->data['resource_locator'] = $ra->getCurrentResourceLocator();
-        $this->data['resource_locator_prefix'] = $ra->getCurrentResourceLocatorPrefix();
+        $this->data['match_type'] = $requestAnalyzer->getCurrentMatchType();
+        $this->data['redirect'] = $requestAnalyzer->getCurrentRedirect();
+        $this->data['portal_url'] = $requestAnalyzer->getCurrentPortalUrl();
+
+        if ($webspace) {
+            $this->data['webspace'] = $webspace->toArray();
+        }
+
+        if ($portal) {
+            $this->data['portal'] = $portal->toArray();
+        }
+
+        if ($segment) {
+            $this->data['segment'] = $segment->toArray();
+        }
+
+        $this->data['localization'] = $requestAnalyzer->getCurrentLocalization();
+        $this->data['resource_locator'] = $requestAnalyzer->getCurrentResourceLocator();
+        $this->data['resource_locator_prefix'] = $requestAnalyzer->getCurrentResourceLocatorPrefix();
 
         $structure = null;
         if ($request->attributes->has('_route_params')) {
