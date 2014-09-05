a5106ccd78362702361582b2df616fab30d7d189
Johannes Wachter <johannes.wachter@massiveart.com>
2014-09-05 15:40:24 +0200

    refactored sitemap generating

---
+++ src/Sulu/Bundle/WebsiteBundle/ContentQuery/ListToTreeConverter.php
@@ -0,0 +1,124 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\WebsiteBundle\ContentQuery;
+
+use Sulu\Component\PHPCR\SessionManager\SessionManagerInterface;
+
+/**
+ * Converts a list of nodes to a tree
+ */
+class ListToTreeConverter
+{
+    /**
+     * @var SessionManagerInterface
+     */
+    private $sessionManager;
+
+    function __construct(SessionManagerInterface $sessionManager)
+    {
+        $this->sessionManager = $sessionManager;
+    }
+
+    /**
+     * generate a tree of the given data with the path property
+     */
+    public function convert($data, $webspaceKey)
+    {
+        $map = array();
+        $contentsPath = $this->sessionManager->getContentNode($webspaceKey)->getPath();
+        foreach ($data as $item) {
+            $map[str_replace($contentsPath, 'root', $item['path'])] = $item;
+        }
+
+        return $this->explodeTree($map, '/')['children']['root'];
+    }
+
+    /**
+     * Explode any single-dimensional array into a full blown tree structure,
+     * based on the delimiters found in it's keys.
+     *
+     * The following code block can be utilized by PEAR's Testing_DocTest
+     * <code>
+     * // Input //
+     * $key_files = array(
+     *   "/etc/php5" => "/etc/php5",
+     *   "/etc/php5/cli" => "/etc/php5/cli",
+     *   "/etc/php5/cli/conf.d" => "/etc/php5/cli/conf.d",
+     *   "/etc/php5/cli/php.ini" => "/etc/php5/cli/php.ini",
+     *   "/etc/php5/conf.d" => "/etc/php5/conf.d",
+     *   "/etc/php5/conf.d/mysqli.ini" => "/etc/php5/conf.d/mysqli.ini",
+     *   "/etc/php5/conf.d/curl.ini" => "/etc/php5/conf.d/curl.ini",
+     *   "/etc/php5/conf.d/snmp.ini" => "/etc/php5/conf.d/snmp.ini",
+     *   "/etc/php5/conf.d/gd.ini" => "/etc/php5/conf.d/gd.ini",
+     *   "/etc/php5/apache2" => "/etc/php5/apache2",
+     *   "/etc/php5/apache2/conf.d" => "/etc/php5/apache2/conf.d",
+     *   "/etc/php5/apache2/php.ini" => "/etc/php5/apache2/php.ini"
+     * );
+     *
+     * // Execute //
+     * $tree = explodeTree($key_files, "/", true);
+     *
+     * // Show //
+     * print_r($tree);
+     *
+     * @author  Kevin van Zonneveld &lt;kevin@vanzonneveld.net>
+     * @author  Lachlan Donald
+     * @author  Takkie
+     * @copyright 2008 Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+     * @license   http://www.opensource.org/licenses/bsd-license.php New BSD Licence
+     * @version   SVN: Release: $Id: explodeTree.inc.php 89 2008-09-05 20:52:48Z kevin $
+     * @link      http://kevin.vanzonneveld.net/
+     *
+     * @param array $array
+     * @param string $delimiter
+     * @param boolean $baseval
+     *
+     * @return array
+     */
+    private function explodeTree($array, $delimiter = '_', $baseval = false)
+    {
+        if (!is_array($array)) {
+            return false;
+        }
+        $splitRE = '/' . preg_quote($delimiter, '/') . '/';
+        $returnArr = array();
+        foreach ($array as $key => $val) {
+            // Get parent parts and the current leaf
+            $parts = preg_split($splitRE, $key, -1, PREG_SPLIT_NO_EMPTY);
+            $leafPart = array_pop($parts);
+
+            // Build parent structure
+            // Might be slow for really deep and large structures
+            $parentArr = & $returnArr;
+            foreach ($parts as $part) {
+                if (isset($parentArr['children'][$part])) {
+                    if (!is_array($parentArr['children'][$part])) {
+                        if ($baseval) {
+                            $parentArr['children'][$part] = array('__base_val' => $parentArr[$part]);
+                        } else {
+                            $parentArr['children'][$part] = array();
+                        }
+                    }
+                    $parentArr = & $parentArr['children'][$part];
+                }
+            }
+
+            // Add the final part to the structure
+            if (empty($parentArr['children'][$leafPart])) {
+                $parentArr['children'][$leafPart] = $val;
+            } elseif ($baseval && is_array($parentArr['children'][$leafPart])) {
+                $parentArr['children'][$leafPart]['__base_val'] = $val;
+            }
+        }
+
+        return $returnArr;
+    }
+} 
--- src/Sulu/Bundle/WebsiteBundle/Sitemap/SitemapContentQueryBuilder.php
+++ src/Sulu/Bundle/WebsiteBundle/Sitemap/SitemapContentQueryBuilder.php
@@ -10,155 +10,33 @@
 
 namespace Sulu\Bundle\WebsiteBundle\Sitemap;
 
-use Sulu\Component\Content\Mapper\Translation\MultipleTranslatedProperties;
-use Sulu\Component\Content\Mapper\Translation\TranslatedProperty;
-use Sulu\Component\Content\PropertyInterface;
-use Sulu\Component\Content\Structure;
-use Sulu\Component\Content\StructureInterface;
-use Sulu\Component\Content\StructureManagerInterface;
+use Sulu\Bundle\WebsiteBundle\ContentQuery\ContentQueryBuilder;
 
 /**
  * Creates query for a minimum content pages (title and url)
- * @package Sulu\Bundle\WebsiteBundle\Sitemap
  */
-class SitemapContentQueryBuilder
+class SitemapContentQueryBuilder extends ContentQueryBuilder
 {
     /**
-     * @var StructureManagerInterface
+     * {@inheritdoc}
      */
-    private $structureManager;
-
-    /**
-     * @var string
-     */
-    private $languageNamespace;
-
-    function __construct(StructureManagerInterface $structureManager, $languageNamespace)
+    protected function buildWhere($webspaceKey, $locale)
     {
-        $this->languageNamespace = $languageNamespace;
-        $this->structureManager = $structureManager;
+        return '';
     }
 
     /**
-     * build query for given webspace and locales
-     * @param string $webspaceKey
-     * @param string $locales
-     * @return string
+     * {@inheritdoc}
      */
-    public function build($webspaceKey, $locales)
+    protected function buildSelect($webspaceKey, $locale, &$additionalFields)
     {
-        // init internal properties
-        $translatedProperties = new MultipleTranslatedProperties(
-            array('template', 'changed', 'nodeType'),
-            $this->languageNamespace
-        );
-
-        // init select / where
-        $select = '';
-        $where = '';
-
-        foreach ($locales as $locale) {
-            $translatedProperties->setLanguage($locale);
-
-            if ($select != '') {
-                $select .= ',';
-            }
-            // select internal properties
-            $select .= sprintf(
-                "route.[jcr:uuid] as routeUuid, page.[jcr:uuid], page.[%s], page.[%s], page.[%s]",
-                $translatedProperties->getName('template'),
-                $translatedProperties->getName('changed'),
-                $translatedProperties->getName('nodeType')
-            );
-
-            $select .= $this->buildSelectForStructures($locale, $this->structureManager->getStructures());
-
-            // where state published
-            $where .= sprintf(
-                "%spage.[i18n:%s-state] = %s",
-                $where !== '' ? 'OR ' : '',
-                $locale,
-                Structure::STATE_PUBLISHED
-            );
-        }
-
-        // build sql2 query string
-        $sql2 = sprintf(
-            "SELECT route.*, page.*, %s
-             FROM [nt:unstructured] AS page
-             LEFT OUTER JOIN [nt:unstructured] AS route ON page.[jcr:uuid] = route.[sulu:content]
-             WHERE page.[jcr:mixinTypes] = 'sulu:content'
-                AND (%s) AND (ISDESCENDANTNODE(page, '/cmf/%s/contents')
-                OR ISSAMENODE(page, '/cmf/%s/contents'))",
-            $select,
-            $where,
-            $webspaceKey,
-            $webspaceKey
-        );
-
-        return $sql2;
+        return '';
     }
 
     /**
-     * Returns select statement with all url and title properties
+     * {@inheritdoc}
      */
-    private function buildSelectForStructures($locale, $structures)
+    public function init(array $options)
     {
-        $names = array();
-        $result = '';
-        // add node name and url to selector
-        /** @var StructureInterface $structure */
-        foreach ($structures as $structure) {
-            $result .= $this->buildSelectForStructure($locale, $structure, $names);
-        }
-
-        return $result;
     }
-
-    /**
-     * Returns select of a single structure with title and url selector
-     */
-    private function buildSelectForStructure($locale, StructureInterface $structure, &$names)
-    {
-        $nodeNameProperty = $structure->getPropertyByTagName('sulu.node.name');
-        $result = '';
-
-        $name = $this->getTranslatedProperty($nodeNameProperty, $locale)->getName();
-        if (!in_array($name, $names)) {
-            $names[] = $name;
-            $result .= ', ' . $this->buildSelector($name);
-        }
-
-        if ($structure->hasTag('sulu.rlp')) {
-            $urlProperty = $structure->getPropertyByTagName('sulu.rlp');
-            $name = $this->getTranslatedProperty($urlProperty, $locale)->getName();
-
-            if ($urlProperty->getContentTypeName() !== 'resource_locator' && !in_array($name, $names)) {
-                $names[] = $name;
-                $result .= ', ' . $this->buildSelector($name);
-            }
-        }
-
-        return $result;
-
-    }
-
-    /**
-     * Returns single select statement
-     */
-    private function buildSelector($name)
-    {
-        return sprintf("page.[%s]", $name);
-    }
-
-    /**
-     * Returns a translated property
-     * @param PropertyInterface $property
-     * @param string $locale
-     * @return PropertyInterface
-     */
-    private function getTranslatedProperty(PropertyInterface $property, $locale)
-    {
-        return new TranslatedProperty($property, $locale, $this->languageNamespace);
-    }
-} 
+}
