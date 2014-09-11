55a48b5aa21dbd31eac8c2393d6b1a9de8e17863
Daniel Rotter <daniel.rotter@massiveart.com>
2014-09-11 18:36:18 +0200

    removed cache files

--- src/Sulu/Bundle/MediaBundle/Tests/Resources/app/cache/test/annotations/67a566ca2d9957c6b5670ff708c7f4069198de7e$createdBy.cache.php
+++
@@ -1,1 +0,0 @@
-<?php return unserialize('a:1:{i:0;O:45:"Doctrine\\ODM\\PHPCR\\Mapping\\Annotations\\String":6:{s:4:"type";s:6:"string";s:10:"translated";b:0;s:8:"property";s:13:"jcr:createdBy";s:10:"multivalue";b:0;s:5:"assoc";N;s:8:"nullable";b:0;}}');
--- src/Sulu/Bundle/MediaBundle/Tests/Resources/app/cache/test/annotations/c401244ac4aa55ca420843810dbee4e4aa3f139f#getCollection.cache.php
+++
@@ -1,1 +0,0 @@
-<?php return unserialize('a:2:{i:0;O:41:"JMS\\Serializer\\Annotation\\VirtualProperty":0:{}i:1;O:40:"JMS\\Serializer\\Annotation\\SerializedName":1:{s:4:"name";s:10:"collection";}}');
