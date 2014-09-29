d843345c4dfb23d1e68c7d81a636e028d7aa914d
Johannes Wachter <johannes.wachter@massiveart.com>
2014-09-29 17:44:37 +0200

    updated html5sortable

--- src/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky.js
+++ src/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky.js
@@ -19127,86 +19127,147 @@ define("husky-validation", function(){});
 
 /*
  * HTML5 Sortable jQuery Plugin
- * http://farhadi.ir/projects/html5sortable
- * 
- * Copyright 2012, Ali Farhadi
+ * https://github.com/voidberg/html5sortable
+ *
+ * Original code copyright 2012 Ali Farhadi.
+ * This version is mantained by Alexandru Badiu <andu@ctrlz.ro>
+ *
+ * Thanks to the following contributors: andyburke, bistoco, daemianmack, drskullster, flying-sheep, OscarGodson, Parikshit N. Samant, rodolfospalenza, ssafejava
+ *
  * Released under the MIT license.
  */
-(function($) {
-    var dragging, placeholders = $();
-    $.fn.sortable = function(options) {
+
+
+(function ($) {
+    var dragging, draggingHeight, placeholders = $();
+    $.fn.sortable = function (options) {
         var method = String(options);
+
         options = $.extend({
-            connectWith: false
+            connectWith: false,
+            placeholder: null,
+            dragImage: null
         }, options);
-        return this.each(function() {
+
+        return this.each(function () {
+            if (method === 'reload') {
+                $(this).children(options.items).off('dragstart.h5s dragend.h5s selectstart.h5s dragover.h5s dragenter.h5s drop.h5s');
+            }
             if (/^enable|disable|destroy$/.test(method)) {
-                var items = $(this).children($(this).data('items')).attr('draggable', method == 'enable');
-                if (method == 'destroy') {
-                    items.add(this).removeData('connectWith items')
-                        .off('dragstart.h5s dragend.h5s selectstart.h5s dragover.h5s dragenter.h5s drop.h5s');
+                var citems = $(this).children($(this).data('items')).attr('draggable', method === 'enable');
+                if (method === 'destroy') {
+                    $(this).off('sortupdate');
+                    $(this).removeData('opts');
+                    citems.add(this).removeData('connectWith items')
+                        .off('dragstart.h5s dragend.h5s selectstart.h5s dragover.h5s dragenter.h5s drop.h5s').off('sortupdate');
                 }
                 return;
             }
+
+            var soptions = $(this).data('opts');
+
+            if (typeof soptions === 'undefined') {
+                $(this).data('opts', options);
+            }
+            else {
+                options = soptions;
+            }
+
             var isHandle, index, items = $(this).children(options.items);
-            var placeholder = $('<' + (/^ul|ol$/i.test(this.tagName) ? 'li' : 'div') + ' class="sortable-placeholder">');
-            items.find(options.handle).mousedown(function() {
+            var startParent, newParent;
+            var placeholder = ( options.placeholder === null ) ? $('<' + (/^ul|ol$/i.test(this.tagName) ? 'li' : 'div') + ' class="sortable-placeholder">') : $(options.placeholder).addClass('sortable-placeholder');
+
+            items.find(options.handle).mousedown(function () {
                 isHandle = true;
-            }).mouseup(function() {
-                    isHandle = false;
-                });
-            $(this).data('items', options.items)
+            }).mouseup(function () {
+                isHandle = false;
+            });
+            $(this).data('items', options.items);
             placeholders = placeholders.add(placeholder);
             if (options.connectWith) {
                 $(options.connectWith).add(this).data('connectWith', options.connectWith);
             }
-            items.attr('draggable', 'true').on('dragstart.h5s', function(e) {
+
+            items.attr('role', 'option');
+            items.attr('aria-grabbed', 'false');
+
+            items.attr('draggable', 'true').on('dragstart.h5s',function (e) {
+                e.stopImmediatePropagation();
                 if (options.handle && !isHandle) {
                     return false;
                 }
                 isHandle = false;
                 var dt = e.originalEvent.dataTransfer;
                 dt.effectAllowed = 'move';
-                dt.setData('Text', 'dummy');
-                index = (dragging = $(this)).addClass('sortable-dragging').index();
-            }).on('dragend.h5s', function() {
-                    if (!dragging) {
-                        return;
-                    }
-                    dragging.removeClass('sortable-dragging').show();
-                    placeholders.detach();
-                    if (index != dragging.index()) {
-                        dragging.parent().trigger('sortupdate', {item: dragging});
-                    }
-                    dragging = null;
-                }).not('a[href], img').on('selectstart.h5s', function() {
-                    this.dragDrop && this.dragDrop();
+                dt.setData('text', '');
+
+                if (options.dragImage && dt.setDragImage) {
+                    dt.setDragImage(options.dragImage, 0, 0);
+                }
+
+                index = (dragging = $(this)).addClass('sortable-dragging').attr('aria-grabbed', 'true').index();
+                draggingHeight = dragging.outerHeight();
+                startParent = $(this).parent();
+            }).on('dragend.h5s',function () {
+                if (!dragging) {
+                    return;
+                }
+                dragging.removeClass('sortable-dragging').attr('aria-grabbed', 'false').show();
+                placeholders.detach();
+                newParent = $(this).parent();
+                if (index !== dragging.index() || startParent.get(0) !== newParent.get(0)) {
+                    dragging.parent().triggerHandler('sortupdate', {item: dragging, oldindex: index, startparent: startParent, endparent: newParent});
+                }
+                dragging = null;
+                draggingHeight = null;
+            }).not('a[href], img').on('selectstart.h5s',function () {
+                if (options.handle && !isHandle) {
+                    return true;
+                }
+
+                if (this.dragDrop) {
+                    this.dragDrop();
+                }
+                return false;
+            }).end().add([this, placeholder]).on('dragover.h5s dragenter.h5s drop.h5s', function (e) {
+                if (!items.is(dragging) && options.connectWith !== $(dragging).parent().data('connectWith')) {
+                    return true;
+                }
+                if (e.type === 'drop') {
+                    e.stopPropagation();
+                    placeholders.filter(':visible').after(dragging);
+                    dragging.trigger('dragend.h5s');
                     return false;
-                }).end().add([this, placeholder]).on('dragover.h5s dragenter.h5s drop.h5s', function(e) {
-                    if (!items.is(dragging) && options.connectWith !== $(dragging).parent().data('connectWith')) {
-                        return true;
+                }
+                e.preventDefault();
+                e.originalEvent.dataTransfer.dropEffect = 'move';
+                if (items.is(this)) {
+                    var thisHeight = $(this).outerHeight();
+                    if (options.forcePlaceholderSize) {
+                        placeholder.height(draggingHeight);
                     }
-                    if (e.type == 'drop') {
-                        e.stopPropagation();
-                        placeholders.filter(':visible').after(dragging);
-                        dragging.trigger('dragend.h5s');
-                        return false;
-                    }
-                    e.preventDefault();
-                    e.originalEvent.dataTransfer.dropEffect = 'move';
-                    if (items.is(this)) {
-                        if (options.forcePlaceholderSize) {
-                            placeholder.height(dragging.outerHeight());
+
+                    // Check if $(this) is bigger than the draggable. If it is, we have to define a dead zone to prevent flickering
+                    if (thisHeight > draggingHeight) {
+                        // Dead zone?
+                        var deadZone = thisHeight - draggingHeight, offsetTop = $(this).offset().top;
+                        if (placeholder.index() < $(this).index() && e.originalEvent.pageY < offsetTop + deadZone) {
+                            return false;
                         }
-                        dragging.hide();
-                        $(this)[placeholder.index() < $(this).index() ? 'after' : 'before'](placeholder);
-                        placeholders.not(placeholder).detach();
-                    } else if (!placeholders.is(this) && !$(this).children(options.items).length) {
-                        placeholders.detach();
-                        $(this).append(placeholder);
+                        else if (placeholder.index() > $(this).index() && e.originalEvent.pageY > offsetTop + thisHeight - deadZone) {
+                            return false;
+                        }
                     }
-                    return false;
-                });
+
+                    dragging.hide();
+                    $(this)[placeholder.index() < $(this).index() ? 'after' : 'before'](placeholder);
+                    placeholders.not(placeholder).detach();
+                } else if (!placeholders.is(this) && !$(this).children(options.items).length) {
+                    placeholders.detach();
+                    $(this).append(placeholder);
+                }
+                return false;
+            });
         });
     };
 })(jQuery);
