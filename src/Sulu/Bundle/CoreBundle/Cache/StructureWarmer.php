4a6e003c466f5bb0d3eaf65a0e629f532a2d7896
dantleech <dan.t.leech@gmail.com>
2014-10-02 11:02:15 +0200

    Added structure cache warmer

---
+++ src/Sulu/Bundle/CoreBundle/Cache/StructureWarmer.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\CoreBundle\Cache;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
+use Sulu\Component\Content\StructureManagerInterface;
+
+/**
+ * Generates the Structure cache files
+ */
+class StructureWarmer implements CacheWarmerInterface
+{
+    /**
+     * @var StructureManager
+     */
+    private $structureManager;
+
+    public function __construct(StructureManagerInterface $structureManager)
+    {
+        $this->structureManager = $structureManager;
+    }
+
+    public function warmUp($cacheDir)
+    {
+        // warmup the pages
+        $this->structureManager->getStructures();
+    }
+
+    public function isOptional()
+    {
+        return true;
+    }
+}
