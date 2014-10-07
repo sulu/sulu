8fe7019fadf48e2b0aeb120bf020343fc2d3e902
mob8607 <mathias.ober@massiveart.com>
2014-10-07 10:03:49 +0200

    removed unnecessary if-statement

--- src/Sulu/Bundle/SecurityBundle/Controller/UserController.php
+++ src/Sulu/Bundle/SecurityBundle/Controller/UserController.php
@@ -199,10 +199,7 @@ class UserController extends RestController implements ClassResourceInterface
                 );
             }
 
-            if ($request->get('enabled') != '') {
-                $user->setEnabled($request->get('enabled'));
-            }
-
+            $user->setEnabled($request->get('enabled'));
             $user->setLocale($request->get('locale'));
 
             if (
