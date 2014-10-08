860749eb4f533336c81566b3e162ac144b8e3144
Johannes Wachter <johannes.wachter@massiveart.com>
2014-10-08 16:45:42 +0200

    added snippet manager

---
+++ src/Sulu/Bundle/SnippetBundle/Controller/SnippetController.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\SnippetBundle\Controller;
+
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use Sulu\Component\Rest\RequestParametersTrait;
+use Sulu\Component\Rest\RestController;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * handles snippets
+ */
+class SnippetController extends RestController implements ClassResourceInterface
+{
+    /**
+     * extract parameter methods
+     */
+    use RequestParametersTrait;
+
+    public function cgetAction(Request $request)
+    {
+
+    }
+}
