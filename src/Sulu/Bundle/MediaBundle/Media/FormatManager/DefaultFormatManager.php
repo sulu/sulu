6874879aa122f56588d910fbd68ea3c4a3ca5950
Alexander Schranz <alexander.schranz@massiveart.com>
2014-10-20 18:34:23 +0200

    activate format cache

--- src/Sulu/Bundle/MediaBundle/Media/FormatManager/DefaultFormatManager.php
+++ src/Sulu/Bundle/MediaBundle/Media/FormatManager/DefaultFormatManager.php
@@ -152,7 +152,7 @@ class DefaultFormatManager implements FormatManagerInterface
                 $status = 200;
 
                 // save image
-                if ($this->saveImage && false) {
+                if ($this->saveImage) {
                     $this->formatCache->save(
                         $this->createTmpFile($image),
                         $media->getId(),
