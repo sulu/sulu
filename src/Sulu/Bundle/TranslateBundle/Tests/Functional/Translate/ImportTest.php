2b7d866cb17bbdf2da7d9d2640ff7f495488c624
dantleech <dan.t.leech@gmail.com>
2014-10-21 11:00:46 +0200

    Fixed other tests

--- src/Sulu/Bundle/TranslateBundle/Tests/Functional/Translate/ImportTest.php
+++ src/Sulu/Bundle/TranslateBundle/Tests/Functional/Translate/ImportTest.php
@@ -10,10 +10,10 @@
 
 namespace Sulu\Bundle\TranslateBundle\Tests\Functional\Translate;
 
-use Sulu\Bundle\TestBundle\Testing\DatabaseTestCase;
+use Sulu\Bundle\TestBundle\Testing\SuluTestCase;
 use Sulu\Bundle\TranslateBundle\Translate\Import;
 
-class ImportTest extends DatabaseTestCase
+class ImportTest extends SuluTestCase
 {
     private static $fixturePath;
 
@@ -35,7 +35,8 @@ class ImportTest extends DatabaseTestCase
 
     public function setUp()
     {
-        $this->setUpSchema();
+        $this->em = $this->db('ORM')->getOm();
+        $this->purgeDatabase();
 
         $hikaruBundle = $this->getMock('Symfony\Component\HttpKernel\Bundle', array('getName', 'getPath'));
         $hikaruBundle->expects($this->any())
@@ -59,27 +60,7 @@ class ImportTest extends DatabaseTestCase
             ->will($this->returnValue(array($sampleBundle, $hikaruBundle)));
 
 
-        $this->import = new Import(self::$em, $kernel);
-    }
-
-    public function tearDown()
-    {
-        parent::tearDown();
-        self::$em->clear();
-        self::$tool->dropSchema(self::$entities);
-    }
-
-    public function setUpSchema()
-    {
-        self::$entities = array(
-            self::$em->getClassMetadata('Sulu\Bundle\TranslateBundle\Entity\Catalogue'),
-            self::$em->getClassMetadata('Sulu\Bundle\TranslateBundle\Entity\Code'),
-            self::$em->getClassMetadata('Sulu\Bundle\TranslateBundle\Entity\Location'),
-            self::$em->getClassMetadata('Sulu\Bundle\TranslateBundle\Entity\Package'),
-            self::$em->getClassMetadata('Sulu\Bundle\TranslateBundle\Entity\Translation'),
-        );
-
-        self::$tool->createSchema(self::$entities);
+        $this->import = new Import($this->em, $kernel);
     }
 
     public function testXliffFromFile()
@@ -88,58 +69,53 @@ class ImportTest extends DatabaseTestCase
         $this->import->setName('Import');
         $this->import->setFormat(Import::XLIFF);
         $this->import->setLocale('de');
-        $this->import->executeFromFile(true, true);
+        $res = $this->import->executeFromFile(true, true);
 
-        $package = self::$em->getRepository('SuluTranslateBundle:Package')->find(1);
-        $this->assertEquals(1, $package->getId());
-        $this->assertEquals('Import', $package->getName());
+        $package = $this->em->getRepository('SuluTranslateBundle:Package')->findOneByName('Import');
+        $this->assertNotNull($package);
 
-        $catalogue = self::$em->getRepository('SuluTranslateBundle:Catalogue')->find(1);
-        $this->assertEquals(1, $catalogue->getId());
-        $this->assertEquals('de', $catalogue->getLocale());
+        $catalogue = $this->em->getRepository('SuluTranslateBundle:Catalogue')->findOneByLocale('de');
+        $this->assertNotNull($catalogue);
 
-        $codes = self::$em->getRepository('SuluTranslateBundle:Code')->findAll();
-        $this->assertEquals(1, $codes[0]->getId());
+        $codes = $this->em->getRepository('SuluTranslateBundle:Code')->findAll();
         $this->assertEquals('sulu.great', $codes[0]->getCode());
         $this->assertEquals(true, $codes[0]->getBackend());
         $this->assertEquals(true, $codes[0]->getFrontend());
         $this->assertEquals(null, $codes[0]->getLength());
-        $this->assertEquals(2, $codes[1]->getId());
         $this->assertEquals('sulu.open', $codes[1]->getCode());
         $this->assertEquals(true, $codes[1]->getBackend());
         $this->assertEquals(true, $codes[1]->getFrontend());
         $this->assertEquals(null, $codes[1]->getLength());
 
-        $translations = self::$em->getRepository('SuluTranslateBundle:Translation')->findAll();
+        $translations = $this->em->getRepository('SuluTranslateBundle:Translation')->findAll();
         $this->assertEquals('Sulu ist toll!', $translations[0]->getValue());
         $this->assertEquals('Sulu ist OpenSource!', $translations[1]->getValue());
 
         // http://stackoverflow.com/questions/18268464/doctrine-lazy-loading-in-symfony-test-environment
-        self::$em->clear();
+        $this->em->clear();
 
         // test new import
         $this->import->setFile(self::$fixturePath . '/samples/import_better.xlf');
         $this->import->setName('Import Update');
         $this->import->setFormat(Import::XLIFF);
         $this->import->setLocale('de');
-        $this->import->setPackageId(1);
+        $this->import->setPackageId($package->getId());
         $this->import->executeFromFile(true, true);
 
-        $package = self::$em->getRepository('SuluTranslateBundle:Package')->find(1);
-        $this->assertEquals(1, $package->getId());
+        $package = $this->em->getRepository('SuluTranslateBundle:Package')->find($package->getId());
+        $this->assertNotNull($package);
         $this->assertEquals('Import Update', $package->getName());
 
-        $catalogue = self::$em->getRepository('SuluTranslateBundle:Catalogue')->find(1);
-        $this->assertEquals(1, $catalogue->getId());
+        $catalogues = $package->getCatalogues();
+        $catalogue = reset($catalogues->toArray());
+        $this->assertNotNull($catalogue);
         $this->assertEquals('de', $catalogue->getLocale());
 
-        $codes = self::$em->getRepository('SuluTranslateBundle:Code')->findAll();
-        $this->assertEquals(1, $codes[0]->getId());
+        $codes = $this->em->getRepository('SuluTranslateBundle:Code')->findAll();
         $this->assertEquals('sulu.great', $codes[0]->getCode());
         $this->assertEquals(true, $codes[0]->getBackend());
         $this->assertEquals(true, $codes[0]->getFrontend());
         $this->assertEquals(null, $codes[0]->getLength());
-        $this->assertEquals(2, $codes[1]->getId());
         $this->assertEquals('sulu.open', $codes[1]->getCode());
         $this->assertEquals(true, $codes[1]->getBackend());
         $this->assertEquals(true, $codes[1]->getFrontend());
@@ -153,7 +129,7 @@ class ImportTest extends DatabaseTestCase
         $this->assertEquals(true, $codes[3]->getFrontend());
         $this->assertEquals(null, $codes[3]->getLength());
 
-        $translations = self::$em->getRepository('SuluTranslateBundle:Translation')->findAll();
+        $translations = $this->em->getRepository('SuluTranslateBundle:Translation')->findAll();
 
         $values = array(
             'Sulu ist wirklich toll!',
@@ -172,44 +148,44 @@ class ImportTest extends DatabaseTestCase
         $this->assertEquals(0, sizeof($values));
 
         // http://stackoverflow.com/questions/18268464/doctrine-lazy-loading-in-symfony-test-environment
-        self::$em->clear();
+        $this->em->clear();
 
         // test new import with new language code
         $this->import->setFile(self::$fixturePath . '/samples/import.xlf');
         $this->import->setName('Import');
         $this->import->setFormat(Import::XLIFF);
         $this->import->setLocale('en');
-        $this->import->setPackageId(1);
+        $this->import->setPackageId($package->getId());
         $this->import->executeFromFile(true, true);
 
-        $package = self::$em->getRepository('SuluTranslateBundle:Package')->find(1);
-        $this->assertEquals(1, $package->getId());
+        $package = $this->em->getRepository('SuluTranslateBundle:Package')->find($package->getId());
         $this->assertEquals('Import', $package->getName());
 
-        $catalogue = self::$em->getRepository('SuluTranslateBundle:Catalogue')->find(2);
-        $this->assertEquals(2, $catalogue->getId());
+        $catalogue = $this->em->getRepository('SuluTranslateBundle:Catalogue')->findOneBy(array(
+            'locale' => 'en',
+            'package' => $package->getId(),
+        ));
+        $this->assertNotNull($catalogue);
         $this->assertEquals('en', $catalogue->getLocale());
 
-        $codes = self::$em->getRepository('SuluTranslateBundle:Code')->findBy(
+        $codes = $this->em->getRepository('SuluTranslateBundle:Code')->findBy(
             array(
-                'package' => 1
+                'package' => $package->getId()
             )
         );
         $this->assertEquals(4, count($codes));
-        $this->assertEquals(1, $codes[0]->getId());
         $this->assertEquals('sulu.great', $codes[0]->getCode());
         $this->assertEquals(true, $codes[0]->getBackend());
         $this->assertEquals(true, $codes[0]->getFrontend());
         $this->assertEquals(null, $codes[0]->getLength());
-        $this->assertEquals(2, $codes[1]->getId());
         $this->assertEquals('sulu.open', $codes[1]->getCode());
         $this->assertEquals(true, $codes[1]->getBackend());
         $this->assertEquals(true, $codes[1]->getFrontend());
         $this->assertEquals(null, $codes[1]->getLength());
 
-        $translations = self::$em->getRepository('SuluTranslateBundle:Translation')->findBy(
+        $translations = $this->em->getRepository('SuluTranslateBundle:Translation')->findBy(
             array(
-                'catalogue' => 2
+                'catalogue' => $catalogue->getId()
             )
         );
         $this->assertEquals('Sulu ist toll!', $translations[0]->getValue());
@@ -259,98 +235,80 @@ class ImportTest extends DatabaseTestCase
         $this->import->executeFromBundles();
 
         // are all packages here ?
-        $packages = self::$em->getRepository('SuluTranslateBundle:Package')->findAll();
+        $packages = $this->em->getRepository('SuluTranslateBundle:Package')->findAll();
         $this->assertEquals(2, count($packages));
         $this->assertEquals('SuluSampleBundle', $packages[0]->getName());
         $this->assertEquals('SuluHikaruBundle', $packages[1]->getName());
 
         // are all catalogues here?
-        $catalogues = self::$em->getRepository('SuluTranslateBundle:Catalogue')->findAll();
+        $catalogues = $this->em->getRepository('SuluTranslateBundle:Catalogue')->findAll();
         $this->assertEquals(2, count($catalogues));
         $this->assertEquals('en', $catalogues[0]->getLocale());
-        $this->assertEquals(1, $catalogues[0]->getPackage()->getId());
         $this->assertEquals('en', $catalogues[1]->getLocale());
-        $this->assertEquals(2, $catalogues[1]->getPackage()->getId());
 
         // are all codes here ?
-        $codes = self::$em->getRepository('SuluTranslateBundle:Code')->findAll();
+        $codes = $this->em->getRepository('SuluTranslateBundle:Code')->findAll();
         $this->assertEquals(8, count($codes));
         // 1. Code
         $this->assertEquals('sample.backend.test1', $codes[0]->getCode());
-        $this->assertEquals(1, $codes[0]->getPackage()->getId());
         $this->assertTrue($codes[0]->getBackend());
         $this->assertFalse($codes[0]->getFrontend());
         // 2. Code
         $this->assertEquals('sample.backend.test2', $codes[1]->getCode());
-        $this->assertEquals(1, $codes[1]->getPackage()->getId());
         $this->assertTrue($codes[1]->getBackend());
         $this->assertFalse($codes[1]->getFrontend());
         // 3. Code
         $this->assertEquals('sample.frontend.test1', $codes[2]->getCode());
-        $this->assertEquals(1, $codes[2]->getPackage()->getId());
         $this->assertFalse($codes[2]->getBackend());
         $this->assertTrue($codes[2]->getFrontend());
         // 4. Code
         $this->assertEquals('sample.frontend.test2', $codes[3]->getCode());
-        $this->assertEquals(1, $codes[3]->getPackage()->getId());
         $this->assertFalse($codes[3]->getBackend());
         $this->assertTrue($codes[3]->getFrontend());
         // 5. Code
         $this->assertEquals('hikaru.backend.test1', $codes[4]->getCode());
-        $this->assertEquals(2, $codes[4]->getPackage()->getId());
         $this->assertTrue($codes[4]->getBackend());
         $this->assertFalse($codes[4]->getFrontend());
         // 6. Code
         $this->assertEquals('hikaru.backend.test2', $codes[5]->getCode());
-        $this->assertEquals(2, $codes[5]->getPackage()->getId());
         $this->assertTrue($codes[5]->getBackend());
         $this->assertFalse($codes[5]->getFrontend());
         // 7. Code
         $this->assertEquals('hikaru.frontend.test1', $codes[6]->getCode());
-        $this->assertEquals(2, $codes[6]->getPackage()->getId());
         $this->assertFalse($codes[6]->getBackend());
         $this->assertTrue($codes[6]->getFrontend());
         // 8. Code
         $this->assertEquals('hikaru.frontend.test2', $codes[7]->getCode());
-        $this->assertEquals(2, $codes[7]->getPackage()->getId());
         $this->assertFalse($codes[7]->getBackend());
         $this->assertTrue($codes[7]->getFrontend());
 
         // are all translations here ?
-        $translations = self::$em->getRepository('SuluTranslateBundle:Translation')->findAll();
+        $translations = $this->em->getRepository('SuluTranslateBundle:Translation')->findAll();
         $this->assertEquals(8, count($translations));
         // 1. Translation
         $this->assertEquals('Sample Backend import 1', $translations[0]->getValue());
         $this->assertEquals('sample.backend.test1', $translations[0]->getCode()->getCode());
-        $this->assertEquals(1, $translations[0]->getCatalogue()->getId());
         // 2. Translation
         $this->assertEquals('Sample Backend import 2', $translations[1]->getValue());
         $this->assertEquals('sample.backend.test2', $translations[1]->getCode()->getCode());
-        $this->assertEquals(1, $translations[1]->getCatalogue()->getId());
         // 3. Translation
         $this->assertEquals('Sample Frontend import 1', $translations[2]->getValue());
         $this->assertEquals('sample.frontend.test1', $translations[2]->getCode()->getCode());
-        $this->assertEquals(1, $translations[2]->getCatalogue()->getId());
         // 4. Translation
         $this->assertEquals('Sample Frontend import 2', $translations[3]->getValue());
         $this->assertEquals('sample.frontend.test2', $translations[3]->getCode()->getCode());
-        $this->assertEquals(1, $translations[3]->getCatalogue()->getId());
         // 5. Translation
         $this->assertEquals('Hikaru Backend import 1', $translations[4]->getValue());
         $this->assertEquals('hikaru.backend.test1', $translations[4]->getCode()->getCode());
-        $this->assertEquals(2, $translations[4]->getCatalogue()->getId());
         // 6. Translation
         $this->assertEquals('Hikaru Backend import 2', $translations[5]->getValue());
         $this->assertEquals('hikaru.backend.test2', $translations[5]->getCode()->getCode());
-        $this->assertEquals(2, $translations[5]->getCatalogue()->getId());
         // 7. Translation
         $this->assertEquals('Hikaru Frontend import 1', $translations[6]->getValue());
         $this->assertEquals('hikaru.frontend.test1', $translations[6]->getCode()->getCode());
-        $this->assertEquals(2, $translations[6]->getCatalogue()->getId());
         // 8. Translation
         $this->assertEquals('Hikaru Frontend import 2', $translations[7]->getValue());
         $this->assertEquals('hikaru.frontend.test2', $translations[7]->getCode()->getCode());
-        $this->assertEquals(2, $translations[7]->getCatalogue()->getId());
     }
 
     public function testResetPackages()
@@ -364,16 +322,16 @@ class ImportTest extends DatabaseTestCase
 
         $this->import->resetPackages();
 
-        $packages = self::$em->getRepository('SuluTranslateBundle:Package')->findAll();
+        $packages = $this->em->getRepository('SuluTranslateBundle:Package')->findAll();
         $this->assertEquals(0, count($packages));
 
-        $catalogues = self::$em->getRepository('SuluTranslateBundle:Catalogue')->findAll();
+        $catalogues = $this->em->getRepository('SuluTranslateBundle:Catalogue')->findAll();
         $this->assertEquals(0, count($catalogues));
 
-        $codes = self::$em->getRepository('SuluTranslateBundle:Code')->findAll();
+        $codes = $this->em->getRepository('SuluTranslateBundle:Code')->findAll();
         $this->assertEquals(0, count($codes));
 
-        $translations = self::$em->getRepository('SuluTranslateBundle:Translation')->findAll();
+        $translations = $this->em->getRepository('SuluTranslateBundle:Translation')->findAll();
         $this->assertEquals(0, count($translations));
     }
 }
