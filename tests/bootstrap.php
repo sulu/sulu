2f50670f2f794f9a7ad0c798f2579e42708ddc4d
Johannes Wachter <johannes.wachter@massiveart.com>
2014-10-25 15:30:38 +0200

    init sulutestcase

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,9 +8,14 @@
  * with this source code in the file LICENSE.
  */
 
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
 $file = __DIR__ . '/../vendor/autoload.php';
 if (!file_exists($file)) {
     throw new RuntimeException('Install dependencies to run test suite.');
 }
 
 $autoload = require_once $file;
+
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($autoload, 'loadClass'));
+AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/phpcr-odm/lib/Doctrine/ODM/PHPCR/Mapping/Annotations/DoctrineAnnotations.php');
