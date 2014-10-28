2605fe506b93382772b81f68bd541164b6577f58
Johannes Wachter <johannes.wachter@massiveart.com>
2014-10-28 11:33:40 +0100

    added load snippet twig extension

---
+++ src/Sulu/Bundle/SnippetBundle/Twig/SnippetTwigExtension.php
@@ -0,0 +1,90 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\SnippetBundle\Twig;
+
+use Sulu\Bundle\WebsiteBundle\Resolver\StructureResolverInterface;
+use Sulu\Component\Content\Mapper\ContentMapperInterface;
+use Sulu\Component\Webspace\Analyzer\RequestAnalyzerInterface;
+
+/**
+ * Provides Twig functions to handle snippets
+ */
+class SnippetTwigExtension extends \Twig_Extension
+{
+    /**
+     * @var ContentMapperInterface
+     */
+    private $contentMapper;
+
+    /**
+     * @var RequestAnalyzerInterface
+     */
+    private $requestAnalyzer;
+
+    /**
+     * @var StructureResolverInterface
+     */
+    private $structureResolver;
+
+    /**
+     * Constructor
+     */
+    function __construct(
+        ContentMapperInterface $contentMapper,
+        RequestAnalyzerInterface $requestAnalyzer,
+        StructureResolverInterface $structureResolver
+    ) {
+        $this->contentMapper = $contentMapper;
+        $this->requestAnalyzer = $requestAnalyzer;
+        $this->structureResolver = $structureResolver;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('snippet_load', array($this, 'loadSnippet'))
+        );
+    }
+
+    /**
+     * Returns snippet
+     *
+     * @param string $uuid
+     * @param string $locale
+     *
+     * @return array
+     */
+    public function loadSnippet($uuid, $locale = null)
+    {
+        if ($locale === null) {
+            $locale = $this->requestAnalyzer->getCurrentLocalization()->getLocalization();
+        }
+
+        $snippet = $this->contentMapper->load($uuid, $this->requestAnalyzer->getCurrentWebspace()->getKey(), $locale);
+        $resolved = $this->structureResolver->resolve($snippet);
+
+        return array(
+            'view' => $resolved['view'],
+            'content' => $resolved['content']
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'sulu_snippet';
+    }
+}
