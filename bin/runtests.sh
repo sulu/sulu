3c8b496cfb830b79a6197fd9eb4a70446fc2cf3c
dantleech <dan.t.leech@gmail.com>
2014-10-31 15:17:29 +0100

    Found a solution to run tests, I hope

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -4,16 +4,32 @@ DB=mysql
 OCWD=`pwd`
 BUNDLE=$1
 
-if [[ "$DB" == "mysql" ]]; then
-    mysql -u root -e "DROP DATABASE sulu_test;"
-    mysql -u root -e "CREATE DATABASE sulu_test;"
-fi
-if [[ "$DB" == "pgsql" ]]; then
-    pgsql -c "DROP DATABASE sulu_test;"
-    pgsql -c "CREATE DATABASE sulu_test;"
-fi
+function header {
+    echo ""
+    echo -e "\e[32m======================================================\e[0m"
+    echo $1
+    echo -e "\e[32m======================================================\e[0m"
+    echo ""
+}
 
-# ./src/Sulu/Bundle/TestBundle/Resources/bin/travis.sh
+function comment {
+    echo ""
+    echo -e "\e[33"$1"\e[0m"
+    echo ""
+}
+
+cat <<EOT
+   _____       _        _____ __  __ ______ 
+  / ____|     | |      / ____|  \/  |  ____|
+ | (___  _   _| |_   _| |    | \  / | |__   
+  \___ \| | | | | | | | |    | |\/| |  __|  
+  ____) | |_| | | |_| | |____| |  | | |     
+ |_____/ \__,_|_|\__,_|\_____|_|  |_|_|     
+                                            
+EOT
+
+header "Initializing database"
+./src/Sulu/Bundle/TestBundle/Resources/bin/travis.sh &> /dev/null
 
 if [ -z $BUNDLE ]; then
     BUNDLES=`find ./src/Sulu/Bundle/* -maxdepth 1 -name "phpunit.xml.dist"`
@@ -26,33 +42,29 @@ for BUNDLE in $BUNDLES; do
     BUNDLE_DIR=`dirname $BUNDLE`
     BUNDLE_NAME=`basename $BUNDLE_DIR`
 
-    echo ""
-    echo "+======================================================+"
-    echo "| "$BUNDLE_NAME
-    echo "+======================================================+"
-    echo "Running tests"
-    echo "+-----------+"
-    echo ""
+    header $BUNDLE_NAME
 
     if [ -e $BUNDLE_DIR"/Tests/Resources/app/AppKernel.php" ]; then
         export KERNEL_DIR=$BUNDLE_DIR"/Tests/Resources/app"
     elif [ -e $BUNDLE_DIR"/Tests/app/AppKernel.php" ]; then
         export KERNEL_DIR=$BUNDLE_DIR"/Tests/app"
     else
-        export KERNEL_DIR=$BUNDLE_DIR"/tests/app"
+        export KERNEL_DIR=""
     fi
 
-    echo "Kernel: "$KERNEL_DIR
 
     cd $BUNDLE_DIR
-    $OCWD"/src/Sulu/Bundle/TestBundle/Resources/bin/travis.sh"
-    php $OCWD/vendor/symfony-cmf/testing/bin/console doctrine:schema:update --force
+
+    if [[ ! -z "$KERNEL_DIR" ]]; then
+        comment "Kernel: "$KERNEL_DIR
+        env KERNEL_DIR=$OCWD"/"$KERNEL_DIR $OCWD/bin/console doctrine:schema:update --force
+    fi
+
     if [ ! -e vendor ]; then
         ln -s $OCWD"/vendor" vendor
     fi
+
     cd -
 
-    echo "RUNNING TESTS"
-    echo "============="
     phpunit --configuration phpunit.travis.xml.dist --stop-on-error $BUNDLE_DIR/Tests
 done
