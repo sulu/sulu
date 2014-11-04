fab039c4fe8164ecfec4999cf166ab556f429353
dantleech <dan.t.leech@gmail.com>
2014-11-04 09:20:01 +0100

    Fixed WebsiteBundle tests
    - The WebsiteBundle tests work without the WebsiteBundle ...

--- src/Sulu/Bundle/WebsiteBundle/Tests/app/AppKernel.php
+++ src/Sulu/Bundle/WebsiteBundle/Tests/app/AppKernel.php
@@ -14,13 +14,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new FOS\RestBundle\FOSRestBundle(),
 
             // Sulu
-            new \Sulu\Bundle\CoreBundle\SuluCoreBundle(),
-            new \Sulu\Bundle\WebsiteBundle\SuluWebsiteBundle(),
 
-            new \Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle()
         );
 
         return $bundles;
