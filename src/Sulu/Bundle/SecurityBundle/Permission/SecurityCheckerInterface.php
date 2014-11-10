aad72d4d793197d484be96b1646afd8f5c6501a3
Daniel Rotter <daniel.rotter@massiveart.com>
2014-11-10 14:29:11 +0100

    added sulu specific security checker

---
+++ src/Sulu/Bundle/SecurityBundle/Permission/SecurityCheckerInterface.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the Sulu CMF.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\SecurityBundle\Permission;
+
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+/**
+ * Interface for checking Sulu specific permissions
+ * @package Sulu\Bundle\SecurityBundle\Permission
+ */
+interface SecurityCheckerInterface
+{
+    /**
+     * Checks a Sulu specific permission based on the subject, a permission type and a locale
+     * @param mixed $subject
+     * @param string $permission
+     * @param string|null $locale
+     * @return boolan
+     * @throws AccessDeniedException
+     */
+    public function checkPermission($subject, $permission, $locale = null);
+} 
