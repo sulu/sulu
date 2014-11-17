18b0b817b4b0b51ff4154fd9a8e63bebce0b23b5
Daniel Rotter <daniel.rotter@massiveart.com>
2014-11-17 10:07:16 +0100

    implemented LocalizationManager

---
+++ src/Sulu/Component/Localization/Manager/LocalizationManager.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * This file is part of the Sulu CMF.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Component\Localization\Manager;
+
+use Sulu\Component\Localization\Localization;
+use Sulu\Component\Localization\Provider\LocalizationProviderInterface;
+
+/**
+ * Manages all the localizations available in the system
+ * @package Sulu\Component\Localization\Manager
+ */
+class LocalizationManager implements LocalizationManagerInterface
+{
+    /**
+     * Contains all the registered LocalizationProviders
+     * @var LocalizationProviderInterface[]
+     */
+    private $localizationProviders = array();
+
+    /**
+     * @return Localization[]
+     */
+    public function getLocalizations()
+    {
+        $localizations = array();
+
+        foreach ($this->localizationProviders as $localizationProvider)
+        {
+            foreach ($localizationProvider->getAllLocalizations() as $localization)
+            {
+                $localizations[$localization->getLocalization()] = $localization;
+            }
+        }
+
+        return $localizations;
+    }
+
+    /**
+     * Adds another LocalizationProvider to the manager
+     * @param LocalizationProviderInterface $localizationProvider
+     * @return mixed
+     */
+    public function addLocalizationProvider(LocalizationProviderInterface $localizationProvider)
+    {
+        $this->localizationProviders[] = $localizationProvider;
+    }
+}
