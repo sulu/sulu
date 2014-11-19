2fa843cbc5f944fbb80d3d2a66e8bec44a7fb0f0
Christian Bader <privat@chrisb.at>
2014-11-19 14:05:05 +0100

    Implemented interface for task runner

---
+++ src/Sulu/Component/Scheduler/TaskInterface.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Component\Scheduler;
+
+/**
+ * Defines the interface for a scheduled task.
+ * @package Sulu\Component\Scheduler
+ */
+interface TaskInterface
+{
+    /**
+     * Periodically called by the Scheduler
+     */
+    public function execute();
+}
