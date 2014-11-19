c01ce196128ca52fd3e324a5308e0f4b7ec52be6
Daniel Rotter <daniel.rotter@massiveart.com>
2014-11-19 11:23:33 +0100

    secured permission tab in contact section

--- src/Sulu/Bundle/SecurityBundle/Admin/SuluSecurityContentNavigation.php
+++ src/Sulu/Bundle/SecurityBundle/Admin/SuluSecurityContentNavigation.php
@@ -4,21 +4,24 @@ namespace Sulu\Bundle\SecurityBundle\Admin;
 
 use Sulu\Bundle\AdminBundle\Navigation\ContentNavigationInterface;
 use Sulu\Bundle\AdminBundle\Navigation\ContentNavigationItem;
+use Sulu\Bundle\SecurityBundle\Permission\SecurityCheckerInterface;
 
 class SuluSecurityContentNavigation implements ContentNavigationInterface
 {
     private $navigation = array();
 
-    public function __construct()
+    public function __construct(SecurityCheckerInterface $securityChecker)
     {
-        $permissions = new ContentNavigationItem('content-navigation.security.permissions');
-        $permissions->setAction('permissions');
-        $permissions->setComponent('permissions@sulusecurity');
-        $permissions->setComponentOptions(array('display'=>'form'));
-        $permissions->setDisplay(array('edit'));
-        $permissions->setGroups(array('contact'));
+        if ($securityChecker->hasPermission('sulu.security.users', 'view')) {
+            $permissions = new ContentNavigationItem('content-navigation.security.permissions');
+            $permissions->setAction('permissions');
+            $permissions->setComponent('permissions@sulusecurity');
+            $permissions->setComponentOptions(array('display' => 'form'));
+            $permissions->setDisplay(array('edit'));
+            $permissions->setGroups(array('contact'));
 
-        $this->navigation[] = $permissions;
+            $this->navigation[] = $permissions;
+        }
     }
 
     public function getNavigationItems()
