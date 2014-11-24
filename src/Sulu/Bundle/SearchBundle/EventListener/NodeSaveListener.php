fc9f14b04684c6c4b8c5abc0548da6fc21c66707
Johannes Wachter <johannes.wachter@massiveart.com>
2014-11-24 10:45:21 +0100

    removed temp parameter

--- src/Sulu/Bundle/SearchBundle/EventListener/NodeSaveListener.php
+++ src/Sulu/Bundle/SearchBundle/EventListener/NodeSaveListener.php
@@ -11,9 +11,7 @@
 namespace Sulu\Bundle\SearchBundle\EventListener;
 
 use Massive\Bundle\SearchBundle\Search\SearchManagerInterface;
-use Sulu\Bundle\SearchBundle\LocalizedSearchManager\LocalizedSearchManagerInterface;
 use Sulu\Component\Content\Event\ContentNodeEvent;
-use Massive\Bundle\SearchBundle\Search\SearchManager;
 use Sulu\Component\Content\Structure;
 use Sulu\Component\PHPCR\SessionManager\SessionManagerInterface;
 
@@ -23,7 +21,7 @@ use Sulu\Component\PHPCR\SessionManager\SessionManagerInterface;
 class NodeSaveListener
 {
     /**
-     * @var LocalizedSearchManagerInterface
+     * @var SearchManagerInterface
      */
     protected $searchManager;
 
@@ -35,36 +33,27 @@ class NodeSaveListener
     /**
      * @var string
      */
-    protected $tempName;
-
-    /**
-     * @var string
-     */
     protected $baseName;
 
-    public function __construct(SearchManagerInterface $searchManager, SessionManagerInterface $sessionManager, $baseName, $tempName)
+    public function __construct(
+        SearchManagerInterface $searchManager,
+        SessionManagerInterface $sessionManager,
+        $baseName
+    )
     {
         $this->searchManager = $searchManager;
         $this->sessionManager = $sessionManager;
-        $this->tempName = $tempName;
         $this->baseName = $baseName;
     }
 
     public function onNodeSave(ContentNodeEvent $event)
     {
         $structure = $event->getStructure();
-        $path = $this->sessionManager->getSession()->getNodeByIdentifier($structure->getUuid())->getPath();
 
-        // TODO: Move logic to determine if node is preview to somewhere else ...
-        preg_match('{/' . $this->baseName . '/(.*?)/(.*?)(/.*)*$}', $path, $matches);
-
-        // only if it is none temp node and it is published
-        if ($matches[2] !== $this->tempName) {
-            if ($structure->getNodeState() === Structure::STATE_PUBLISHED) {
-                $this->searchManager->index($structure);
-            } else {
-                $this->searchManager->deindex($structure);
-            }
+        if ($structure->getNodeState() === Structure::STATE_PUBLISHED) {
+            $this->searchManager->index($structure);
+        } else {
+            $this->searchManager->deindex($structure);
         }
     }
 }
