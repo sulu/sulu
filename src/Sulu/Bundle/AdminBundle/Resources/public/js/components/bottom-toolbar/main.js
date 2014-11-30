89fc49f944bf66ae49a25bc1d5b23586c6fc2971
ShahparShabani <shahpar.sh@gmail.com>
2014-11-30 16:48:27 +0100

    added dropdown to settings icon of toolbar

--- src/Sulu/Bundle/AdminBundle/Resources/public/js/components/bottom-toolbar/main.js
+++ src/Sulu/Bundle/AdminBundle/Resources/public/js/components/bottom-toolbar/main.js
@@ -20,13 +20,18 @@ define([],
         'use strict';
 
         var defaults = {
-                instanceName: 'contact',
+                // instanceName: 'contact',
+                instanceName: 'content',
                 template: 'default',
                 parentTemplate: null,
                 listener: 'default',
-                parentListener: null
+                parentListener: null,
+                columnOptions: {
+                    disabled: false,
+                    data: [],
+                    key: null
+                }
             },
-
             constants = {
                 bottomToolbarId: '#bottom-list-toolbar'
             },
@@ -57,45 +62,255 @@ define([],
                         },
                         {
                             id: 'settings',
-                            icon: 'magic',
+                            icon: 'gear',
                             position: 30,
-                            disabled: true
+                            items: [
+                                {
+                                    title: this.sandbox.translate('sulu.list-toolbar.import'),
+                                    disabled: true
+                                },
+                                {
+                                    title: this.sandbox.translate('sulu.list-toolbar.export'),
+                                    disabled: true
+                                },
+                                {
+                                    type: 'columnOptions'
+                                }
+                            ]
                         }
                     ];
+                },
+                defaultNoSettings: function() {
+                    var defaults = listTemplate.default.call(this);
+                    defaults.splice(2, 1);
+                    return defaults;
+                },
+                onlyAdd: function() {
+                    var defaults = listTemplate.default.call(this);
+                    defaults.splice(1, 2);
+                    return defaults;
                 }
+            },
+
+            listener = {
+                default: function() {
+                    var instanceName = this.options.instanceName ? this.options.instanceName + '.' : '',
+                        postfix;
+                    this.sandbox.on('husky.datagrid.number.selections', function(number) {
+                        postfix = number > 0 ? 'enable' : 'disable';
+                        this.sandbox.emit('husky.toolbar.' + instanceName + 'item.' + postfix, 'delete', false);
+                    }.bind(this));
+
+                    this.sandbox.on('sulu.bottom-toolbar.' + instanceName + 'delete.state-change', function(enable) {
+                        postfix = !!enable ? 'enable' : 'disable';
+                        this.sandbox.emit('husky.toolbar.' + instanceName + 'item.' + postfix, 'delete', false);
+                    }.bind(this));
+
+                    this.sandbox.on('sulu.bottom-toolbar.' + instanceName + 'edit.state-change', function(enable) {
+                        postfix = !!enable ? 'enable' : 'disable';
+                        this.sandbox.emit('husky.toolbar.' + instanceName + 'item.' + postfix, 'edit', false);
+                    }.bind(this));
+
+//                    this.sandbox.on('husky.datagrid.number.selections', function(number) {
+//                        if (number > 0) {
+//                            this.sandbox.emit('husky.toolbar.activities.item.enable', 'delete');
+//                        } else {
+//                            this.sandbox.emit('husky.toolbar.activities.item.disable', 'delete');
+//                        }
+//                    }.bind(this));
+                },
+
+                defaultEditableList: function() {
+                    var instanceName = this.options.instanceName ? this.options.instanceName + '.' : '';
+                    listener.default.call(this);
+
+                    this.sandbox.on('husky.datagrid.data.changed', function() {
+                        this.sandbox.emit('husky.toolbar.' + instanceName + 'item.enable', 'save');
+                    }.bind(this));
+                }
+            },
+
+            getColumnOptionsTemplate = function() {
+                return {
+                    title: this.sandbox.translate('list-toolbar.column-options'),
+                    disabled: false,
+                    callback: function() {
+                        var instanceName;
+
+                        this.sandbox.dom.append('body', '<div id="column-options-overlay" />');
+                        this.sandbox.start([
+                            {
+                                name: 'column-options@husky',
+                                options: {
+                                    el: '#column-options-overlay',
+                                    data: this.sandbox.sulu.getUserSetting(this.options.columnOptions.key),
+                                    hidden: false,
+                                    instanceName: this.options.instanceName,
+                                    trigger: '.toggle'
+                                }
+                            }
+                        ]);
+                        instanceName = this.options.instanceName ? this.options.instanceName + '.' : '';
+                        this.sandbox.once('husky.column-options.' + instanceName + 'saved', function(data) {
+                            this.sandbox.sulu.saveUserSetting(this.options.columnOptions.key, data);
+                        }.bind(this));
+                    }.bind(this)
+                };
+            },
+
+            /**
+             * merges two templates and replaces parent items with child items if they have the same id
+             * @param parentTemplate
+             * @param childTemplate
+             * @returns {Array}
+             */
+            mergeTemplates = function(parentTemplate, childTemplate) {
+                var template = parentTemplate.slice(0),
+                    parentIds = [];
+
+                // get parent ids
+                this.sandbox.util.foreach(parentTemplate, function(parent) {
+                    parentIds.push(parent.id);
+                }.bind(this));
+
+                // now merge arrays
+                this.sandbox.util.foreach(childTemplate, function(child) {
+                    var parentIndex = parentIds.indexOf(child.id);
+                    if (parentIndex < 0) {
+                        template.push(child);
+                    } else {
+                        // if parent contains item with same id, replace it with child item
+                        template[parentIndex] = child;
+                    }
+                }.bind(this));
+                return template;
+            },
+
+
+            /**
+             * returns parsed template
+             * @param template
+             * @param defaultTemplates
+             * @returns {*}
+             */
+            parseTemplate = function(template, defaultTemplates) {
+                // parse template, if it is a string
+                if (typeof template === 'string') {
+                    try {
+                        template = JSON.parse(template);
+                    } catch (e) {
+                        // load template from variables
+                        if (!!defaultTemplates[template]) {
+                            template = defaultTemplates[template].call(this);
+                        } else {
+                            this.sandbox.logger.log('no template found!');
+                        }
+                    }
+                } else if (typeof template === 'function') {
+                    template = template.call(this);
+                }
+                return template;
+            },
+
+            parseTemplateTypes = function(template) {
+                var i, len, item;
+
+                for (i = -1, len = template.length; ++i < len;) {
+                    item = template[i];
+                    if (item.hasOwnProperty('items')) {
+                        // call recursively
+                        item.items = parseTemplateTypes.call(this, item.items);
+                    }
+                    if (item.hasOwnProperty('type')) {
+                        if (item.type === 'columnOptions') {
+                            template[i] = this.sandbox.util.extend({}, getColumnOptionsTemplate.call(this), item);
+                        }
+                    }
+                }
+                return template;
+            },
+
+
+            /**
+             * Delegates the start of the toolbar to the header
+             */
+            startToolbarInHeader = function(options) {
+                // remove configured el (let header decide which container to use)
+                this.sandbox.emit('sulu.header.set-toolbar', options);
+            },
+            /**
+             * Starts the husky-toolbar with given options
+             * @param options {object} options The options to pass to the toolbar-component
+             */
+            startToolbarComponent = function(options) {
+                this.sandbox.start([
+                    {
+                        name: 'toolbar@husky',
+                        options: options
+                    }
+                ]);
             };
 
         return {
             initialize: function() {
+                // merge defaults
+                this.options = this.sandbox.util.extend(true, {}, defaults, this.options);
 
-                this.options = this.sandbox.util.extend({}, defaults, this.options);
-                this.instanceName = this.options.instanceName;
+                this.options.template = parseTemplate.call(this, this.options.template, listTemplate);
+                this.options.listener = parseTemplate.call(this, this.options.listener, listener);
 
-                this.render();
-                this.bindCustomEvents();
+                // check if parent template is set
+                if (!!this.options.parentTemplate) {
+                    this.options.parentTemplate = parseTemplate.call(this, this.options.parentTemplate, listTemplate);
+                    this.options.parentListener = parseTemplate.call(this, this.options.parentListener, listener);
 
-            },
+                    this.options.template = mergeTemplates.call(this, this.options.parentTemplate, this.options.template);
 
-            bindCustomEvents: function() {
-                var instanceName = this.options.instanceName ? this.options.instanceName + '.' : '',
-                    postfix;
-                this.sandbox.on('husky.datagrid.number.selections', function(number) {
-                    postfix = number > 0 ? 'enable' : 'disable';
-                    this.sandbox.emit('husky.toolbar.' + instanceName + 'item.' + postfix, 'delete', false);
-                }.bind(this));
-            },
+                }
+                this.options.template = parseTemplateTypes.call(this, this.options.template);
 
-            render: function() {
-                this.sandbox.start([
-                    {
-                        name: 'toolbar@husky',
-                        options: {
-                            el: constants.bottomToolbarId,
-                            data: listTemplate.default.call(this),
-                            instanceName: this.options.instanceName
-                        }
-                    }
-                ]);
+                var $container,
+                    options = {
+                        el: constants.bottomToolbarId,
+                        data: this.options.template,
+                        instanceName: this.options.instanceName,
+                        showTitleAsTooltip: true
+                    };
+
+
+                // see if template has listener set
+                if (this.options.listener) {
+                    this.options.listener.call(this);
+                }
+                // see if template has listener set
+                if (this.options.parentListener) {
+                    this.options.parentListener.call(this);
+                }
+
+                // start the toolbar right ahead or delegate the initialization to the header
+                if (this.options.inHeader !== true) {
+                    $container = this.sandbox.dom.createElement('<div />');
+                    this.html($container);
+                    options.el = $container;
+                    startToolbarComponent.call(this, options);
+                } else {
+                    // hide element-container, because toolbar gets rendered in header
+                    this.sandbox.dom.hide(this.$el);
+                    startToolbarInHeader.call(this, options);
+                }
+
+                // startToolbarComponent.call(this, this.options);
             }
+
+//            listener: function() {
+//                var instanceName = this.options.instanceName ? this.options.instanceName + '.' : '',
+//                    postfix;
+//                this.sandbox.on('husky.datagrid.number.selections', function(number) {
+//                    postfix = number > 0 ? 'enable' : 'disable';
+//                    this.sandbox.emit('husky.toolbar.' + instanceName + 'item.' + postfix, 'delete', false);
+//                }.bind(this));
+//            }
         };
     });
+
+
