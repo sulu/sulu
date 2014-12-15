b53b12f998515e0ab4a5bd0327aa32dbac240406
Daniel Rotter <daniel.rotter@massiveart.com>
2014-12-15 18:53:32 +0100

    secured tag controller

--- src/Sulu/Bundle/TagBundle/Admin/SuluTagAdmin.php
+++ src/Sulu/Bundle/TagBundle/Admin/SuluTagAdmin.php
@@ -13,24 +13,37 @@ namespace Sulu\Bundle\TagBundle\Admin;
 use Sulu\Bundle\AdminBundle\Admin\Admin;
 use Sulu\Bundle\AdminBundle\Navigation\Navigation;
 use Sulu\Bundle\AdminBundle\Navigation\NavigationItem;
+use Sulu\Bundle\SecurityBundle\Permission\SecurityChecker;
+use Sulu\Bundle\SecurityBundle\Permission\SecurityCheckerInterface;
 
 class SuluTagAdmin extends Admin
 {
+    /**
+     * @var SecurityChecker
+     */
+    private $securityChecker;
 
-    public function __construct($title)
+    public function __construct(SecurityCheckerInterface $securityChecker, $title)
     {
+        $this->securityChecker = $securityChecker;
+
         $rootNavigationItem = new NavigationItem($title);
         $section = new NavigationItem('');
 
         $settings = new NavigationItem('navigation.settings');
         $settings->setIcon('settings');
 
-        $roles = new NavigationItem('navigation.settings.tags', $settings);
-        $roles->setAction('settings/tags');
-        $roles->setIcon('settings');
+        if ($this->securityChecker->hasPermission('sulu.settings.tags', 'view')) {
+            $roles = new NavigationItem('navigation.settings.tags', $settings);
+            $roles->setAction('settings/tags');
+            $roles->setIcon('settings');
+        }
 
-        $section->addChild($settings);
-        $rootNavigationItem->addChild($section);
+        if ($settings->hasChildren()) {
+            $section->addChild($settings);
+            $rootNavigationItem->addChild($section);
+        }
+
         $this->setNavigation(new Navigation($rootNavigationItem));
     }
 
@@ -48,5 +61,16 @@ class SuluTagAdmin extends Admin
     public function getJsBundleName()
     {
         return 'sulutag';
+    }
+
+    public function getSecurityContexts()
+    {
+        return array(
+            'Sulu' => array(
+                'Settings' => array(
+                    'sulu.settings.tags'
+                )
+            )
+        );
     }
 }
