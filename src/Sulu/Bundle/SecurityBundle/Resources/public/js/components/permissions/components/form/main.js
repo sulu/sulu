5c01ac229b683e3ea3dc72a1da4a4e1e4fb1f1b6
Michael Zangerle <michael.zangerle@massiveart.com>
2015-01-26 13:23:36 +0100

    made new build in admin and adjusted permissions tab

--- src/Sulu/Bundle/SecurityBundle/Resources/public/js/components/permissions/components/form/main.js
+++ src/Sulu/Bundle/SecurityBundle/Resources/public/js/components/permissions/components/form/main.js
@@ -139,6 +139,20 @@ define(['app-config'], function(AppConfig) {
                 this.sandbox.emit('sulu.header.toolbar.state.change', type, saved, true);
             }
             this.saved = saved;
+            this.propagateState();
+        },
+
+        /**
+         * Propagates the state of the content with an event
+         *  sulu.content.saved when the content has been saved
+         *  sulu.content.changed when the content has been changed
+         */
+        propagateState: function() {
+            if (!!this.saved) {
+                this.sandbox.emit('sulu.content.saved');
+            } else {
+                this.sandbox.emit('sulu.content.changed');
+            }
         },
 
         listenForChange: function() {
