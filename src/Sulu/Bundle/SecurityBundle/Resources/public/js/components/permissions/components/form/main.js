bb5483119530ea5bd21d521135cab8fe88c6b2af
Michael Zangerle <michael.zangerle@massiveart.com>
2015-01-26 14:45:50 +0100

    adjusted event propagation for changed and saved content

--- src/Sulu/Bundle/SecurityBundle/Resources/public/js/components/permissions/components/form/main.js
+++ src/Sulu/Bundle/SecurityBundle/Resources/public/js/components/permissions/components/form/main.js
@@ -137,9 +137,9 @@ define(['app-config'], function(AppConfig) {
             if (saved !== this.saved) {
                 var type = (!!this.options.data && !!this.options.data.id) ? 'edit' : 'add';
                 this.sandbox.emit('sulu.header.toolbar.state.change', type, saved, true);
+                this.propagateState(saved);
             }
             this.saved = saved;
-            this.propagateState();
         },
 
         /**
@@ -147,8 +147,8 @@ define(['app-config'], function(AppConfig) {
          *  sulu.content.saved when the content has been saved
          *  sulu.content.changed when the content has been changed
          */
-        propagateState: function() {
-            if (!!this.saved) {
+        propagateState: function(saved) {
+            if (!!saved) {
                 this.sandbox.emit('sulu.content.saved');
             } else {
                 this.sandbox.emit('sulu.content.changed');
