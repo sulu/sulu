adf055532f7a8e9de5076a0117bf60d55f67ba5b
Philip Nachbauer <hello@naba.at>
2015-02-05 18:01:50 +0100

    removed unused grunt clean task

--- src/Sulu/Bundle/CategoryBundle/Gruntfile.js
+++ src/Sulu/Bundle/CategoryBundle/Gruntfile.js
@@ -21,22 +21,9 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         copy: {
-            public: {
-                files: [
-                    {expand: true, cwd: 'Resources/public', src: ['**', '!**/scss/**'], dest: '../../../../../../../web/bundles/sulucategory/'}
-                ]
-            }
         },
         clean: {
-            options: { force: true },
-            public: {
-                files: [
-                    {
-                        dot: true,
-                        src: ['../../../../../../../web/bundles/sulucategory/']
-                    }
-                ]
-            }
+            options: { force: true }
         },
         watch: {
             options: {
@@ -90,15 +77,9 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('publish', [
-        'clean:public',
-        'copy:public'
-    ]);
-
     grunt.registerTask('build', [
         'uglify',
-        'replace:build',
-        'publish'
+        'replace:build'
     ]);
 
     grunt.registerTask('default', [
