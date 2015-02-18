1420806d076989daada09d90204dc66483cf3754
Michael Zangerle <michael.zangerle@massiveart.com>
2015-02-18 15:49:28 +0100

    refactored delete dialog of accounts

--- src/Sulu/Bundle/ContactBundle/Resources/public/js/main.js
+++ src/Sulu/Bundle/ContactBundle/Resources/public/js/main.js
@@ -13,7 +13,8 @@ require.config({
         'accountsutil/header': '../../sulucontact/js/components/accounts/util/header',
         'type/bic-input': '../../sulucontact/js/input-type/bic-input',
         'type/vat-input': '../../sulucontact/js/input-type/vat-input',
-        'contactsutil/delete-dialog': '../../sulucontact/js/components/contacts/util/deleteDialog'
+        'contactsutil/delete-dialog': '../../sulucontact/js/components/contacts/util/deleteDialog',
+        'accountsutil/delete-dialog': '../../sulucontact/js/components/accounts/util/deleteDialog'
     }
 });
 
