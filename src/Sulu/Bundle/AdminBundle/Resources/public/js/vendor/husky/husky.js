879460e4287c716add4282df652317e53ad379ec
Johannes Wachter <johannes.wachter@massiveart.at>
2015-03-10 16:42:20 +0100

    added dist files

--- src/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky.js
+++ src/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky.js
@@ -48908,27 +48908,30 @@ define("datepicker-zh-TW", function(){});
     define('husky_extensions/infinite-scroll',[],function() {
 
         var scrollHandler = function(event, sandbox, padding, callback) {
-            var $e = sandbox.dom.find(event.currentTarget),
+            var $currentTarget = sandbox.dom.find(event.currentTarget),
                 $inner = sandbox.dom.first(
-                    sandbox.dom.find('div.iscroll-inner', $e)
+                    sandbox.dom.find('div.iscroll-inner', $currentTarget)
                 ),
-                borderTopWidth = parseInt(sandbox.dom.css($e, 'borderTopWidth')),
+                borderTopWidth = parseInt(sandbox.dom.css($currentTarget, 'borderTopWidth')),
                 borderTopWidthInt = isNaN(borderTopWidth) ? 0 : borderTopWidth,
-                iContainerTop = parseInt(sandbox.dom.css($e, 'paddingTop')) + borderTopWidthInt,
-                iTopHeight = sandbox.dom.offset($e).top,
+                iContainerTop = parseInt(sandbox.dom.css($currentTarget, 'paddingTop')) + borderTopWidthInt,
+                iTopHeight = sandbox.dom.offset($currentTarget).top,
                 innerTop = $inner.length ? sandbox.dom.offset($inner).top : 0,
-                iTotalHeight = Math.ceil(iTopHeight - innerTop + $e.height() + iContainerTop);
+                iTotalHeight = Math.ceil(iTopHeight - innerTop + $currentTarget.height() + iContainerTop);
 
-            if (sandbox.dom.data($e, 'enabled') === 'on' && iTotalHeight + (isNaN(padding) ? 0 : padding) >= $inner.outerHeight()) {
-                sandbox.dom.data($e, 'enabled', 'off');
+            if (
+                sandbox.dom.data($currentTarget, 'blocked') === 'on' &&
+                iTotalHeight + (isNaN(padding) ? 0 : padding) >= $inner.outerHeight()
+            ) {
+                sandbox.dom.data($currentTarget, 'blocked', 'off');
                 var result = callback();
 
                 if (!!result && !!result.then) {
                     result.then(function() {
-                        sandbox.dom.data($e, 'enabled', 'on');
+                        sandbox.dom.data($currentTarget, 'blocked', 'on');
                     });
                 } else {
-                    sandbox.dom.data($e, 'enabled', 'on');
+                    sandbox.dom.data($currentTarget, 'blocked', 'on');
                 }
             }
         };
@@ -48939,7 +48942,7 @@ define("datepicker-zh-TW", function(){});
 
             initialize: function(app) {
                 app.sandbox.infiniteScroll = function(selector, callback, padding) {
-                    app.sandbox.dom.data(app.sandbox.dom.find(selector), 'enabled', 'on');
+                    app.sandbox.dom.data(app.sandbox.dom.find(selector), 'blocked', 'on');
 
                     app.sandbox.dom.on(selector, 'scroll', function(event) {
                         scrollHandler(event, app.sandbox, padding, callback);
