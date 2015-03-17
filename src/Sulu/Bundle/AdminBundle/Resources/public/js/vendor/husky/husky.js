17e70b817ecb34e6b856084b4f7b3994264815a7
Johannes Wachter <johannes.wachter@massiveart.at>
2015-03-17 18:41:11 +0100

    added husky lib

--- src/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky.js
+++ src/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky.js
@@ -43887,6 +43887,7 @@ define('__component__$data-navigation@husky',[
     return {
 
         page: 1,
+        loading: false,
 
         /**
          * @method initialize
@@ -43977,7 +43978,7 @@ define('__component__$data-navigation@husky',[
         loadNextPage: function() {
             var def = this.sandbox.data.deferred();
 
-            if (!!this.data.hasNextPage) {
+            if (!!this.data.hasNextPage && !this.loading) {
                 this.showLoader();
 
                 this.page++;
@@ -44070,10 +44071,16 @@ define('__component__$data-navigation@husky',[
          */
         load: function(url) {
             this.page = 1;
+            this.loading = true;
 
             return this.sandbox.util.load(this.getUrl(url))
                 .then(this.parse.bind(this))
-                .then(this.hideSearch.bind(this));
+                .then(this.hideSearch.bind(this))
+                .then(function(data) {
+                    this.loading = false;
+
+                    return data;
+                }.bind(this));
         },
 
         hideLoader: function() {
