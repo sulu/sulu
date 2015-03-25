67a5e395cb8a49ad0d8025b4fd3affb20964da1e
Daniel Rotter <daniel.rotter@gmail.com>
2015-03-25 10:32:49 +0100

    fixed role list column order

--- src/Sulu/Bundle/SecurityBundle/Controller/RoleController.php
+++ src/Sulu/Bundle/SecurityBundle/Controller/RoleController.php
@@ -71,17 +71,17 @@ class RoleController extends RestController implements ClassResourceInterface, S
             array(),
             false, false, '', '50px'
         );
-        $this->fieldDescriptors['system'] = new DoctrineFieldDescriptor(
-            'system',
-            'system',
-            static::$entityName,
-            'security.roles.system'
-        );
         $this->fieldDescriptors['name'] = new DoctrineFieldDescriptor(
             'name',
             'name',
             static::$entityName,
             'public.name'
+        );
+        $this->fieldDescriptors['system'] = new DoctrineFieldDescriptor(
+            'system',
+            'system',
+            static::$entityName,
+            'security.roles.system'
         );
         $this->fieldDescriptors['created'] = new DoctrineFieldDescriptor(
             'created',
