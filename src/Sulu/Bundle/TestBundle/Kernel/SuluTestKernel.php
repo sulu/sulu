fb7a8faef1239c8d1b3f0cb80048f1cd90136923
Alexander Schranz <alexander.schranz@massiveart.com>
2015-04-09 14:00:31 +0200

    delete assetic bundle from sulu test kernel

--- src/Sulu/Bundle/TestBundle/Kernel/SuluTestKernel.php
+++ src/Sulu/Bundle/TestBundle/Kernel/SuluTestKernel.php
@@ -17,7 +17,6 @@ class SuluTestKernel extends SuluKernel
             new \Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle(),
             new \FOS\RestBundle\FOSRestBundle(),
             new \JMS\SerializerBundle\JMSSerializerBundle(),
-            new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new \Liip\ThemeBundle\LiipThemeBundle(),
             new \Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
