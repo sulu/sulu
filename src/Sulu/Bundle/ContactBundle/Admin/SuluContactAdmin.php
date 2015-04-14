29914dc188782ce2b172bb51eb9bb70e858e6e33
Johannes Wachter <johannes.wachter@massiveart.at>
2015-04-14 15:31:06 +0200

    added contact & account security context

--- src/Sulu/Bundle/ContactBundle/Admin/SuluContactAdmin.php
+++ src/Sulu/Bundle/ContactBundle/Admin/SuluContactAdmin.php
@@ -13,31 +13,38 @@ namespace Sulu\Bundle\ContactBundle\Admin;
 use Sulu\Bundle\AdminBundle\Admin\Admin;
 use Sulu\Bundle\AdminBundle\Navigation\Navigation;
 use Sulu\Bundle\AdminBundle\Navigation\NavigationItem;
+use Sulu\Component\Security\Authorization\SecurityCheckerInterface;
 
 class SuluContactAdmin extends Admin
 {
-
-    public function __construct($title)
+    public function __construct(SecurityCheckerInterface $securityChecker, $title)
     {
-        $rootNavigationItem = new NavigationItem($title);
+        $this->securityChecker = $securityChecker;
 
+        $rootNavigationItem = new NavigationItem($title);
         $section = new NavigationItem('');
 
         $contacts = new NavigationItem('navigation.contacts');
         $contacts->setIcon('user');
-        $section->addChild($contacts);
 
-        $people = new NavigationItem('navigation.contacts.people');
-        $people->setIcon('users');
-        $people->setAction('contacts/contacts');
-        $contacts->addChild($people);
+        if ($this->securityChecker->hasPermission('sulu.contact.people', 'view')) {
+            $people = new NavigationItem('navigation.contacts.people');
+            $people->setIcon('users');
+            $people->setAction('contacts/contacts');
+            $contacts->addChild($people);
+        }
 
-        $companies = new NavigationItem('navigation.contacts.companies');
-        $companies->setIcon('building');
-        $companies->setAction('contacts/accounts');
-        $contacts->addChild($companies);
+        if ($this->securityChecker->hasPermission('sulu.contact.organizations', 'view')) {
+            $companies = new NavigationItem('navigation.contacts.companies');
+            $companies->setIcon('building');
+            $companies->setAction('contacts/accounts');
+            $contacts->addChild($companies);
+        }
 
-        $rootNavigationItem->addChild($section);
+        if($contacts->hasChildren()) {
+            $rootNavigationItem->addChild($section);
+            $section->addChild($contacts);
+        }
 
         $this->setNavigation(new Navigation($rootNavigationItem));
     }
@@ -56,5 +63,20 @@ class SuluContactAdmin extends Admin
     public function getJsBundleName()
     {
         return 'sulucontact';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSecurityContexts()
+    {
+        return array(
+            'Sulu' => array(
+                'Contacts' => array(
+                    'sulu.contact.people',
+                    'sulu.contact.organizations'
+                )
+            )
+        );
     }
 }
