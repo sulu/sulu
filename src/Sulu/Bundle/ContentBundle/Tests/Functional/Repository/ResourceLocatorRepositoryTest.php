c32573b3ace06767d7680790ef8777700294a108
dantleech <dan.t.leech@gmail.com>
2015-04-17 13:12:15 +0200

    Resourcelocator tests pass

--- src/Sulu/Bundle/ContentBundle/Tests/Functional/Repository/ResourceLocatorRepositoryTest.php
+++ src/Sulu/Bundle/ContentBundle/Tests/Functional/Repository/ResourceLocatorRepositoryTest.php
@@ -22,12 +22,13 @@ use Sulu\Component\PHPCR\PathCleanup;
 use Sulu\Component\Localization\Localization;
 use Sulu\Component\Webspace\Portal;
 use Sulu\Component\Webspace\Webspace;
+use Sulu\Bundle\TestBundle\Testing\SuluTestCase;
 
 /**
  * @group functional
  * @group repository
  */
-class ResourceLocatorRepositoryTest extends PhpcrTestCase
+class ResourceLocatorRepositoryTest extends SuluTestCase
 {
     /**
      * @var ResourceLocatorRepositoryInterface
@@ -41,97 +42,9 @@ class ResourceLocatorRepositoryTest extends PhpcrTestCase
 
     protected function setUp()
     {
-        $this->prepareMapper();
-        $this->prepareResourceLocatorRepository();
-    }
-
-    /**
-     * prepares webspace manager
-     */
-    protected function prepareWebspaceManager()
-    {
-        if ($this->webspaceManager === null) {
-            $this->webspaceManager = $this->getMock('Sulu\Component\Webspace\Manager\WebspaceManagerInterface');
-
-            $this->webspace = new Webspace();
-            $locale = new Localization();
-            $locale->setLanguage('de');
-            $locale->setDefault(true);
-            $this->webspace->addLocalization($locale);
-            $portal = new Portal();
-            $portal->addLocalization($locale);
-            $this->webspace->addPortal($portal);
-
-            $this->webspaceManager->expects($this->any())->method('findWebspaceByKey')->will(
-                $this->returnValue($this->webspace)
-            );
-        }
-    }
-
-    private function prepareResourceLocatorRepository()
-    {
-        $strategy = new TreeStrategy(
-            new PhpcrMapper($this->sessionManager, '/cmf/routes'),
-            new PathCleanup(),
-            $this->structureManager,
-            $this->contentTypeManager,
-            $this->nodeHelper
-        );
-        $this->repository = new ResourceLocatorRepository(
-            $strategy,
-            $this->structureManager,
-            $this->containerValueMap['sulu.content.type.resource_locator'],
-            $this->mapper
-        );
-    }
-
-    public function structureCallback()
-    {
-        $args = func_get_args();
-        $structureKey = $args[0];
-
-        if ($structureKey == 'overview') {
-            return $this->getStructureMock();
-        }
-
-        return null;
-    }
-
-    public function getStructureMock()
-    {
-        $structureMock = $this->getMockForAbstractClass(
-            '\Sulu\Component\Content\Compat\Structure\Page',
-            array('overview', 'asdf', 'asdf', 2400)
-        );
-
-        $method = new ReflectionMethod(
-            get_class($structureMock), 'addChild'
-        );
-
-        $method->setAccessible(true);
-        $method->invokeArgs(
-            $structureMock,
-            array(
-                new Property(
-                    'title', 'title', 'text_line', false, false, 1, 1, array(),
-                    array(
-                        new PropertyTag('sulu.rlp.part', 100)
-                    )
-                )
-            )
-        );
-
-        $method->invokeArgs(
-            $structureMock,
-            array(
-                new Property('url', 'url', 'resource_locator', false, false, 1, 1, array(),
-                    array(
-                        new PropertyTag('sulu.rlp', 100)
-                    ))
-            )
-        );
-
-        return $structureMock;
+        $this->initPhpcr();
+        $this->mapper = $this->getContainer()->get('sulu.content.mapper');
+        $this->repository = $this->getContainer()->get('sulu_content.rl_repository');
     }
 
     /**
@@ -139,7 +52,16 @@ class ResourceLocatorRepositoryTest extends PhpcrTestCase
      */
     private function prepareGenerateTestData()
     {
-        return $this->mapper->save(array('title' => 'asdf', 'url' => '/asdf'), 'overview', 'default', 'en', 1);
+        return $this->mapper->save(
+            array(
+                'title' => 'test',
+                'url' => '/test'
+            ), 
+            'overview',
+            'sulu_io',
+            'en',
+            1
+        );
     }
 
     public function testGenerate()
@@ -147,15 +69,17 @@ class ResourceLocatorRepositoryTest extends PhpcrTestCase
         $structure = $this->prepareGenerateTestData();
 
         $result = $this->repository->generate(
-            array('title' => 'test'),
+            array(
+                'title' => 'test'
+            ),
             $structure->getUuid(),
             null,
-            'default',
+            'sulu_io',
             'en',
             'overview'
         );
 
-        $this->assertEquals('/asdf/test', $result['resourceLocator']);
+        $this->assertEquals('/test/test', $result['resourceLocator']);
     }
 
     /**
@@ -163,22 +87,40 @@ class ResourceLocatorRepositoryTest extends PhpcrTestCase
      */
     private function prepareHistoryTestData()
     {
-        $structure = $this->mapper->save(array('title' => 'asdf-1', 'url' => '/asdf'), 'overview', 'default', 'en', 1);
-        sleep(1);
         $structure = $this->mapper->save(
-            array('title' => 'asdf-1', 'url' => '/asdf-1'),
+            array(
+                'title' => 'test-1', 
+                'url' => '/test'
+            ), 
             'overview',
-            'default',
+            'sulu_io',
+            'en',
+            1
+        );
+        sleep(1);
+
+        $structure = $this->mapper->save(
+            array(
+                'title' => 'test-1',
+                'url' => '/test-1'
+            ),
+            'overview',
+            'sulu_io',
             'en',
             1,
             true,
             $structure->getUuid()
         );
+
         sleep(1);
+
         $structure = $this->mapper->save(
-            array('title' => 'asdf-1', 'url' => '/asdf-2'),
+            array(
+                'title' => 'test-1',
+                'url' => '/test-2'
+            ),
             'overview',
-            'default',
+            'sulu_io',
             'en',
             1,
             true,
@@ -192,36 +134,36 @@ class ResourceLocatorRepositoryTest extends PhpcrTestCase
     {
         $structure = $this->prepareHistoryTestData();
 
-        $result = $this->repository->getHistory($structure->getUuid(), 'default', 'en');
+        $result = $this->repository->getHistory($structure->getUuid(), 'sulu_io', 'en');
 
         $this->assertEquals(2, sizeof($result['_embedded']['resourcelocators']));
-        $this->assertEquals('/asdf-1', $result['_embedded']['resourcelocators'][0]['resourceLocator']);
-        $this->assertEquals('/asdf', $result['_embedded']['resourcelocators'][1]['resourceLocator']);
+        $this->assertEquals('/test-1', $result['_embedded']['resourcelocators'][0]['resourceLocator']);
+        $this->assertEquals('/test', $result['_embedded']['resourcelocators'][1]['resourceLocator']);
     }
 
     public function testRestore()
     {
         $structure = $this->prepareHistoryTestData();
 
-        $result = $this->repository->restore('/asdf', 1, 'default', 'en');
+        $result = $this->repository->restore('/test', 1, 'sulu_io', 'en');
 
-        $this->assertEquals('/asdf', $result['resourceLocator']);
+        $this->assertEquals('/test', $result['resourceLocator']);
 
-        $result = $this->repository->getHistory($structure->getUuid(), 'default', 'en');
+        $result = $this->repository->getHistory($structure->getUuid(), 'sulu_io', 'en');
 
         $this->assertEquals(2, sizeof($result['_embedded']['resourcelocators']));
-        $this->assertEquals('/asdf-2', $result['_embedded']['resourcelocators'][0]['resourceLocator']);
-        $this->assertEquals('/asdf-1', $result['_embedded']['resourcelocators'][1]['resourceLocator']);
+        $this->assertEquals('/test-2', $result['_embedded']['resourcelocators'][0]['resourceLocator']);
+        $this->assertEquals('/test-1', $result['_embedded']['resourcelocators'][1]['resourceLocator']);
     }
 
     public function testDelete()
     {
         $structure = $this->prepareHistoryTestData();
 
-        $this->repository->delete('/asdf', 'default', 'en');
-        $result = $this->repository->getHistory($structure->getUuid(), 'default', 'en');
+        $this->repository->delete('/test', 'sulu_io', 'en');
+        $result = $this->repository->getHistory($structure->getUuid(), 'sulu_io', 'en');
 
         $this->assertEquals(1, sizeof($result['_embedded']['resourcelocators']));
-        $this->assertEquals('/asdf-1', $result['_embedded']['resourcelocators'][0]['resourceLocator']);
+        $this->assertEquals('/test-1', $result['_embedded']['resourcelocators'][0]['resourceLocator']);
     }
 }
