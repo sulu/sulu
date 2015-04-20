4a99f1e3abe6bdc5c1dcb8d245e1a6d797ac962d
dantleech <dan.t.leech@gmail.com>
2015-04-20 09:54:42 +0200

    Removed unused deps from ContntMapper

--- tests/app/AppKernel.php
+++ tests/app/AppKernel.php
@@ -3,13 +3,20 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Sulu\Bundle\TestBundle\Kernel\SuluTestKernel;
+use Sulu\Bundle\TestBundle\SuluTestBundle;
 
 class AppKernel extends SuluTestKernel
 {
-
     public function registerBundles()
     {
         $bundles = parent::registerBundles();
         return $bundles;
     }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(SuluTestBundle::getConfigDir() . '/config.php');
+        $loader->load(__DIR__ . '/config/config.yml');
+    }
+
 }
