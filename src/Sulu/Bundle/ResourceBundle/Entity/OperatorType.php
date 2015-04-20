3dda0a77d2ebe9125dc3f72a62d616510907e894
Michael Zangerle <michael.zangerle@massiveart.com>
2015-04-20 17:55:21 +0200

    initialized filters

---
+++ src/Sulu/Bundle/ResourceBundle/Entity/OperatorType.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Sulu\Bundle\ResourceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * OperatorType
+ */
+class OperatorType
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var \Sulu\Bundle\ResourceBundle\Entity\Operator
+     */
+    private $operator;
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return OperatorType
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set operator
+     *
+     * @param \Sulu\Bundle\ResourceBundle\Entity\Operator $operator
+     * @return OperatorType
+     */
+    public function setOperator(\Sulu\Bundle\ResourceBundle\Entity\Operator $operator)
+    {
+        $this->operator = $operator;
+
+        return $this;
+    }
+
+    /**
+     * Get operator
+     *
+     * @return \Sulu\Bundle\ResourceBundle\Entity\Operator 
+     */
+    public function getOperator()
+    {
+        return $this->operator;
+    }
+}
