eb1e7bc2920de0d67f3c4bd56c6da19961570b58
dantleech <dan.t.leech@gmail.com>
2015-04-22 12:23:04 +0200

    Refactored remove/dereference logic

---
+++ src/Sulu/Component/Content/Document/Subscriber/ContentRemoveSubscriber.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Sulu\Component\Content\Document\Subscriber;
+
+use Sulu\Bundle\ContentBundle\Document\RouteDocument;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Sulu\Component\DocumentManager\Events;
+use Sulu\Component\DocumentManager\DocumentManager;
+use Sulu\Component\DocumentManager\DocumentInspector;
+use Sulu\Component\DocumentManager\Event\RemoveEvent;
+use Sulu\Component\Content\Document\Behavior\ContentBehavior;
+use PHPCR\NodeInterface;
+use PHPCR\PropertyInterface;
+use Sulu\Component\DocumentManager\MetadataFactory;
+
+/**
+ * Remove routes and references associated with content
+ */
+class ContentRemoveSubscriber implements EventSubscriberInterface
+{
+    private $inspector;
+    private $documentManager;
+    private $metadataFactory;
+
+    public function __construct(
+        DocumentManager $documentManager,
+        DocumentInspector $inspector,
+        MetadataFactory $metadataFactory
+    ) {
+        $this->documentManager = $documentManager;
+        $this->inspector = $inspector;
+        $this->metadataFactory = $metadataFactory;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            Events::REMOVE => array('handleRemove', 550),
+        );
+    }
+
+    public function handleRemove(RemoveEvent $event)
+    {
+        $document = $event->getDocument();
+
+        // TODO: This is not a good indicator. There should be a RoutableBehavior here.
+        if (!$document instanceof ContentBehavior) {
+            return;
+        }
+
+        $this->removeReferences($document);
+        $this->recursivelyRemoveRoutes($document);
+    }
+
+    private function recursivelyRemoveRoutes($document)
+    {
+        $referrers = $this->inspector->getReferrers($document);
+
+        foreach ($referrers as $document) {
+            if ($document instanceof RouteDocument) {
+                $this->recursivelyRemoveRoutes($document);
+                $this->documentManager->remove($document);
+                continue;
+            }
+        }
+    }
+
+    private function removeReferences($document)
+    {
+        $node = $this->inspector->getNode($document);
+
+        $references = $node->getReferences();
+
+        foreach ($references as $reference) {
+            $referrer = $reference->getParent();
+            $metadata = $this->metadataFactory->getMetadataForPhpcrNode($referrer);
+
+            if ($metadata->getClass() === RouteDocument::class) {
+                continue;
+            }
+
+            $this->dereferenceProperty($node, $reference);
+        }
+    }
+
+    /**
+     * Remove the given property, or the value which references the node (when
+     * multi-valued).
+     *
+     * @param NodeInterface $node
+     * @param PropertyInterface $property
+     */
+    private function dereferenceProperty(NodeInterface $node, PropertyInterface $property)
+    {
+        if (false === $property->isMultiple()) {
+            $property->remove();
+            return;
+        }
+
+        // dereference from multi-valued referring properties
+        $values = $property->getValue();
+        foreach ($values as $i => $referencedNode) {
+            if ($referencedNode->getIdentifier() === $node->getIdentifier()) {
+                unset($values[$i]);
+            }
+        }
+
+        $property->getParent()->setProperty($property->getName(), $values);
+    }
+}
