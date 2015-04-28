76f7a9cbc9da6e99667b405c55a0711292c3c206
Michael Zangerle <michael.zangerle@massiveart.com>
2015-04-28 09:00:16 +0200

    added test for put

--- src/Sulu/Bundle/ResourceBundle/Filter/FilterManager.php
+++ src/Sulu/Bundle/ResourceBundle/Filter/FilterManager.php
@@ -177,14 +177,12 @@ class FilterManager implements FilterManagerInterface
             }
             $filter = new Filter($filter, $locale);
         } else {
+            $this->checkData($data, true); // TODO
             $filter = new Filter(new FilterEntity(), $locale);
             $filter->setCreated(new \DateTime());
-            $filter->setChanged(new \DateTime());
             $filter->setCreator($user);
             $this->em->persist($filter->getEntity());
         }
-        $this->checkData($data, $id === null);
-        $user = $this->userRepository->findUserById($userId);
 
         // set direct properties and translation
         $filter->setChanged(new \DateTime());
@@ -197,34 +195,25 @@ class FilterManager implements FilterManagerInterface
 
         // update condition groups and conditions
         if (isset($data['conditionGroups'])) {
-//            if (isset($data['id']) && ($filter->getId() == $data['id'])) {
-//                $compare = function (ConditionGroup $conditionGroup, $data) {
-//                    if (isset($data['id'])) {
-//                        return $data['id'] == $conditionGroup->getId();
-//                    } else {
-//                        return $this->conditionGroupHasChanged($data, $conditionGroup);
-//                    }
-//                };
-//            } else {
-//                $compare = function (ConditionGroup $conditionGroup, $data) {
-//                    return $this->conditionGroupHasChanged($data, $conditionGroup);
-//                };
-//            }
 
-            $get = function (ConditionGroup $conditionGroup) {
+            $get = function (ConditionGroupEntity $conditionGroup) {
                 return $conditionGroup->getId();
             };
-            $add = function ($conditionGroupData) use ($filter, $locale) {
-                return $this->addConditionGroup($filter, $locale, $conditionGroupData);
+
+            $add = function ($conditionGroupData) use ($filter) {
+                return $this->addConditionGroup($filter, $conditionGroupData);
             };
-            $delete = function (ConditionGroup $conditionGroup) use ($filter) {
-                $filter->removeConditionGroup($conditionGroup);
+
+            $delete = function (ConditionGroupEntity $conditionGroup) use ($filter) {
+                $this->em->remove($conditionGroup);
 
                 return true;
             };
-            $update = function (ConditionGroup $conditionGroup, $matchedEntry, $locale) {
-                return $this->updateConditionGroup($conditionGroup, $matchedEntry, $locale);
+
+            $update = function (ConditionGroupEntity $conditionGroup, $matchedEntry) {
+                return $this->updateConditionGroup($conditionGroup, $matchedEntry);
             };
+
             $this->processSubEntities(
                 $filter->getEntity()->getConditionGroups(),
                 $data['conditionGroups'],
@@ -233,15 +222,6 @@ class FilterManager implements FilterManagerInterface
                 $update,
                 $delete
             );
-
-//            $this->compareEntitiesWithData(
-//                $filter->getConditionGroups(),
-//                $data['conditionGroups'],
-//                $compare,
-//                $add,
-//                $update,
-//                $delete
-//            );
         }
 
         $this->em->flush();
@@ -249,50 +229,43 @@ class FilterManager implements FilterManagerInterface
         return $filter;
     }
 
-//    /**
-//     * Checks if the condition group has changed
-//     *
-//     * @param array $data
-//     * @param ConditionGroup $conditionGroup
-//     * @return bool
-//     */
-//    private function conditionGroupHasChanged($data, $conditionGroup)
-//    {
-//        if(count($data['conditions']) === count($conditionGroup->getConditions())){
-//            foreach()
-//        }
-//        return false;
-//    }
-
     /**
      * Updates the given condition group with the values from the given array
      *
-     * @param ConditionGroup $conditionGroup
+     * @param ConditionGroupEntity $conditionGroup
      * @param array $matchedEntry
-     * @param string $locale
      * @return bool
      * @throws FilterDependencyNotFoundException
      */
-    private function updateConditionGroup(ConditionGroup $conditionGroup, $matchedEntry, $locale)
+    private function updateConditionGroup(ConditionGroupEntity $conditionGroup, $matchedEntry)
     {
         if (isset($matchedEntry['conditions'])) {
 
             foreach ($matchedEntry['conditions'] as $conditionData) {
-                /** @var ConditionEntity $conditionEntity */
-                $conditionEntity = $this->conditionGroupRepository->findById($conditionData['id']);
-                if (!$conditionEntity) {
-                    throw new FilterDependencyNotFoundException(
-                        self::$conditionEntityName,
-                        $conditionData['id']
-                    );
+
+                if (array_key_exists('id', $conditionData)) {
+
+                    /** @var ConditionEntity $conditionEntity */
+                    $conditionEntity = $this->conditionGroupRepository->findById($conditionData['id']);
+                    if (!$conditionEntity) {
+                        throw new FilterDependencyNotFoundException(
+                            self::$conditionEntityName,
+                            $conditionData['id']
+                        );
+                    }
+                } else {
+                    $conditionEntity = new ConditionEntity();
+                    $conditionEntity->setConditionGroup($conditionGroup);
+                    $this->em->persist($conditionEntity);
                 }
-                $condition = new Condition($conditionEntity, $locale);
-                $condition->setField($this->getProperty($conditionData, 'field', $condition->getField()));
-                $condition->setOperator($this->getProperty($conditionData, 'operator', $condition->getOperator()));
-                $condition->setValue($this->getProperty($conditionData, 'value', $condition->getValue()));
-                $condition->setType($this->getProperty($conditionData, 'type', $condition->getType()));
-                $condition->setConditionGroup($conditionGroup);
-                $conditionGroup->addCondition($condition);
+
+                $conditionEntity->setField($this->getProperty($conditionData, 'field', $conditionEntity->getField()));
+                $conditionEntity->setOperator(
+                    $this->getProperty($conditionData, 'operator', $conditionEntity->getOperator())
+                );
+                $conditionEntity->setValue($this->getProperty($conditionData, 'value', $conditionEntity->getValue()));
+                $conditionEntity->setType($this->getProperty($conditionData, 'type', $conditionEntity->getType()));
+                $conditionGroup->addCondition($conditionEntity);
             }
 
         }
@@ -304,37 +277,37 @@ class FilterManager implements FilterManagerInterface
      * Adds a condition group to the given filter
      *
      * @param Filter $filter The filter to add the condition group to
-     * @param string $locale
      * @param array $conditionGroupData The array containing the data for the additional condition group
      * @return bool
      * @throws EntityIdAlreadySetException
      * @throws FilterDependencyNotFoundException
      */
-    protected function addConditionGroup(Filter $filter, $locale, $conditionGroupData)
+    protected function addConditionGroup(Filter $filter, $conditionGroupData)
     {
         if (array_key_exists('id', $conditionGroupData)) {
             throw new EntityIdAlreadySetException(self::$conditionGroupEntityName, $conditionGroupData['id']);
         } elseif (array_key_exists('conditions', $conditionGroupData)) {
-            $conditionGroup = new ConditionGroup(new ConditionGroupEntity(), $locale);
-            $conditionGroup->setFilter($filter);
+            $conditionGroup = new ConditionGroupEntity();
+            $conditionGroup->setFilter($filter->getEntity());
 
             foreach ($conditionGroupData['conditions'] as $conditionData) {
                 if (array_key_exists('id', $conditionData)) {
                     throw new EntityIdAlreadySetException(self::$conditionEntityName, $conditionData['id']);
                 } elseif ($this->isValidConditionData($conditionData)) {
-                    $condition = new Condition(new ConditionEntity(), $locale);
+                    $condition = new ConditionEntity();
                     $condition->setValue($conditionData['value']);
                     $condition->setType($conditionData['type']);
                     $condition->setOperator($conditionData['operator']);
                     $condition->setField($conditionData['field']);
                     $condition->setConditionGroup($conditionGroup);
                     $conditionGroup->addCondition($condition);
-                    $this->em->persist($condition->getEntity());
+                    $conditionGroup->setFilter($filter->getEntity());
+                    $this->em->persist($condition);
                 }
             }
 
-            $filter->addConditionGroup($conditionGroup);
-            $this->em->persist($conditionGroup->getEntity());
+            $filter->getEntity()->addConditionGroup($conditionGroup);
+            $this->em->persist($conditionGroup);
         }
 
         return true;
@@ -361,9 +334,9 @@ class FilterManager implements FilterManagerInterface
      */
     protected function checkData($data, $create)
     {
-        $this->checkDataSet($data, 'name', $create) && $this->checkDataSet($data, 'andCombination', $create);
-        $this->checkDataSet($data, 'andCombination', $create) && $this->checkDataSet($data, 'andCombination', $create);
-        $this->checkDataSet($data, 'entityName', $create) && $this->checkDataSet($data, 'entityName', $create);
+        $this->checkDataSet($data, 'name', $create);
+        $this->checkDataSet($data, 'andCombination', $create);
+        $this->checkDataSet($data, 'entityName', $create);
     }
 
     /**
