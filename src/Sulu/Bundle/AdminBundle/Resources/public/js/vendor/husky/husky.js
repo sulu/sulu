38f19a45d09fe72daa92dc47cb92cf8a46179c88
Christian Bader <privat@chrisb.at>
2015-06-09 12:44:20 +0200

    Updated husky

--- src/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky.js
+++ src/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky.js
@@ -51170,6 +51170,20 @@ define('husky_extensions/util',[],function() {
                     return _.contains(list, value);
                 };
 
+            app.core.util.isAlphaNumeric = function(str) {
+                var code, i, len;
+
+                for (i = 0, len = str.length; i < len; i++) {
+                    code = str.charCodeAt(i);
+                    if (!(code > 47 && code < 58) && // numeric (0-9)
+                        !(code > 64 && code < 91) && // upper alpha (A-Z)
+                        !(code > 96 && code < 123)) { // lower alpha (a-z)
+                      return false;
+                    }
+                }
+                return true;
+            };
+
             app.core.util.uniqueId = function(prefix) {
                 return _.uniqueId(prefix);
             };
