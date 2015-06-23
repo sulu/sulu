c8923cf15974bc200f8af05ac4c858f5464bd38d
Michael Zangerle <michael.zangerle@massiveart.com>
2015-06-23 11:09:59 +0200

    extended list builder

--- src/Sulu/Component/Rest/ListBuilder/AbstractListBuilder.php
+++ src/Sulu/Component/Rest/ListBuilder/AbstractListBuilder.php
@@ -127,6 +127,13 @@ abstract class AbstractListBuilder implements ListBuilderInterface
     protected $page = 1;
 
     /**
+     * All field descriptors for the current context.
+     *
+     * @var AbstractFieldDescriptor[]
+     */
+    protected $fieldDescriptors;
+
+    /**
      * {@inheritDoc}
      */
     public function setSelectFields($fieldDescriptors)
@@ -162,6 +169,26 @@ abstract class AbstractListBuilder implements ListBuilderInterface
     public function hasSelectField($name)
     {
         return array_key_exists($name, $this->selectFields);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setFieldDescriptors(array $fieldDescriptors)
+    {
+        $this->fieldDescriptors = $fieldDescriptors;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getFieldDescriptor($fieldName)
+    {
+        if (array_key_exists($fieldName, $this->fieldDescriptors)) {
+            return $this->fieldDescriptors[$fieldName];
+        }
+
+        return null;
     }
 
     /**
