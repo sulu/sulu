c5a58c282420ac1ba9974f2759d9a57f347b20ff
Johannes Wachter <johannes.wachter@massiveart.at>
2015-06-23 09:44:13 +0200

    added default template testcase

--- tests/Sulu/Component/Webspace/ThemeTest.php
+++ tests/Sulu/Component/Webspace/ThemeTest.php
@@ -28,4 +28,26 @@ class ThemeTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($expected, $this->theme->toArray());
     }
+
+    public function testAddErrorTemplate()
+    {
+        $expected = array(
+            'key' => 'foo',
+            'excludedTemplates' => array(),
+            'errorTemplates' => array(),
+            'defaultTemplates' => array('page' => 'default', 'homepage' => 'overview'),
+        );
+
+        $theme = new Theme();
+        $theme->setKey($expected['key']);
+        $theme->addDefaultTemplate('page', 'default');
+        $theme->addDefaultTemplate('homepage', 'overview');
+
+        $this->assertEquals($expected['defaultTemplates'], $theme->getDefaultTemplates());
+        $this->assertEquals($expected['defaultTemplates']['page'], $theme->getDefaultTemplate('page'));
+        $this->assertEquals($expected['defaultTemplates']['homepage'], $theme->getDefaultTemplate('homepage'));
+        $this->assertNull($theme->getDefaultTemplate('other-type'));
+
+        $this->assertEquals($expected, $theme->toArray());
+    }
 }
