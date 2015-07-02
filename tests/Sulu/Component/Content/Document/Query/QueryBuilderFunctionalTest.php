b8d9e3ac231a7495d6a58b82702c3e85dfa17263
dantleech <dan.t.leech@gmail.com>
2015-07-02 17:09:10 +0200

    CS fixes

--- tests/Sulu/Component/Content/Document/Query/QueryBuilderFunctionalTest.php
+++ tests/Sulu/Component/Content/Document/Query/QueryBuilderFunctionalTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Sulu\Component\Content\Document\Query;
 
 use Sulu\Bundle\TestBundle\Testing\SuluTestCase;
