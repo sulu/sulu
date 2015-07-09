6dec7dcb420d83aa33b2bc30d785098594f7bd5a
Michael Zangerle <michael.zangerle@massiveart.com>
2015-07-09 14:21:48 +0200

    added space

--- src/Sulu/Bundle/ResourceBundle/Resources/public/js/components/condition-selection/main.js
+++ src/Sulu/Bundle/ResourceBundle/Resources/public/js/components/condition-selection/main.js
@@ -237,7 +237,7 @@ define([], function() {
         removesUsedField = function(fieldName) {
             var value;
 
-            if(!fieldName){
+            if (!fieldName) {
                 return;
             }
 
