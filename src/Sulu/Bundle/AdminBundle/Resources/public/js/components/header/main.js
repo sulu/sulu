c3649985337c8758aa01c561055ca06117789dc0
Marcel Moosbrugger <marcelmoosbrugger@Marcelinhos-iMac.local>
2015-07-16 17:04:35 +0200

    toolbar header markup

--- src/Sulu/Bundle/AdminBundle/Resources/public/js/components/header/main.js
+++ src/Sulu/Bundle/AdminBundle/Resources/public/js/components/header/main.js
@@ -84,8 +84,8 @@ define([], function () {
         templates = {
             toolbarRow: [
                 '<div class="toolbar-row">',
-                '   <div class="left-container">',
-                '       <span class="fa-' + constants.backIcon + ' ' + constants.backClass + '"></span>',
+                '   <div class="left-container ' + constants.backClass + '">',
+                '       <span class="fa-' + constants.backIcon + '"></span>',
                 '   </div>',
                 '   <div class="toolbar-container">',
                 '       <div class="' + constants.toolbarClass + '"></div>',
@@ -696,6 +696,9 @@ define([], function () {
                     this.enableLanguageChanger();
                 }
             }.bind(this));
+
+            this.sandbox.on('husky.toolbar.' + this.toolbarInstanceName + '.dropdown.opened', this.lockToolbarScroll.bind(this));
+            this.sandbox.on('husky.toolbar.' + this.toolbarInstanceName + '.dropdown.closed', this.unlockToolbarScroll.bind(this));
 
             // changes the saved state of the toolbar
             this.sandbox.on(TOOLBAR_STATE_CHANGE.call(this), this.changeToolbarState.bind(this));
