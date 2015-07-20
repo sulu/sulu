04e9e16ee25a843a0b10c3b608ffb329c5f40561
Johannes Wachter <johannes.wachter@massiveart.at>
2015-07-20 14:06:10 +0200

    fixed get-preview for collection

--- src/Sulu/Bundle/MediaBundle/Collection/Manager/CollectionManager.php
+++ src/Sulu/Bundle/MediaBundle/Collection/Manager/CollectionManager.php
@@ -533,17 +533,16 @@ class CollectionManager implements CollectionManagerInterface
                 foreach ($file->getFileVersions() as $fileVersion) {
                     if ($fileVersion->getVersion() == $file->getVersion()) {
                         $format = $this->getPreviewsFromFileVersion($media->getId(), $fileVersion, $locale);
+                        
                         if (!empty($format)) {
-                            $media = $format;
+                            return $format;
                         }
-                        break;
                     }
                 }
-                break;
             }
         }
 
-        return $media;
+        return null;
     }
 
     /**
