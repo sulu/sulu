f3abb70a78bffa4ebedf9de537f1b2f8eaab80e6
Niklas Natter <niklas.natter@gmail.com>
2015-08-05 08:52:15 +0200

    began to implement new delete-dialog for contacts

--- src/Sulu/Bundle/ContactBundle/Resources/public/js/components/contacts/util/delete-dialog.js
+++ src/Sulu/Bundle/ContactBundle/Resources/public/js/components/contacts/util/delete-dialog.js
@@ -7,49 +7,32 @@
  * with this source code in the file LICENSE.
  */
 
-define([], function() {
+define(['services/husky/mediator'], function(mediator) {
 
     'use strict';
-
-    /**
-     * @var ids - array of ids to delete
-     * @var callback - callback function returns true or false if data got deleted
-     */
-    var confirmDeleteDialog = function(callbackFunction) {
-        // check if callback is a function
-        if (!!callbackFunction && typeof(callbackFunction) !== 'function') {
-            throw 'callback is not a function';
-        }
-        // show dialog
-        this.sandbox.emit('sulu.overlay.show-warning',
-            'sulu.overlay.be-careful',
-            'sulu.overlay.delete-desc',
-            callbackFunction.bind(this, false),
-            callbackFunction.bind(this, true)
-        );
-    };
-
     return {
 
-        /**
-         * Shows a dialog when a contact should be deleted
-         * @param sandbox
-         * @param contact
-         */
-        show: function(sandbox, contact) {
-            if (!!sandbox && !!contact) {
-                this.sandbox = sandbox;
-                confirmDeleteDialog.call(this, function(wasConfirmed) {
-                    if (wasConfirmed) {
-                        this.sandbox.emit('sulu.header.toolbar.item.loading', 'options-button');
-                        contact.destroy({
-                            success: function() {
-                                this.sandbox.emit('sulu.router.navigate', 'contacts/contacts');
-                            }.bind(this)
-                        });
-                    }
-                }.bind(this));
+        showConfirmDialog: function(callbackFunction) {
+            // check if callback is a function
+            if (!!callbackFunction && typeof(callbackFunction) !== 'function') {
+                throw 'callback is not a function';
             }
+
+            // show warning dialog
+            mediator.emit('sulu.overlay.show-warning',
+                'sulu.overlay.be-careful',
+                'sulu.overlay.delete-desc',
+
+                function() {
+                    // cancel callback
+                    callbackFunction(false);
+                }.bind(this),
+
+                function() {
+                    // ok callback
+                    callbackFunction(true);
+                }.bind(this)
+            );
         }
     };
 });
