84d91e4abbfdd173fa9d5a71129fd26d6f008602
Daniel Rotter <daniel.rotter@gmail.com>
2015-08-17 10:23:29 +0200

    added missing file header

--- src/Sulu/Bundle/ContentBundle/Command/MaintainNodeSecurityCommand.php
+++ src/Sulu/Bundle/ContentBundle/Command/MaintainNodeSecurityCommand.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Sulu\Bundle\ContentBundle\Command;
 
 use Sulu\Bundle\ContentBundle\Document\BasePageDocument;
