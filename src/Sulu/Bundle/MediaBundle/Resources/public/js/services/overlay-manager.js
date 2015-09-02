9afff4c1b89461873e6099f01a31eba17ab741d2
Niklas Natter <niklas.natter@gmail.com>
2015-09-02 14:09:39 +0200

    removed unused dependencies in overlay-manager

--- src/Sulu/Bundle/MediaBundle/Resources/public/js/services/overlay-manager.js
+++ src/Sulu/Bundle/MediaBundle/Resources/public/js/services/overlay-manager.js
@@ -7,157 +7,150 @@
  * with this source code in the file LICENSE.
  */
 
-define([
-        'services/husky/util',
-        'services/husky/mediator',
-        'services/sulumedia/media-router'
-    ], function(util,
-                mediator,
-                MediaRouter) {
+define(function() {
 
-        'use strict';
+    'use strict';
 
-        var instance = null,
+    var instance = null,
 
-            /**
-             * Create div-container with given containerId and append it to the body
-             * @param containerId
-             * @returns {*|jQuery|HTMLElement} created div-container
-             */
-            getOverlayContainer = function(containerId) {
-                var $element = $('<div id="' + containerId + '"/>');
-                $('body').append($element);
+        /**
+         * Create div-container with given containerId and append it to the body
+         * @param containerId
+         * @returns {*|jQuery|HTMLElement} created div-container
+         */
+        getOverlayContainer = function(containerId) {
+            var $element = $('<div id="' + containerId + '"/>');
+            $('body').append($element);
 
-                return $element;
-            };
+            return $element;
+        };
 
 
-        /** @constructor **/
-        function OverlayManager() {
-        }
+    /** @constructor **/
+    function OverlayManager() {
+    }
 
-        OverlayManager.prototype = {
-            /**
-             * Start collection-create overlay
-             * @param sandbox parent sandbox for the overlay
-             * @param parentCollection of the created collection
-             */
-            startCreateCollectionOverlay: function(sandbox, parentCollection) {
-                var parentId = (!!parentCollection && !!parentCollection.id) ? parentCollection.id : null,
-                    $container = getOverlayContainer('create-collection-overlay');
+    OverlayManager.prototype = {
+        /**
+         * Start collection-create overlay
+         * @param sandbox parent sandbox for the overlay
+         * @param parentCollection of the created collection
+         */
+        startCreateCollectionOverlay: function(sandbox, parentCollection) {
+            var parentId = (!!parentCollection && !!parentCollection.id) ? parentCollection.id : null,
+                $container = getOverlayContainer('create-collection-overlay');
 
-                sandbox.start([{
-                    name: 'collections/collection-create-overlay@sulumedia',
-                    options: {
-                        el: $container,
-                        parent: parentId
-                    }
-                }]);
-            },
-
-            /**
-             * Start collection-select overlay for moving medias
-             * @param sandbox parent sandbox for the overlay
-             * @param disableIds collectionIds which cannot get selected
-             * @param locale to display collection titles
-             */
-            startMoveMediaOverlay: function(sandbox, disableIds, locale) {
-                if (!$.isArray(disableIds)) {
-                    disableIds = [disableIds];
+            sandbox.start([{
+                name: 'collections/collection-create-overlay@sulumedia',
+                options: {
+                    el: $container,
+                    parent: parentId
                 }
-                var $container = getOverlayContainer('select-collection-overlay');
+            }]);
+        },
 
-                sandbox.start([{
-                    name: 'collections/collection-select-overlay@sulumedia',
-                    options: {
-                        el: $container,
-                        instanceName: 'move-media',
-                        title: sandbox.translate('sulu.media.move.overlay-title'),
-                        locale: locale,
-                        disableIds: disableIds
-                    }
-                }]);
-            },
+        /**
+         * Start collection-select overlay for moving medias
+         * @param sandbox parent sandbox for the overlay
+         * @param disableIds collectionIds which cannot get selected
+         * @param locale to display collection titles
+         */
+        startMoveMediaOverlay: function(sandbox, disableIds, locale) {
+            if (!$.isArray(disableIds)) {
+                disableIds = [disableIds];
+            }
+            var $container = getOverlayContainer('select-collection-overlay');
 
-            /**
-             * Start collection-select overlay for moving collection
-             * @param sandbox parent sandbox for the overlay
-             * @param disableIds collectionIds which cannot get selected
-             * @param locale to display collection titles
-             */
-            startMoveCollectionOverlay: function(sandbox, disableIds, locale) {
-                if (!$.isArray(disableIds)) {
-                    disableIds = [disableIds];
+            sandbox.start([{
+                name: 'collections/collection-select-overlay@sulumedia',
+                options: {
+                    el: $container,
+                    instanceName: 'move-media',
+                    title: sandbox.translate('sulu.media.move.overlay-title'),
+                    locale: locale,
+                    disableIds: disableIds
                 }
-                var $container = getOverlayContainer('select-collection-overlay');
+            }]);
+        },
 
-                sandbox.start([{
-                    name: 'collections/collection-select-overlay@sulumedia',
+        /**
+         * Start collection-select overlay for moving collection
+         * @param sandbox parent sandbox for the overlay
+         * @param disableIds collectionIds which cannot get selected
+         * @param locale to display collection titles
+         */
+        startMoveCollectionOverlay: function(sandbox, disableIds, locale) {
+            if (!$.isArray(disableIds)) {
+                disableIds = [disableIds];
+            }
+            var $container = getOverlayContainer('select-collection-overlay');
+
+            sandbox.start([{
+                name: 'collections/collection-select-overlay@sulumedia',
+                options: {
+                    el: $container,
+                    instanceName: 'move-collection',
+                    title: sandbox.translate('sulu.collection.move.overlay-title'),
+                    rootCollection: true,
+                    disableIds: disableIds,
+                    disabledChildren: true,
+                    locale: locale
+                }
+            }]);
+        },
+
+        /**
+         * Start media-edit overlay to edit given mediaIds
+         * @param sandbox parent sandbox for the overlay
+         * @param mediaIds medias to edit in overlay
+         * @param locale medias are saved for given locale
+         */
+        startEditMediaOverlay: function(sandbox, mediaIds, locale) {
+            if (!$.isArray(mediaIds)) {
+                mediaIds = [mediaIds];
+            }
+            var $container = getOverlayContainer('edit-media-overlay');
+
+            sandbox.start([
+                {
+                    name: 'collections/media-edit-overlay@sulumedia',
                     options: {
                         el: $container,
-                        instanceName: 'move-collection',
-                        title: sandbox.translate('sulu.collection.move.overlay-title'),
-                        rootCollection: true,
-                        disableIds: disableIds,
-                        disabledChildren: true,
+                        mediaIds: mediaIds,
                         locale: locale
                     }
-                }]);
-            },
-
-            /**
-             * Start media-edit overlay to edit given mediaIds
-             * @param sandbox parent sandbox for the overlay
-             * @param mediaIds medias to edit in overlay
-             * @param locale medias are saved for given locale
-             */
-            startEditMediaOverlay: function(sandbox, mediaIds, locale) {
-                if (!$.isArray(mediaIds)) {
-                    mediaIds = [mediaIds];
                 }
-                var $container = getOverlayContainer('edit-media-overlay');
+            ]);
+        },
 
-                sandbox.start([
-                    {
-                        name: 'collections/media-edit-overlay@sulumedia',
-                        options: {
-                            el: $container,
-                            mediaIds: mediaIds,
-                            locale: locale
-                        }
+        /**
+         * Start collection-edit overlay for given collectionId
+         * @param sandbox parent sandbox for the overlay
+         * @param collectionId collection to edit
+         * @param locale collection data is saved for the given locale
+         */
+        startEditCollectionOverlay: function(sandbox, collectionId, locale) {
+            var $container = getOverlayContainer('edit-collection-overlay');
+
+            sandbox.start([
+                {
+                    name: 'collections/collection-edit-overlay@sulumedia',
+                    options: {
+                        el: $container,
+                        collectionId: collectionId,
+                        locale: locale
                     }
-                ]);
-            },
+                }
+            ]);
+        }
+    };
 
-            /**
-             * Start collection-edit overlay for given collectionId
-             * @param sandbox parent sandbox for the overlay
-             * @param collectionId collection to edit
-             * @param locale collection data is saved for the given locale
-             */
-            startEditCollectionOverlay: function(sandbox, collectionId, locale) {
-                var $container = getOverlayContainer('edit-collection-overlay');
+    OverlayManager.getInstance = function() {
+        if (instance === null) {
+            instance = new OverlayManager();
+        }
+        return instance;
+    };
 
-                sandbox.start([
-                    {
-                        name: 'collections/collection-edit-overlay@sulumedia',
-                        options: {
-                            el: $container,
-                            collectionId: collectionId,
-                            locale: locale
-                        }
-                    }
-                ]);
-            }
-        };
-
-        OverlayManager.getInstance = function() {
-            if (instance === null) {
-                instance = new OverlayManager();
-            }
-            return instance;
-        };
-
-        return OverlayManager.getInstance();
-    }
-);
+    return OverlayManager.getInstance();
+});
