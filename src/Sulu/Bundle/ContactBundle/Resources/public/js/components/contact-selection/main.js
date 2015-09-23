d16ff8d7c006a83d1cd9d34ae0df7696a8e29201
Johannes Wachter <johannes.wachter@massiveart.at>
2015-09-23 14:55:32 +0200

    fixed code style and translations

--- src/Sulu/Bundle/ContactBundle/Resources/public/js/components/contact-selection/main.js
+++ src/Sulu/Bundle/ContactBundle/Resources/public/js/components/contact-selection/main.js
@@ -23,8 +23,6 @@ define([], function() {
             accountResultKey: 'accounts',
             dataAttribute: 'contact-selection',
             dataDefault: [],
-            hidePositionElement: true,
-            hideConfigButton: true,
             contact: true,
             contactUrl: null,
             account: true,
