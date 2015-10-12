1f22217e8ace96ff7593cf484365e9f25a513962
Alexander Schranz <alexander.schranz@massiveart.com>
2015-10-12 19:08:53 +0200

    webspace export add command and example xliff

---
+++ src/Sulu/Component/Content/Export/WebspaceInterface.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Component\Content\Export;
+
+/**
+ * Interface for Webspace export
+ */
+interface WebspaceInterface
+{
+    /**
+     * @param string $webspaceKey
+     * @param string $locale
+     * @param string $format
+     * @param string $uuid
+     *
+     * @return string
+     */
+    public function export(
+        $webspaceKey,
+        $locale,
+        $format = '1.2.xliff',
+        $uuid = null
+    );
+}
