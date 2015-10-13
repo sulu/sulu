6d0a00be020b9b404a843ee973951640948a7abd
Daniel Rotter <daniel.rotter@gmail.com>
2015-10-13 10:57:26 +0200

    fixed snippet referential integrity overlay

--- src/Sulu/Bundle/SnippetBundle/Resources/public/js/components/snippet/main.js
+++ src/Sulu/Bundle/SnippetBundle/Resources/public/js/components/snippet/main.js
@@ -14,18 +14,36 @@ define([
 
     'use strict';
 
-    var CONTENT_LANGUAGE = 'contentLanguage';
+    var CONTENT_LANGUAGE = 'contentLanguage',
 
-    var BaseSnippet = function() {
-    };
+        BaseSnippet = function() {
+        },
 
-    var translationKeys = {
-        deleteReferencedByFollowing: 'snippet.delete-referenced-by-following',
-        deleteConfirmText: 'snippet.delete-confirm-text',
-        deleteConfirmTitle: 'snippet.delete-confirm-title',
-        deleteDoIt: 'snippet.delete-do-it',
-        deleteNoSnippetsSelected: 'snippet.delete-no-snippets-selected'
-    };
+        translationKeys = {
+            deleteReferencedByFollowing: 'snippet.delete-referenced-by-following',
+            deleteConfirmText: 'snippet.delete-confirm-text',
+            deleteConfirmTitle: 'snippet.delete-confirm-title',
+            deleteDoIt: 'snippet.delete-do-it',
+            deleteNoSnippetsSelected: 'snippet.delete-no-snippets-selected'
+        },
+
+        templates = {
+            referentialIntegrityMessage: function(pageTitles) {
+                var message = [];
+
+                message.push('<p>', this.sandbox.translate(translationKeys.deleteReferencedByFollowing), '</p>');
+                message.push('<ul>');
+
+                this.sandbox.util.foreach(pageTitles, function(pageTitle) {
+                    message.push('<li>', pageTitle, '</li>');
+                });
+
+                message.push('</ul>');
+                message.push('<p>', this.sandbox.translate(translationKeys.deleteConfirmText), '</p>');
+
+                return message.join('');
+            }
+        };
 
     BaseSnippet.prototype = {
         bindModelEvents: function() {
@@ -73,7 +91,7 @@ define([
         del: function() {
             this.sandbox.sulu.showDeleteDialog(function(wasConfirmed) {
                 if (wasConfirmed) {
-                    this.destroySnippet(this.model, function () {
+                    this.destroySnippet(this.model, function() {
                         this.sandbox.emit('sulu.router.navigate', 'snippet/snippets');
                     }.bind(this));
 
@@ -82,12 +100,12 @@ define([
             }.bind(this));
         },
 
-        destroySnippet: function (snippet, successCallback) {
+        destroySnippet: function(snippet, successCallback) {
             snippet.destroy({
                 success: function() {
                     successCallback();
                 }.bind(this),
-                error: function (model, response) {
+                error: function(model, response) {
                     if (response.status == 409) {
                         this.referentialIntegrityDialog(snippet, response.responseJSON, successCallback);
                     }
@@ -95,17 +113,12 @@ define([
             });
         },
 
-        referentialIntegrityDialog: function (snippet, data, successCallback) {
-            var message = [];
-            message.push(this.sandbox.translate(translationKeys.deleteReferencedByFollowing));
-            message.push('');
+        referentialIntegrityDialog: function(snippet, data, successCallback) {
+            var pageTitles = [];
 
-            this.sandbox.util.foreach(data.structures, function (structure) {
-                message.push(' - ' + structure.title);
+            this.sandbox.util.foreach(data.structures, function(structure) {
+                pageTitles.push(structure.title);
             });
-
-            message.push('');
-            message.push(this.sandbox.translate(translationKeys.deleteConfirmText));
 
             var $element = $('<div/>');
             $('body').append($element);
@@ -117,7 +130,7 @@ define([
                         el: $element,
                         openOnStart: true,
                         title: this.sandbox.translate(translationKeys.deleteConfirmTitle),
-                        message: message.join('<br/>'),
+                        message: templates.referentialIntegrityMessage.call(this, pageTitles),
                         okDefaultText: this.sandbox.translate(translationKeys.deleteDoIt),
                         type: 'alert',
                         closeCallback: function() {
@@ -199,11 +212,11 @@ define([
                 return;
             }
 
-            var deleteSnippetFromStack = function () {
+            var deleteSnippetFromStack = function() {
                 var id = ids.shift();
                 if (id !== undefined) {
                     var snippet = new Snippet({id: id});
-                    this.destroySnippet(snippet, function () {
+                    this.destroySnippet(snippet, function() {
                         this.sandbox.emit('husky.datagrid.record.remove', id);
                         deleteSnippetFromStack();
                     }.bind(this));
