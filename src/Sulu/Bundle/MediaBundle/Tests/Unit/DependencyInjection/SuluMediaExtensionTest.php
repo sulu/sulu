33b9009a9d7ec9b1add4a19bdb37b49d80478eff
Alexander Schranz <alexander.schranz@massiveart.com>
2015-11-02 19:04:28 +0100

    fix media extension test

--- src/Sulu/Bundle/MediaBundle/Tests/Unit/DependencyInjection/SuluMediaExtensionTest.php
+++ src/Sulu/Bundle/MediaBundle/Tests/Unit/DependencyInjection/SuluMediaExtensionTest.php
@@ -34,8 +34,6 @@ class SuluMediaExtensionTest extends AbstractExtensionTestCase
             'Cache-Control' => 'public',
         ]);
         $this->assertContainerBuilderHasParameter('sulu_media.search.default_image_format', '170x170');
-        $this->assertContainerBuilderHasParameter('sulu_media.media.storage.local.path', '%kernel.root_dir%/../uploads/media');
-        $this->assertContainerBuilderHasParameter('sulu_media.media.storage.local.segments', 10);
         $this->assertContainerBuilderHasParameter('sulu_media.storage.adapters', [
             'local' => [
                 'type' => 'local',
