e594bccf47e1b518c7b9d6baccc05bb93d3067a1
Johannes Wachter <johannes.wachter@massiveart.at>
2015-12-14 13:05:32 +0100

    bootstrap analytic key component, api and entities

---
+++ src/Sulu/Bundle/WebsiteBundle/Controller/AnalyticKeysController.php
@@ -0,0 +1,124 @@
+<?php
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\WebsiteBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations\RouteResource;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use Hateoas\Representation\CollectionRepresentation;
+use Hateoas\Representation\RouteAwareRepresentation;
+use Sulu\Bundle\ContentBundle\Admin\ContentAdmin;
+use Sulu\Component\Rest\RestController;
+use Sulu\Component\Security\SecuredControllerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Provides webspace analytic keys rest-endpoint.
+ *
+ * @RouteResource("analytic-keys")
+ */
+class AnalyticKeysController extends RestController implements ClassResourceInterface, SecuredControllerInterface
+{
+    const RESULT_KEY = 'analytic-keys';
+
+    /**
+     * Returns webspace analytic keys by webspace key.
+     *
+     * @param Request $request
+     * @param string $webspaceKey
+     *
+     * @return Response
+     */
+    public function cgetAction(Request $request, $webspaceKey)
+    {
+        $entities = $this->get('sulu_website.analytic_keys.manager')->findAll($webspaceKey);
+
+        $list = new RouteAwareRepresentation(
+            new CollectionRepresentation($entities, self::RESULT_KEY),
+            'cget_webspace_analytic-keys',
+            array_merge($request->request->all(), ['webspaceKey' => $webspaceKey])
+        );
+
+        return $this->handleView($this->view($list, 200));
+    }
+
+    /**
+     * Returns a single analytics key by id.
+     *
+     * @param string $webspaceKey
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function getAction($webspaceKey, $id)
+    {
+        $entity = $this->get('sulu_website.analytic_keys.manager')->find($id);
+
+        return $this->handleView($this->view($entity, 200));
+    }
+
+    /**
+     * Creates a analytics key for given webspace.
+     *
+     * @param Request $request
+     * @param string $webspaceKey
+     *
+     * @return Response
+     */
+    public function postAction(Request $request, $webspaceKey)
+    {
+        $entity = $this->get('sulu_website.analytic_keys.manager')->create($webspaceKey, $request->request->all());
+
+        return $this->handleView($this->view($entity, 200));
+    }
+
+    /**
+     * Updates analytics key with given id.
+     *
+     * @param Request $request
+     * @param string $webspaceKey
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function putAction(Request $request, $webspaceKey, $id)
+    {
+        $entity = $this->get('sulu_website.analytic_keys.manager')->update($id, $request->request->all());
+
+        return $this->handleView($this->view($entity, 200));
+    }
+
+    /**
+     * Removes given analytics key.
+     *
+     * @param string $webspaceKey
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function deleteAction($webspaceKey, $id)
+    {
+        $this->get('sulu_website.analytic_keys.manager')->remove($id);
+
+        return $this->handleView($this->view(null, 204));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSecurityContext()
+    {
+        $request = $this->container->get('request_stack')->getCurrentRequest();
+        $webspaceKey = $request->get('webspaceKey');
+
+        return ContentAdmin::SECURITY_CONTEXT_PREFIX . $webspaceKey;
+    }
+}
---
+++ src/Sulu/Bundle/WebsiteBundle/Entity/AnalyticKeyRepository.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\WebsiteBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Repository for analytic keys.
+ */
+class AnalyticKeyRepository extends EntityRepository
+{
+    /**
+     * Returns list of analytic-keys.
+     *
+     * @param $webspaceKey
+     *
+     * @return AnalyticKey[]
+     */
+    public function findByWebspaceKey($webspaceKey)
+    {
+        $queryBuilder = $this->createQueryBuilder('a')
+            ->addSelect('domains')
+            ->leftJoin('a.domains', 'domains')
+            ->where('a.webspaceKey = :webspaceKey');
+
+        $query = $queryBuilder->getQuery();
+        $query->setParameter('webspaceKey', $webspaceKey);
+
+        return $query->getResult();
+    }
+
+    /**
+     * Returns analytic-key by id.
+     *
+     * @param int $id
+     *
+     * @return AnalyticKey
+     */
+    public function findById($id)
+    {
+        $queryBuilder = $this->createQueryBuilder('a')
+            ->select('domains')
+            ->leftJoin('a.domains', 'domains')
+            ->where('a.id = :id');
+
+        $query = $queryBuilder->getQuery();
+        $query->setParameter('id', $id);
+
+        return $query->getResult();
+    }
+}
