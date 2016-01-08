865116d8f044494bfd746d8e87f4168aa2d451df
bastien <admin@cws-studio.com>
2016-01-08 16:50:41 +0100

    fix styleCi

--- src/Sulu/Bundle/ContentBundle/Document/BasePageDocument.php
+++ src/Sulu/Bundle/ContentBundle/Document/BasePageDocument.php
@@ -59,24 +59,28 @@ class BasePageDocument implements
 {
     /**
      * The name of this node
+     *
      * @var string
      */
     protected $nodeName;
 
     /**
      * Datetime of create document
+     *
      * @var \DateTime
      */
     protected $created;
 
     /**
      * Datetime of update document
+     *
      * @var \DateTime
      */
     protected $changed;
 
     /**
      * Datetime of creator
+     *
      * @var \DateTime
      */
     protected $creator;
@@ -88,18 +92,21 @@ class BasePageDocument implements
 
     /**
      * Document's parent
+     *
      * @var object
      */
     protected $parent;
 
     /**
      * Title of document
+     *
      * @var string
      */
     protected $title;
 
     /**
      * Segment
+     *
      * @var string
      */
     protected $resourceSegment;
@@ -111,102 +118,119 @@ class BasePageDocument implements
 
     /**
      * Type of redirection
+     *
      * @var int
      */
     protected $redirectType;
 
     /**
      * The target of redirection
+     *
      * @var object
      */
     protected $redirectTarget;
 
     /**
      * The External redirect
+     *
      * @var string
      */
     protected $redirectExternal;
 
     /**
      * Workflow Stage currently Test or Published
+     *
      * @var int
      */
     protected $workflowStage;
 
     /**
      * Is Document is published
+     *
      * @var bool
      */
     protected $published;
 
     /**
      * Shadow locale is enabled
+     *
      * @var bool
      */
     protected $shadowLocaleEnabled = false;
 
     /**
      * Shadow locale
+     *
      * @var string
      */
     protected $shadowLocale;
 
     /**
      * Universal Identifier
+     *
      * @var string
      */
     protected $uuid;
 
     /**
      * Document's type of structure ie default, complex, ...
+     *
      * @var string
      */
     protected $structureType;
 
     /**
      * Structure
+     *
      * @var StructureInterface
      */
     protected $structure;
 
     /**
      * Document's locale
+     *
      * @var string
      */
     protected $locale;
 
     /**
      * Document's children
+     *
      * @var ChildrenCollection
      */
     protected $children;
 
     /**
      * Document's path
+     *
      * @var string
      */
     protected $path;
 
     /**
      * Document's extensions ie seo, ...
+     *
      * @var ExtensionContainer
      */
     protected $extensions;
 
     /**
-     * Document's Webspace
+     * Document's webspace name
+     *
      * @var string
      */
     protected $webspaceName;
 
     /**
      * Document's order
+     *
      * @var int
      */
     protected $suluOrder;
 
     /**
      * List of permissions
+     *
      * @var array
      */
     protected $permissions;
