74c9172bb5245634d879436f26b9af1deeadac21
Johannes Wachter <johannes.wachter@massiveart.at>
2016-01-13 12:15:01 +0100

    removed form deprecations

--- src/Sulu/Bundle/ContentBundle/Form/Type/BasePageDocumentType.php
+++ src/Sulu/Bundle/ContentBundle/Form/Type/BasePageDocumentType.php
@@ -11,8 +11,12 @@
 
 namespace Sulu\Bundle\ContentBundle\Form\Type;
 
+use Sulu\Component\Content\Form\Type\DocumentObjectType;
 use Sulu\Component\DocumentManager\DocumentManager;
 use Sulu\Component\PHPCR\SessionManager\SessionManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
@@ -60,20 +64,20 @@ abstract class BasePageDocumentType extends AbstractStructureBehaviorType
     {
         parent::buildForm($builder, $options);
 
-        $builder->add('parent', 'document_object');
-        $builder->add('extensions', 'text', ['property_path' => 'extensionsData']);
-        $builder->add('resourceSegment', 'text');
+        $builder->add('parent', DocumentObjectType::class);
+        $builder->add('extensions', TextType::class, ['property_path' => 'extensionsData']);
+        $builder->add('resourceSegment', TextType::class);
         $builder->add('navigationContexts', 'collection', [
-            'type' => 'text',
+            'type' => TextType::class,
             'allow_add' => true,
             'allow_delete' => true,
         ]);
-        $builder->add('redirectType', 'integer');
-        $builder->add('redirectTarget', 'document_object');
-        $builder->add('redirectExternal', 'text');
-        $builder->add('workflowStage', 'integer');
-        $builder->add('shadowLocaleEnabled', 'checkbox');
-        $builder->add('shadowLocale', 'text'); // TODO: Should be choice of available shadow locales
+        $builder->add('redirectType', IntegerType::class);
+        $builder->add('redirectTarget', DocumentObjectType::class);
+        $builder->add('redirectExternal', TextType::class);
+        $builder->add('workflowStage', IntegerType::class);
+        $builder->add('shadowLocaleEnabled', CheckboxType::class);
+        $builder->add('shadowLocale', TextType::class); // TODO: Should be choice of available shadow locales
         $builder->setAttribute('webspace_key', $options['webspace_key']);
 
         $builder->addEventListener(FormEvents::POST_SUBMIT, [$this, 'postSubmitDocumentParent']);
