31e5eb2e0288a828c995b89c56a7c71a06699ee8
Johannes Wachter <johannes.wachter@massiveart.at>
2016-02-02 09:54:46 +0100

    fixed snippet-content unit tests

--- src/Sulu/Bundle/SnippetBundle/Tests/Unit/Content/SnippetContentTest.php
+++ src/Sulu/Bundle/SnippetBundle/Tests/Unit/Content/SnippetContentTest.php
@@ -14,7 +14,6 @@ use Sulu\Bundle\SnippetBundle\Content\SnippetContent;
 use Sulu\Bundle\SnippetBundle\Snippet\DefaultSnippetManagerInterface;
 use Sulu\Bundle\SnippetBundle\Snippet\SnippetResolverInterface;
 use Sulu\Component\Content\Compat\PropertyInterface;
-use Sulu\Component\Content\Compat\Structure\SnippetBridge;
 use Sulu\Component\Content\Compat\Structure\StructureBridge;
 
 class SnippetContentTest extends \PHPUnit_Framework_TestCase
@@ -22,7 +21,7 @@ class SnippetContentTest extends \PHPUnit_Framework_TestCase
     public function testGetContentData()
     {
         $defaultSnippetManager = $this->prophesize(DefaultSnippetManagerInterface::class);
-        $structureResolver = $this->prophesize(SnippetResolverInterface::class);
+        $snippetResolver = $this->prophesize(SnippetResolverInterface::class);
 
         $property = $this->prophesize(PropertyInterface::class);
         $structure = $this->prophesize(StructureBridge::class);
@@ -32,25 +31,14 @@ class SnippetContentTest extends \PHPUnit_Framework_TestCase
         $structure->getShadowBaseLanguage()->wilLReturn('en');
         $property->getStructure()->willReturn($structure->reveal());
         $property->getValue()->willReturn(['123-123-123']);
+        $property->getParams()->willReturn([]);
 
-        $referenceDe = $this->prophesize(SnippetBridge::class);
-        $referenceEn = $this->prophesize(SnippetBridge::class);
+        $snippetResolver->resolve(['123-123-123'], 'sulu_io', 'de', 'en')
+            ->willReturn(
+                [['content' => ['title' => 'test-1'], 'view' => ['title' => 'test-2', 'template' => 'default']]]
+            );
 
-        $referenceDe->getHasTranslation()->willReturn(false);
-        $referenceEn->getHasTranslation()->willReturn(true);
-
-        $defaultSnippetManager->load('123-123-123', 'sulu_io', 'de')->willReturn($referenceDe->reveal());
-        $defaultSnippetManager->load('123-123-123', 'sulu_io', 'en')->willReturn($referenceEn->reveal());
-
-        $referenceEn->setIsShadow(true)->shouldBeCalled();
-        $referenceEn->setShadowBaseLanguage('en')->shouldBeCalled();
-        $referenceEn->getKey()->willReturn('default');
-
-        $structureResolver->resolve($referenceEn)->willReturn(
-            ['content' => ['title' => 'test-1'], 'view' => ['title' => 'test-2']]
-        );
-
-        $type = new SnippetContent($defaultSnippetManager->reveal(), $structureResolver->reveal(), false, '');
+        $type = new SnippetContent($defaultSnippetManager->reveal(), $snippetResolver->reveal(), false, '');
 
         $result = $type->getContentData($property->reveal());
 
@@ -60,7 +48,7 @@ class SnippetContentTest extends \PHPUnit_Framework_TestCase
     public function testGetViewData()
     {
         $defaultSnippetManager = $this->prophesize(DefaultSnippetManagerInterface::class);
-        $structureResolver = $this->prophesize(SnippetResolverInterface::class);
+        $snippetResolver = $this->prophesize(SnippetResolverInterface::class);
 
         $property = $this->prophesize(PropertyInterface::class);
         $structure = $this->prophesize(StructureBridge::class);
@@ -70,25 +58,14 @@ class SnippetContentTest extends \PHPUnit_Framework_TestCase
         $structure->getShadowBaseLanguage()->wilLReturn('en');
         $property->getStructure()->willReturn($structure->reveal());
         $property->getValue()->willReturn(['123-123-123']);
+        $property->getParams()->willReturn([]);
 
-        $referenceDe = $this->prophesize(SnippetBridge::class);
-        $referenceEn = $this->prophesize(SnippetBridge::class);
+        $snippetResolver->resolve(['123-123-123'], 'sulu_io', 'de', 'en')
+            ->willReturn(
+                [['content' => ['title' => 'test-1'], 'view' => ['title' => 'test-2', 'template' => 'default']]]
+            );
 
-        $referenceDe->getHasTranslation()->willReturn(false);
-        $referenceEn->getHasTranslation()->willReturn(true);
-
-        $defaultSnippetManager->load('123-123-123', 'sulu_io', 'de')->willReturn($referenceDe->reveal());
-        $defaultSnippetManager->load('123-123-123', 'sulu_io', 'en')->willReturn($referenceEn->reveal());
-
-        $referenceEn->setIsShadow(true)->shouldBeCalled();
-        $referenceEn->setShadowBaseLanguage('en')->shouldBeCalled();
-        $referenceEn->getKey()->willReturn('default');
-
-        $structureResolver->resolve($referenceEn)->willReturn(
-            ['content' => ['title' => 'test-1'], 'view' => ['title' => 'test-2']]
-        );
-
-        $type = new SnippetContent($defaultSnippetManager->reveal(), $structureResolver->reveal(), false, '');
+        $type = new SnippetContent($defaultSnippetManager->reveal(), $snippetResolver->reveal(), false, '');
 
         $result = $type->getViewData($property->reveal());
 
