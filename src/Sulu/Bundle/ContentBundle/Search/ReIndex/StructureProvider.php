70eb2173d2da251b27164938346e1819c9a42ea6
dantleech <dan.t.leech@gmail.com>
2016-02-19 08:14:11 +0000

    Adding provider

---
+++ src/Sulu/Bundle/ContentBundle/Search/ReIndex/StructureProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sulu\Bundle\ContentBundle\Search\ReIndex;
+
+class StructureProvider implements ReIndexProviderInterface
+{
+    public function provide($classFqn, $offset, $maxResults)
+    {
+    }
+
+    public function getCount($classFqn)
+    {
+    }
+
+    public function getClassFqns()
+    {
+    }
+}
