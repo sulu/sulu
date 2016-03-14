1700c6d974baebef6c7b778260bedb0f65075019
Daniel Rotter <daniel.rotter@gmail.com>
2016-03-14 16:39:34 +0100

    fixed permissions for page REST request

--- src/Sulu/Component/Content/Tests/Unit/Document/Subscriber/SecuritySubscriberTest.php
+++ src/Sulu/Component/Content/Tests/Unit/Document/Subscriber/SecuritySubscriberTest.php
@@ -11,9 +11,10 @@
 
 namespace Sulu\Component\Content\Tests\Unit\Document\Subscriber;
 
+use PHPCR\NodeInterface;
+use PHPCR\PropertyInterface;
 use Sulu\Component\Content\Document\Behavior\SecurityBehavior;
 use Sulu\Component\Content\Document\Subscriber\SecuritySubscriber;
-use Sulu\Component\DocumentManager\Event\PersistEvent;
 
 class SecuritySubscriberTest extends SubscriberTestCase
 {
@@ -26,7 +27,7 @@ class SecuritySubscriberTest extends SubscriberTestCase
     {
         parent::setUp();
 
-        $this->subscriber = new SecuritySubscriber();
+        $this->subscriber = new SecuritySubscriber(['view' => 64, 'add' => 32, 'edit' => 16, 'delete' => 8]);
     }
 
     public function testPersist()
@@ -42,5 +43,44 @@ class SecuritySubscriberTest extends SubscriberTestCase
         $this->subscriber->handlePersist($this->persistEvent->reveal());
 
         $this->node->setProperty('sec:role-1', ['view', 'add', 'edit'])->shouldBeCalled();
+    }
+
+    public function testHydrate()
+    {
+        /** @var SecurityBehavior $document */
+        $document = $this->prophesize(SecurityBehavior::class);
+        $node = $this->prophesize(NodeInterface::class);
+
+        /** @var PropertyInterface $roleProperty1 */
+        $roleProperty1 = $this->prophesize(PropertyInterface::class);
+        $roleProperty1->getName()->willReturn('sec:role-1');
+        $roleProperty1->getValue()->willReturn(['view', 'add', 'edit']);
+
+        /** @var PropertyInterface $roleProperty2 */
+        $roleProperty2 = $this->prophesize(PropertyInterface::class);
+        $roleProperty2->getName()->willReturn('sec:role-2');
+        $roleProperty2->getValue()->willReturn(['view', 'edit']);
+
+        $node->getProperties('sec:*')->willReturn([$roleProperty1->reveal(), $roleProperty2->reveal()]);
+
+        $this->hydrateEvent->getDocument()->willReturn($document);
+        $this->hydrateEvent->getNode()->willReturn($node);
+
+        $this->subscriber->handleHydrate($this->hydrateEvent->reveal());
+
+        $document->setPermissions([
+            1 => [
+                'view' => true,
+                'add' => true,
+                'edit' => true,
+                'delete' => false,
+            ],
+            2 => [
+                'view' => true,
+                'add' => false,
+                'edit' => true,
+                'delete' => false,
+            ],
+        ])->shouldBeCalled();
     }
 }
