664f034de62e0d90ee4cd38fee8f6b7494aabd23
Johannes Wachter <johannes.wachter@massiveart.at>
2016-03-31 11:34:26 +0200

    extracted collaboration start into extension and added it to snippet form

--- src/Sulu/Bundle/SnippetBundle/Resources/public/js/components/snippet/form/main.js
+++ src/Sulu/Bundle/SnippetBundle/Resources/public/js/components/snippet/form/main.js
@@ -102,6 +102,13 @@ define([
         };
     };
 
+    SnippetForm.prototype.collaboration = function() {
+        return {
+            id: this.options.id,
+            type: 'snippet'
+        };
+    };
+
     SnippetForm.prototype.initialize = function() {
         this.type = (!!this.options.id ? 'edit' : 'add');
 
