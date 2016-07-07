f204f0fac0afecd8b9340606ce63c946fcc574d8
Johannes Wachter <johannes.wachter@massiveart.at>
2016-07-07 14:47:51 +0200

    add autocomplete form validation type with husky update

--- src/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky.js
+++ src/Sulu/Bundle/AdminBundle/Resources/public/js/vendor/husky/husky.js
@@ -27599,6 +27599,77 @@ define('type/husky-select',[
 });
 
 /*
+ * This file is part of the Husky Validation.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+define('type/husky-auto-complete',[
+    'type/default',
+    'form/util'
+], function(Default) {
+
+    'use strict';
+
+    return function($el, options) {
+        var defaults = {
+                id: 'id',
+                name: 'name',
+                required: false
+            },
+
+            typeInterface = {
+                setValue: function(data) {
+                    var $input = $el.find('input');
+
+                    if (!$input.length) {
+                        $el.data('value', data);
+                        $el.attr('data-value', JSON.stringify(data));
+                    }
+
+                    $input.data('id', data[this.options.id]);
+                    $input.attr('data-id', data[this.options.id]);
+                    $input.typeahead('val', data[this.options.name]);
+                },
+
+                getValue: function() {
+                    var $input = $el.find('input');
+
+                    if (!$input.data('id') || $input.data('id') === 'null') {
+                        return;
+                    }
+
+                    var value = {};
+
+                    value[this.options.id] = $input.data('id');
+                    value[this.options.name] = $input.val();
+
+                    return value;
+                },
+
+                needsValidation: function() {
+                    var val = this.getValue();
+                    return !!val;
+                },
+
+                validate: function() {
+                    var value = this.getValue();
+                    if (typeof value === 'object' && value.hasOwnProperty('id')) {
+                        return !!value.id;
+                    } else {
+                        return value !== '' && typeof value !== 'undefined';
+                    }
+                }
+            };
+
+        return new Default($el, defaults, options, 'husky-auto-complete', typeInterface);
+    };
+});
+
+/*
  * This file is part of the Sulu CMS.
  *
  * (c) MASSIVE ART WebServices GmbH
