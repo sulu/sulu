deee305eca09fc3df37dad2eee8afd94112e61f5
Johannes Wachter <johannes.wachter@massiveart.com>
2016-09-21 07:54:14 +0200

    fixed use in contact-interface

--- src/Sulu/Component/Contact/Model/ContactInterface.php
+++ src/Sulu/Component/Contact/Model/ContactInterface.php
@@ -23,6 +23,7 @@ use Sulu\Bundle\ContactBundle\Entity\Fax;
 use Sulu\Bundle\ContactBundle\Entity\Note;
 use Sulu\Bundle\ContactBundle\Entity\Phone;
 use Sulu\Bundle\ContactBundle\Entity\Url;
+use Sulu\Bundle\MediaBundle\Entity\Media;
 use Sulu\Bundle\MediaBundle\Entity\MediaInterface;
 use Sulu\Bundle\TagBundle\Entity\Tag;
 
