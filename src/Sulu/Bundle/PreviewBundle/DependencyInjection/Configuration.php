b1a8714e4d9986c4e2ed5076c0e40aebd955fd0e
Thomas Schedler <thomas@chirimoya.at>
2016-12-06 15:27:44 +0100

    added configuration for preview & websocket cache

--- src/Sulu/Bundle/PreviewBundle/DependencyInjection/Configuration.php
+++ src/Sulu/Bundle/PreviewBundle/DependencyInjection/Configuration.php
@@ -45,9 +45,79 @@ class Configuration implements ConfigurationInterface
                     ->defaultValue(500)
                     ->info('Used for the delayed send of changes')
                 ->end()
+                ->arrayNode('cache')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('type')->defaultValue('file_system')->end()
+                        ->scalarNode('namespace')->defaultNull()->end()
+                        ->append($this->addBasicProviderNode('apc'))
+                        ->append($this->addBasicProviderNode('apcu'))
+                        ->append($this->addBasicProviderNode('array'))
+                        ->append($this->addFileSystemNode())
+                        ->append($this->addRedisNode())
+                    ->end()
+                ->end()
             ->end()
         ->end();
 
         return $treeBuilder;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\NodeDefinition
+     */
+    private function addBasicProviderNode($name)
+    {
+        $builder = new TreeBuilder();
+        $node = $builder->root($name);
+
+        return $node;
+    }
+
+    /**
+     * Build file_system node configuration definition.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\NodeDefinition
+     */
+    private function addFileSystemNode()
+    {
+        $builder = new TreeBuilder();
+        $node = $builder->root('file_system');
+
+        $node
+            ->addDefaultsIfNotSet()
+            ->children()
+                ->scalarNode('directory')->defaultValue('%sulu.cache_dir%/preview')->end()
+                ->scalarNode('extension')->defaultNull()->end()
+                ->integerNode('umask')->defaultValue(0002)->end()
+            ->end();
+
+        return $node;
+    }
+
+    /**
+     * Build redis node configuration definition.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\NodeDefinition
+     */
+    private function addRedisNode()
+    {
+        $builder = new TreeBuilder();
+        $node = $builder->root('redis');
+
+        $node
+            ->addDefaultsIfNotSet()
+            ->children()
+                ->scalarNode('connection_id')->defaultNull()->end()
+                ->scalarNode('host')->defaultValue('127.0.0.1')->end()
+                ->scalarNode('port')->defaultValue('6379')->end()
+                ->scalarNode('password')->defaultNull()->end()
+                ->scalarNode('timeout')->defaultNull()->end()
+                ->scalarNode('database')->defaultNull()->end()
+            ->end();
+
+        return $node;
     }
 }
