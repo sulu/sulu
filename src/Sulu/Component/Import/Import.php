0e68cfafc81bbb68e885d81d5c30b70d26f91978
dominikmatt <mail@matt-dominik.at>
2016-12-19 12:14:51 +0100

    added snippet import

---
+++ src/Sulu/Component/Import/Import.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Component\Import;
+
+use Sulu\Component\Import\Exception\FormatImporterNotFoundException;
+use Sulu\Component\Content\Compat\PropertyInterface;
+use PHPCR\NodeInterface;
+use Sulu\Component\Content\Compat\StructureInterface;
+use Sulu\Component\Import\Manager\ImportManager;
+use Sulu\Component\Import\Manager\ImportManagerInterface;
+
+class Import {
+    /**
+     * @var WebspaceFormatImportInterface[]
+     */
+    protected $formatFilePaths = [];
+
+    /**
+     * @var ImportManagerInterface
+     */
+    protected $importManager;
+
+    /**
+     * @var LegacyPropertyFactory
+     */
+    protected $legacyPropertyFactory;
+
+    /**
+     * @var array
+     */
+    protected $exceptionStore = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function add($service, $format)
+    {
+        $this->formatFilePaths[$format] = $service;
+    }
+
+    /**
+     * Returns the correct parser like XLIFF1.2.
+     *
+     * @param $format
+     *
+     * @return WebspaceFormatImportInterface
+     *
+     * @throws WebspaceFormatImporterNotFoundException
+     */
+    protected function getParser($format)
+    {
+        if (!isset($this->formatFilePaths[$format])) {
+            throw new FormatImporterNotFoundException($format);
+        }
+
+        return $this->formatFilePaths[$format];
+    }
+
+    /**
+     * Prepare document-property and import them.
+     *
+     * @param PropertyInterface $property
+     * @param NodeInterface $node
+     * @param string $value
+     * @param string $webspaceKey
+     * @param string $locale
+     * @param string $format
+     */
+    protected function importProperty(
+        PropertyInterface $property,
+        NodeInterface $node,
+        StructureInterface $structure,
+        $value,
+        $webspaceKey,
+        $locale,
+        $format
+    ) {
+        $contentType = $property->getContentTypeName();
+
+        if (!$this->importManager->hasImport($contentType, $format)) {
+            return;
+        }
+
+        $translateProperty = $this->legacyPropertyFactory->createTranslatedProperty($property, $locale, $structure);
+        $this->importManager->import($contentType, $node, $translateProperty, $value, null, $webspaceKey, $locale);
+    }
+
+    /**
+     * Add a specific import exception/warning to the exception store.
+     * This messages will print after the import is done.
+     *
+     * @param string $msg
+     * @param string $type
+     */
+    protected function addException($msg = null, $type = 'info')
+    {
+        if (null === $msg) {
+            return;
+        }
+
+        if (!isset($this->exceptionStore[$type])) {
+            $this->exceptionStore[$type] = [];
+        }
+
+        $this->exceptionStore[$type][] = $msg;
+    }
+}
---
+++ src/Sulu/Component/Snippet/Import/SnippetImport.php
@@ -0,0 +1,246 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Component\Snippet\Import;
+
+use Psr\Log\LoggerInterface;
+use Sulu\Bundle\SnippetBundle\Document\SnippetDocument;
+use Sulu\Component\Import\Import;
+use Sulu\Component\Import\Format\FormatImportInterface;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\NullOutput;
+use Sulu\Component\Content\Compat\Structure;
+use Sulu\Component\DocumentManager\DocumentManager;
+use Sulu\Component\Content\Compat\StructureManagerInterface;
+use Sulu\Component\DocumentManager\DocumentRegistry;
+use Sulu\Component\Import\Manager\ImportManagerInterface;
+use Sulu\Component\Content\Compat\Structure\LegacyPropertyFactory;
+use Sulu\Component\DocumentManager\Exception\DocumentManagerException;
+
+class SnippetImport extends Import
+{
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var DocumentManager
+     */
+    private $documentManager;
+
+    /**
+     * @var StructureManagerInterface
+     */
+    protected $structureManager;
+
+    /**
+     * @var DocumentRegistry
+     */
+    protected $documentRegistry;
+
+    /**
+     * @var FormatImportInterface[]
+     */
+    protected $formatFilePaths = [];
+
+    /**
+     * SnippetImport constructor.
+     *
+     * @param DocumentManager $documentManager
+     * @param StructureManagerInterface $structureManager
+     * @param DocumentRegistry $documentRegistry
+     * @param ImportManagerInterface $importManager
+     * @param LegacyPropertyFactory $legacyPropertyFactory
+     * @param LoggerInterface $logger
+     * @param FormatImportInterface $xliff12
+     */
+    public function __construct(
+        DocumentManager $documentManager,
+        StructureManagerInterface $structureManager,
+        DocumentRegistry $documentRegistry,
+        ImportManagerInterface $importManager,
+        LegacyPropertyFactory $legacyPropertyFactory,
+        LoggerInterface $logger,
+        FormatImportInterface $xliff12
+    ) {
+        $this->documentManager = $documentManager;
+        $this->documentRegistry = $documentRegistry;
+        $this->structureManager = $structureManager;
+        $this->importManager = $importManager; // only import/import
+        $this->legacyPropertyFactory = $legacyPropertyFactory;
+         // only import/import
+        $this->logger = $logger;
+        $this->add($xliff12, '1.2.xliff');
+    }
+
+    /**
+     * Import Snippet by given XLIFF-File
+     *
+     * @param $locale
+     * @param $filePath
+     * @param $output
+     * @param $format
+     *
+     * @return \stdClass
+     */
+    public function import(
+        $locale,
+        $filePath,
+        $output,
+        $format
+    ) {
+        $parsedDataList = $this->getParser($format)->parse($filePath, $locale);
+        $failedImports = [];
+        $importedCounter = 0;
+        $successCounter = 0;
+
+        if (null === $output) {
+            $output = new NullOutput();
+        }
+
+        $progress = new ProgressBar($output, count($parsedDataList));
+        $progress->setFormat(' %current%/%max% [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %memory:6s%');
+        $progress->start();
+
+        foreach ($parsedDataList as $parsedData) {
+            ++$importedCounter;
+
+            if (!$this->importDocument($parsedData, $locale, $format)) {
+                $failedImports[] = $parsedData;
+            } else {
+                ++$successCounter;
+            }
+
+            $this->logger->info(sprintf('Document %s/%s', $importedCounter, count($parsedDataList)));
+
+            $progress->advance();
+        }
+
+
+        $progress->finish();
+
+        $return = new \stdClass();
+        $return->count = $importedCounter;
+        $return->fails = count($failedImports);
+        $return->successes = $successCounter;
+        $return->failed = $failedImports;
+        $return->exceptionStore = $this->exceptionStore;
+
+        return $return;
+    }
+
+    /**
+     * Import document by locale into given webspace.
+     *
+     * @param array $parsedData
+     * @param string $locale
+     * @param string $format
+     *
+     * @return bool
+     */
+    protected function importDocument(array $parsedData, $locale, $format)
+    {
+        try {
+            $uuid = $parsedData['uuid'];
+            $data = $parsedData['data'];
+            $documentType = Structure::TYPE_SNIPPET;
+
+            /** @var SnippetDocument $document */
+            $document = $this->documentManager->find(
+                $uuid,
+                $locale,
+                [
+                    'type' => $documentType,
+                    'load_ghost_content' => false,
+                ]
+            );
+
+            if (!$document instanceof SnippetDocument) {
+                throw new \Exception(
+                    sprintf('Document(%s) is not an instanecof SnippetDocument', $uuid)
+                );
+            }
+
+            if (!$this->setDocumentData($document, $locale, $format, $data)) {
+                return false;
+            }
+
+            // set required data
+            $document->setTitle($this->getParser($format)->getPropertyData('title', $data));
+
+            // save document
+            $this->documentManager->persist($document, $locale);
+            $this->documentManager->publish($document, $locale);
+            $this->documentManager->flush();
+            $this->documentRegistry->clear(); // FIXME else it failed on multiple page import
+
+            return true;
+        } catch (\Exception $e) {
+            if ($e instanceof DocumentManagerException) {
+                return false;
+            }
+
+            $this->logger->error(
+                sprintf(
+                    '<info>%s</info>%s: <error>%s</error>%s',
+                    $uuid,
+                    PHP_EOL . get_class($e),
+                    $e->getMessage(),
+                    PHP_EOL . $e->getTraceAsString()
+                )
+            );
+
+            $this->documentManager->flush();
+            $this->documentManager->clear();
+        }
+
+        return false;
+    }
+
+    /**
+     * Loops all documents and imports all properties of the documents.
+     *
+     * @param SnippetDocument $document
+     * @param string $locale
+     * @param string $format
+     * @param Array $data
+     */
+    protected function setDocumentData($document, $locale, $format, $data)
+    {
+        $structure = $this->structureManager->getStructure($document->getStructureType(), Structure::TYPE_SNIPPET);
+        $properties = $structure->getProperties(true);
+        $node = $this->documentRegistry->getNodeForDocument($document);
+        $node->setProperty(sprintf('i18n:%s-template', $locale), $document->getStructureType());
+        $state = $this->getParser($format)->getPropertyData('state', $data, null, null, 2);
+        $node->setProperty(sprintf('i18n:%s-state', $locale), $state);
+
+        // Check title is set in xliff-file.
+        if ($this->getParser($format)->getPropertyData('title', $data) === '') {
+            $this->addException(sprintf('Snippet(%s) has not set any title', $document->getUuid()), 'ignore');
+
+            return false;
+        }
+
+        // Import document-property.
+        foreach ($properties as $property) {
+            $value = $this->getParser($format)->getPropertyData(
+                $property->getName(),
+                $data,
+                $property->getContentTypeName()
+            );
+
+            $this->importProperty($property, $node, $structure, $value, null, $locale, $format);
+        }
+
+        return true;
+    }
+}
