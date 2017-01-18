13dc24dc138b050f8c1d829de20e0ba1a8f76fb7
Daniel Rotter <daniel.rotter@gmail.com>
2017-01-18 11:19:30 +0100

    prefixed 170x170 image format

--- src/Sulu/Bundle/MediaBundle/DependencyInjection/Configuration.php
+++ src/Sulu/Bundle/MediaBundle/DependencyInjection/Configuration.php
@@ -61,7 +61,7 @@ class Configuration implements ConfigurationInterface
             ->arrayNode('search')
                 ->addDefaultsIfNotSet()
                 ->children()
-                    ->scalarNode('default_image_format')->defaultValue('170x170')->end()
+                    ->scalarNode('default_image_format')->defaultValue('sulu-170x170')->end()
                     ->booleanNode('enabled')->info(
                         'Enable integration with SuluMediaBundle'
                     )->defaultValue(false)->end()
