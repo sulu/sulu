48605b283d6fbc0228190091f1d605bd25752c40
Ollie Harridge <ollie@sabbaticalsoul.co.uk>
2017-02-07 15:09:22 +0000

    Update XmlLegacyLoader.php

--- src/Sulu/Component/Content/Metadata/Loader/XmlLegacyLoader.php
+++ src/Sulu/Component/Content/Metadata/Loader/XmlLegacyLoader.php
@@ -95,6 +95,9 @@ class XmlLegacyLoader implements LoaderInterface
 
         $schemaPath = __DIR__ . static::SCHEME_PATH;
 
+        $cwd = getcwd();
+        @chdir(dirname($resource));
+        
         // read file
         $xmlDocument = XmlUtils::loadFile(
             $resource,
@@ -105,6 +108,8 @@ class XmlLegacyLoader implements LoaderInterface
                 return @$dom->schemaValidate($schemaPath);
             }
         );
+        
+        @chdir($cwd);
 
         // generate xpath for file
         $xpath = new \DOMXPath($xmlDocument);
