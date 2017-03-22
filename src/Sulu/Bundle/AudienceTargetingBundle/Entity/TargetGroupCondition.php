79cfe4e10f5ee00f721d98b59f614b2f7391e049
TurboEle <elias.hiller@massiveart.com>
2017-03-22 15:28:13 +0100

    Implemented target group management in sulu admin (#3215)
    * Implemented target group entities
    
    * Implemented target group mapper service and api controller as well as
    UI.
    
    * Cleanups
    
    * Further cleanups

---
+++ src/Sulu/Bundle/AudienceTargetingBundle/Entity/TargetGroupCondition.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\AudienceTargetingBundle\Entity;
+
+/**
+ * Entity that holds conditions for target group rules.
+ */
+class TargetGroupCondition implements TargetGroupConditionInterface
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @var string
+     */
+    private $condition;
+
+    /**
+     * @var TargetGroupRuleInterface
+     */
+    private $rule;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCondition()
+    {
+        return $this->condition;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCondition($condition)
+    {
+        $this->condition = $condition;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRule()
+    {
+        return $this->rule;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setRule(TargetGroupRuleInterface $rule)
+    {
+        $this->rule = $rule;
+
+        return $this;
+    }
+}
---
+++ src/Sulu/Bundle/AudienceTargetingBundle/Entity/TargetGroupWebspaceRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\AudienceTargetingBundle\Entity;
+
+use Sulu\Component\Persistence\Repository\ORM\EntityRepository;
+
+/**
+ * Repository class for target groups webspaces.
+ */
+class TargetGroupWebspaceRepository extends EntityRepository implements TargetGroupWebspaceRepositoryInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function findOrCreate(TargetGroupInterface $targetGroup, $webspaceKey)
+    {
+        $targetGroupWebspace = null;
+
+        if ($targetGroup->getId()) {
+            $targetGroupWebspace = $this->findOneBy(
+                [
+                    'targetGroup' => $targetGroup,
+                    'webspaceKey' => $webspaceKey,
+                ]
+            );
+        }
+
+        if (!$targetGroupWebspace) {
+            $targetGroupWebspace = $this->createNew();
+        }
+
+        return $targetGroupWebspace;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove(TargetGroupWebspaceInterface $targetGroupWebspace)
+    {
+        $this->getEntityManager()->remove($targetGroupWebspace);
+    }
+}
