3e65bb66e60a4bd53bff9084916a91348c8e8df7
TurboEle <elias.hiller@massiveart.com>
2017-03-22 15:28:13 +0100

    Implemented target group management in sulu admin (#3215)
    * Implemented target group entities
    
    * Implemented target group mapper service and api controller as well as
    UI.
    
    * Cleanups
    
    * Further cleanups

---
+++ src/Sulu/Bundle/AudienceTargetingBundle/Admin/TargetGroupContentNavigationProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\AudienceTargetingBundle\Admin;
+
+use Sulu\Bundle\AdminBundle\Navigation\ContentNavigationItem;
+use Sulu\Bundle\AdminBundle\Navigation\ContentNavigationProviderInterface;
+
+/**
+ * Provides basic-tabs for target group edit page.
+ */
+class TargetGroupContentNavigationProvider implements ContentNavigationProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getNavigationItems(array $options = [])
+    {
+        $details = new ContentNavigationItem('public.details');
+        $details->setAction('details');
+        $details->setPosition(10);
+        $details->setComponent('target-groups/edit/details@suluaudiencetargeting');
+
+        return [$details];
+    }
+}
---
+++ src/Sulu/Bundle/AudienceTargetingBundle/Entity/TargetGroupRuleInterface.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\AudienceTargetingBundle\Entity;
+
+use Doctrine\Common\Collections\Collection;
+
+/**
+ * Interface for target group rule entity.
+ */
+interface TargetGroupRuleInterface
+{
+    /**
+     * @return int
+     */
+    public function getId();
+
+    /**
+     * @return string
+     */
+    public function getTitle();
+
+    /**
+     * @param string $title
+     *
+     * @return $this
+     */
+    public function setTitle($title);
+
+    /**
+     * @return int
+     */
+    public function getFrequency();
+
+    /**
+     * @param int $frequency
+     *
+     * @return $this
+     */
+    public function setFrequency($frequency);
+
+    /**
+     * @return TargetGroupInterface
+     */
+    public function getTargetGroup();
+
+    /**
+     * @param TargetGroupInterface $targetGroup
+     *
+     * @return $this
+     */
+    public function setTargetGroup(TargetGroupInterface $targetGroup);
+
+    /**
+     * @return Collection
+     */
+    public function getConditions();
+
+    /**
+     * @param TargetGroupConditionInterface $condition
+     *
+     * @return $this
+     */
+    public function addCondition(TargetGroupConditionInterface $condition);
+
+    /**
+     * @param TargetGroupConditionInterface $condition
+     *
+     * @return $this
+     */
+    public function removeCondition(TargetGroupConditionInterface $condition);
+}
