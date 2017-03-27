e6a7c61379f87b6bd036ed2e0716349fdab37ac4
Johannes Wachter <johannes.wachter@massiveart.com>
2017-03-27 08:59:09 +0200

    used composer lock to read sulu version (#3278)

---
+++ src/Sulu/Component/Util/Tests/Unit/SuluVersionPassTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Component\Util\Tests\Unit;
+
+use Sulu\Component\Util\SuluVersionPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class SuluVersionPassTest extends \PHPUnit_Framework_TestCase
+{
+    public function testProcess()
+    {
+        $versionPass = new SuluVersionPass();
+        $container = $this->prophesize(ContainerBuilder::class);
+        $container->getParameter('kernel.root_dir')->willReturn(dirname(__DIR__) . '/Resources/VersionPass/app');
+
+        $container->setParameter('sulu.version', '1.5.2')->shouldBeCalled();
+        $container->setParameter('app.version', '1.2.3')->shouldBeCalled();
+
+        $versionPass->process($container->reveal());
+    }
+}
