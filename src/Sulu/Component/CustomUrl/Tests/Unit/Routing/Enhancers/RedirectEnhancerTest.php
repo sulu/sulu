fbced11258a56f8be77e54de8ea3d211c2915226
Johannes Wachter <johannes.wachter@massiveart.com>
2017-05-15 08:13:15 +0200

    added redirect parameter of custom-url

--- src/Sulu/Component/CustomUrl/Tests/Unit/Routing/Enhancers/RedirectEnhancerTest.php
+++ src/Sulu/Component/CustomUrl/Tests/Unit/Routing/Enhancers/RedirectEnhancerTest.php
@@ -60,4 +60,47 @@ class RedirectEnhancerTest extends \PHPUnit_Framework_TestCase
             $defaults
         );
     }
+
+    public function testEnhanceWithQueryString()
+    {
+        $request = $this->prophesize(Request::class);
+        $webspace = $this->prophesize(Webspace::class);
+        $webspace->getKey()->willReturn('sulu_io');
+
+        $customUrl = $this->prophesize(CustomUrlDocument::class);
+        $customUrl->isRedirect()->willReturn(true);
+        $customUrl->getTargetLocale()->willReturn('de');
+
+        $target = $this->prophesize(PageDocument::class);
+        $target->getResourceSegment()->willReturn('/test');
+        $customUrl->getTargetDocument()->willReturn($target);
+
+        $webspaceManager = $this->prophesize(WebspaceManagerInterface::class);
+        $webspaceManager->findUrlByResourceLocator(
+            '/test',
+            'prod',
+            'de',
+            'sulu_io'
+        )->willReturn('sulu.io/test');
+
+        $request->getQueryString()->willReturn('param=1');
+
+        $enhancer = new RedirectEnhancer($webspaceManager->reveal());
+
+        $defaults = $enhancer->enhance(
+            ['_custom_url' => $customUrl->reveal(), '_webspace' => $webspace->reveal(), '_environment' => 'prod'],
+            $request->reveal()
+        );
+
+        $this->assertEquals(
+            [
+                '_custom_url' => $customUrl->reveal(),
+                '_webspace' => $webspace->reveal(),
+                '_environment' => 'prod',
+                '_controller' => 'SuluWebsiteBundle:Redirect:redirect',
+                'url' => 'sulu.io/test?param=1',
+            ],
+            $defaults
+        );
+    }
 }
