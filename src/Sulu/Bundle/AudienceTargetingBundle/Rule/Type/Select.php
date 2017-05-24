e011d763e5eb8a48ae6e6a2aff4a8ca6389d980f
Daniel Rotter <daniel.rotter@gmail.com>
2017-05-24 09:48:04 +0200

    added BrowserRule

---
+++ src/Sulu/Bundle/AudienceTargetingBundle/Rule/Type/Select.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\AudienceTargetingBundle\Rule\Type;
+
+class Select implements RuleTypeInterface
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string[]
+     */
+    private $options;
+
+    /**
+     * @param string $name
+     */
+    public function __construct($name, $options)
+    {
+        $this->name = $name;
+        $this->options = $options;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTemplate()
+    {
+        return '<div class="grid-12">
+                <div data-condition-name="' . $this->name . '"
+                    data-rule-type="select"
+                    data-aura-component="select@husky"
+                    data-aura-data=\'' . json_encode($this->options) . '\'
+                    ></div>
+            </div>';
+    }
+}
