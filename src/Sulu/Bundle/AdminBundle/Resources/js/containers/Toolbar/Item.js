f0ed30ad01f0c5a93bcdef10880df1dec9651c3c
Daniel Rotter <daniel.rotter@gmail.com>
2017-07-17 09:16:32 +0200

    use disabled attribute instead of separate class (#3445)

--- src/Sulu/Bundle/AdminBundle/Resources/js/containers/Toolbar/Item.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/containers/Toolbar/Item.js
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import classNames from 'classnames';
 import type {Item as ItemType} from './types';
 import Icon from '../../components/Icon';
 import itemStyles from './item.scss';
@@ -19,13 +18,8 @@ export default class Item extends React.PureComponent {
     };
 
     render() {
-        const liClassNames = classNames({
-            [`${itemStyles['item']}`]: true,
-            [`${itemStyles['item-disabled']}`]: !this.props.enabled,
-        });
-
         return (
-            <button className={liClassNames} onClick={this.handleClick}>
+            <button className={itemStyles.item} disabled={!this.props.enabled} onClick={this.handleClick}>
                 <Icon className={itemStyles.icon} name={this.props.icon} />
                 <span className={itemStyles.title}>{this.props.title}</span>
             </button>
