a4a7c7501ab94c185699224bb15229b3c9c1327d
Daniel Rotter <daniel.rotter@gmail.com>
2017-07-18 14:28:19 +0200

    log a warning when translation is missing (#3449)

--- src/Sulu/Bundle/AdminBundle/Resources/js/services/Translator/tests/Translator.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/services/Translator/tests/Translator.test.js
@@ -1,5 +1,10 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
+import * as log from 'loglevel';
 import {setTranslations, clearTranslations, translate} from '../Translator';
+
+jest.mock('loglevel', () => ({
+    warn: jest.fn(),
+}));
 
 beforeEach(() => {
     clearTranslations();
@@ -12,6 +17,7 @@ test('Translator should translate translations', () => {
     expect(translate('delete')).toBe('Delete');
 });
 
-test('Translator should return key when translating non-existing keys', () => {
+test('Translator should return key when translating non-existing keys and log a warning', () => {
     expect(translate('not-existing')).toBe('not-existing');
+    expect(log.warn).toBeCalled();
 });
