96269019ec7f68bffe4d559f9a2b611dfb827cf9
L91 <alex.schranz91@gmail.com>
2017-08-02 14:20:15 +0200

    Fixed problem textarea is interpreted as html code (#3465)
    * fixed problem textarea is interpreted as html code
    
    * fix interpretion of code in texteditor

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@ CHANGELOG for Sulu
 ==================
 
 * dev-master
+    * HOTFIX      #3465 [ContentBundle]           Fixed interpretion of code in block preview
     * ENHANCEMENT #3464 [ContentBundle]           Added additional-slides configuration for teaser-provider
     
 * 1.6.2 (2017-07-31)
--- src/Sulu/Bundle/ContentBundle/Resources/public/js/validation/types/block.js
+++ src/Sulu/Bundle/ContentBundle/Resources/public/js/validation/types/block.js
@@ -161,14 +161,14 @@ define([
                     text = $('<div/>').append($field.data('element').getValue()).text();
 
                     if (!!text) {
-                        $block.find('.collapsed-container .text').html(text);
+                        $block.find('.collapsed-container .text').text(text);
                         return true;
                     }
                 }
 
                 if ($field.is('textarea')) {
                     if (!!$field.data('element').getValue()) {
-                        $block.find('.collapsed-container .text').html($field.data('element').getValue());
+                        $block.find('.collapsed-container .text').text($field.data('element').getValue());
                         return true;
                     }
                 }
