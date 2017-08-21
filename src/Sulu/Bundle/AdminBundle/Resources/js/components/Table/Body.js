2b3ed858d9f9ec203b29fad3eb609b313ffb1616
3rfan <erfan.ebrahimnia@gmail.com>
2017-08-21 11:17:34 +0200

    removed classname prop

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Table/Body.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Table/Body.js
@@ -6,19 +6,16 @@ import Row from './Row';
 type Props = {
     /** Child nodes of the table body */
     children: ChildrenArray<Element<typeof Row>>,
-    /** CSS classes to apply custom styles */
-    className?: string,
 };
 
 export default class Body extends React.PureComponent<Props> {
     render() {
         const {
             children,
-            className,
         } = this.props;
 
         return (
-            <tbody className={className}>
+            <tbody>
                 {children}
             </tbody>
         );
