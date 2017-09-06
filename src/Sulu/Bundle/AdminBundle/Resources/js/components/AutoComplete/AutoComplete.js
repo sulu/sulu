eec100a67c9bd5634c838aff94930de898656835
3rfan <erfan.ebrahimnia@gmail.com>
2017-09-06 17:36:51 +0200

    created autocomplete setup

---
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/AutoComplete/AutoComplete.js
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react';
+import Popover from '../Popover';
+
+type Props = {
+};
+
+export default class AutoComplete extends React.PureComponent<Props> {
+    render() {
+        return (
+            <div />
+        );
+    }
+}
