e4253b3591a2157763b4a71bba39b74ae16da236
3rfan <erfan.ebrahimnia@gmail.com>
2017-09-15 11:11:09 +0200

    added array-from polyfill to styleguidist config, fixed php code style, removed types file

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Masonry/types.js
+++
@@ -1,8 +0,0 @@
-// @flow
-export type MasonryItem = {
-    id: string | number,
-    selected: boolean,
-    icon?: string,
-    onClick?: (itemId: string | number) => void,
-    onSelectionChange?: (itemId: string | number, checked: boolean) => void,
-};
