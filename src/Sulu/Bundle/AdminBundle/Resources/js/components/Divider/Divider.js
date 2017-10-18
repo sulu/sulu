2de06323f261364a147a244bd1b608c2964ef455
3rfan <erfan.ebrahimnia@gmail.com>
2017-10-18 16:51:10 +0200

    made children prop optional, removed js class to detect if the divider is empty and used empty-pseudo-selector instead

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Divider/Divider.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Divider/Divider.js
@@ -1,24 +1,17 @@
 // @flow
 import React from 'react';
-import classNames from 'classnames';
 import dividerStyles from './divider.scss';
 
 type Props = {
-    children: string,
+    children?: string,
 };
 
 export default class Divider extends React.PureComponent<Props> {
     render() {
         const {children} = this.props;
-        const dividerClass = classNames(
-            dividerStyles.divider,
-            {
-                [dividerStyles.noText]: !children,
-            }
-        );
 
         return (
-            <div className={dividerClass}>
+            <div className={dividerStyles.divider}>
                 {children}
             </div>
         );
