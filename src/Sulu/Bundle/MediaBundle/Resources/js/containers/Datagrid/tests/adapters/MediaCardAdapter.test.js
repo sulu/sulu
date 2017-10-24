3e5b31bd94c22553c6e17c0adc5ee80cf0270c77
3rfan <erfan.ebrahimnia@gmail.com>
2017-10-24 10:32:11 +0200

    uncommented test

--- src/Sulu/Bundle/MediaBundle/Resources/js/containers/Datagrid/tests/adapters/MediaCardAdapter.test.js
+++ src/Sulu/Bundle/MediaBundle/Resources/js/containers/Datagrid/tests/adapters/MediaCardAdapter.test.js
@@ -48,40 +48,40 @@ test('Render a basic Masonry view with MediaCards', () => {
     expect(mediaCardAdapter).toMatchSnapshot();
 });
 
-// test('MediaCard should call the the appropriate handler', () => {
-//     const mediaCardClickSpy = jest.fn();
-//     const mediaCardSelectionChangeSpy = jest.fn();
-//     const thumbnails = {
-//         'sulu-260x': 'http://lorempixel.com/260/100',
-//         'sulu-100x100': 'http://lorempixel.com/100/100',
-//     };
-//     const data = [
-//         {
-//             id: 1,
-//             title: 'Title 1',
-//             mimeType: 'image/png',
-//             size: 12345,
-//             url: 'http://lorempixel.com/500/500',
-//             thumbnails: thumbnails,
-//         },
-//         {
-//             id: 2,
-//             title: 'Title 1',
-//             mimeType: 'image/jpeg',
-//             size: 54321,
-//             url: 'http://lorempixel.com/500/500',
-//             thumbnails: thumbnails,
-//         },
-//     ];
-//     const mediaCardAdapter = shallow(
-//         <MediaCardAdapter
-//             data={data}
-//             selections={[]}
-//             onItemClick={mediaCardClickSpy}
-//             onItemSelectionChange={mediaCardSelectionChangeSpy}
-//         />
-//     );
+test('MediaCard should call the the appropriate handler', () => {
+    const mediaCardClickSpy = jest.fn();
+    const mediaCardSelectionChangeSpy = jest.fn();
+    const thumbnails = {
+        'sulu-260x': 'http://lorempixel.com/260/100',
+        'sulu-100x100': 'http://lorempixel.com/100/100',
+    };
+    const data = [
+        {
+            id: 1,
+            title: 'Title 1',
+            mimeType: 'image/png',
+            size: 12345,
+            url: 'http://lorempixel.com/500/500',
+            thumbnails: thumbnails,
+        },
+        {
+            id: 2,
+            title: 'Title 1',
+            mimeType: 'image/jpeg',
+            size: 54321,
+            url: 'http://lorempixel.com/500/500',
+            thumbnails: thumbnails,
+        },
+    ];
+    const mediaCardAdapter = shallow(
+        <MediaCardAdapter
+            data={data}
+            selections={[]}
+            onItemClick={mediaCardClickSpy}
+            onItemSelectionChange={mediaCardSelectionChangeSpy}
+        />
+    );
 
-//     expect(mediaCardAdapter.find('MediaCard').get(0).props.onClick).toBe(mediaCardClickSpy);
-//     expect(mediaCardAdapter.find('MediaCard').get(0).props.onSelectionChange).toBe(mediaCardSelectionChangeSpy);
-// });
+    expect(mediaCardAdapter.find('MediaCard').get(0).props.onClick).toBe(mediaCardClickSpy);
+    expect(mediaCardAdapter.find('MediaCard').get(0).props.onSelectionChange).toBe(mediaCardSelectionChangeSpy);
+});
