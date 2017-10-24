4ae452ea3d40c350ab37c1235e85354d47b3cec9
Johannes Wachter <johannes.wachter@massiveart.com>
2017-10-24 10:55:53 +0200

    Refactored with-toolbar to use inheritance-inversion (#3577)
    * refactored with-toolbar to use inheritance-inversion
    
    * fixed code-style

--- src/Sulu/Bundle/AdminBundle/Resources/js/containers/Toolbar/withToolbar.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/containers/Toolbar/withToolbar.js
@@ -1,33 +1,38 @@
 // @flow
 import {autorun} from 'mobx';
-import React from 'react';
-import type {ComponentType} from 'react';
+import type {Component} from 'react';
 import {buildHocDisplayName} from '../../services/react';
 import type {ToolbarConfig} from './types';
 import toolbarStorePool, {DEFAULT_STORE_KEY} from './stores/ToolbarStorePool';
 
 export default function withToolbar(
-    Component: ComponentType<*>,
+    Component: Class<Component<*, *>>,
     toolbar: () => ToolbarConfig,
     toolbarStoreKey: string = DEFAULT_STORE_KEY
 ) {
-    const WithToolbarComponent = class extends React.Component<*> {
-        disposer: Function;
+    const WithToolbarComponent = class extends Component {
+        toolbarDisposer: Function;
 
-        setToolbarConfig = (component: React$Element<*>) => {
-            this.disposer = autorun(() => {
-                if (component) {
-                    toolbarStorePool.setToolbarConfig(toolbarStoreKey, toolbar.call(component));
-                }
+        componentWillMount() {
+            if (super.hasOwnProperty('toolbarDisposer')) {
+                throw new Error('Component passed to withToolbar cannot declare a property called "toolbarDisposer".');
+            }
+
+            if (super.componentWillMount) {
+                super.componentWillMount();
+            }
+
+            this.toolbarDisposer = autorun(() => {
+                toolbarStorePool.setToolbarConfig(toolbarStoreKey, toolbar.call(this));
             });
-        };
-
-        componentWillUnmount() {
-            this.disposer();
         }
 
-        render() {
-            return <Component ref={this.setToolbarConfig} {...this.props} />;
+        componentWillUnmount() {
+            if (super.componentWillUnmount) {
+                super.componentWillUnmount();
+            }
+
+            this.toolbarDisposer();
         }
     };
 
