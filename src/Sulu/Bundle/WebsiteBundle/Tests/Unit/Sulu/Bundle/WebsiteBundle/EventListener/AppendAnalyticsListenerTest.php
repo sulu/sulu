13f74d5417fd1a57994f8fcfa432c49235f0334f
Patrick Hopfner <patrick.hopfner@massiveart.com>
2017-11-08 16:27:58 +0100

    Fixed tests

--- src/Sulu/Bundle/WebsiteBundle/Tests/Unit/Sulu/Bundle/WebsiteBundle/EventListener/AppendAnalyticsListenerTest.php
+++ src/Sulu/Bundle/WebsiteBundle/Tests/Unit/Sulu/Bundle/WebsiteBundle/EventListener/AppendAnalyticsListenerTest.php
@@ -67,9 +67,9 @@ class AppendAnalyticsListenerTest extends \PHPUnit_Framework_TestCase
         $requestAnalyzer = $this->prophesize(RequestAnalyzerInterface::class);
         $analyticsRepository = $this->prophesize(AnalyticsRepository::class);
         $analytics = $this->prophesize(Analytics::class);
+
         $analytics->getType()->willReturn('google');
         $analytics->getContent()->willReturn('<script>var i = 0;</script>');
-
         $portalInformation = $this->prophesize(PortalInformation::class);
         $portalInformation->getUrlExpression()->willReturn('sulu.lo/{localization}');
         $portalInformation->getType()->willReturn(RequestAnalyzerInterface::MATCH_TYPE_FULL);
@@ -77,7 +77,7 @@ class AppendAnalyticsListenerTest extends \PHPUnit_Framework_TestCase
         $requestAnalyzer->getPortalInformation()->willReturn($portalInformation->reveal());
         $requestAnalyzer->getAttribute('urlExpression')->willReturn('sulu.lo/{localization}');
 
-        $analyticsRepository->findByUrl('sulu.lo/{localization}', 'sulu_io', 'prod')->willReturn($analytics);
+        $analyticsRepository->findByUrl('sulu.lo/{localization}', 'sulu_io', 'prod')->willReturn([$analytics]);
         $listener = new AppendAnalyticsListener(
             $engine->reveal(),
             $requestAnalyzer->reveal(),
@@ -93,11 +93,25 @@ class AppendAnalyticsListenerTest extends \PHPUnit_Framework_TestCase
         $response->reveal()->headers = new ParameterBag(['Content-Type' => 'text/html']);
         $event->getResponse()->willReturn($response->reveal());
 
-        $engine->render('SuluWebsiteBundle:Analytics:website.html.twig', ['analytics' => $analytics])
+        $engine->exists('SuluWebsiteBundle:Analytics:google/head-open.html.twig')->shouldBeCalled()->willReturn(false);
+        $engine->render('SuluWebsiteBundle:Analytics:google/head-open.html.twig', ['analytics' => $analytics])
+            ->shouldNotBeCalled();
+
+        $engine->exists('SuluWebsiteBundle:Analytics:google/head-close.html.twig')->shouldBeCalled()->willReturn(true);
+        $engine->render('SuluWebsiteBundle:Analytics:google/head-close.html.twig', ['analytics' => $analytics])
             ->shouldBeCalled()->willReturn('<script>var i = 0;</script>');
+
+        $engine->exists('SuluWebsiteBundle:Analytics:google/body-open.html.twig')->shouldBeCalled()->willReturn(false);
+        $engine->render('SuluWebsiteBundle:Analytics:google/body-open.html.twig', ['analytics' => $analytics])
+            ->shouldNotBeCalled();
+
+        $engine->exists('SuluWebsiteBundle:Analytics:google/body-close.html.twig')->shouldBeCalled()->willReturn(false);
+        $engine->render('SuluWebsiteBundle:Analytics:google/body-close.html.twig', ['analytics' => $analytics])
+            ->shouldNotBeCalled();
+
         $response->getContent()->willReturn('<html><head><title>Test</title></head><body><h1>Title</h1></body></html>');
         $response->setContent(
-            '<html><head><title>Test</title></head><body><h1>Title</h1><script>var i = 0;</script></body></html>'
+            '<html><head><title>Test</title><script>var i = 0;</script></head><body><h1>Title</h1></body></html>'
         )->shouldBeCalled();
         $listener->onResponse($event->reveal());
     }
@@ -118,7 +132,7 @@ class AppendAnalyticsListenerTest extends \PHPUnit_Framework_TestCase
         $requestAnalyzer->getPortalInformation()->willReturn($portalInformation->reveal());
         $requestAnalyzer->getAttribute('urlExpression')->willReturn('1.sulu.lo/2');
 
-        $analyticsRepository->findByUrl('1.sulu.lo/2', 'sulu_io', 'prod')->willReturn($analytics);
+        $analyticsRepository->findByUrl('1.sulu.lo/2', 'sulu_io', 'prod')->willReturn([$analytics]);
         $listener = new AppendAnalyticsListener(
             $engine->reveal(),
             $requestAnalyzer->reveal(),
@@ -136,11 +150,194 @@ class AppendAnalyticsListenerTest extends \PHPUnit_Framework_TestCase
         $response->reveal()->headers = new ParameterBag(['Content-Type' => 'text/html']);
         $event->getResponse()->willReturn($response->reveal());
 
-        $engine->render('SuluWebsiteBundle:Analytics:website.html.twig', ['analytics' => ['test' => 1]])
+        $engine->exists('SuluWebsiteBundle:Analytics:google/head-open.html.twig')->shouldBeCalled()->willReturn(false);
+        $engine->render('SuluWebsiteBundle:Analytics:google/head-open.html.twig', ['analytics' => $analytics])
+            ->shouldNotBeCalled();
+
+        $engine->exists('SuluWebsiteBundle:Analytics:google/head-close.html.twig')->shouldBeCalled()->willReturn(true);
+        $engine->render('SuluWebsiteBundle:Analytics:google/head-close.html.twig', ['analytics' => $analytics])
             ->shouldBeCalled()->willReturn('<script>var i = 0;</script>');
+
+        $engine->exists('SuluWebsiteBundle:Analytics:google/body-open.html.twig')->shouldBeCalled()->willReturn(false);
+        $engine->render('SuluWebsiteBundle:Analytics:google/body-open.html.twig', ['analytics' => $analytics])
+            ->shouldNotBeCalled();
+
+        $engine->exists('SuluWebsiteBundle:Analytics:google/body-close.html.twig')->shouldBeCalled()->willReturn(false);
+        $engine->render('SuluWebsiteBundle:Analytics:google/body-close.html.twig', ['analytics' => $analytics])
+            ->shouldNotBeCalled();
+
         $response->getContent()->willReturn('<html><head><title>Test</title></head><body><h1>Title</h1></body></html>');
         $response->setContent(
-            '<html><head><title>Test</title></head><body><h1>Title</h1><script>var i = 0;</script></body></html>'
+            '<html><head><title>Test</title><script>var i = 0;</script></head><body><h1>Title</h1></body></html>'
+        )->shouldBeCalled();
+
+        $listener->onResponse($event->reveal());
+    }
+
+    public function testAppendGoogleTagManager()
+    {
+        $engine = $this->prophesize(EngineInterface::class);
+        $requestAnalyzer = $this->prophesize(RequestAnalyzerInterface::class);
+        $analyticsRepository = $this->prophesize(AnalyticsRepository::class);
+        $analytics = $this->prophesize(Analytics::class);
+
+        $analytics->getType()->willReturn('google_tag_manager');
+        $analytics->getContent()->willReturn('<script>var i = 0;</script>');
+        $portalInformation = $this->prophesize(PortalInformation::class);
+        $portalInformation->getUrlExpression()->willReturn('sulu.lo/{localization}');
+        $portalInformation->getType()->willReturn(RequestAnalyzerInterface::MATCH_TYPE_FULL);
+        $portalInformation->getWebspaceKey()->willReturn('sulu_io');
+        $requestAnalyzer->getPortalInformation()->willReturn($portalInformation->reveal());
+        $requestAnalyzer->getAttribute('urlExpression')->willReturn('sulu.lo/{localization}');
+
+        $analyticsRepository->findByUrl('sulu.lo/{localization}', 'sulu_io', 'prod')->willReturn([$analytics]);
+        $listener = new AppendAnalyticsListener(
+            $engine->reveal(),
+            $requestAnalyzer->reveal(),
+            $analyticsRepository->reveal(),
+            'prod'
+        );
+
+        $event = $this->prophesize(FilterResponseEvent::class);
+        $request = $this->prophesize(Request::class);
+        $event->getRequest()->willReturn($request->reveal());
+        $request->getRequestFormat()->willReturn('html');
+        $response = $this->prophesize(Response::class);
+        $response->reveal()->headers = new ParameterBag(['Content-Type' => 'text/html']);
+        $event->getResponse()->willReturn($response->reveal());
+
+        $engine->exists('SuluWebsiteBundle:Analytics:google_tag_manager/head-open.html.twig')->shouldBeCalled()->willReturn(true);
+        $engine->render('SuluWebsiteBundle:Analytics:google_tag_manager/head-open.html.twig', ['analytics' => $analytics])
+            ->shouldBeCalled()->willReturn('<script>var i = 0;</script>');
+
+        $engine->exists('SuluWebsiteBundle:Analytics:google_tag_manager/head-close.html.twig')->shouldBeCalled()->willReturn(false);
+        $engine->render('SuluWebsiteBundle:Analytics:google_tag_manager/head-close.html.twig', ['analytics' => $analytics])
+            ->shouldNotBeCalled();
+
+        $engine->exists('SuluWebsiteBundle:Analytics:google_tag_manager/body-open.html.twig')->shouldBeCalled()->willReturn(true);
+        $engine->render('SuluWebsiteBundle:Analytics:google_tag_manager/body-open.html.twig', ['analytics' => $analytics])
+            ->shouldBeCalled()->willReturn('<noscript><div>Blabla</div></noscript>');
+
+        $engine->exists('SuluWebsiteBundle:Analytics:google_tag_manager/body-close.html.twig')->shouldBeCalled()->willReturn(false);
+        $engine->render('SuluWebsiteBundle:Analytics:google_tag_manager/body-close.html.twig', ['analytics' => $analytics])
+            ->shouldNotBeCalled();
+
+        $response->getContent()
+            ->willReturn('<html><head><title>Test</title></head><body class="test"><h1>Title</h1></body></html>');
+        $response->setContent(
+            '<html><head><script>var i = 0;</script><title>Test</title></head><body class="test"><noscript><div>Blabla</div></noscript><h1>Title</h1></body></html>'
+        )->shouldBeCalled();
+        $listener->onResponse($event->reveal());
+    }
+
+    public function testAppendPiwik()
+    {
+        $engine = $this->prophesize(EngineInterface::class);
+        $requestAnalyzer = $this->prophesize(RequestAnalyzerInterface::class);
+        $analyticsRepository = $this->prophesize(AnalyticsRepository::class);
+        $analytics = $this->prophesize(Analytics::class);
+
+        $analytics->getType()->willReturn('piwik');
+        $analytics->getContent()->willReturn('<script>var i = 0;</script>');
+        $portalInformation = $this->prophesize(PortalInformation::class);
+        $portalInformation->getUrlExpression()->willReturn('sulu.lo/{localization}');
+        $portalInformation->getType()->willReturn(RequestAnalyzerInterface::MATCH_TYPE_FULL);
+        $portalInformation->getWebspaceKey()->willReturn('sulu_io');
+        $requestAnalyzer->getPortalInformation()->willReturn($portalInformation->reveal());
+        $requestAnalyzer->getAttribute('urlExpression')->willReturn('sulu.lo/{localization}');
+
+        $analyticsRepository->findByUrl('sulu.lo/{localization}', 'sulu_io', 'prod')->willReturn([$analytics]);
+        $listener = new AppendAnalyticsListener(
+            $engine->reveal(),
+            $requestAnalyzer->reveal(),
+            $analyticsRepository->reveal(),
+            'prod'
+        );
+
+        $event = $this->prophesize(FilterResponseEvent::class);
+        $request = $this->prophesize(Request::class);
+        $event->getRequest()->willReturn($request->reveal());
+        $request->getRequestFormat()->willReturn('html');
+        $response = $this->prophesize(Response::class);
+        $response->reveal()->headers = new ParameterBag(['Content-Type' => 'text/html']);
+        $event->getResponse()->willReturn($response->reveal());
+
+        $engine->exists('SuluWebsiteBundle:Analytics:piwik/head-open.html.twig')->shouldBeCalled()->willReturn(false);
+        $engine->render('SuluWebsiteBundle:Analytics:piwik/head-open.html.twig', ['analytics' => $analytics])
+            ->shouldNotBeCalled();
+
+        $engine->exists('SuluWebsiteBundle:Analytics:piwik/head-close.html.twig')->shouldBeCalled()->willReturn(true);
+        $engine->render('SuluWebsiteBundle:Analytics:piwik/head-close.html.twig', ['analytics' => $analytics])
+            ->shouldBeCalled()->willReturn('<script>var i = 0;</script>');
+
+        $engine->exists('SuluWebsiteBundle:Analytics:piwik/body-open.html.twig')->shouldBeCalled()->willReturn(true);
+        $engine->render('SuluWebsiteBundle:Analytics:piwik/body-open.html.twig', ['analytics' => $analytics])
+            ->shouldBeCalled()->willReturn('<noscript><div>Blabla</div></noscript>');
+
+        $engine->exists('SuluWebsiteBundle:Analytics:piwik/body-close.html.twig')->shouldBeCalled()->willReturn(false);
+        $engine->render('SuluWebsiteBundle:Analytics:piwik/body-close.html.twig', ['analytics' => $analytics])
+            ->shouldNotBeCalled();
+
+        $response->getContent()
+            ->willReturn('<html><head><title>Test</title></head><body class="test"><h1>Title</h1></body></html>');
+        $response->setContent(
+            '<html><head><title>Test</title><script>var i = 0;</script></head><body class="test"><noscript><div>Blabla</div></noscript><h1>Title</h1></body></html>'
+        )->shouldBeCalled();
+        $listener->onResponse($event->reveal());
+    }
+
+    public function testAppendCustom()
+    {
+        $engine = $this->prophesize(EngineInterface::class);
+        $requestAnalyzer = $this->prophesize(RequestAnalyzerInterface::class);
+        $analyticsRepository = $this->prophesize(AnalyticsRepository::class);
+        $analytics = $this->prophesize(Analytics::class);
+
+        $analytics->getType()->willReturn('custom');
+        $analytics->getContent()->willReturn('{"position":"headOpen","value":"<script>XYZ</script>"}');
+        $portalInformation = $this->prophesize(PortalInformation::class);
+        $portalInformation->getUrlExpression()->willReturn('sulu.lo/{localization}');
+        $portalInformation->getType()->willReturn(RequestAnalyzerInterface::MATCH_TYPE_FULL);
+        $portalInformation->getWebspaceKey()->willReturn('sulu_io');
+        $requestAnalyzer->getPortalInformation()->willReturn($portalInformation->reveal());
+        $requestAnalyzer->getAttribute('urlExpression')->willReturn('sulu.lo/{localization}');
+
+        $analyticsRepository->findByUrl('sulu.lo/{localization}', 'sulu_io', 'prod')->willReturn([$analytics]);
+        $listener = new AppendAnalyticsListener(
+            $engine->reveal(),
+            $requestAnalyzer->reveal(),
+            $analyticsRepository->reveal(),
+            'prod'
+        );
+
+        $event = $this->prophesize(FilterResponseEvent::class);
+        $request = $this->prophesize(Request::class);
+        $event->getRequest()->willReturn($request->reveal());
+        $request->getRequestFormat()->willReturn('html');
+        $response = $this->prophesize(Response::class);
+        $response->reveal()->headers = new ParameterBag(['Content-Type' => 'text/html']);
+        $event->getResponse()->willReturn($response->reveal());
+
+        $engine->exists('SuluWebsiteBundle:Analytics:custom/head-open.html.twig')->shouldBeCalled()->willReturn(true);
+        $engine->render('SuluWebsiteBundle:Analytics:custom/head-open.html.twig', ['analytics' => $analytics])
+            ->shouldBeCalled()->willReturn('<script>var nice_var = false;</script>');
+
+        $engine->exists('SuluWebsiteBundle:Analytics:custom/head-close.html.twig')->shouldBeCalled()->willReturn(true);
+        $engine->render('SuluWebsiteBundle:Analytics:custom/head-close.html.twig', ['analytics' => $analytics])
+            ->shouldBeCalled()->willReturn('');
+
+        $engine->exists('SuluWebsiteBundle:Analytics:custom/body-open.html.twig')->shouldBeCalled()->willReturn(true);
+        $engine->render('SuluWebsiteBundle:Analytics:custom/body-open.html.twig', ['analytics' => $analytics])
+            ->shouldBeCalled()->willReturn('');
+
+        $engine->exists('SuluWebsiteBundle:Analytics:custom/body-close.html.twig')->shouldBeCalled()->willReturn(true);
+        $engine->render('SuluWebsiteBundle:Analytics:custom/body-close.html.twig', ['analytics' => $analytics])
+            ->shouldBeCalled()->willReturn('');
+
+        $response->getContent()
+            ->willReturn('<html><head maybe-a-attribute-here="true"><title>Test</title></head><body><header><h1>Title</h1></header></body></html>');
+        $response->setContent(
+            '<html><head maybe-a-attribute-here="true"><script>var nice_var = false;</script><title>Test</title></head><body><header><h1>Title</h1></header></body></html>'
         )->shouldBeCalled();
         $listener->onResponse($event->reveal());
     }
