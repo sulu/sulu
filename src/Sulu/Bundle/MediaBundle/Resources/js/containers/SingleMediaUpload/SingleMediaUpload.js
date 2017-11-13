a8488b7ef8151639cb3773933046fb9d9f9c66ae
3rfan <erfan.ebrahimnia@gmail.com>
2017-11-13 11:29:23 +0100

    started single media upload component

---
+++ src/Sulu/Bundle/MediaBundle/Resources/js/containers/SingleMediaUpload/SingleMediaUpload.js
@@ -0,0 +1,12 @@
+// @flow
+import React from 'react';
+
+type Props = {};
+
+export default class SingleMediaUpload extends React.PureComponent<Props> {
+    render() {
+        return (
+            <div />
+        );
+    }
+}
