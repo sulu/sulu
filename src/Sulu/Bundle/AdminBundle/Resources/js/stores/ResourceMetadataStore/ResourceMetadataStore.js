bdb25296bd1c80b315cc51147452fd050016bf56
Daniel Rotter <daniel.rotter@gmail.com>
2017-11-14 18:40:33 +0100

    add metadata for media form

--- src/Sulu/Bundle/AdminBundle/Resources/js/stores/ResourceMetadataStore/ResourceMetadataStore.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/stores/ResourceMetadataStore/ResourceMetadataStore.js
@@ -58,6 +58,30 @@ class ResourceMetadataStore {
             },
         },
         media: {
+            form: {
+                title: {
+                    label: 'Titel',
+                    type: 'text_line',
+                },
+                description: {
+                    label: 'Description',
+                    type: 'text_area',
+                },
+                taxonomies: {
+                    type: 'section',
+                    label: 'Taxonomien',
+                    properties: {
+                        tags: {
+                            label: 'Tags',
+                            type: 'tag_selection',
+                        },
+                        categories: {
+                            label: 'Categories',
+                            type: 'category_selection',
+                        },
+                    },
+                },
+            },
             list: {
                 id: {},
                 title: {},
