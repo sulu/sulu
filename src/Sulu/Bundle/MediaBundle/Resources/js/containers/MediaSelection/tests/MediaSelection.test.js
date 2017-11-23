a0d0f57e6f3d1650d9d0208f918c6987f294d516
3rfan <erfan.ebrahimnia@gmail.com>
2017-11-23 15:06:31 +0100

    added mediaselection overlay tests

--- src/Sulu/Bundle/MediaBundle/Resources/js/containers/MediaSelection/tests/MediaSelection.test.js
+++ src/Sulu/Bundle/MediaBundle/Resources/js/containers/MediaSelection/tests/MediaSelection.test.js
@@ -3,11 +3,9 @@ import {mount, render, shallow} from 'enzyme';
 import pretty from 'pretty';
 import React from 'react';
 import datagridAdapterRegistry from 'sulu-admin-bundle/containers/Datagrid/registries/DatagridAdapterRegistry';
-import {DatagridStore} from 'sulu-admin-bundle/containers';
 import MediaSelection from '../MediaSelection';
 import MediaCardSelectionAdapter from '../../Datagrid/adapters/MediaCardSelectionAdapter';
 import MediaSelectionStore from '../stores/MediaSelectionStore';
-import CollectionStore from '../../../stores/CollectionStore';
 
 jest.mock('../stores/MediaSelectionStore', () => jest.fn());
 
@@ -224,65 +222,6 @@ test('Clicking on the "add media" button should open up an overlay', () => {
     expect(pretty(body.innerHTML)).toMatchSnapshot();
 });
 
-test('Should instantiate the needed stores when the overlay opens', () => {
-    MediaSelectionStore.mockImplementation(function() {
-        this.selectedMedia = [];
-        this.selectedMediaIds = [];
-    });
-
-    const locale = 'en';
-    const mediaResourceKey = 'media';
-    const collectionResourceKey = 'collections';
-    const mediaSelectionInstance = shallow(<MediaSelection locale={locale} />).instance();
-
-    mediaSelectionInstance.openMediaOverlay();
-    expect(mediaSelectionInstance.mediaPage.get()).toBe(1);
-    expect(mediaSelectionInstance.collectionPage.get()).toBe(1);
-
-    expect(CollectionStore.mock.calls[0][0]).toBe(undefined);
-    expect(CollectionStore.mock.calls[0][1]).toBe(locale);
-
-    expect(DatagridStore.mock.calls[0][0]).toBe(mediaResourceKey);
-    expect(DatagridStore.mock.calls[0][1].locale).toBe(locale);
-    expect(DatagridStore.mock.calls[0][1].page.get()).toBe(1);
-    expect(DatagridStore.mock.calls[0][2].fields).toEqual([
-        'id',
-        'type',
-        'name',
-        'size',
-        'title',
-        'mimeType',
-        'subVersion',
-        'thumbnails',
-    ].join(','));
-    expect(DatagridStore.mock.calls[0][3]).toBe(true);
-    expect(typeof DatagridStore.mock.calls[0][4]).toBe('function');
-    expect(DatagridStore.mock.calls[0][5]).toEqual([]);
-
-    expect(DatagridStore.mock.calls[1][0]).toBe(collectionResourceKey);
-    expect(DatagridStore.mock.calls[1][1].locale).toBe(locale);
-    expect(DatagridStore.mock.calls[1][1].page.get()).toBe(1);
-});
-
-test('Should add and remove media ids', () => {
-    MediaSelectionStore.mockImplementation(function() {
-        this.selectedMedia = [];
-        this.selectedMediaIds = [];
-    });
-
-    const mediaSelectionInstance = shallow(<MediaSelection />).instance();
-
-    expect(mediaSelectionInstance.selectedMediaIds).toEqual([]);
-
-    mediaSelectionInstance.handleMediaSelection(1, true);
-    mediaSelectionInstance.handleMediaSelection(2, true);
-    mediaSelectionInstance.handleMediaSelection(3, true);
-    expect(mediaSelectionInstance.selectedMediaIds).toEqual([1, 2, 3]);
-
-    mediaSelectionInstance.handleMediaSelection(2, false);
-    expect(mediaSelectionInstance.selectedMediaIds).toEqual([1, 3]);
-});
-
 test('Should remove media from the selection', () => {
     MediaSelectionStore.mockImplementation(function() {
         this.selectedMedia = [];
@@ -293,7 +232,7 @@ test('Should remove media from the selection', () => {
     const changeSpy = jest.fn();
     const mediaSelectionInstance = shallow(<MediaSelection onChange={changeSpy} />).instance();
 
-    mediaSelectionInstance.handleMediaRemove(1);
+    mediaSelectionInstance.handleRemove(1);
     expect(changeSpy).toBeCalled();
     expect(mediaSelectionInstance.mediaSelectionStore.removeById).toBeCalledWith(1);
 });
@@ -308,76 +247,48 @@ test('Should move media inside the selection', () => {
     const changeSpy = jest.fn();
     const mediaSelectionInstance = shallow(<MediaSelection onChange={changeSpy} />).instance();
 
-    mediaSelectionInstance.handleMediaSorted(1, 3);
+    mediaSelectionInstance.handleSorted(1, 3);
     expect(changeSpy).toBeCalled();
     expect(mediaSelectionInstance.mediaSelectionStore.move).toBeCalledWith(1, 3);
 });
 
-test('Should reset the selection array and add the selected medias to the selection store on confirm', () => {
+test('Should add the selected medias to the selection store on confirm', () => {
     MediaSelectionStore.mockImplementation(function() {
         this.selectedMedia = [];
         this.selectedMediaIds = [];
         this.add = jest.fn();
     });
 
+    const thumbnails = {
+        'sulu-260x': 'http://lorempixel.com/260/100',
+        'sulu-25x25': 'http://lorempixel.com/25/25',
+    };
     const changeSpy = jest.fn();
     const mediaSelectionInstance = shallow(<MediaSelection onChange={changeSpy} />).instance();
 
     mediaSelectionInstance.openMediaOverlay();
-    mediaSelectionInstance.handleMediaSelection(1, true);
-    mediaSelectionInstance.handleMediaSelection(2, true);
-    expect(mediaSelectionInstance.selectedMediaIds).toEqual([1, 2]);
-
-    mediaSelectionInstance.handleOverlayConfirm();
+    mediaSelectionInstance.handleOverlayConfirm([
+        {
+            id: 1,
+            title: 'Title 1',
+            mimeType: 'image/png',
+            size: 12345,
+            url: 'http://lorempixel.com/500/500',
+            thumbnails: thumbnails,
+        },
+        {
+            id: 2,
+            title: 'Title 2',
+            mimeType: 'image/jpeg',
+            size: 54321,
+            url: 'http://lorempixel.com/500/500',
+            thumbnails: thumbnails,
+        },
+    ]);
     expect(mediaSelectionInstance.mediaSelectionStore.add.mock.calls[0][0].id).toBe(1);
     expect(mediaSelectionInstance.mediaSelectionStore.add.mock.calls[0][0].title).toBe('Title 1');
     expect(mediaSelectionInstance.mediaSelectionStore.add.mock.calls[1][0].id).toBe(2);
     expect(mediaSelectionInstance.mediaSelectionStore.add.mock.calls[1][0].title).toBe('Title 2');
     expect(changeSpy).toBeCalled();
     expect(mediaSelectionInstance.overlayOpen).toBe(false);
-    expect(mediaSelectionInstance.selectedMediaIds).toEqual([]);
-});
-
-test('Should reset the selection array when the "Reset Selection" button was clicked', () => {
-    MediaSelectionStore.mockImplementation(function() {
-        this.selectedMedia = [];
-        this.selectedMediaIds = [];
-    });
-
-    const changeSpy = jest.fn();
-    const mediaSelectionInstance = shallow(<MediaSelection onChange={changeSpy} />).instance();
-
-    mediaSelectionInstance.openMediaOverlay();
-    mediaSelectionInstance.handleMediaSelection(1, true);
-    mediaSelectionInstance.handleMediaSelection(2, true);
-    expect(mediaSelectionInstance.selectedMediaIds).toEqual([1, 2]);
-
-    mediaSelectionInstance.handleSelectionReset();
-    expect(mediaSelectionInstance.selectedMediaIds).toEqual([]);
-    expect(mediaSelectionInstance.mediaDatagridStore.deselectEntirePage).toBeCalled();
-});
-
-test('Should destroy the stores and cleanup all states when the overlay is closed', () => {
-    MediaSelectionStore.mockImplementation(function() {
-        this.selectedMedia = [];
-        this.selectedMediaIds = [];
-    });
-
-    const changeSpy = jest.fn();
-    const mediaSelectionInstance = shallow(<MediaSelection onChange={changeSpy} />).instance();
-
-    mediaSelectionInstance.openMediaOverlay();
-    mediaSelectionInstance.handleMediaSelection(1, true);
-    mediaSelectionInstance.handleMediaSelection(2, true);
-    mediaSelectionInstance.setCollectionId(1);
-
-    expect(mediaSelectionInstance.collectionId).toBe(1);
-    expect(mediaSelectionInstance.selectedMediaIds).toEqual([1, 2]);
-
-    mediaSelectionInstance.closeMediaOverlay();
-    expect(mediaSelectionInstance.collectionId).toBe(undefined);
-    expect(mediaSelectionInstance.selectedMediaIds).toEqual([]);
-    expect(mediaSelectionInstance.collectionStore.destroy).toBeCalled();
-    expect(mediaSelectionInstance.mediaDatagridStore.destroy).toBeCalled();
-    expect(mediaSelectionInstance.collectionDatagridStore.destroy).toBeCalled();
 });
--- src/Sulu/Bundle/MediaBundle/Resources/js/containers/MediaSelection/tests/MediaSelectionOverlay.test.js
+++ src/Sulu/Bundle/MediaBundle/Resources/js/containers/MediaSelection/tests/MediaSelectionOverlay.test.js
@@ -70,6 +70,7 @@ jest.mock('sulu-admin-bundle/containers', () => {
             this.clearSelection = jest.fn();
             this.setAppendRequestData = jest.fn();
             this.deselectEntirePage = jest.fn();
+            this.select = jest.fn();
             this.getSchema = jest.fn().mockReturnValue({});
         }),
     };
@@ -167,4 +168,187 @@ test('Render an open MediaSelectionOverlay', () => {
     ).render();
 
     expect(pretty(body.innerHTML)).toMatchSnapshot();
+});
+
+test('Should instantiate the needed stores when the overlay opens', () => {
+    const mediaResourceKey = 'media';
+    const collectionResourceKey = 'collections';
+    const locale = observable();
+    const mediaSelectionOverlayInstance = shallow(
+        <MediaSelectionOverlay
+            open={true}
+            locale={locale}
+            excludedIds={[]}
+            onClose={jest.fn()}
+            onConfirm={jest.fn()}
+        />
+    ).instance();
+
+    expect(mediaSelectionOverlayInstance.mediaPage.get()).toBe(1);
+    expect(mediaSelectionOverlayInstance.collectionPage.get()).toBe(1);
+
+    expect(CollectionStore.mock.calls[0][0]).toBe(undefined);
+    expect(CollectionStore.mock.calls[0][1]).toBe(locale);
+
+    expect(DatagridStore.mock.calls[0][0]).toBe(mediaResourceKey);
+    expect(DatagridStore.mock.calls[0][1].locale).toBe(locale);
+    expect(DatagridStore.mock.calls[0][1].page.get()).toBe(1);
+    expect(DatagridStore.mock.calls[0][2].fields).toEqual([
+        'id',
+        'type',
+        'name',
+        'size',
+        'title',
+        'mimeType',
+        'subVersion',
+        'thumbnails',
+    ].join(','));
+    expect(DatagridStore.mock.calls[0][3]).toBe(true);
+    expect(typeof DatagridStore.mock.calls[0][4]).toBe('function');
+
+    expect(DatagridStore.mock.calls[1][0]).toBe(collectionResourceKey);
+    expect(DatagridStore.mock.calls[1][1].locale).toBe(locale);
+    expect(DatagridStore.mock.calls[1][1].page.get()).toBe(1);
+});
+
+test('Should add and remove media ids', () => {
+    const thumbnails = {
+        'sulu-260x': 'http://lorempixel.com/260/100',
+        'sulu-25x25': 'http://lorempixel.com/25/25',
+    };
+    const locale = observable();
+    const mediaSelectionOverlayInstance = shallow(
+        <MediaSelectionOverlay
+            open={true}
+            locale={locale}
+            excludedIds={[]}
+            onClose={jest.fn()}
+            onConfirm={jest.fn()}
+        />
+    ).instance();
+
+    expect(mediaSelectionOverlayInstance.selectedMedia).toEqual([]);
+
+    mediaSelectionOverlayInstance.handleMediaSelectionChange(1, true);
+    mediaSelectionOverlayInstance.handleMediaSelectionChange(2, true);
+    expect(mediaSelectionOverlayInstance.selectedMedia).toEqual([
+        {
+            id: 1,
+            title: 'Title 1',
+            mimeType: 'image/png',
+            size: 12345,
+            url: 'http://lorempixel.com/500/500',
+            thumbnails: thumbnails,
+        },
+        {
+            id: 2,
+            title: 'Title 2',
+            mimeType: 'image/jpeg',
+            size: 54321,
+            url: 'http://lorempixel.com/500/500',
+            thumbnails: thumbnails,
+        },
+    ]);
+
+    mediaSelectionOverlayInstance.handleMediaSelectionChange(2, false);
+    expect(mediaSelectionOverlayInstance.selectedMedia).toEqual([
+        {
+            id: 1,
+            title: 'Title 1',
+            mimeType: 'image/png',
+            size: 12345,
+            url: 'http://lorempixel.com/500/500',
+            thumbnails: thumbnails,
+        },
+    ]);
+});
+
+test('Should reset the selection array when the "Reset Selection" button was clicked', () => {
+    const thumbnails = {
+        'sulu-260x': 'http://lorempixel.com/260/100',
+        'sulu-25x25': 'http://lorempixel.com/25/25',
+    };
+    const locale = observable();
+    const mediaSelectionOverlayInstance = shallow(
+        <MediaSelectionOverlay
+            open={true}
+            locale={locale}
+            excludedIds={[]}
+            onClose={jest.fn()}
+            onConfirm={jest.fn()}
+        />
+    ).instance();
+
+    mediaSelectionOverlayInstance.handleMediaSelectionChange(1, true);
+    mediaSelectionOverlayInstance.handleMediaSelectionChange(2, true);
+    expect(mediaSelectionOverlayInstance.selectedMedia).toEqual([
+        {
+            id: 1,
+            title: 'Title 1',
+            mimeType: 'image/png',
+            size: 12345,
+            url: 'http://lorempixel.com/500/500',
+            thumbnails: thumbnails,
+        },
+        {
+            id: 2,
+            title: 'Title 2',
+            mimeType: 'image/jpeg',
+            size: 54321,
+            url: 'http://lorempixel.com/500/500',
+            thumbnails: thumbnails,
+        },
+    ]);
+
+    mediaSelectionOverlayInstance.handleSelectionReset();
+    expect(mediaSelectionOverlayInstance.selectedMedia).toEqual([]);
+    expect(mediaSelectionOverlayInstance.mediaDatagridStore.deselectEntirePage).toBeCalled();
+});
+
+test('Should destroy the stores and cleanup all states when the overlay is closed', () => {
+    const thumbnails = {
+        'sulu-260x': 'http://lorempixel.com/260/100',
+        'sulu-25x25': 'http://lorempixel.com/25/25',
+    };
+    const locale = observable();
+    const mediaSelectionOverlayInstance = shallow(
+        <MediaSelectionOverlay
+            open={true}
+            locale={locale}
+            excludedIds={[]}
+            onClose={jest.fn()}
+            onConfirm={jest.fn()}
+        />
+    ).instance();
+
+    mediaSelectionOverlayInstance.handleMediaSelectionChange(1, true);
+    mediaSelectionOverlayInstance.handleMediaSelectionChange(2, true);
+    mediaSelectionOverlayInstance.setCollectionId(1);
+
+    expect(mediaSelectionOverlayInstance.collectionId).toBe(1);
+    expect(mediaSelectionOverlayInstance.selectedMedia).toEqual([
+        {
+            id: 1,
+            title: 'Title 1',
+            mimeType: 'image/png',
+            size: 12345,
+            url: 'http://lorempixel.com/500/500',
+            thumbnails: thumbnails,
+        },
+        {
+            id: 2,
+            title: 'Title 2',
+            mimeType: 'image/jpeg',
+            size: 54321,
+            url: 'http://lorempixel.com/500/500',
+            thumbnails: thumbnails,
+        },
+    ]);
+
+    mediaSelectionOverlayInstance.handleClose();
+    expect(mediaSelectionOverlayInstance.collectionId).toBe(undefined);
+    expect(mediaSelectionOverlayInstance.selectedMedia).toEqual([]);
+    expect(mediaSelectionOverlayInstance.collectionStore.destroy).toBeCalled();
+    expect(mediaSelectionOverlayInstance.mediaDatagridStore.destroy).toBeCalled();
+    expect(mediaSelectionOverlayInstance.collectionDatagridStore.destroy).toBeCalled();
 });
