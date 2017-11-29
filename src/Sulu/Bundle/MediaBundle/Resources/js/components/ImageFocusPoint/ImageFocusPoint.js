83aadcc0e4f376b1cda4b59f214da84aa3840408
3rfan <erfan.ebrahimnia@gmail.com>
2017-11-29 15:38:57 +0100

    added focus point logic

--- src/Sulu/Bundle/MediaBundle/Resources/js/components/ImageFocusPoint/ImageFocusPoint.js
+++ src/Sulu/Bundle/MediaBundle/Resources/js/components/ImageFocusPoint/ImageFocusPoint.js
@@ -1,9 +1,15 @@
 // @flow
 import React from 'react';
+import classNames from 'classnames';
+import {Icon} from 'sulu-admin-bundle/components';
 import type {FocusPoint} from './types';
 import imageFocusPointStyles from './imageFocusPoint.scss';
 
 const FOCUS_POINT_MATRIX_SIZE = 3;
+const ARROW_UP_ICON = 'arrow-up';
+const ARROW_DOWN_ICON = 'arrow-down';
+const ARROW_LEFT_ICON = 'arrow-left';
+const ARROW_RIGHT_ICON = 'arrow-right';
 
 type Props = {
     image: string,
@@ -12,48 +18,61 @@ type Props = {
 };
 
 export default class ImageFocusPoint extends React.PureComponent<Props> {
-    createMatrix(selectedPoint: FocusPoint) {
-        const matrix = [];
-
-        for (let rowIndex = 0; rowIndex < FOCUS_POINT_MATRIX_SIZE; rowIndex++) {
-            const row = [];
-            for (let columnIndex = 0; columnIndex < FOCUS_POINT_MATRIX_SIZE; columnIndex++) {
-                if (selectedPoint.x === columnIndex && selectedPoint.y === rowIndex) {
-                    row.push(1);
-                } else {
-                    row.push(0);
-                }
+    createFocusPoint(icon: ?string, active: boolean = false) {
+        const focusPointClass = classNames(
+            imageFocusPointStyles.focusPoint,
+            {
+                [imageFocusPointStyles.active]: active,
             }
+        );
 
-            matrix.push();
-        }
-
-        return matrix;
+        return (
+            <button className={focusPointClass}>
+                {icon &&
+                    <Icon name={icon} />
+                }
+            </button>
+        );
     }
 
-    createFocusPoints() {
+    createFocusPoints(selectedPoint: FocusPoint) {
         const points = [];
 
         for (let x = 0; x < FOCUS_POINT_MATRIX_SIZE; x++) {
             for (let y = 0; y < FOCUS_POINT_MATRIX_SIZE; y++) {
-                points.push(
-                    <button className={imageFocusPointStyles.focusPoint}>
-                        {`${x}, ${y}`}
-                    </button>
-                );
+                if (selectedPoint.x === x && selectedPoint.y === y) {
+                    points.push(this.createFocusPoint(null, true));
+                } else if (this.isLeftOfSelectedPoint(selectedPoint, x, y)) {
+                    points.push(this.createFocusPoint(ARROW_LEFT_ICON));
+                } else if (this.isRightOfSelectedPoint(selectedPoint, x, y)) {
+                    points.push(this.createFocusPoint(ARROW_RIGHT_ICON));
+                } else {
+                    points.push(this.createFocusPoint(null));
+                }
             }
         }
 
         return points;
     }
 
+    isLeftOfSelectedPoint(selectedPoint: FocusPoint, x: number, y: number) {
+        return selectedPoint.x + 1 === x && selectedPoint.x + 1 <= FOCUS_POINT_MATRIX_SIZE && selectedPoint.y === y;
+    }
+
+    isRightOfSelectedPoint(selectedPoint: FocusPoint, x: number, y: number) {
+        return selectedPoint.x + 1 === x && selectedPoint.x + 1 <= FOCUS_POINT_MATRIX_SIZE && selectedPoint.y === y;
+    }
+
     render() {
-        const {image} = this.props;
+        const {
+            image,
+            value,
+        } = this.props;
 
         return (
             <div className={imageFocusPointStyles.imageFocusPoint}>
                 <div className={imageFocusPointStyles.focusPoints}>
-                    {this.createFocusPoints()}
+                    {this.createFocusPoints(value)}
                 </div>
                 <img className={imageFocusPointStyles.image} src={image} />
             </div>
--- src/Sulu/Bundle/MediaBundle/Resources/js/components/ImageFocusPoint/README.md
+++ src/Sulu/Bundle/MediaBundle/Resources/js/components/ImageFocusPoint/README.md
@@ -1,5 +1,13 @@
 ```javascript
+initialState = {
+    focusPoint: {
+        x: 1,
+        y: 1,
+    }
+};
+
 <ImageFocusPoint
+    value={state.focusPoint}
     image="https://source.unsplash.com/random/600x400"
 />
 ```
