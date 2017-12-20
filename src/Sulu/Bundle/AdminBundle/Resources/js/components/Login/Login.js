a17dde7764bcc288e068864f310f753fd4d044f4
3rfan <erfan.ebrahimnia@gmail.com>
2017-12-20 17:58:23 +0100

    added login styling

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Login/Login.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Login/Login.js
@@ -3,27 +3,36 @@ import React from 'react';
 import type {ViewProps} from '../../containers/ViewRenderer';
 import Icon from '../../components/Icon';
 import Input from '../../components/Input';
-import Button from '../../components/Button';
 import {translate} from '../../utils';
 import loginStyles from './login.scss';
 import logo from './logo.svg';
 
-const BACK_BUTTON_ARROW_LEFT_ICON = 'chevron-left';
+const BACK_LINK_ARROW_LEFT_ICON = 'chevron-left';
 
 export default class Login extends React.PureComponent<ViewProps> {
     render() {
         return (
             <div className={loginStyles.login}>
-                <div className={loginStyles.loginContainer}>
-                    <a href="/">
-                        <Icon name={BACK_BUTTON_ARROW_LEFT_ICON} />
+                <div className={loginStyles.formContainer}>
+                    <a className={loginStyles.backLink} href="/">
+                        <Icon name={BACK_LINK_ARROW_LEFT_ICON} className={loginStyles.backLinkIcon} />
                         {translate('sulu_admin.back_to_website')}
                     </a>
-                    <img src={logo} />
+                    <img className={loginStyles.logo} src={logo} />
                     <form className={loginStyles.form}>
-                        <Input />
-                        <Input />
-                        <Button />
+                        <label className={loginStyles.inputField}>
+                            <div className={loginStyles.labelText}>
+                                {translate('sulu_admin.username_or_email')}
+                            </div>
+                            <Input />
+                        </label>
+                        <label className={loginStyles.inputField}>
+                            <div className={loginStyles.labelText}>
+                                {translate('sulu_admin.password')}
+                            </div>
+                            <Input />
+                        </label>
+                        <button className={loginStyles.loginButton}>{translate('sulu_admin.login')}</button>
                     </form>
                 </div>
             </div>
