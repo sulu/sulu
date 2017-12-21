ab01c97085d0bbd8e93508d1e517e4ce09d024ca
3rfan <erfan.ebrahimnia@gmail.com>
2017-12-21 14:53:25 +0100

    added backLink prop

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Login/Login.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Login/Login.js
@@ -9,18 +9,27 @@ import logo from './logo.svg';
 const BACK_LINK_ARROW_LEFT_ICON = 'chevron-left';
 
 type Props = {
+    backLink: string,
     onLogin: (user: string, password: string) => void,
     onResetPassword: (user: string) => void,
 };
 
 export default class Login extends React.PureComponent<Props> {
+    static defaultProps = {
+        backLink: '/',
+    };
+
     render() {
-        const {onLogin, onResetPassword} = this.props;
+        const {
+            backLink,
+            onLogin,
+            onResetPassword,
+        } = this.props;
 
         return (
             <div className={loginStyles.login}>
                 <div className={loginStyles.formContainer}>
-                    <a className={loginStyles.backLink} href="/">
+                    <a className={loginStyles.backLink} href={backLink}>
                         <Icon name={BACK_LINK_ARROW_LEFT_ICON} className={loginStyles.backLinkIcon} />
                         {translate('sulu_admin.back_to_website')}
                     </a>
