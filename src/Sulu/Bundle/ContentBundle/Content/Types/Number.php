d6452848d9d62890f9954c35e2639355eab82cc4
Christian Ruttorf <chris@oafx.de>
2018-02-12 14:02:56 +0100

    Create content type class

---
+++ src/Sulu/Bundle/ContentBundle/Content/Types/Number.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\ContentBundle\Content\Types;
+
+use Sulu\Component\Content\SimpleContentType;
+
+/**
+ * ContentType for Numbers.
+ */
+class Number extends SimpleContentType
+{
+    private $template;
+
+    public function __construct($template)
+    {
+        parent::__construct('Number', '');
+
+        $this->template = $template;
+    }
+
+    /**
+     * returns a template to render a form.
+     *
+     * @return string
+     */
+    public function getTemplate()
+    {
+        return $this->template;
+    }
+}
