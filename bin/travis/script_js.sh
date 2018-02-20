34d213d561180195f54641806ef44303404747bd
Patrick Hopfner <patrick.hopfner@gmail.com>
2018-02-20 14:34:31 +0100

    Speed up travis

--- bin/travis/script_js.sh
+++ bin/travis/script_js.sh
@@ -5,6 +5,6 @@ npm run lint:js || EXIT_STATUS=$?
 npm run lint:scss || EXIT_STATUS=$?
 npm run flow || EXIT_STATUS=$?
 npm run styleguide:build || EXIT_STATUS=$?
-npm test || EXIT_STATUS=$?
+npm test -- --maxWorkers=4 || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
