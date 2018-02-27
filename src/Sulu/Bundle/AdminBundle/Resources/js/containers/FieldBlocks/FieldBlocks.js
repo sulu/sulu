02378a8d80c7875553a561f300a52a40ebeb3044
Daniel Rotter <daniel.rotter@gmail.com>
2018-02-27 13:50:27 +0100

    add missing new line

--- src/Sulu/Bundle/AdminBundle/Resources/js/containers/FieldBlocks/FieldBlocks.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/containers/FieldBlocks/FieldBlocks.js
@@ -20,6 +20,7 @@ export default class FieldBlocks extends React.Component<FieldTypeProps<Array<Bl
 
     handleSortEnd = () => {
         const {onFinish} = this.props;
+
         if (onFinish) {
             onFinish();
         }
