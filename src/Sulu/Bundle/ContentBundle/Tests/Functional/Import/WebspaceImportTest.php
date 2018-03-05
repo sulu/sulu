0e7286e3ec1784493924de8aaeb7c985a9becc66
Patrick Hopfner <patrick.hopfner@gmail.com>
2018-03-05 16:49:07 +0100

    WebspaceImport: Bugfix when no url is given
    Try and catch
    
    Added test
    
    Added improvments
    
    Correct exception

--- src/Sulu/Bundle/ContentBundle/Tests/Functional/Import/WebspaceImportTest.php
+++ src/Sulu/Bundle/ContentBundle/Tests/Functional/Import/WebspaceImportTest.php
@@ -11,8 +11,10 @@
 
 namespace Sulu\Bundle\ContentBundle\Tests\Functional\Import;
 
+use Sulu\Bundle\ContentBundle\Document\BasePageDocument;
 use Sulu\Bundle\ContentBundle\Document\PageDocument;
 use Sulu\Bundle\TestBundle\Testing\SuluTestCase;
+use Sulu\Component\Content\Import\WebspaceImport;
 use Sulu\Component\DocumentManager\DocumentManagerInterface;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Filesystem\Filesystem;
@@ -30,15 +32,20 @@ class WebspaceImportTest extends SuluTestCase
     /**
      * @var object
      */
-    private $parent;
+    private $homeDocument;
 
     private $pages = [];
 
+    /**
+     * @var WebspaceImport
+     */
     private $webspaceImporter;
 
     protected $distPath = '/../../app/Resources/import/export.xliff.dist';
+    protected $distPathRU = '/../../app/Resources/import/export_ru.xliff.dist';
 
     protected $path = '/../../app/Resources/import/export.xliff';
+    protected $pathRU = '/../../app/Resources/import/export_ru.xliff';
 
     /**
      * Setup data for import.
@@ -123,6 +130,84 @@ class WebspaceImportTest extends SuluTestCase
         $this->assertEquals($loadedDocuments[0]->getPath(), '/cmf/sulu_io/contents/test-0-imported');
         $this->assertEquals($loadedDocuments[1]->getPath(), '/cmf/sulu_io/contents/test-1-imported');
 
+        // resource segment
+        $this->assertEquals($loadedDocuments[0]->getResourceSegment(), '/parent');
+        $this->assertEquals($loadedDocuments[1]->getResourceSegment(), '/child');
+
+        // seo
+        $this->assertEquals($loadedDocuments[0]->getExtensionsData()->toArray()['seo']['title'], '');
+        $this->assertEquals($loadedDocuments[1]->getExtensionsData()->toArray()['seo']['title'], 'SEO Title');
+
+        // excerpt
+        $this->assertEquals($loadedDocuments[0]->getExtensionsData()->toArray()['excerpt']['title'], 'Excerpt title');
+        $this->assertEquals($loadedDocuments[1]->getExtensionsData()->toArray()['excerpt']['title'], '');
+    }
+
+    /**
+     * Run tests for language "ru" import:
+     * - import data
+     * - get documents
+     * - test document data.
+     */
+    public function testImport12XliffRU()
+    {
+        // run language import
+        // import it to FR (because RU isn't initialized)
+        $importData = [
+            'webspaceKey' => 'sulu_io',
+            'locale' => 'fr',
+            'format' => '1.2.xliff',
+            'filePath' => __DIR__ . $this->pathRU,
+        ];
+
+        $import = $this->webspaceImporter->import(
+            $importData['webspaceKey'],
+            $importData['locale'],
+            $importData['filePath'],
+            null,
+            $importData['format'],
+            '',
+            false
+        );
+
+        // testing imported data
+        $loadedDocuments = [];
+
+        /** @var BasePageDocument $document */
+        $loadedDocuments[0] = $this->documentManager->find(
+            $this->pages[0]->getUuid(),
+            'fr',
+            [
+                'type' => 'page',
+                'load_ghost_content' => false,
+            ]
+        );
+
+        /** @var BasePageDocument $document */
+        $loadedDocuments[1] = $this->documentManager->find(
+            $this->pages[1]->getUuid(),
+            'fr',
+            [
+                'type' => 'page',
+                'load_ghost_content' => false,
+            ]
+        );
+
+        // import
+        $this->assertEquals($import->successes, 2);
+
+        // structure
+        $this->assertEquals($loadedDocuments[0]->getTitle(), 'привет');
+        $this->assertEquals($loadedDocuments[1]->getTitle(), 'привет привет привет');
+
+        // path
+        $this->assertEquals($loadedDocuments[0]->getPath(), '/cmf/sulu_io/contents/parent');
+        $this->assertEquals($loadedDocuments[1]->getPath(), '/cmf/sulu_io/contents/child');
+
+        // resource segment
+        $this->assertEquals($loadedDocuments[0]->getResourceSegment(), '/parent-ru-new-shit');
+        $this->assertEquals($loadedDocuments[1]->getResourceSegment(), '/child-ru-new-shit');
+
         // seo
         $this->assertEquals($loadedDocuments[0]->getExtensionsData()->toArray()['seo']['title'], '');
         $this->assertEquals($loadedDocuments[1]->getExtensionsData()->toArray()['seo']['title'], 'SEO Title');
@@ -141,6 +226,7 @@ class WebspaceImportTest extends SuluTestCase
             $fs = new Filesystem();
 
             $fs->remove(__DIR__ . $this->path);
+            $fs->remove(__DIR__ . $this->pathRU);
         } catch (IOExceptionInterface $e) {
             echo 'An error occurred while creating your directory at ' . $e->getPath();
         }
@@ -166,6 +252,23 @@ class WebspaceImportTest extends SuluTestCase
             ], $distContent);
 
             file_put_contents(__DIR__ . $this->path, $newContent);
+        } catch (IOExceptionInterface $e) {
+            echo 'An error occurred while creating your directory at ' . $e->getPath();
+        }
+
+        try {
+            $fs->copy(__DIR__ . $this->distPathRU, __DIR__ . $this->pathRU);
+
+            $distContent = file_get_contents(__DIR__ . $this->pathRU, true);
+            $newContent = str_replace([
+                '%uuid_page_0%',
+                '%uuid_page_1%',
+            ], [
+                $this->pages[0]->getUuid(),
+                $this->pages[1]->getUuid(),
+            ], $distContent);
+
+            file_put_contents(__DIR__ . $this->pathRU, $newContent);
         } catch (IOExceptionInterface $e) {
             echo 'An error occurred while creating your directory at ' . $e->getPath();
         }
