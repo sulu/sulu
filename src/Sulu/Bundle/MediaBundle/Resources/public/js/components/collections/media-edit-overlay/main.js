1be1204fb253c81e5415565afdaf86ab2a417031
Dmitry Prokopenko <hellsigner@gmail.com>
2018-03-06 08:30:06 +0200

    Fix forgotten context binding for resetPreviewUrl method (#3819)
    * Fix#3819: source JS
    
    * Fix#3819: dist JS
    
    * Updated changelog
    
    * fix additional code styling

--- src/Sulu/Bundle/MediaBundle/Resources/public/js/components/collections/media-edit-overlay/main.js
+++ src/Sulu/Bundle/MediaBundle/Resources/public/js/components/collections/media-edit-overlay/main.js
@@ -1037,7 +1037,7 @@ define([
             var mediaId = this.media.id;
 
             $.ajax({
-                url: resetPreviewUrl(mediaId),
+                url: resetPreviewUrl.call(this, mediaId),
                 type: 'DELETE',
                 success: function() {
                     this.previewImageChangeHandler.call(this);
