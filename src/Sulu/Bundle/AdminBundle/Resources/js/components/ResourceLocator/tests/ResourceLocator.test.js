27692ce3c4721cc9a4a700eb5c409e087daa7e55
Patrick Hopfner <patrick.hopfner@gmail.com>
2018-03-08 08:14:29 +0100

    Renamed also in ResourceLocator component

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/ResourceLocator/tests/ResourceLocator.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/ResourceLocator/tests/ResourceLocator.test.js
@@ -6,14 +6,14 @@ import ResourceLocator from '../ResourceLocator';
 test('ResourceLocator should render with type full', () => {
     const onChange = jest.fn();
     const value = '/parent';
-    expect(render(<ResourceLocator value={value} mode="full" onChange={onChange} onFinish={jest.fn()} />))
+    expect(render(<ResourceLocator value={value} mode="full" onChange={onChange} onBlur={jest.fn()} />))
         .toMatchSnapshot();
 });
 
 test('ResourceLocator should render with type leaf', () => {
     const onChange = jest.fn();
     const value = '/parent/child';
-    expect(render(<ResourceLocator value={value} mode="leaf" onChange={onChange} onFinish={jest.fn()} />))
+    expect(render(<ResourceLocator value={value} mode="leaf" onChange={onChange} onBlur={jest.fn()} />))
         .toMatchSnapshot();
 });
 
@@ -21,7 +21,7 @@ test('ResourceLocator should call the onChange callback when the input changes w
     const onChange = jest.fn();
     const value = '/parent';
     const resourceLocator = mount(
-        <ResourceLocator value={value} mode="full" onChange={onChange} onFinish={jest.fn()} />
+        <ResourceLocator value={value} mode="full" onChange={onChange} onBlur={jest.fn()} />
     );
     resourceLocator.find('Input').props().onChange('parent-new');
     expect(onChange).toHaveBeenCalledWith('/parent-new');
@@ -31,17 +31,17 @@ test('ResourceLocator should call the onChange callback when the input changes w
     const onChange = jest.fn();
     const value = '/parent/child';
     const resourceLocator = mount(
-        <ResourceLocator value={value} mode="leaf" onChange={onChange} onFinish={jest.fn()} />
+        <ResourceLocator value={value} mode="leaf" onChange={onChange} onBlur={jest.fn()} />
     );
     resourceLocator.find('Input').props().onChange('child-new');
     expect(onChange).toHaveBeenCalledWith('/parent/child-new');
 });
 
-test('ResourceLocator should call the onFinish callback when the Input finishes editing', () => {
+test('ResourceLocator should call the onBlur callback when the Input finishes editing', () => {
     const finishSpy = jest.fn();
 
     const resourceLocator = shallow(
-        <ResourceLocator value="/some/url" mode="leaf" onChange={jest.fn()} onFinish={finishSpy} />
+        <ResourceLocator value="/some/url" mode="leaf" onChange={jest.fn()} onBlur={finishSpy} />
     );
 
     resourceLocator.find('Input').simulate('blur');
