27a26ffe32b84e400887f937d6680a96da15e10e
Daniel Rotter <daniel.rotter@gmail.com>
2018-05-19 11:45:55 +0200

    update to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,15 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 /* eslint-disable import/no-nodejs-modules*/
 const path = require('path');
-const CleanObsoleteChunksPlugin = require('webpack-clean-obsolete-chunks');
-const webpack = require('webpack');
 const glob = require('glob');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const CleanObsoleteChunksPlugin = require('webpack-clean-obsolete-chunks');
 const ManifestPlugin = require('webpack-manifest-plugin');
 
 const entries = glob.sync(
     path.resolve(__dirname, 'src/Sulu/Bundle/*/Resources/js/index.js') // eslint-disable-line no-undef
 );
 const entriesCount = entries.length;
-const basePath = 'admin/build';
 
 entries.unshift('core-js/fn/array/includes');
 entries.unshift('core-js/fn/array/find-index');
@@ -22,58 +20,56 @@ entries.unshift('core-js/fn/symbol');
 entries.unshift('whatwg-fetch');
 entries.unshift('url-search-params-polyfill');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const basePath = 'admin/build';
+
 module.exports = { // eslint-disable-line no-undef
     entry: entries,
     output: {
         path: path.resolve('web'),
         filename: basePath + '/[name].[chunkhash].js',
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            BUNDLE_ENTRIES_COUNT: entriesCount,
+        }),
+        new MiniCssExtractPlugin({
+            filename: basePath + '/[name].[chunkhash].css',
+        }),
+        new ManifestPlugin({
+            fileName: basePath + '/manifest.json',
+        }),
+        new CleanObsoleteChunksPlugin(),
+    ],
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.(scss)$/,
-                use: ExtractTextPlugin.extract({
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: true,
-                                importLoaders: 1,
-                                camelCase: true,
-                                localIdentName: '[local]--[hash:base64:10]',
-                            },
-                        },
-                        'postcss-loader',
-                    ],
-                }),
+                use: 'babel-loader',
             },
             {
                 test: /\.css/,
-                use: ExtractTextPlugin.extract({
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: false,
-                            },
-                        },
-                    ],
-                }),
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
             },
             {
-                test: /\.(jpg|gif|png)(\?.*$|$)/,
+                test: /\.(scss)$/,
                 use: [
+                    MiniCssExtractPlugin.loader,
                     {
-                        loader: 'file-loader',
+                        loader: 'css-loader',
                         options: {
-                            name: '/' + basePath + '/images/[name].[hash].[ext]',
+                            modules: true,
+                            importLoaders: 1,
+                            camelCase: true,
+                            localeIdentName: '[local]--[hash:base64:10]',
                         },
                     },
+                    'postcss-loader',
                 ],
             },
             {
@@ -87,16 +83,17 @@ module.exports = { // eslint-disable-line no-undef
                     },
                 ],
             },
+            {
+                test: /\.(jpg|gif|png)(\?.*$|$)/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '/' + basePath + '/images/[name].[hash].[ext]',
+                        },
+                    },
+                ],
+            },
         ],
     },
-    plugins: [
-        new CleanObsoleteChunksPlugin(),
-        new webpack.DefinePlugin({
-            BUNDLE_ENTRIES_COUNT: entriesCount,
-        }),
-        new ManifestPlugin({
-            fileName: basePath + '/manifest.json',
-        }),
-        new ExtractTextPlugin(basePath + '/main.[hash].css'),
-    ],
 };
