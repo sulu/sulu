2e1a4cf3ea3c9a9f55eba2a36361918d9dcfe4d9
Patrick Hopfner <patrick.hopfner@gmail.com>
2018-05-25 15:45:52 +0200

    Corrected default

--- src/Sulu/Component/Rest/ListBuilder/Metadata/General/Driver/XmlDriver.php
+++ src/Sulu/Component/Rest/ListBuilder/Metadata/General/Driver/XmlDriver.php
@@ -114,7 +114,7 @@ class XmlDriver extends AbstractFileDriver implements DriverInterface
                 '@visibility',
                 $xpath,
                 $propertyNode,
-                FieldDescriptorInterface::VISIBILITY_YES
+                FieldDescriptorInterface::VISIBILITY_NO
             )
         );
         $propertyMetadata->setSearchability(
