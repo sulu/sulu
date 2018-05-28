f13333bf833fdea2270f6f5462d8be87c284f473
Daniel Rotter <daniel.rotter@gmail.com>
2018-05-28 10:22:15 +0200

    fix tests for latest symfony version

--- src/Sulu/Bundle/TestBundle/DependencyInjection/SuluTestExtension.php
+++ src/Sulu/Bundle/TestBundle/DependencyInjection/SuluTestExtension.php
@@ -16,11 +16,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-/**
- * This is the class that loads and manages your bundle configuration.
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
 class SuluTestExtension extends Extension
 {
     /**
