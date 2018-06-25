7cd0bbe7a5d1b0ace3d41f51b370c4b4bf8da1fa
Daniel Rotter <daniel.rotter@gmail.com>
2018-06-25 13:21:41 +0200

    copy and adjust official ckeditor react wrapper

---
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/TextEditor/README.md
@@ -0,0 +1,6 @@
+This component uses the [CKEditor 5](https://ckeditor.com/ckeditor-5/) to display a text editor.
+
+```javascript
+const handleChange = (data) => console.log(data);
+<TextEditor onChange={handleChange} />
+```
---
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/TextEditor/TextEditor.js
@@ -0,0 +1,79 @@
+// @flow
+import React from 'react';
+import type {ElementRef} from 'react';
+import log from 'loglevel';
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials';
+
+type Props = {
+    data: string,
+    onChange: (data: string) => void,
+};
+
+export default class TextEditor extends React.Component<Props> {
+    domContainer: ?ElementRef<'div'>;
+    editorInstance: any;
+
+    static defaultProps = {
+        data: '',
+        config: {},
+    };
+
+    constructor(props: Props) {
+        super(props);
+
+        this.editorInstance = null;
+    }
+
+    setDomContainerRef = (domContainer: ?ElementRef<'div'>) => {
+        this.domContainer = domContainer;
+    };
+
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    componentDidUpdate() {
+        const {data} = this.props;
+        if (this.editorInstance && data) {
+            this.editorInstance.setData(data);
+        }
+    }
+
+    componentDidMount() {
+        ClassicEditor
+            .create(this.domContainer, {
+                plugins: [
+                    EssentialsPlugin,
+                ],
+            })
+            .then((editor) => {
+                this.editorInstance = editor;
+
+                // TODO: Pass data via constructor.
+                this.editorInstance.setData(this.props.data);
+
+                if (this.props.onChange) {
+                    const document = this.editorInstance.model.document;
+                    document.on('change', () => {
+                        if (document.differ.getChanges().length > 0) {
+                            this.props.onChange(editor.getData());
+                        }
+                    });
+                }
+            })
+            .catch((error) => {
+                log.error(error);
+            });
+    }
+
+    componentWillUnmount() {
+        if (this.editorInstance) {
+            this.editorInstance.destroy();
+        }
+    }
+
+    render() {
+        return <div ref={this.setDomContainerRef}></div>;
+    }
+}
