ee4b707174d82e64e55f3b04c2c03915cbe9c9fa
Daniel Rotter <daniel.rotter@gmail.com>
2018-08-14 16:47:32 +0200

    export classes required to build own ToolbarActions in bundles

--- src/Sulu/Bundle/AdminBundle/Resources/js/views/index.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/views/index.js
@@ -1,6 +1,9 @@
 // @flow
 import ResourceTabs from './ResourceTabs';
+import {AbstractToolbarAction, toolbarActionRegistry} from './Form';
 
 export {
+    AbstractToolbarAction,
     ResourceTabs,
+    toolbarActionRegistry,
 };
