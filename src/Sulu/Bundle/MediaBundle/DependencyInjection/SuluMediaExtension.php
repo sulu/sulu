efbac94a9a1ec12c0fcb6003835085fd6e75ed62
Alexander Schranz <alexander@sulu.io>
2018-09-20 19:42:31 +0200

    fix image formats folder path

--- src/Sulu/Bundle/MediaBundle/DependencyInjection/SuluMediaExtension.php
+++ src/Sulu/Bundle/MediaBundle/DependencyInjection/SuluMediaExtension.php
@@ -54,7 +54,7 @@ class SuluMediaExtension extends Extension implements PrependExtensionInterface
                         ],
                     ],
                     'image_format_files' => [
-                        '%kernel.root_dir%/config/image-formats.xml',
+                        '%kernel.project_dir%/config/image-formats.xml',
                         __DIR__ . '/../Resources/config/image-formats.xml',
                     ],
                     'search' => ['enabled' => $container->hasExtension('massive_search')],
