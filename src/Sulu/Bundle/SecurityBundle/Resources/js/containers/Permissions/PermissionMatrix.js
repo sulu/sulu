bacfebe285133ac7a6de93821f127a7a8abf8f91
Patrick Hopfner <patrick.hopfner@gmail.com>
2018-09-21 14:43:03 +0200

    Code clean up

---
+++ src/Sulu/Bundle/SecurityBundle/Resources/js/containers/Permissions/PermissionMatrix.js
@@ -0,0 +1,129 @@
+// @flow
+import React from 'react';
+import {observer} from 'mobx-react';
+import permissionsStyle from "./permissions.scss";
+import Matrix from "../../../../../AdminBundle/Resources/js/components/Matrix";
+import {translate} from "../../../../../AdminBundle/Resources/js/utils/Translator";
+import type {Actions, SecurityContexts} from '../../stores/SecurityContextsStore/types';
+import type {MatrixValues} from '../../../../../AdminBundle/Resources/js/components/Matrix/types';
+import type {ContextPermission} from './types';
+
+type Props = {
+    contextPermissions: Array<ContextPermission>,
+    onChange: (value: Array<ContextPermission>) => void,
+    securityContexts: SecurityContexts,
+    subTitle?: string,
+    title?: string,
+};
+
+@observer
+export default class PermissionMatrix extends React.Component<Props> {
+    getIcon = (action: string) => {
+        switch (action) {
+            case 'view':
+                return 'su-eye';
+            case 'add':
+                return 'su-plus-circle';
+            case 'edit':
+                return 'su-pen';
+            case 'delete':
+                return 'su-trash-alt';
+            case 'security':
+                return 'su-lock';
+            case 'live':
+                return 'fa-signal';
+            default:
+                throw new Error('No icon defined for "' + action + '"');
+        }
+    };
+
+    getMatrixValueFromContextPermission = (searchedKey: string) => {
+        const {contextPermissions} = this.props;
+        if (!contextPermissions) {
+            return;
+        }
+
+        for (const contextPermission of contextPermissions) {
+            if (searchedKey === contextPermission.context) {
+                return contextPermission.permissions;
+            }
+        }
+    };
+
+    handleMatrixChange = (matrixValues: MatrixValues) => {
+        const {onChange, contextPermissions} = this.props;
+
+        Object.keys(matrixValues).map((matrixValuesKey) => {
+            const matrixValue = matrixValues[matrixValuesKey];
+
+            let valueSet = false;
+            for (const contextPermission of contextPermissions) {
+                if (matrixValuesKey === contextPermission.context) {
+                    contextPermission.permissions = matrixValue;
+
+                    valueSet = true;
+                    break;
+                }
+            }
+
+            if (!valueSet) {
+                const newContextPermission: ContextPermission = {
+                    'id': undefined,
+                    'context': matrixValuesKey,
+                    'permissions': matrixValue,
+                };
+                contextPermissions.push(newContextPermission);
+            }
+        });
+
+        onChange(contextPermissions);
+    };
+    
+    renderMatrixRow(rowIndex: number, securityContextKey: string, actions: Actions) {
+        const secondPointPosition = securityContextKey.indexOf('.', securityContextKey.indexOf('.') + 1) + 1;
+        const title = securityContextKey.substring(secondPointPosition);
+
+        return (
+            <Matrix.Row key={'row-' + rowIndex} name={securityContextKey} title={title}>
+                {actions.map((action, itemIndex) => (
+                    <Matrix.Item
+                        icon={this.getIcon(action)}
+                        key={'item-' + itemIndex}
+                        name={action}
+                        title={translate('sulu_security.' + action)}
+                    />
+                ))}
+            </Matrix.Row>
+        );
+    }
+
+    render() {
+        const {title, subTitle, securityContexts} = this.props;
+        const matrixValues = {};
+        const matrixRows = [];
+
+        Object.keys(securityContexts).map((securityContextKey, rowIndex) => {
+            const actions = securityContexts[securityContextKey];
+            matrixValues[securityContextKey] = this.getMatrixValueFromContextPermission(securityContextKey);
+
+            matrixRows.push(this.renderMatrixRow(rowIndex, securityContextKey, actions));
+        });
+
+        return (
+            <div className={permissionsStyle.matrixContainer}>
+                {title &&
+                    <h2>{title}</h2>
+                }
+                {subTitle &&
+                    <h3>{subTitle}</h3>
+                }
+                <Matrix
+                    onChange={this.handleMatrixChange}
+                    values={matrixValues}
+                >
+                    {matrixRows}
+                </Matrix>
+            </div>
+        );
+    }
+}
---
+++ src/Sulu/Bundle/SecurityBundle/Resources/js/containers/Permissions/Permissions.js
@@ -0,0 +1,229 @@
+// @flow
+import React, {Fragment} from 'react';
+import {action, computed, observable, toJS} from 'mobx';
+import {observer} from 'mobx-react';
+import {Loader, Matrix} from 'sulu-admin-bundle/components';
+import MultiSelect from 'sulu-admin-bundle/components/MultiSelect/MultiSelect';
+import webspaceStore from 'sulu-content-bundle/stores/WebspaceStore';
+import type {Webspace} from 'sulu-content-bundle/stores/WebspaceStore/types';
+import securityContextsStore from '../../stores/SecurityContextsStore';
+import type {SecurityContextGroups, SecurityContexts} from '../../stores/SecurityContextsStore/types';
+import type {ContextPermission} from './types';
+import permissionsStyle from './permissions.scss';
+import PermissionMatrix from './PermissionMatrix';
+
+type Props = {
+    system: string,
+    onChange: (value: Array<ContextPermission>) => void,
+    value: Array<ContextPermission>,
+};
+
+@observer
+export default class Permissions extends React.Component<Props> {
+    static webspacePlaceholder = '#webspace#';
+
+    @observable securityContextGroups: SecurityContextGroups = {};
+    @observable availableWebspaces: Array<Webspace> = [];
+
+    @computed get webspaceContextPermissionPrefix(): string {
+        if (this.webspaceSecurityContextGroupKey) {
+            const securityContextGroup = this.securityContextGroups[this.webspaceSecurityContextGroupKey];
+            for (const securityContextKey of Object.keys(securityContextGroup)) {
+                if (securityContextKey.indexOf(Permissions.webspacePlaceholder) !== -1) {
+                    return securityContextKey.substring(0, securityContextKey.indexOf('#'));
+                }
+            }
+        }
+
+        throw new Error('Webspace context permission prefix not found');
+    }
+
+    @computed get webspaceSecurityContextGroupKey(): ?string {
+        for (const securityContextGroupKey of Object.keys(this.securityContextGroups)) {
+            const securityContextGroup = this.securityContextGroups[securityContextGroupKey];
+            for (const securityContextKey of Object.keys(securityContextGroup)) {
+                if (securityContextKey.indexOf(Permissions.webspacePlaceholder) !== -1) {
+                    return securityContextGroupKey;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    @computed get selectedWebspaces(): Array<string> {
+        if (!this.props.value) {
+            return [];
+        }
+
+        const selectedWebspaces = [];
+        for (const contextPermission of this.props.value) {
+            if (contextPermission.context.startsWith(this.webspaceContextPermissionPrefix)) {
+                const webspaceKey = contextPermission.context.replace(this.webspaceContextPermissionPrefix, '');
+
+                if (webspaceKey.indexOf('.') !== -1) {
+                    continue;
+                }
+
+                selectedWebspaces.push(webspaceKey);
+            }
+        }
+
+        return selectedWebspaces.sort();
+    }
+
+    @action componentDidMount() {
+        securityContextsStore.loadSecurityContextGroups(this.props.system).then(action((securityContextGroups) => {
+            this.securityContextGroups = securityContextGroups;
+
+            if (this.securityContextGroups.hasOwnProperty(this.webspaceSecurityContextGroupKey)) {
+                webspaceStore.loadAllWebspaces().then(action((webspaces) => {
+                    this.availableWebspaces = webspaces;
+                }));
+            }
+        }));
+    }
+
+    getWebspaceSecurityContexts(webspace: string): SecurityContexts {
+        if (!this.webspaceSecurityContextGroupKey) {
+            return {};
+        }
+
+        const webspaceSecurityContextGroup = this.securityContextGroups[this.webspaceSecurityContextGroupKey];
+
+        const securityContexts = {};
+
+        Object.keys(webspaceSecurityContextGroup).sort().map((securityContextKey) => {
+            securityContexts[securityContextKey.replace(Permissions.webspacePlaceholder, webspace)]
+                = webspaceSecurityContextGroup[securityContextKey];
+        });
+
+        return securityContexts;
+    }
+
+    handleChange = (value: Array<ContextPermission>) => {
+        const {onChange} = this.props;
+
+        onChange(value);
+    };
+
+    @action handleWebspaceSelectChange = (newSelectedWebspaces: Array<string | number>) => {
+        const {value} = this.props;
+        const contextPermissions = value ? value : [];
+        const newContextPermissions = [];
+
+        for (const contextPermission of contextPermissions) {
+            if (contextPermission.context.startsWith(this.webspaceContextPermissionPrefix)) {
+                const suffix = contextPermission.context.replace(this.webspaceContextPermissionPrefix, '');
+                const webspaceKey = suffix.indexOf('.') === -1 ? suffix : suffix.substring(0, suffix.indexOf('.'));
+
+                if (newSelectedWebspaces.indexOf(webspaceKey) === -1) {
+                    continue;
+                }
+            }
+
+            newContextPermissions.push(contextPermission);
+        }
+
+        const webspacesToAdd = newSelectedWebspaces.filter((newSelectedWebspace) => {
+            return this.selectedWebspaces.indexOf(newSelectedWebspace) < 0;
+        });
+        for (const webspaceToAdd of webspacesToAdd) {
+            const securityContexts = this.getWebspaceSecurityContexts(webspaceToAdd.toString());
+
+            Object.keys(securityContexts).map((securityContextKey) => {
+                const permissions = {};
+                const actions = securityContexts[securityContextKey];
+
+                for (const action of actions) {
+                    permissions[action] = false;
+                }
+
+                const newContextPermission: ContextPermission = {
+                    'id': undefined,
+                    'context': securityContextKey,
+                    'permissions': permissions,
+                };
+                newContextPermissions.push(newContextPermission);
+            });
+        }
+
+        this.handleChange(newContextPermissions);
+    };
+
+    renderWebspaceSelect() {
+        if (!this.availableWebspaces || this.availableWebspaces.length < 1) {
+            return <Loader />;
+        }
+
+        return (
+            <div className={permissionsStyle.selectContainer}>
+                <MultiSelect
+                    allSelectedText="All selected"
+                    noneSelectedText="None selected"
+                    onChange={this.handleWebspaceSelectChange}
+                    values={this.selectedWebspaces}
+                >
+                    {this.availableWebspaces.map((webspace, index) => (
+                        <MultiSelect.Option key={index} value={webspace.key}>{webspace.name}</MultiSelect.Option>
+                    ))}
+                </MultiSelect>
+            </div>
+        );
+    }
+
+    renderWebspaceMatrixes() {
+        if (!this.webspaceSecurityContextGroupKey) {
+            return null;
+        }
+
+        return (
+            <Fragment>
+                <h2>{this.webspaceSecurityContextGroupKey}</h2>
+                {this.renderWebspaceSelect()}
+                <div className={permissionsStyle.matrixContainer}>
+                    {this.selectedWebspaces.map((webspace, matrixIndex) => {
+                        return (
+                            <PermissionMatrix
+                                key={matrixIndex}
+                                contextPermissions={this.props.value}
+                                onChange={this.handleChange}
+                                securityContexts={this.getWebspaceSecurityContexts(webspace)}
+                                subTitle={webspace}
+                            />
+                        );
+                    })}
+                </div>
+            </Fragment>
+        );
+    }
+
+    render() {
+        if (!this.securityContextGroups) {
+            return <Loader />;
+        }
+
+        return (
+            <Fragment>
+                {this.renderWebspaceMatrixes()}
+                {Object.keys(this.securityContextGroups).sort().map((securityContextGroupKey, matrixIndex) => {
+                    if (this.webspaceSecurityContextGroupKey
+                        && this.webspaceSecurityContextGroupKey === securityContextGroupKey
+                    )  {
+                        return null;
+                    }
+
+                    return (
+                        <PermissionMatrix
+                            key={matrixIndex}
+                            contextPermissions={this.props.value}
+                            onChange={this.handleChange}
+                            securityContexts={this.securityContextGroups[securityContextGroupKey]}
+                            title={securityContextGroupKey}
+                        />
+                    );
+                })}
+            </Fragment>
+        );
+    }
+}
