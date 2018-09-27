06e9b11b498427b75b72b8a39142851ce8ac7ce8
Patrick Hopfner <patrick.hopfner@gmail.com>
2018-09-27 13:18:28 +0200

    Fix StyleCI

--- src/Sulu/Bundle/AdminBundle/Controller/AdminController.php
+++ src/Sulu/Bundle/AdminBundle/Controller/AdminController.php
@@ -35,7 +35,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Translation\TranslatorBagInterface;
 
