1e37c015e7f4fd750a0b0fd2b9cf2490f60e1ca2
Niklas Natter <niklas@sulu.io>
2018-11-05 12:53:18 +0100

    implemented disabled property of number component and added test case

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Number/tests/Number.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Number/tests/Number.test.js
@@ -7,6 +7,10 @@ test('Number should render', () => {
     expect(render(<Number onChange={jest.fn()} value={undefined} />)).toMatchSnapshot();
 });
 
+test('Number should render when disabled', () => {
+    expect(render(<Number disabled={true} onChange={jest.fn()} value={8} />)).toMatchSnapshot();
+});
+
 test('Number should call onChange with parsed value', () => {
     const onChange = jest.fn();
     const number = shallow(<Number onChange={onChange} value={undefined} />);
