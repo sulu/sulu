0761d2d10965d2caae07696b83a9cd4a213e1436
Niklas Natter <niklas@sulu.io>
2018-11-26 16:33:44 +0100

    adjusted snapshot tests to updated media-selection-item structure

--- src/Sulu/Bundle/MediaBundle/Resources/js/components/MediaSelectionItem/tests/MediaSelectionItem.test.js
+++ src/Sulu/Bundle/MediaBundle/Resources/js/components/MediaSelectionItem/tests/MediaSelectionItem.test.js
@@ -14,10 +14,34 @@ test('Should render a MediaSelectionItem', () => {
     )).toMatchSnapshot();
 });
 
+test('Should render a MediaSelectionItem with custom size and margin', () => {
+    expect(render(
+        <MediaSelectionItem
+            mimeType="application/vnd.ms-excel"
+            thumbnail="http://lorempixel.com/25/25"
+            thumbnailMargin={10}
+            thumbnailSize={15}
+        >
+            test media
+        </MediaSelectionItem>
+    )).toMatchSnapshot();
+});
+
 test('Should render a MediaSelectionItem without thumbnail', () => {
     expect(render(
         <MediaSelectionItem
             mimeType="application/vnd.ms-excel"
+        >
+            test media
+        </MediaSelectionItem>
+    )).toMatchSnapshot();
+});
+
+test('Should render a MediaSelectionItem with custom size and without thumbnail', () => {
+    expect(render(
+        <MediaSelectionItem
+            mimeType="application/vnd.ms-excel"
+            thumbnailSize={40}
         >
             test media
         </MediaSelectionItem>
