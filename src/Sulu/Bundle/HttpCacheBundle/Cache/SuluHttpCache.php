71a89e9872813de2e347c13af1fd7a795ab9682f
Patrick Hopfner <patrick.hopfner@gmail.com>
2019-01-07 17:04:22 +0100

    Add tag invalidation for Symfony http cache

--- src/Sulu/Bundle/HttpCacheBundle/Cache/SuluHttpCache.php
+++ src/Sulu/Bundle/HttpCacheBundle/Cache/SuluHttpCache.php
@@ -17,6 +17,7 @@ use FOS\HttpCache\SymfonyCache\CustomTtlListener;
 use FOS\HttpCache\SymfonyCache\DebugListener;
 use FOS\HttpCache\SymfonyCache\EventDispatchingHttpCache;
 use FOS\HttpCache\SymfonyCache\PurgeListener;
+use FOS\HttpCache\SymfonyCache\CleanupCacheTagsListener;
 use FOS\HttpCache\SymfonyCache\PurgeTagsListener;
 use FOS\HttpCache\TagHeaderFormatter\TagHeaderFormatter;
 use Sulu\Component\HttpKernel\SuluKernel;
