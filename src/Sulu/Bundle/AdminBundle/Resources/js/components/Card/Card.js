817fada52c037cc28910392144b6ff6211cc79f2
Daniel Rotter <daniel.rotter@gmail.com>
2019-01-23 15:53:59 +0100

    Add id to Card to pass to callbacks

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Card/Card.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Card/Card.js
@@ -4,21 +4,38 @@ import type {Node} from 'react';
 import Icon from '../Icon';
 import cardStyles from './card.scss';
 
-type Props = {|
+type Props<T> = {|
     children: Node,
-    onEdit?: () => void,
-    onRemove?: () => void,
+    id?: T,
+    onEdit?: (?T) => void,
+    onRemove?: (?T) => void,
 |};
 
-export default class Card extends React.Component<Props> {
+export default class Card<T: string | number> extends React.Component<Props<T>> {
+    handleEditClick = () => {
+        const {id, onEdit} = this.props;
+
+        if (onEdit) {
+            onEdit(id);
+        }
+    };
+
+    handleRemoveClick = () => {
+        const {id, onRemove} = this.props;
+
+        if (onRemove) {
+            onRemove(id);
+        }
+    };
+
     render() {
         const {children, onEdit, onRemove} = this.props;
 
         return (
             <section className={cardStyles.card}>
                 <div className={cardStyles.icons}>
-                    {onEdit && <Icon name="su-pen" onClick={onEdit} />}
-                    {onRemove && <Icon name="su-trash-alt" onClick={onRemove} />}
+                    {onEdit && <Icon name="su-pen" onClick={this.handleEditClick} />}
+                    {onRemove && <Icon name="su-trash-alt" onClick={this.handleRemoveClick} />}
                 </div>
                 {children}
             </section>
--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Card/tests/Card.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Card/tests/Card.test.js
@@ -13,18 +13,18 @@ test('Render a card with its children, edit and remove icon', () => {
 
 test('Call onEdit callback when edit icon is clicked', () => {
     const editSpy = jest.fn();
-    const card = shallow(<Card onEdit={editSpy}>Content</Card>);
+    const card = shallow(<Card id={6} onEdit={editSpy}>Content</Card>);
 
     card.find('Icon[name="su-pen"]').simulate('click');
 
-    expect(editSpy).toBeCalledWith();
+    expect(editSpy).toBeCalledWith(6);
 });
 
 test('Call onRemove callback when remove icon is clicked', () => {
     const removeSpy = jest.fn();
-    const card = shallow(<Card onRemove={removeSpy}>Content</Card>);
+    const card = shallow(<Card id={2} onRemove={removeSpy}>Content</Card>);
 
     card.find('Icon[name="su-trash-alt"]').simulate('click');
 
-    expect(removeSpy).toBeCalledWith();
+    expect(removeSpy).toBeCalledWith(2);
 });
