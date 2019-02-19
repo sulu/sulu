8e66e61b82632bef01c0318753cdf2583d34d04c
Markus Moosbrugger <markus.moosbrugger@outlook.com>
2019-02-19 17:40:09 +0100

    fixed coding style according to eslint and flow

--- src/Sulu/Bundle/AdminBundle/Resources/js/views/Datagrid/toolbarActions/ExportToolbarAction.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/views/Datagrid/toolbarActions/ExportToolbarAction.js
@@ -1,62 +1,61 @@
-import AbstractToolbarAction from "./AbstractToolbarAction";
-import {translate} from "../../../utils/Translator";
-import {action, observable} from "mobx";
-import Overlay from "../../../components/Overlay"
-import SingleSelect from "../../../components/SingleSelect"
-import Form from "../../../components/Form"
-import React, {Node} from "react";
-import type, {Action, Size} from "../../../components/Overlay/types";
-import exportToolbarActionStyles from './exportToolbarAction.scss';
+// @flow
+import {action, observable} from 'mobx';
+import React from 'react';
+import {translate} from '../../../utils/Translator';
+import Overlay from '../../../components/Overlay';
+import Form from '../../../components/Form';
+import SingleSelect from '../../../components/SingleSelect';
 import resourceEndpointRegistry from '../../../services/ResourceRequester/registries/ResourceEndpointRegistry';
 import {buildQueryString} from '../../../utils/Request';
+import exportToolbarActionStyles from './exportToolbarAction.scss';
+import AbstractToolbarAction from './AbstractToolbarAction';
 
 export default class ExportToolbarAction extends AbstractToolbarAction {
     @observable showOverlay = false;
-    @observable delimiter = 0;
-    @observable enclosure = 0;
-    @observable escape = 0;
-    @observable newLine = 0;
-
+    @observable delimiter: string;
+    @observable enclosure: string;
+    @observable escape: string;
+    @observable newLine: string;
 
     getNode() {
         return (
             <Overlay
-                title={translate('sulu_admin.export_overlay_title')}
                 confirmDisabled={false}
-                key="sulu_admin.export"
-                size={"small"}
                 confirmLoading={false}
                 confirmText={translate('sulu_admin.export')}
+                key="sulu_admin.export"
+                onClose={this.handleClose}
                 onConfirm={this.handleConfirm}
                 open={this.showOverlay}
-                onClose={this.handleClose}
+                size="small"
+                title={translate('sulu_admin.export_overlay_title')}
             >
                 <div className={exportToolbarActionStyles.overlay}>
                     <Form>
                         <Form.Section size={6}>
-                            <Form.Field label={translate('sulu_admin.delimiter')} description={translate('sulu_admin.delimiter_description')}>
-                                <SingleSelect value={this.delimiter} onChange={this.handleDelimiterChanged}>
-                                    <SingleSelect.Option value={';'}>;</SingleSelect.Option>
-                                    <SingleSelect.Option value={','}>,</SingleSelect.Option>
+                            <Form.Field description={translate('sulu_admin.delimiter_description')} label={translate('sulu_admin.delimiter')}>
+                                <SingleSelect onChange={this.handleDelimiterChanged} value={this.delimiter}>
+                                    <SingleSelect.Option value=";">;</SingleSelect.Option>
+                                    <SingleSelect.Option value=",">,</SingleSelect.Option>
                                     <SingleSelect.Option value={'\\t'}>\t</SingleSelect.Option>
                                 </SingleSelect>
                             </Form.Field>
-                            <Form.Field label={translate('sulu_admin.enclosure')} description={translate('sulu_admin.enclosure_description')}>
-                                <SingleSelect value={this.enclosure} onChange={this.handleEnclosureChanged}>
-                                    <SingleSelect.Option value={'\"'}>"</SingleSelect.Option>
-                                    <SingleSelect.Option value={' '}> </SingleSelect.Option>
+                            <Form.Field description={translate('sulu_admin.enclosure_description')} label={translate('sulu_admin.enclosure')}>
+                                <SingleSelect onChange={this.handleEnclosureChanged} value={this.enclosure}>
+                                    <SingleSelect.Option value={'"'}>"</SingleSelect.Option>
+                                    <SingleSelect.Option value=" "> </SingleSelect.Option>
                                 </SingleSelect>
                             </Form.Field>
                         </Form.Section>
                         <Form.Section size={6}>
-                            <Form.Field label={translate('sulu_admin.escape')} description={translate('sulu_admin.escape_description')}>
-                                <SingleSelect value={this.escape} onChange={this.handleEscapeChanged}>
+                            <Form.Field description={translate('sulu_admin.escape_description')} label={translate('sulu_admin.escape')}>
+                                <SingleSelect onChange={this.handleEscapeChanged} value={this.escape}>
                                     <SingleSelect.Option value={'\\'}>\</SingleSelect.Option>
-                                    <SingleSelect.Option value={'\"'}>"</SingleSelect.Option>
+                                    <SingleSelect.Option value={'"'}>\"</SingleSelect.Option>
                                 </SingleSelect>
                             </Form.Field>
-                            <Form.Field label={translate('sulu_admin.new_line')} description={translate('sulu_admin.new_line_description')}>
-                                <SingleSelect value={this.newLine} onChange={this.handleNewLineChanged}>
+                            <Form.Field description={translate('sulu_admin.new_line_description')} label={translate('sulu_admin.new_line')}>
+                                <SingleSelect onChange={this.handleNewLineChanged} value={this.newLine}>
                                     <SingleSelect.Option value={'\\n'}>\n</SingleSelect.Option>
                                     <SingleSelect.Option value={'\\r\\n'}>\r\n</SingleSelect.Option>
                                     <SingleSelect.Option value={'\\r'}>\r</SingleSelect.Option>
@@ -69,7 +68,6 @@ export default class ExportToolbarAction extends AbstractToolbarAction {
             </Overlay>
         );
     }
-
 
     getToolbarItemConfig() {
         return {
@@ -86,40 +84,40 @@ export default class ExportToolbarAction extends AbstractToolbarAction {
         this.showOverlay = false;
     };
 
-    @action handleDelimiterChanged = (value) => {
+    @action handleDelimiterChanged = (value: string) => {
         this.delimiter = value;
-    }
-    @action handleEnclosureChanged = (value) => {
+    };
+    @action handleEnclosureChanged = (value: string) => {
         this.enclosure = value;
-    }
-    @action handleEscapeChanged = (value) => {
+    };
+    @action handleEscapeChanged = (value: string) => {
         this.escape = value;
-    }
-    @action handleNewLineChanged = (value) => {
+    };
+    @action handleNewLineChanged = (value: string) => {
         this.newLine = value;
-    }
+    };
 
-    @action handleConfirm = (item: Object) => {
-        console.log(resourceEndpointRegistry.getEndpoint(this.datagridStore.resourceKey) + ".csv" + buildQueryString(
-          {
-              flat: true,
-              delimiter: this.delimiter,
-              escape: this.escape,
-              enclosure: this.enclosure,
-              newLine: this.newLine
-          }
-      ));
-
-        window.location.assign(resourceEndpointRegistry.getEndpoint(this.datagridStore.resourceKey) + ".csv" + buildQueryString(
+    @action handleConfirm = () => {
+        console.log(resourceEndpointRegistry.getEndpoint(this.datagridStore.resourceKey) + '.csv' + buildQueryString(
             {
                 flat: true,
                 delimiter: this.delimiter,
                 escape: this.escape,
                 enclosure: this.enclosure,
-                newLine: this.newLine
+                newLine: this.newLine,
             }
         ));
 
-       return
+        window.location.assign(resourceEndpointRegistry.getEndpoint(this.datagridStore.resourceKey) + '.csv' + buildQueryString(
+            {
+                flat: true,
+                delimiter: this.delimiter,
+                escape: this.escape,
+                enclosure: this.enclosure,
+                newLine: this.newLine,
+            }
+        ));
+
+        return;
     };
 }
