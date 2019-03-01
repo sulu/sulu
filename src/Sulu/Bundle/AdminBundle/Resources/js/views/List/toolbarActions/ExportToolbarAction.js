880d3184dc421099b0495cacd81ab778ac2cdba6
Daniel <daniel.mathis@sulu.io>
2019-03-01 09:28:31 +0100

    fixed coding style according to eslint and flow

--- src/Sulu/Bundle/AdminBundle/Resources/js/views/List/toolbarActions/ExportToolbarAction.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/views/List/toolbarActions/ExportToolbarAction.js
@@ -2,20 +2,20 @@
 import {action, observable} from 'mobx';
 import React from 'react';
 import {translate} from '../../../utils/Translator';
-import SingleSelect from '../../../components/SingleSelect';
 import Overlay from '../../../components/Overlay';
 import Form from '../../../components/Form';
-import {buildQueryString} from '../../../utils/Request';
+import SingleSelect from '../../../components/SingleSelect';
 import resourceEndpointRegistry from '../../../services/ResourceRequester/registries/ResourceEndpointRegistry';
-import AbstractToolbarAction from './AbstractToolbarAction';
+import {buildQueryString} from '../../../utils/Request';
 import exportToolbarActionStyles from './exportToolbarAction.scss';
+import AbstractToolbarAction from './AbstractToolbarAction';
 
 export default class ExportToolbarAction extends AbstractToolbarAction {
     @observable showOverlay = false;
-    @observable delimiter: string = ';';
-    @observable enclosure: string = '"';
-    @observable escape: string = '\\';
-    @observable newLine: string = '\\n';
+    @observable delimiter: string;
+    @observable enclosure: string;
+    @observable escape: string;
+    @observable newLine: string;
 
     getNode() {
         return (
@@ -27,7 +27,7 @@ export default class ExportToolbarAction extends AbstractToolbarAction {
                 onClose={this.handleClose}
                 onConfirm={this.handleConfirm}
                 open={this.showOverlay}
-                size={'small'}
+                size="small"
                 title={translate('sulu_admin.export_overlay_title')}
             >
                 <div className={exportToolbarActionStyles.overlay}>
@@ -38,8 +38,8 @@ export default class ExportToolbarAction extends AbstractToolbarAction {
                                 label={translate('sulu_admin.delimiter')}
                             >
                                 <SingleSelect onChange={this.handleDelimiterChanged} value={this.delimiter}>
-                                    <SingleSelect.Option value={';'}>;</SingleSelect.Option>
-                                    <SingleSelect.Option value={','}>,</SingleSelect.Option>
+                                    <SingleSelect.Option value=";">;</SingleSelect.Option>
+                                    <SingleSelect.Option value=",">,</SingleSelect.Option>
                                     <SingleSelect.Option value={'\\t'}>\t</SingleSelect.Option>
                                 </SingleSelect>
                             </Form.Field>
@@ -49,7 +49,7 @@ export default class ExportToolbarAction extends AbstractToolbarAction {
                             >
                                 <SingleSelect onChange={this.handleEnclosureChanged} value={this.enclosure}>
                                     <SingleSelect.Option value={'"'}>&quot;</SingleSelect.Option>
-                                    <SingleSelect.Option value={' '}>&nbsp;</SingleSelect.Option>
+                                    <SingleSelect.Option value=" ">&nbsp;</SingleSelect.Option>
                                 </SingleSelect>
                             </Form.Field>
                         </Form.Section>
@@ -76,6 +76,7 @@ export default class ExportToolbarAction extends AbstractToolbarAction {
                         </Form.Section>
                     </Form>
                 </div>
+
             </Overlay>
         );
     }
@@ -98,15 +99,12 @@ export default class ExportToolbarAction extends AbstractToolbarAction {
     @action handleDelimiterChanged = (value: string) => {
         this.delimiter = value;
     };
-
     @action handleEnclosureChanged = (value: string) => {
         this.enclosure = value;
     };
-
     @action handleEscapeChanged = (value: string) => {
         this.escape = value;
     };
-
     @action handleNewLineChanged = (value: string) => {
         this.newLine = value;
     };
