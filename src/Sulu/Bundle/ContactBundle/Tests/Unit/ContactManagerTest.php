d66218b31786568c3c089d9dd65c44f3287988e7
Frank Klein Koerkamp <frank.kleinkoerkamp@webstores.nl>
2019-06-13 07:22:55 +0200

    Fixed PHPCS

--- src/Sulu/Bundle/ContactBundle/Tests/Unit/ContactManagerTest.php
+++ src/Sulu/Bundle/ContactBundle/Tests/Unit/ContactManagerTest.php
@@ -2,10 +2,19 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Sulu\Bundle\ContactBundle\Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
 use Doctrine\Common\Persistence\ObjectManager;
+use PHPUnit\Framework\TestCase;
 use Sulu\Bundle\ContactBundle\Contact\ContactManager;
 use Sulu\Bundle\ContactBundle\Entity\AccountRepositoryInterface;
 use Sulu\Bundle\ContactBundle\Entity\Contact;
