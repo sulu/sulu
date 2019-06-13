bad1706ed59550ed626b1d58516cc21f1b514645
Frank Klein Koerkamp <frank.kleinkoerkamp@webstores.nl>
2019-06-13 07:11:01 +0200

    Using alias so that every project can override template

--- src/Sulu/Bundle/AdminBundle/Controller/AdminController.php
+++ src/Sulu/Bundle/AdminBundle/Controller/AdminController.php
@@ -233,7 +233,7 @@ class AdminController
         ];
 
         return $this->engine->renderResponse(
-            'SuluAdminBundle:Admin:main.html.twig',
+            '@SuluAdmin\Admin\main.html.twig',
             [
                 'translations' => $this->translations,
                 'fallback_locale' => $this->fallbackLocale,
