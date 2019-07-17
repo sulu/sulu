2d01da3b7455bf8b2021280be6ac196a621c58df
marco.stastny <marco.stastny@massiveart.com>
2019-07-17 16:27:52 +0200

    Added fix for saving already existing conditions

--- src/Sulu/Bundle/AudienceTargetingBundle/Controller/TargetGroupController.php
+++ src/Sulu/Bundle/AudienceTargetingBundle/Controller/TargetGroupController.php
@@ -166,8 +166,10 @@ class TargetGroupController extends RestController implements ClassResourceInter
         // Unset IDs of Conditions, otherwise they won't be able to save as id is null.
         foreach ($data['rules'] as $ruleKey => $rule) {
             foreach ($rule['conditions'] as $key => $condition) {
-                unset($condition['id']);
-                $rule['conditions'][$key] = $condition;
+                if (is_null($condition['id'])) {
+                    unset($condition['id']);
+                    $rule['conditions'][$key] = $condition;
+                }
             }
 
             $data['rules'][$ruleKey] = $rule;
