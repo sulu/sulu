611aeb74afdaa839d72a8aeb0d39f712a263e00f
Daniel Rotter <daniel.rotter@gmail.com>
2019-08-20 16:17:20 +0200

    Fix permissions in page list for roles without any permission

--- src/Sulu/Component/Content/Tests/Functional/Repository/ContentRepositoryTest.php
+++ src/Sulu/Component/Content/Tests/Functional/Repository/ContentRepositoryTest.php
@@ -99,7 +99,8 @@ class ContentRepositoryTest extends SuluTestCase
             $this->webspaceManager,
             $this->localizationFinder,
             $this->structureManager,
-            $this->nodeHelper
+            $this->nodeHelper,
+            ['view' => 64, 'add' => 32, 'edit' => 16, 'delete' => 8]
         );
 
         $this->initPhpcr();
@@ -719,7 +720,84 @@ class ContentRepositoryTest extends SuluTestCase
         );
 
         $this->assertEquals(
-            [1 => ['edit' => true], 2 => ['view' => true, 'archive' => true]],
+            [
+                1 => ['view' => false, 'add' => false, 'delete' => false, 'edit' => true],
+                2 => ['view' => true, 'add' => false, 'edit' => false, 'delete' => false, 'archive' => true]
+            ],
+            $result->getPermissions()
+        );
+    }
+
+    public function testFindWithoutPermissions()
+    {
+        $role1 = $this->prophesize(RoleInterface::class);
+        $role1->getId()->willReturn(1);
+        $role1->getIdentifier()->willReturn('ROLE_SULU_ROLE 1');
+        $role2 = $this->prophesize(RoleInterface::class);
+        $role2->getId()->willReturn(2);
+        $role2->getIdentifier()->willReturn('ROLE_SULU_ROLE-2');
+
+        $user = $this->prophesize(UserInterface::class);
+        $user->getRoleObjects()->willReturn([$role1->reveal(), $role2->reveal()]);
+
+        $page = $this->createPage(
+            'test-1',
+            'de',
+            [],
+            null
+        );
+
+        $result = $this->contentRepository->find(
+            $page->getUuid(),
+            'de',
+            'sulu_io',
+            MappingBuilder::create()->getMapping(),
+            $user->reveal()
+        );
+
+        $this->assertEquals(
+            [],
+            $result->getPermissions()
+        );
+    }
+
+    public function testFindWithPermissionsNotGranted()
+    {
+        $role1 = $this->prophesize(RoleInterface::class);
+        $role1->getId()->willReturn(1);
+        $role1->getIdentifier()->willReturn('ROLE_SULU_ROLE 1');
+        $role2 = $this->prophesize(RoleInterface::class);
+        $role2->getId()->willReturn(2);
+        $role2->getIdentifier()->willReturn('ROLE_SULU_ROLE-2');
+
+        $user = $this->prophesize(UserInterface::class);
+        $user->getRoleObjects()->willReturn([$role1->reveal(), $role2->reveal()]);
+
+        $page = $this->createPage(
+            'test-1',
+            'de',
+            [],
+            null,
+            [
+                1 => ['edit' => false],
+                2 => ['view' => false, 'archive' => false],
+                3 => ['add' => false],
+            ]
+        );
+
+        $result = $this->contentRepository->find(
+            $page->getUuid(),
+            'de',
+            'sulu_io',
+            MappingBuilder::create()->getMapping(),
+            $user->reveal()
+        );
+
+        $this->assertEquals(
+            [
+                1 => ['view' => false, 'add' => false, 'delete' => false, 'edit' => false],
+                2 => ['view' => false, 'add' => false, 'edit' => false, 'delete' => false]
+            ],
             $result->getPermissions()
         );
     }
@@ -1035,7 +1113,7 @@ class ContentRepositoryTest extends SuluTestCase
      *
      * @return PageDocument
      */
-    private function createPage($title, $locale, $data = [], $parentDocument = null, array $permissions = [])
+    private function createPage($title, $locale, $data = [], $parentDocument = null, array $permissions = null)
     {
         /** @var PageDocument $document */
         $document = $this->documentManager->create('page');
@@ -1057,7 +1135,9 @@ class ContentRepositoryTest extends SuluTestCase
         $document->setRedirectType(RedirectType::NONE);
         $document->setShadowLocaleEnabled(false);
         $document->getStructure()->bind($data);
-        $document->setPermissions($permissions);
+        if ($permissions) {
+            $document->setPermissions($permissions);
+        }
         $this->documentManager->persist(
             $document,
             $locale,
