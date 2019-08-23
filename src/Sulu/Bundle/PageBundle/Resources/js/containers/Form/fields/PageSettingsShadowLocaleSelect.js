ccedb5cc2dc35bf37661b0432b88f961585e0ed3
Johannes Wachter <johannes@sulu.io>
2019-08-23 11:56:29 +0200

    add message when no shadow locale is available

--- src/Sulu/Bundle/PageBundle/Resources/js/containers/Form/fields/PageSettingsShadowLocaleSelect.js
+++ src/Sulu/Bundle/PageBundle/Resources/js/containers/Form/fields/PageSettingsShadowLocaleSelect.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import {toJS} from 'mobx';
 import {SingleSelect} from 'sulu-admin-bundle/components';
+import {translate} from 'sulu-admin-bundle/utils';
 import type {FieldTypeProps} from 'sulu-admin-bundle/types';
 
 export default class PageSettingsShadowLocaleSelect extends React.Component<FieldTypeProps<string>> {
@@ -21,25 +22,31 @@ export default class PageSettingsShadowLocaleSelect extends React.Component<Fiel
             throw new Error('The "contentLocales" should be an array!');
         }
 
+        const filteredContentLocales = contentLocales.filter(
+            (contentLocale) => locale && contentLocale !== locale.get()
+        );
+
         return (
             <SingleSelect disabled={!!disabled} onChange={this.handleChange} value={value}>
-                {contentLocales
-                    .filter((contentLocale) => locale && contentLocale !== locale.get())
-                    .map((contentLocale) => {
-                        if (typeof contentLocale !== 'string') {
-                            throw new Error('All entries in the "contentLocales" array must be strings!');
-                        }
+                {filteredContentLocales.length > 0 && filteredContentLocales.map((contentLocale) => {
+                    if (typeof contentLocale !== 'string') {
+                        throw new Error('All entries in the "contentLocales" array must be strings!');
+                    }
 
-                        return (
-                            <SingleSelect.Option
-                                key={contentLocale}
-                                value={contentLocale}
-                            >
-                                {contentLocale}
-                            </SingleSelect.Option>
-                        );
-                    })
-                }
+                    return (
+                        <SingleSelect.Option
+                            key={contentLocale}
+                            value={contentLocale}
+                        >
+                            {contentLocale}
+                        </SingleSelect.Option>
+                    );
+                })}
+                {filteredContentLocales.length === 0 && (
+                    <SingleSelect.Option disabled={true}>
+                        {translate('sulu_page.no_shadow_locale')}
+                    </SingleSelect.Option>
+                )}
             </SingleSelect>
         );
     }
