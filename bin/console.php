cdc011556fbbe423d57d0b1bf6974e3b181c47d7
Alexander Schranz <alexander@sulu.io>
2019-09-26 17:37:29 +0200

    Test building of container and update config files

--- bin/console.php
+++ bin/console.php
@@ -13,7 +13,6 @@ use App\Kernel;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Debug\Debug;
-use Symfony\Component\Dotenv\Dotenv;
 
 set_time_limit(0);
 
@@ -23,18 +22,18 @@ if (!class_exists(Application::class)) {
     throw new \RuntimeException('You need to add "symfony/framework-bundle" as a Composer dependency.');
 }
 
-if (!isset($_SERVER['APP_ENV'])) {
-    if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
-    }
-    (new Dotenv())->load(__DIR__ . '/../.env');
+$input = new ArgvInput();
+if (null !== $env = $input->getParameterOption(['--env', '-e'], null, true)) {
+    putenv('APP_ENV=' . $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = $env);
 }
 
-$input = new ArgvInput();
-$env = $input->getParameterOption(['--env', '-e'], $_SERVER['APP_ENV'] ?? 'dev', true);
-$debug = (bool) ($_SERVER['APP_DEBUG'] ?? ('prod' !== $env)) && !$input->hasParameterOption('--no-debug', true);
+if ($input->hasParameterOption('--no-debug', true)) {
+    putenv('APP_DEBUG=' . $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = '0');
+}
 
-if ($debug) {
+require dirname(__DIR__) . '/config/bootstrap.php';
+
+if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 
     if (class_exists(Debug::class)) {
@@ -42,6 +41,6 @@ if ($debug) {
     }
 }
 
-$kernel = new Kernel($env, $debug, $suluContext);
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG'], $suluContext);
 $application = new Application($kernel);
 $application->run($input);
--- tests/Kernel.php
+++ tests/Kernel.php
@@ -20,8 +20,36 @@ namespace App;
  * with this source code in the file LICENSE.
  */
 
+use FOS\HttpCache\SymfonyCache\HttpCacheProvider;
+use Sulu\Bundle\HttpCacheBundle\Cache\SuluHttpCache;
 use Sulu\Component\HttpKernel\SuluKernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
-class Kernel extends SuluKernel
+class Kernel extends SuluKernel implements HttpCacheProvider
 {
+    /**
+     * @var HttpKernelInterface|null
+     */
+    private $httpCache;
+
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
+    {
+        // Feel free to remove the "container.autowiring.strict_mode" parameter
+        // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
+        $container->setParameter('container.autowiring.strict_mode', true);
+        $container->setParameter('container.dumper.inline_class_loader', true);
+
+        parent::configureContainer($container, $loader);
+    }
+
+    public function getHttpCache()
+    {
+        if (!$this->httpCache) {
+            $this->httpCache = new SuluHttpCache($this);
+        }
+
+        return $this->httpCache;
+    }
 }
