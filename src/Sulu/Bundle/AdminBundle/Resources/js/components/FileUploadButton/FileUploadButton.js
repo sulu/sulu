f23fc05dfc3fa96e2a345fdcad2b1de7b1b026e4
Daniel Rotter <daniel.rotter@gmail.com>
2019-11-29 08:24:46 +0100

    Remove unused code from FileUploadButton

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/FileUploadButton/FileUploadButton.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/FileUploadButton/FileUploadButton.js
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import type {ElementRef} from 'react';
 import Dropzone from 'react-dropzone';
 import Button from '../Button';
 import type {ButtonSkin} from '../Button';
@@ -20,12 +19,6 @@ export default class FileUploadButton extends React.Component<Props> {
         skin: undefined,
     };
 
-    dropzoneRef: ElementRef<typeof Dropzone>;
-
-    setDropzoneRef = (ref: ElementRef<typeof Dropzone>) => {
-        this.dropzoneRef = ref;
-    };
-
     handleDrop = (files: Array<File>) => {
         const file = files[0];
 
@@ -38,7 +31,6 @@ export default class FileUploadButton extends React.Component<Props> {
         return (
             <Dropzone
                 onDrop={this.handleDrop}
-                ref={this.setDropzoneRef}
                 style={{}}
             >
                 <Button disabled={disabled} icon={icon} skin={skin}>
