6567eabe0954f4c1376c92385b47bcd1139af823
Daniel Rotter <daniel.rotter@gmail.com>
2020-03-23 15:30:44 +0100

    Adjust AnalyticsDomainSelect to new webspaceStore

--- src/Sulu/Bundle/WebsiteBundle/Resources/js/containers/Form/fields/AnalyticsDomainSelect.js
+++ src/Sulu/Bundle/WebsiteBundle/Resources/js/containers/Form/fields/AnalyticsDomainSelect.js
@@ -1,23 +1,12 @@
 // @flow
 import React from 'react';
-import {action, observable} from 'mobx';
 import {observer} from 'mobx-react';
 import {MultiSelect} from 'sulu-admin-bundle/components';
 import type {FieldTypeProps} from 'sulu-admin-bundle/types';
 import {webspaceStore} from 'sulu-page-bundle/stores';
-import type {Webspace} from 'sulu-page-bundle/types';
 
 @observer
 class AnalyticsDomainSelect extends React.Component<FieldTypeProps<Array<string>>> {
-    @observable webspace: Webspace;
-
-    componentDidMount() {
-        const {formInspector} = this.props;
-        webspaceStore.loadWebspace(formInspector.options.webspace).then(action((webspace) => {
-            this.webspace = webspace;
-        }));
-    }
-
     handleChange = (value: Array<string>) => {
         const {onChange, onFinish} = this.props;
 
@@ -26,11 +15,7 @@ class AnalyticsDomainSelect extends React.Component<FieldTypeProps<Array<string>
     };
 
     render() {
-        const {disabled, value} = this.props;
-
-        if (!this.webspace) {
-            return null;
-        }
+        const {disabled, formInspector, value} = this.props;
 
         return (
             <MultiSelect
@@ -38,7 +23,7 @@ class AnalyticsDomainSelect extends React.Component<FieldTypeProps<Array<string>
                 onChange={this.handleChange}
                 values={value || []}
             >
-                {this.webspace.urls.map(({url}) => (
+                {webspaceStore.getWebspace(formInspector.options.webspace).urls.map(({url}) => (
                     <MultiSelect.Option key={url} value={url}>
                         {url}
                     </MultiSelect.Option>
