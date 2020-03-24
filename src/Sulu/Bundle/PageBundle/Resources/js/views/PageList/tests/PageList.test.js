9dc9170d60e518ebe0c824abb6a88ba596d63a98
Alexander Schranz <alexander@sulu.io>
2020-03-24 19:38:39 +0100

    Fix code style

--- src/Sulu/Bundle/PageBundle/Resources/js/views/PageList/tests/PageList.test.js
+++ src/Sulu/Bundle/PageBundle/Resources/js/views/PageList/tests/PageList.test.js
@@ -241,7 +241,10 @@ test('Should load webspace and active route attribute from listStore and userSto
     ListStore.getActiveSetting.mockReturnValueOnce('some-uuid');
 
     // $FlowFixMe
-    expect(PageList.getDerivedRouteAttributes(undefined, {webspace: 'abc'})).toEqual({active: 'some-uuid', locale: 'en'});
+    expect(PageList.getDerivedRouteAttributes(undefined, {webspace: 'abc'})).toEqual({
+        active: 'some-uuid',
+        locale: 'en',
+    });
     expect(ListStore.getActiveSetting).toBeCalledWith('pages', 'page_list_abc');
 });
 
