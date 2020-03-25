e35832240fefb464037dfa3f34b6dde8868c41e9
Adrian Sieber <adrian.sieber@MBP0007.local>
2020-03-25 10:49:08 +0100

    Fixed correct locale for article route generation

--- src/Sulu/Bundle/RouteBundle/Generator/SymfonyExpressionTokenProvider.php
+++ src/Sulu/Bundle/RouteBundle/Generator/SymfonyExpressionTokenProvider.php
@@ -38,7 +38,12 @@ class SymfonyExpressionTokenProvider implements TokenProviderInterface
     public function provide($entity, $name)
     {
         try {
-            return $this->expressionLanguage->evaluate($name, ['object' => $entity, 'translator' => $this->translator]);
+            $locale = $this->translator->getLocale();
+            $this->translator->setLocale($entity->getLocale());
+            $result = $this->expressionLanguage->evaluate($name, ['object' => $entity, 'translator' => $this->translator]);
+            $this->translator->setLocale($locale);
+
+            return $result;
         } catch (\Exception $e) {
             throw new CannotEvaluateTokenException($name, $entity, $e);
         }
