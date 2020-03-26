7e651ba559860d96e81c8245c75b7a446c814596
Daniel Rotter <daniel.rotter@gmail.com>
2020-03-26 14:26:57 +0100

    Allow to filter with only from or to date (#5144)

--- src/Sulu/Component/Rest/Tests/Unit/ListBuilder/Filter/DateTimeFilterTypeTest.php
+++ src/Sulu/Component/Rest/Tests/Unit/ListBuilder/Filter/DateTimeFilterTypeTest.php
@@ -55,12 +55,54 @@ class DateTimeFilterTypeTest extends TestCase
         $this->listBuilder->between($fieldDescriptor->reveal(), $expected)->shouldBeCalled();
     }
 
+    public function provideFilterFromOnly()
+    {
+        return [
+            ['created', ['from' => '2020-02-05'], '2020-02-05'],
+            ['changed', ['from' => '2013-08-01'], '2013-08-01'],
+        ];
+    }
+
+    /**
+     * @dataProvider provideFilterFromOnly
+     */
+    public function testFilterFromOnly($fieldName, $value, $expected)
+    {
+        $fieldDescriptor = $this->prophesize(FieldDescriptor::class);
+
+        $this->dateTimeFilterType->filter($this->listBuilder->reveal(), $fieldDescriptor->reveal(), $value);
+
+        $this->listBuilder
+             ->where($fieldDescriptor->reveal(), $expected, ListBuilderInterface::WHERE_COMPARATOR_GREATER)
+             ->shouldBeCalled();
+    }
+
+    public function provideFilterToOnly()
+    {
+        return [
+            ['created', ['to' => '2020-02-05'], '2020-02-05'],
+            ['changed', ['to' => '2013-08-01'], '2013-08-01'],
+        ];
+    }
+
+    /**
+     * @dataProvider provideFilterToOnly
+     */
+    public function testFilterToOnly($fieldName, $value, $expected)
+    {
+        $fieldDescriptor = $this->prophesize(FieldDescriptor::class);
+
+        $this->dateTimeFilterType->filter($this->listBuilder->reveal(), $fieldDescriptor->reveal(), $value);
+
+        $this->listBuilder
+             ->where($fieldDescriptor->reveal(), $expected, ListBuilderInterface::WHERE_COMPARATOR_LESS)
+             ->shouldBeCalled();
+    }
+
     public function provideFilterWithInvalidOptions()
     {
         return [
             [[]],
-            [['from' => '2019-10-01']],
-            [['to' => '2020-10-01']],
             ['Test'],
         ];
     }
