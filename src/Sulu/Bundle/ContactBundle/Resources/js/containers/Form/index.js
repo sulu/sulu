9c8ac0a56916f730e595baa185e3bbe93e497741
Daniel Rotter <daniel.rotter@gmail.com>
2020-04-02 13:11:58 +0200

    Fix import for form fields in ContactBundle

---
+++ src/Sulu/Bundle/ContactBundle/Resources/js/containers/Form/index.js
@@ -0,0 +1,7 @@
+// @flow
+import Bic from './fields/Bic';
+import ContactAccountSelection from './fields/ContactAccountSelection';
+import ContactDetails from './fields/ContactDetails';
+import Iban from './fields/Iban';
+
+export {Bic, ContactAccountSelection, ContactDetails, Iban};
