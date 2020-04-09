51b18d886928d83abb964aba4e45cafc17a659cd
Daniel Rotter <daniel.rotter@gmail.com>
2020-04-09 13:54:06 +0200

    Fix minimum size calculation for crops (#5197)
    * Fix minimum size calculation for crops
    
    * Fix failing recognition of minimal size

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/ImageRectangleSelection/tests/ImageRectangleSelection.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/ImageRectangleSelection/tests/ImageRectangleSelection.test.js
@@ -5,6 +5,10 @@ import {ImageRectangleSelection} from '../ImageRectangleSelection';
 
 jest.mock('../../../utils/DOM/afterElementsRendered');
 
+jest.mock('../../../utils/Translator', () => ({
+    translate: jest.fn((key) => key),
+}));
+
 jest.mock('../../withContainerSize/withContainerSize');
 
 test('The component should render with image source', () => {
@@ -99,28 +103,37 @@ test('The component should pass a value of undefined', () => {
     expect(changeSpy).toBeCalledWith(undefined);
 });
 
-test('The component should render with minWidth and minHeight', () => {
-    const view = mount(
-        <ImageRectangleSelection
-            containerHeight={360}
-            containerWidth={640}
-            image="//:0"
-            minHeight={300}
-            minWidth={600}
-        />
-    );
+test.each([
+    [300, 600, 360, 640, 300, 600],
+    [200, 200, 300, 400, 200, 200],
+    [800, 800, 300, 400, 300, 300],
+    [800, 1000, 200, 400, 200, 250],
+    [1000, 800, 200, 400, 200, 160],
+])(
+    'The component should render with minHeight %s, minWidth %s, containerHeight %s and containerWidth %s',
+    (minHeight, minWidth, containerHeight, containerWidth, expectedMinHeight, expectedMinWidth) => {
+        const view = mount(
+            <ImageRectangleSelection
+                containerHeight={containerHeight}
+                containerWidth={containerWidth}
+                image="//:0"
+                minHeight={minHeight}
+                minWidth={minWidth}
+            />
+        );
 
-    const onImageLoad = view.instance().image.onload;
-    view.instance().image = {
-        naturalWidth: 1920,
-        naturalHeight: 1080,
-    };
-    onImageLoad();
+        const onImageLoad = view.instance().image.onload;
+        view.instance().image = {
+            naturalWidth: 1920,
+            naturalHeight: 1080,
+        };
+        onImageLoad();
 
-    view.update();
+        view.update();
 
-    const rectangle = view.find('RectangleSelection');
-    expect(rectangle.length).toBe(1);
-    expect(rectangle.props().minWidth).toBe(200);
-    expect(rectangle.props().minHeight).toBe(100);
-});
+        const rectangle = view.find('RectangleSelection');
+        expect(rectangle.length).toBe(1);
+        expect(rectangle.props().minHeight).toEqual(expectedMinHeight);
+        expect(rectangle.props().minWidth).toEqual(expectedMinWidth);
+    }
+);
