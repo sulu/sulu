88766308a1ff7f2d33166718f0185a29d8345484
Mário Franco <mario.ffranco@gmail.com>
2020-04-14 15:46:27 +0100

    Handle non ajax logout request (#5208)
    Co-authored-by: Mario Franco <mario@vivasuperstars.com>

--- src/Sulu/Bundle/SecurityBundle/Security/LogoutSuccessHandler.php
+++ src/Sulu/Bundle/SecurityBundle/Security/LogoutSuccessHandler.php
@@ -12,14 +12,32 @@
 namespace Sulu\Bundle\SecurityBundle\Security;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
 
 class LogoutSuccessHandler implements LogoutSuccessHandlerInterface
 {
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
     public function onLogoutSuccess(Request $request)
     {
-        return new JsonResponse(null, Response::HTTP_OK);
+        if ($request->isXmlHttpRequest()) {
+            $response = new JsonResponse(null, Response::HTTP_OK);
+        } else {
+            $response = new RedirectResponse($this->router->generate('sulu_admin'));
+        }
+
+        return $response;
     }
 }
