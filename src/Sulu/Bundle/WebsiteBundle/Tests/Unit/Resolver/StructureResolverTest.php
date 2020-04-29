1ddedeb2a9809bbba9a5f7b2e3a666e72caecdb5
Alexander Schranz <alexander@sulu.io>
2020-04-29 17:04:08 +0200

    Move WebsiteBundle Unit Test files to correct namespace

---
+++ src/Sulu/Bundle/WebsiteBundle/Tests/Unit/Resolver/StructureResolverTest.php
@@ -0,0 +1,191 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\WebsiteBundle\Tests\Unit\Resolver;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Sulu\Bundle\WebsiteBundle\Resolver\StructureResolver;
+use Sulu\Bundle\WebsiteBundle\Resolver\StructureResolverInterface;
+use Sulu\Component\Content\ContentTypeInterface;
+use Sulu\Component\Content\ContentTypeManagerInterface;
+use Sulu\Component\Content\Document\Behavior\ExtensionBehavior;
+use Sulu\Component\Content\Document\Behavior\LocalizedAuthorBehavior;
+use Sulu\Component\Content\Document\Extension\ExtensionContainer;
+use Sulu\Component\Content\Extension\ExtensionManagerInterface;
+
+class StructureResolverTest extends TestCase
+{
+    /**
+     * @var StructureResolverInterface
+     */
+    private $structureResolver;
+
+    /**
+     * @var ContentTypeManagerInterface
+     */
+    private $contentTypeManager;
+
+    /**
+     * @var ContentTypeInterface
+     */
+    private $contentType;
+
+    /**
+     * @var ExtensionManagerInterface
+     */
+    private $extensionManager;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->contentTypeManager = $this->prophesize(ContentTypeManagerInterface::class);
+        $this->extensionManager = $this->prophesize(ExtensionManagerInterface::class);
+        $this->contentType = $this->prophesize(ContentTypeInterface::class);
+
+        $this->structureResolver = new StructureResolver(
+            $this->contentTypeManager->reveal(),
+            $this->extensionManager->reveal()
+        );
+    }
+
+    public function testResolve()
+    {
+        $this->contentTypeManager->get('content_type')->willReturn($this->contentType);
+
+        $this->contentType->getViewData(Argument::any())->willReturn('view');
+        $this->contentType->getContentData(Argument::any())->willReturn('content');
+
+        $excerptExtension = $this->prophesize('Sulu\Component\Content\Extension\ExtensionInterface');
+        $excerptExtension->getContentData(['test1' => 'test1'])->willReturn(['test1' => 'test1']);
+        $this->extensionManager->getExtension('test', 'excerpt')->willReturn($excerptExtension);
+
+        $property = $this->prophesize('Sulu\Component\Content\Compat\PropertyInterface');
+        $property->getName()->willReturn('property');
+        $property->getContentTypeName()->willReturn('content_type');
+
+        $structure = $this->prophesize('Sulu\Component\Content\Compat\Structure\PageBridge');
+        $structure->getKey()->willReturn('test');
+        $structure->getExt()->willReturn(new ExtensionContainer(['excerpt' => ['test1' => 'test1']]));
+        $structure->getUuid()->willReturn('some-uuid');
+        $structure->getProperties(true)->willReturn([$property->reveal()]);
+        $structure->getCreator()->willReturn(1);
+        $structure->getChanger()->willReturn(1);
+        $structure->getCreated()->willReturn('date');
+        $structure->getChanged()->willReturn('date');
+        $structure->getPublished()->willReturn('date');
+        $structure->getPath()->willReturn('test-path');
+        $structure->getUrls()->willReturn(['en' => '/description', 'de' => '/beschreibung', 'es' => null]);
+        $structure->getShadowBaseLanguage()->willReturn('en');
+        $structure->getWebspaceKey()->willReturn('test');
+
+        $authored = new \DateTime();
+
+        $document = $this->prophesize()->willImplement(LocalizedAuthorBehavior::class);
+        $document->willImplement(ExtensionBehavior::class);
+        $structure->getDocument()->willReturn($document->reveal());
+        $document->getAuthored()->willReturn($authored);
+        $document->getAuthor()->willReturn(1);
+
+        $expected = [
+            'extension' => [
+                'excerpt' => ['test1' => 'test1'],
+            ],
+            'id' => 'some-uuid',
+            'uuid' => 'some-uuid',
+            'view' => [
+                'property' => 'view',
+            ],
+            'content' => [
+                'property' => 'content',
+            ],
+            'creator' => 1,
+            'changer' => 1,
+            'created' => 'date',
+            'changed' => 'date',
+            'published' => 'date',
+            'template' => 'test',
+            'urls' => ['en' => '/description', 'de' => '/beschreibung', 'es' => null],
+            'path' => 'test-path',
+            'shadowBaseLocale' => 'en',
+            'authored' => $authored,
+            'author' => 1,
+            'webspaceKey' => 'test',
+        ];
+
+        $this->assertEquals($expected, $this->structureResolver->resolve($structure->reveal()));
+    }
+
+    public function testResolveWithoutExtensions()
+    {
+        $this->contentTypeManager->get('content_type')->willReturn($this->contentType);
+
+        $this->contentType->getViewData(Argument::any())->willReturn('view');
+        $this->contentType->getContentData(Argument::any())->willReturn('content');
+
+        $excerptExtension = $this->prophesize('Sulu\Component\Content\Extension\ExtensionInterface');
+        $excerptExtension->getContentData(['test1' => 'test1'])->willReturn(['test1' => 'test1']);
+        $this->extensionManager->getExtension('test', 'excerpt')->willReturn($excerptExtension);
+
+        $property = $this->prophesize('Sulu\Component\Content\Compat\PropertyInterface');
+        $property->getName()->willReturn('property');
+        $property->getContentTypeName()->willReturn('content_type');
+
+        $structure = $this->prophesize('Sulu\Component\Content\Compat\Structure\PageBridge');
+        $structure->getKey()->willReturn('test');
+        $structure->getExt()->willReturn(new ExtensionContainer(['excerpt' => ['test1' => 'test1']]));
+        $structure->getUuid()->willReturn('some-uuid');
+        $structure->getProperties(true)->willReturn([$property->reveal()]);
+        $structure->getCreator()->willReturn(1);
+        $structure->getChanger()->willReturn(1);
+        $structure->getCreated()->willReturn('date');
+        $structure->getChanged()->willReturn('date');
+        $structure->getPublished()->willReturn('date');
+        $structure->getPath()->willReturn('test-path');
+        $structure->getUrls()->willReturn(['en' => '/description', 'de' => '/beschreibung', 'es' => null]);
+        $structure->getShadowBaseLanguage()->willReturn('en');
+        $structure->getWebspaceKey()->willReturn('test');
+
+        $authored = new \DateTime();
+
+        $document = $this->prophesize()->willImplement(LocalizedAuthorBehavior::class);
+        $document->willImplement(ExtensionBehavior::class);
+        $structure->getDocument()->willReturn($document->reveal());
+        $document->getAuthored()->willReturn($authored);
+        $document->getAuthor()->willReturn(1);
+
+        $expected = [
+            'id' => 'some-uuid',
+            'uuid' => 'some-uuid',
+            'view' => [
+                'property' => 'view',
+            ],
+            'content' => [
+                'property' => 'content',
+            ],
+            'creator' => 1,
+            'changer' => 1,
+            'created' => 'date',
+            'changed' => 'date',
+            'published' => 'date',
+            'template' => 'test',
+            'urls' => ['en' => '/description', 'de' => '/beschreibung', 'es' => null],
+            'path' => 'test-path',
+            'shadowBaseLocale' => 'en',
+            'authored' => $authored,
+            'author' => 1,
+            'webspaceKey' => 'test',
+        ];
+
+        $this->assertEquals($expected, $this->structureResolver->resolve($structure->reveal(), false));
+    }
+}
---
+++ src/Sulu/Bundle/WebsiteBundle/Tests/Unit/Sitemap/XmlSitemapRendererTest.php
@@ -0,0 +1,178 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\WebsiteBundle\Tests\Unit\Sitemap;
+
+use PHPUnit\Framework\TestCase;
+use Sulu\Bundle\WebsiteBundle\Sitemap\Sitemap;
+use Sulu\Bundle\WebsiteBundle\Sitemap\SitemapProviderInterface;
+use Sulu\Bundle\WebsiteBundle\Sitemap\SitemapProviderPoolInterface;
+use Sulu\Bundle\WebsiteBundle\Sitemap\SitemapUrl;
+use Sulu\Bundle\WebsiteBundle\Sitemap\XmlSitemapRenderer;
+use Sulu\Component\Localization\Localization;
+use Sulu\Component\Webspace\Portal;
+use Sulu\Component\Webspace\Webspace;
+use Twig\Environment;
+
+class XmlSitemapRendererTest extends TestCase
+{
+    /**
+     * @var SitemapProviderPoolInterface
+     */
+    protected $providerPoolInterface;
+
+    /**
+     * @var Environment
+     */
+    protected $engine;
+
+    /**
+     * @var XmlSitemapRenderer
+     */
+    protected $renderer;
+
+    public function setUp(): void
+    {
+        $this->providerPoolInterface = $this->prophesize(SitemapProviderPoolInterface::class);
+        $this->engine = $this->prophesize(Environment::class);
+
+        $this->renderer = new XmlSitemapRenderer($this->providerPoolInterface->reveal(), $this->engine->reveal(), '/');
+    }
+
+    public function testRenderIndexNoNeed()
+    {
+        $sitemaps = [new Sitemap('pages', 1)];
+
+        $pagesProvider = $this->prophesize(SitemapProviderInterface::class);
+        $this->providerPoolInterface->getProviders()->willReturn(['pages' => $pagesProvider->reveal()]);
+        $this->providerPoolInterface->hasProvider('pages')->willReturn(true);
+        $this->providerPoolInterface->getProvider('pages')->willReturn($pagesProvider);
+        $this->providerPoolInterface->getIndex('http', 'sulu.io')->willReturn($sitemaps);
+
+        $this->assertEquals(null, $this->renderer->renderIndex('http', 'sulu.io'));
+    }
+
+    public function testRenderIndexNoNeedMultipleProviders()
+    {
+        $sitemaps = [
+            new Sitemap('test', 0),
+            new Sitemap('pages', 1),
+        ];
+
+        $pagesProvider = $this->prophesize(SitemapProviderInterface::class);
+        $testProvider = $this->prophesize(SitemapProviderInterface::class);
+        $this->providerPoolInterface->getProviders()->willReturn([
+            'test' => $testProvider->reveal(),
+            'pages' => $pagesProvider->reveal(),
+        ]);
+        $this->providerPoolInterface->hasProvider('pages')->willReturn(true);
+        $this->providerPoolInterface->hasProvider('test')->willReturn(true);
+        $this->providerPoolInterface->getProvider('pages')->willReturn($pagesProvider);
+        $this->providerPoolInterface->getProvider('test')->willReturn($testProvider);
+        $this->providerPoolInterface->getIndex('http', 'sulu.io')->willReturn($sitemaps);
+
+        $this->assertEquals(null, $this->renderer->renderIndex('http', 'sulu.io'));
+    }
+
+    public function testRenderIndexMorePages()
+    {
+        $sitemaps = [new Sitemap('pages', 2)];
+
+        $pagesProvider = $this->prophesize(SitemapProviderInterface::class);
+        $this->providerPoolInterface->getProviders()->willReturn(['pages' => $pagesProvider->reveal()]);
+        $this->providerPoolInterface->hasProvider('pages')->willReturn(true);
+        $this->providerPoolInterface->getProvider('pages')->willReturn($pagesProvider);
+        $this->providerPoolInterface->getIndex('http', 'sulu.io')->willReturn($sitemaps);
+
+        $this->engine->render(
+            '@SuluWebsite/Sitemap/sitemap-index.xml.twig',
+            ['sitemaps' => $sitemaps, 'domain' => 'sulu.io', 'scheme' => 'http']
+        )->willReturn('<html/>');
+
+        $this->assertEquals('<html/>', $this->renderer->renderIndex('http', 'sulu.io'));
+    }
+
+    public function testRenderIndexMoreProviders()
+    {
+        $sitemaps = [new Sitemap('pages', 1), new Sitemap('article', 1)];
+
+        $pagesProvider = $this->prophesize(SitemapProviderInterface::class);
+        $articleProvider = $this->prophesize(SitemapProviderInterface::class);
+        $this->providerPoolInterface->getProviders()->willReturn(
+            ['pages' => $pagesProvider->reveal(), 'article' => $articleProvider->reveal()]
+        );
+        $this->providerPoolInterface->hasProvider('pages')->willReturn(true);
+        $this->providerPoolInterface->getProvider('pages')->willReturn($pagesProvider);
+        $this->providerPoolInterface->getIndex('http', 'sulu.io')->willReturn($sitemaps);
+
+        $this->engine->render(
+            '@SuluWebsite/Sitemap/sitemap-index.xml.twig',
+            ['sitemaps' => $sitemaps, 'domain' => 'sulu.io', 'scheme' => 'http']
+        )->willReturn('<html/>');
+
+        $this->assertEquals('<html/>', $this->renderer->renderIndex('http', 'sulu.io'));
+    }
+
+    public function testRenderIndexWithSchemeAndDomain()
+    {
+        $sitemaps = [new Sitemap('pages', 1), new Sitemap('article', 1)];
+
+        $pagesProvider = $this->prophesize(SitemapProviderInterface::class);
+        $articleProvider = $this->prophesize(SitemapProviderInterface::class);
+        $this->providerPoolInterface->getProviders()->willReturn(
+            ['pages' => $pagesProvider->reveal(), 'article' => $articleProvider->reveal()]
+        );
+        $this->providerPoolInterface->hasProvider('pages')->willReturn(true);
+        $this->providerPoolInterface->getProvider('pages')->willReturn($pagesProvider);
+        $this->providerPoolInterface->getIndex('http', 'sulu.io')->willReturn($sitemaps);
+
+        $this->engine->render(
+            '@SuluWebsite/Sitemap/sitemap-index.xml.twig',
+            ['sitemaps' => $sitemaps, 'domain' => 'sulu.io', 'scheme' => 'http']
+        )->willReturn('<html/>');
+
+        $this->assertEquals('<html/>', $this->renderer->renderIndex('http', 'sulu.io'));
+    }
+
+    public function testRenderSitemap()
+    {
+        $pagesProvider = $this->prophesize(SitemapProviderInterface::class);
+        $this->providerPoolInterface->getProviders()->willReturn(['pages' => $pagesProvider->reveal()]);
+        $this->providerPoolInterface->hasProvider('pages')->willReturn(true);
+        $this->providerPoolInterface->getProvider('pages')->willReturn($pagesProvider);
+
+        $entries = [new SitemapUrl('http://sulu.io', 'en', 'en')];
+        $pagesProvider->build(1, 'http', 'sulu.io')->willReturn($entries);
+        $pagesProvider->getMaxPage('http', 'sulu.io')->willReturn(1);
+
+        $webspace = $this->prophesize(Webspace::class);
+        $webspace->getKey()->willReturn('sulu');
+
+        $portal = $this->prophesize(Portal::class);
+        $portal->getKey()->willReturn('sulu_io');
+        $portal->getWebspace()->willReturn($webspace->reveal());
+        $portal->getXDefaultLocalization()->willReturn(new Localization('de'));
+
+        $this->engine->render(
+            '@SuluWebsite/Sitemap/sitemap.xml.twig',
+            [
+                'domain' => 'sulu.io',
+                'scheme' => 'http',
+                'entries' => $entries,
+            ]
+        )->willReturn('<html/>');
+
+        $this->assertEquals(
+            '<html/>',
+            $this->renderer->renderSitemap('pages', 1, 'http', 'sulu.io')
+        );
+    }
+}
