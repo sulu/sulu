62981c61d49d0b9bad8c079665e3ba0a0909ad35
Daniel Rotter <daniel.rotter@gmail.com>
2020-05-04 14:11:38 +0200

    Use nowrap instead of set a fix width

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/PublishIndicator/PublishIndicator.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/PublishIndicator/PublishIndicator.js
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import classNames from 'classnames';
 import publishIndicatorStyles from './publishIndicator.scss';
 
 type Props = {
@@ -17,16 +16,8 @@ export default class PublishIndicator extends React.Component<Props> {
     render() {
         const {draft, published} = this.props;
 
-        const publishIndicatorClass = classNames(
-            publishIndicatorStyles.publishIndicator,
-            {
-                [publishIndicatorStyles.hasPublished]: published,
-                [publishIndicatorStyles.hasDraft]: draft,
-            }
-        );
-
         return (
-            <div className={publishIndicatorClass}>
+            <div className={publishIndicatorStyles.publishIndicator}>
                 {published && <span className={publishIndicatorStyles.published} />}
                 {draft && <span className={publishIndicatorStyles.draft} />}
             </div>
