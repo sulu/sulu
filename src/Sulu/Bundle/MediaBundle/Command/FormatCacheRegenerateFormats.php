0a234f92418ae8a2d11dd7fdfe124815c36bdd29
Jordy Groote <jordygroote@gmail.com>
2020-07-05 18:03:54 +0200

    Added command to regenerate image format cache

--- src/Sulu/Bundle/MediaBundle/Command/FormatCacheRegenerateFormats.php
+++ src/Sulu/Bundle/MediaBundle/Command/FormatCacheRegenerateFormats.php
@@ -12,18 +12,45 @@
 namespace Sulu\Bundle\MediaBundle\Command;
 
 use Sulu\Bundle\MediaBundle\Media\FormatCache\FormatCacheClearerInterface;
+use Sulu\Bundle\MediaBundle\Media\FormatManager\FormatManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 class FormatCacheRegenerateFormats extends Command
 {
     protected static $defaultName = 'sulu:media:regenerate:formats';
 
-    public function __construct()
-    {
+    /**
+     * @var Filesystem
+     */
+    private $fileSystem;
+
+    /**
+     * @var FormatManagerInterface
+     */
+    private $formatManager;
+
+    /**
+     * @var string
+     */
+    private $localFormatCachePath;
+
+    public function __construct(
+        Filesystem $filesystem,
+        FormatManagerInterface $formatManager,
+        string $localFormatCachePath
+    ) {
         parent::__construct();
+
+        $this->fileSystem = $filesystem;
+        $this->formatManager = $formatManager;
+        $this->localFormatCachePath = $localFormatCachePath;
     }
 
     protected function configure()
@@ -34,8 +61,52 @@ class FormatCacheRegenerateFormats extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        dd('dsa');
+        $ui = new SymfonyStyle($input, $output);
+
+        $finder = new Finder();
+        $finder->in(\realpath($this->localFormatCachePath));
+        $files = $finder->files();
+
+        $progressBar = $ui->createProgressBar(\count($files));
+
+        if (!count($files)) {
+            $ui->writeln('No images to regenerate');
+            return 0;
+        }
+
+        $ui->writeln('Starting to regenerate: ' . count($files) .' images');
+        $ui->writeln('');
+
+        /** @var SplFileInfo $file */
+        foreach ($files as $file) {
+            $fileInformation = $this->getFileInformationArrayFromPath($file->getRelativePathname());
+
+            $this->formatManager->returnImage(
+                $fileInformation['id'],
+                $fileInformation['formatKey'],
+                $fileInformation['fileName']
+            );
+
+            $progressBar->advance();
+        }
+
+        $ui->writeln('');
+        $ui->writeln('');
+        $ui->writeln('DONE');
+
+        $progressBar->finish();
 
         return 0;
+    }
+
+    private function getFileInformationArrayFromPath($path): array
+    {
+        $exploded = explode('/', $path);
+
+        return [
+            'id' => $exploded[1],
+            'formatKey' => $exploded[0],
+            'fileName' => $exploded[2]
+        ];
     }
 }
