7184c9ead9548cd53471babde69c42063f16f942
Jordy Groote <jordygroote@gmail.com>
2020-07-06 21:48:15 +0200

    Fix phpcs github errors

--- src/Sulu/Bundle/MediaBundle/Command/FormatCacheRegenerateFormats.php
+++ src/Sulu/Bundle/MediaBundle/Command/FormatCacheRegenerateFormats.php
@@ -73,7 +73,7 @@ class FormatCacheRegenerateFormats extends Command
 
         $progressBar = $ui->createProgressBar(\count($files));
 
-        $ui->writeln('Starting to regenerate: ' . \count($files) .' images');
+        $ui->writeln('Starting to regenerate: ' . \count($files) . ' images');
         $ui->writeln('');
 
         /** @var SplFileInfo $file */
