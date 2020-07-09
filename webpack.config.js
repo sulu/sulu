61e2c9295b2a5fd3101b51345474ab6c9386b54c
Alexander Schranz <alexander@sulu.io>
2020-07-09 14:51:23 +0200

    Do not compress the cache files

--- webpack.config.js
+++ webpack.config.js
@@ -70,6 +70,7 @@ module.exports = (env, argv) => { // eslint-disable-line no-undef
                         options: {
                             ...babelConfig,
                             cacheDirectory: true,
+                            cacheCompression: false,
                         },
                     },
                 },
