bdea7690b91e845bc3d38b0efec25128e3f7bed4
Daniel Rotter <daniel.rotter@gmail.com>
2020-07-17 15:11:07 +0200

    Export security system for each webspace to frontend application

--- src/Sulu/Bundle/AdminBundle/Resources/js/utils/TestHelper/defaultWebspace.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/utils/TestHelper/defaultWebspace.js
@@ -11,6 +11,7 @@ const defaultWebspace = {
     resourceLocatorStrategy: {
         inputType: 'resource-locator-strategy-input-type',
     },
+    security: undefined,
     segments: [],
     urls: [],
 };
