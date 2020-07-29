99102b11e59609a051ef6f78ecd469cfd830066d
Alexander Schoonderwaldt <alexander@weprovide.com>
2020-07-29 20:36:29 +0200

    Added testcase for DoctrineCountFieldDescriptor.php

---
+++ src/Sulu/Component/Rest/Tests/Unit/ListBuilder/Doctrine/FieldDescriptor/DoctrineCountFieldDescriptorTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Component\Rest\Tests\Unit\ListBuilder\Doctrine\FieldDescriptor;
+
+use PHPUnit\Framework\TestCase;
+use Sulu\Component\Rest\ListBuilder\Doctrine\FieldDescriptor\DoctrineCountFieldDescriptor;
+use Sulu\Component\Rest\ListBuilder\Doctrine\FieldDescriptor\DoctrineFieldDescriptor;
+use Sulu\Component\Rest\ListBuilder\FieldDescriptorInterface;
+
+class DoctrineCountFieldDescriptorTest extends TestCase
+{
+    public function testGetSelect()
+    {
+        $doctrineCountFieldDescriptor = new DoctrineCountFieldDescriptor(
+            'test',
+            'test',
+            'TestEntity',
+            'translation',
+            [],
+            FieldDescriptorInterface::VISIBILITY_YES,
+            FieldDescriptorInterface::SEARCHABILITY_NEVER,
+            '',
+            true,
+            false
+        );
+
+        $this->assertEquals(
+            'COUNT(TestEntity.test)',
+            $doctrineCountFieldDescriptor->getSelect()
+        );
+    }
+
+    public function testGetSelectWithDistinct()
+    {
+        $doctrineCountFieldDescriptor = new DoctrineCountFieldDescriptor(
+            'test',
+            'test',
+            'TestEntity',
+            'translation',
+            [],
+            FieldDescriptorInterface::VISIBILITY_YES,
+            FieldDescriptorInterface::SEARCHABILITY_NEVER,
+            '',
+            true,
+            true
+        );
+
+        $this->assertEquals(
+            'COUNT(DISTINCT TestEntity.test)',
+            $doctrineCountFieldDescriptor->getSelect()
+        );
+    }
+}
