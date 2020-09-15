96f73e43c3e18653930e53f8ccccfe6a4093f2c6
Daniel Rotter <daniel.rotter@gmail.com>
2020-09-15 11:17:45 +0200

    Fix tests for different timezone (#5492)

--- src/Sulu/Bundle/AdminBundle/Resources/js/utils/Date/tests/transformDateForUrl.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/utils/Date/tests/transformDateForUrl.test.js
@@ -2,9 +2,9 @@
 import transformDateForUrl from '../transformDateForUrl';
 
 test.each([
-    [new Date('2020-02-28'), '2020-02-28 00:00'],
+    [new Date('2020-02-28 00:00'), '2020-02-28 00:00'],
     [new Date('2000-08-31 12:00'), '2000-08-31 12:00'],
-    [new Date('2006-12-31'), '2006-12-31 00:00'],
+    [new Date('2006-12-31 00:00'), '2006-12-31 00:00'],
     [new Date('1940-12-01 12:00'), '1940-12-01 12:00'],
 ])('Transform date "%s"', (date, expectedValue) => {
     expect(transformDateForUrl(date)).toEqual(expectedValue);
