5777bbc2b4cff1e656362008647729ec41564796
nnatter <niklas@sulu.io>
2020-10-15 09:54:36 +0200

    Pass content-locale of user to TextEditor if Form has no locale (#5520)

--- src/Sulu/Bundle/AdminBundle/Resources/js/containers/Form/fields/TextEditor.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/containers/Form/fields/TextEditor.js
@@ -1,17 +1,21 @@
 // @flow
 import React from 'react';
+import {observable} from 'mobx';
 import TextEditorContainer from '../../../containers/TextEditor';
 import type {FieldTypeProps} from '../../../types';
+import userStore from '../../../stores/userStore';
 
 export default class TextEditor extends React.Component<FieldTypeProps<?string>> {
     render() {
         const {disabled, formInspector, onChange, onFinish, schemaOptions, value} = this.props;
 
+        const locale = formInspector.locale ? formInspector.locale : observable.box(userStore.contentLocale);
+
         return (
             <TextEditorContainer
                 adapter="ckeditor5"
                 disabled={!!disabled}
-                locale={formInspector.locale}
+                locale={locale}
                 onBlur={onFinish}
                 onChange={onChange}
                 options={schemaOptions}
