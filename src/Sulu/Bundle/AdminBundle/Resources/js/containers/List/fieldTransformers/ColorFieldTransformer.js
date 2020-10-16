745e6e8d95d2d6105c00ccddbc6a81199fcb8d85
Nicolas Sittler <sittler.n@gmail.com>
2020-10-16 16:57:46 +0200

    AdminBundle ColorFieldTransformer: Correction eslint tests

--- src/Sulu/Bundle/AdminBundle/Resources/js/containers/List/fieldTransformers/ColorFieldTransformer.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/containers/List/fieldTransformers/ColorFieldTransformer.js
@@ -5,6 +5,6 @@ import type {FieldTransformer} from '../types';
 
 export default class ColorFieldTransformer implements FieldTransformer {
     transform(value: *): Node {
-        return <div style={{ 'backgroundColor': value, 'width': '40px', 'height': '20px' }}></div>;
+        return <div style={{'backgroundColor': value, 'width': '40px', 'height': '20px'}}></div>;
     }
 }
