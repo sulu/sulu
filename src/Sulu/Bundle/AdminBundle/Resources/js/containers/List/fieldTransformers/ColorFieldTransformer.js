6e932e408a5a113306411f2c7cf8821fd10bde2b
Nicolas Sittler <sittler.n@gmail.com>
2020-10-16 21:47:52 +0200

    AdminBundle ColorFieldTransformer: Add logger, add control hexadecimal color and scss

--- src/Sulu/Bundle/AdminBundle/Resources/js/containers/List/fieldTransformers/ColorFieldTransformer.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/containers/List/fieldTransformers/ColorFieldTransformer.js
@@ -1,10 +1,21 @@
 // @flow
 import React from 'react';
+import log from 'loglevel';
 import type {Node} from 'react';
 import type {FieldTransformer} from '../types';
+import colorFieldTransformerStyles from './colorFieldTransformer.scss';
 
 export default class ColorFieldTransformer implements FieldTransformer {
     transform(value: *): Node {
-        return <div style={{'backgroundColor': value, 'width': '40px', 'height': '20px'}}></div>;
+        //test si contain # et 3 ou 6 charateres
+        if (!/^#[0-9a-f]{3}([0-9a-f]{3})?$/i.test(value)) {
+            log.error(`Transformer parameter "${value}" needs to be of type hexadecimal color.`);
+            return value;
+        }
+
+        const style = {};
+        style.backgroundColor = value;
+
+        return <div className={colorFieldTransformerStyles.colorBox} style={style}></div>;
     }
 }
