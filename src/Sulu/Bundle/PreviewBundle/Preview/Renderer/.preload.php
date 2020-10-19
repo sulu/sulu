ef6b30898fba45a1286f85b7f18cab834eacbb2d
Pablo Lozano <lozanomunarriz@gmail.com>
2020-10-19 09:19:53 +0200

    remove preload

--- src/Sulu/Bundle/PreviewBundle/Preview/Renderer/.preload.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (file_exists(__DIR__.'/../../../../../../../../../var/cache/preview/dev/App_KernelDevDebugContainer.preload.php')) {
-    require __DIR__.'/../../../../../../../../../var/cache/preview/dev/App_KernelDevDebugContainer.preload.php';
-}
