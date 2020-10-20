a95b4b9f02a8ac42493f85d2dde74d913a6ebc62
Niklas Natter <niklas@sulu.io>
2020-10-20 10:53:48 +0200

    Enable refresh button immediately if some part changes

--- src/Sulu/Bundle/AdminBundle/Resources/js/containers/Form/fields/ResourceLocator.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/containers/Form/fields/ResourceLocator.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import {action, observable, toJS} from 'mobx';
+import {action, comparer, computed, observable, reaction, toJS} from 'mobx';
 import {observer} from 'mobx-react';
 import ResourceLocatorComponent from '../../../components/ResourceLocator';
 import ResourceLocatorHistory from '../../../containers/ResourceLocatorHistory';
@@ -18,59 +18,12 @@ const HOMEPAGE_RESOURCE_LOCATOR = '/';
 class ResourceLocator extends React.Component<FieldTypeProps<?string>> {
     @observable mode: string;
     @observable enableRefreshButton: boolean = false;
+    @observable wasModifiedManually: boolean = false;
 
-    constructor(props: FieldTypeProps<?string>) {
-        super(props);
+    partsChangeDisposer: () => mixed;
 
+    @computed get parts(): {[string]: mixed} {
         const {
-            dataPath,
-            fieldTypeOptions: {
-                generationUrl,
-                modeResolver,
-            },
-            formInspector,
-            value: initialValue,
-        } = this.props;
-
-        if (!modeResolver) {
-            throw new Error('The "modeResolver" must be a function returning a promise with the desired mode');
-        }
-
-        modeResolver(this.props).then(action((mode) => this.mode = mode));
-
-        if (!generationUrl) {
-            return;
-        }
-
-        if (typeof generationUrl !== 'string') {
-            throw new Error('The "generationUrl" fieldTypeOption must be a string!');
-        }
-
-        if (initialValue === HOMEPAGE_RESOURCE_LOCATOR) {
-            return;
-        }
-
-        formInspector.addFinishFieldHandler(action((finishedFieldDataPath, finishedFieldSchemaPath) => {
-            const {tags: finishedFieldTags} = formInspector.getSchemaEntryByPath(finishedFieldSchemaPath) || {};
-            if (!finishedFieldTags || !finishedFieldTags.some((tag) => tag.name === PART_TAG)) {
-                return;
-            }
-
-            if (initialValue === undefined && !formInspector.isFieldModified(dataPath)) {
-                this.refreshResourceLocator();
-            } else {
-                this.enableRefreshButton = true;
-            }
-        }));
-    }
-
-    refreshResourceLocator = () => {
-        const {
-            onChange,
-            fieldTypeOptions: {
-                generationUrl,
-                resourceStorePropertiesToRequest = {},
-            },
             formInspector,
         } = this.props;
 
@@ -86,9 +39,84 @@ class ResourceLocator extends React.Component<FieldTypeProps<?string>> {
                 return [path, value];
             });
 
-        if (partEntries.length === 0) {
+        return Object.fromEntries(partEntries);
+    }
+
+    @computed get enableAutoGeneration(): boolean {
+        const {
+            formInspector: {
+                id,
+            },
+        } = this.props;
+
+        return !id && !this.wasModifiedManually;
+    }
+
+    constructor(props: FieldTypeProps<?string>) {
+        super(props);
+
+        const {
+            fieldTypeOptions: {
+                generationUrl,
+                modeResolver,
+            },
+            formInspector,
+            value: initialValue,
+        } = this.props;
+
+        if (!modeResolver) {
+            throw new Error('The "modeResolver" must be a function returning a promise with the desired mode');
+        }
+
+        modeResolver(this.props).then(action((mode) => this.mode = mode));
+
+        if (initialValue === HOMEPAGE_RESOURCE_LOCATOR) {
             return;
         }
+
+        if (!generationUrl) {
+            return;
+        }
+
+        if (typeof generationUrl !== 'string') {
+            throw new Error('The "generationUrl" fieldTypeOption must be a string!');
+        }
+
+        this.partsChangeDisposer = reaction(
+            () => (this.parts),
+            action(() => {
+                if (!this.enableAutoGeneration) {
+                    this.enableRefreshButton = true;
+                }
+            }),
+            {equals: comparer.structural}
+        );
+
+        formInspector.addFinishFieldHandler(action((finishedFieldDataPath, finishedFieldSchemaPath) => {
+            const {tags: finishedFieldTags} = formInspector.getSchemaEntryByPath(finishedFieldSchemaPath) || {};
+            if (!finishedFieldTags || !finishedFieldTags.some((tag) => tag.name === PART_TAG)) {
+                return;
+            }
+
+            if (this.enableAutoGeneration) {
+                this.refreshResourceLocator();
+            }
+        }));
+    }
+
+    componentWillUnmount() {
+        this.partsChangeDisposer();
+    }
+
+    @action refreshResourceLocator = () => {
+        const {
+            fieldTypeOptions: {
+                generationUrl,
+                resourceStorePropertiesToRequest = {},
+            },
+            formInspector,
+            onChange,
+        } = this.props;
 
         const requestOptions = {...formInspector.options};
 
@@ -99,23 +127,33 @@ class ResourceLocator extends React.Component<FieldTypeProps<?string>> {
             }
         });
 
+        this.enableRefreshButton = false;
+
         Requester.post(
             generationUrl,
             {
-                parts: Object.fromEntries(partEntries),
+                parts: this.parts,
                 resourceKey: formInspector.resourceKey,
                 locale: formInspector.locale ? formInspector.locale.get() : undefined,
                 ...requestOptions,
             }
         ).then(action((response) => {
-            this.enableRefreshButton = false;
             onChange(response.resourcelocator);
         }));
     };
 
-    handleBlur = () => {
+    handleInputBlur = () => {
         const {onFinish} = this.props;
         onFinish();
+    };
+
+    @action handleInputChange = (value: ?string) => {
+        const {onChange} = this.props;
+
+        this.enableRefreshButton = true;
+        this.wasModifiedManually = true;
+
+        onChange(value);
     };
 
     handleRefreshButtonClick = () => {
@@ -146,7 +184,6 @@ class ResourceLocator extends React.Component<FieldTypeProps<?string>> {
             dataPath,
             disabled,
             formInspector,
-            onChange,
             value,
         } = this.props;
 
@@ -160,8 +197,8 @@ class ResourceLocator extends React.Component<FieldTypeProps<?string>> {
                     disabled={!!disabled}
                     id={dataPath}
                     mode={this.mode}
-                    onBlur={this.handleBlur}
-                    onChange={onChange}
+                    onBlur={this.handleInputBlur}
+                    onChange={this.handleInputChange}
                     value={value}
                 />
                 <div className={resourceLocatorStyles.buttonContainer}>
