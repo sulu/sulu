4b09504b30bfd5a221b56aa605f888cb38d30c23
nnatter <niklas@sulu.io>
2020-11-11 13:11:01 +0100

    Include postcss.config.js in UpdateBuildCommand (#5620)

--- src/Sulu/Bundle/AdminBundle/Command/UpdateBuildCommand.php
+++ src/Sulu/Bundle/AdminBundle/Command/UpdateBuildCommand.php
@@ -102,6 +102,7 @@ class UpdateBuildCommand extends Command
             'package.json',
             'webpack.config.js',
             '.babelrc',
+            'postcss.config.js',
         ];
 
         $needManualBuild = false;
