92a98198f50b228e0f626c48ae957875b4791c68
Alexander Schranz <alexander@sulu.io>
2020-12-01 11:35:20 +0100

    Use same logic as in symfony for parameters

--- src/Sulu/Bundle/TestBundle/Testing/JsonRequestTrait.php
+++ src/Sulu/Bundle/TestBundle/Testing/JsonRequestTrait.php
@@ -17,26 +17,35 @@ use Symfony\Component\DomCrawler\Crawler;
 trait JsonRequestTrait
 {
     /**
+     * Method which converts parameters into request content to work around a regression in Symfony 5.2 where its not
+     * longer possible to send float or integers as parameters.
+     *
      * @param mixed[] $parameters
      * @param mixed[] $server
      */
     protected static function jsonRequest(KernelBrowser $client, string $method, string $uri, array $parameters = [], array $server = [], bool $changeHistory = true): Crawler
     {
-        $content = null;
-
-        // GET parameters are still used as query parameters
-        // for all other Methods like POST, PUT, ... the request parameters are converted into a json object
-        if ('GET' !== $method) {
-            // JSON_THROW_ON_ERROR requires at least php 7.3
-            $content = \json_encode($parameters, \defined('JSON_THROW_ON_ERROR') ? \JSON_THROW_ON_ERROR : 0);
-            $parameters = [];
+        // Based on https://github.com/symfony/symfony/blob/v5.2.0/src/Symfony/Component/HttpFoundation/Request.php#L388-L404
+        // the logic in symfony above we convert parameters to json content.
+        switch (\strtoupper($method)) {
+            case 'POST':
+            case 'PUT':
+            case 'DELETE':
+            case 'PATCH':
+                $content = \json_encode($parameters, \defined('JSON_THROW_ON_ERROR') ? \JSON_THROW_ON_ERROR : 0);
+                $query = [];
+                break;
+            default:
+                $content = null;
+                $query = $parameters;
+                break;
         }
 
         return $client->request(
             $method,
             $uri,
-            $parameters,
-            [], // files are not supported for json requests
+            $query,
+            [],
             \array_merge([
                 'CONTENT_TYPE' => 'application/json',
                 'HTTP_ACCEPT' => 'application/json',
