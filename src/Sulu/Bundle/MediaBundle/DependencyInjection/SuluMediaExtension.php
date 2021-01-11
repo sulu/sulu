7f7e5470dcc91d27519dc0dea50519d7cf64a7c1
Alexander Schranz <alexander@sulu.io>
2021-01-11 10:03:23 +0100

    Fix configure ghostscript as env variable

--- src/Sulu/Bundle/MediaBundle/DependencyInjection/SuluMediaExtension.php
+++ src/Sulu/Bundle/MediaBundle/DependencyInjection/SuluMediaExtension.php
@@ -221,6 +221,11 @@ class SuluMediaExtension extends Extension implements PrependExtensionInterface
 
         // converter
         $ghostScriptPath = $config['ghost_script']['path'];
+
+        if (\method_exists($container, 'resolveEnvPlaceholders')) {
+            $ghostScriptPath = $container->resolveEnvPlaceholders($ghostScriptPath, true);
+        }
+
         $container->setParameter('sulu_media.ghost_script.path', $ghostScriptPath);
 
         // storage
