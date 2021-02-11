c9269e639ab3797a8adc33644c1480d4bb5f41e4
Prokyonn <daniel.mathis@sulu.io>
2021-02-11 15:11:55 +0100

    Move list padding to correct element

--- src/Sulu/Bundle/AdminBundle/Resources/js/views/List/List.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/views/List/List.js
@@ -16,6 +16,7 @@ import listToolbarActionRegistry from './registries/listToolbarActionRegistry';
 import listItemActionRegistry from './registries/listItemActionRegistry';
 import AbstractListToolbarAction from './toolbarActions/AbstractListToolbarAction';
 import AbstractListItemAction from './itemActions/AbstractListItemAction';
+import listStyles from './list.scss';
 
 const DEFAULT_USER_SETTINGS_KEY = 'list';
 const DEFAULT_LIMIT = 10;
@@ -373,21 +374,23 @@ class List extends React.Component<Props> {
 
         return (
             <Fragment>
-                <ListContainer
-                    adapters={adapters}
-                    header={title && <h1>{title}</h1>}
-                    itemActionsProvider={this.getItemActionConfigs}
-                    itemDisabledCondition={itemDisabledCondition}
-                    onItemAdd={onItemAdd || addView ? this.addItem : undefined}
-                    onItemClick={onItemClick || editView ? this.handleItemClick : undefined}
-                    pagination={pagination}
-                    ref={this.setListRef}
-                    searchable={searchable}
-                    selectable={selectable}
-                    store={this.listStore}
-                />
-                {this.toolbarActions.map((toolbarAction) => toolbarAction.getNode())}
-                {this.itemActions.map((itemAction) => itemAction.getNode())}
+                <div className={listStyles.listContainer}>
+                    <ListContainer
+                        adapters={adapters}
+                        header={title && <h1>{title}</h1>}
+                        itemActionsProvider={this.getItemActionConfigs}
+                        itemDisabledCondition={itemDisabledCondition}
+                        onItemAdd={onItemAdd || addView ? this.addItem : undefined}
+                        onItemClick={onItemClick || editView ? this.handleItemClick : undefined}
+                        pagination={pagination}
+                        ref={this.setListRef}
+                        searchable={searchable}
+                        selectable={selectable}
+                        store={this.listStore}
+                    />
+                    {this.toolbarActions.map((toolbarAction) => toolbarAction.getNode())}
+                    {this.itemActions.map((itemAction) => itemAction.getNode())}
+                </div>
             </Fragment>
         );
     }
