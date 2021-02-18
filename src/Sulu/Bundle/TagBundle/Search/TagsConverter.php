c5d69f796c8e7da79bd097e9175aba239a1a29ec
Luca Rath-Heel <luca@sulu.io>
2021-02-18 15:48:49 +0100

    Index embedded structures

--- src/Sulu/Bundle/TagBundle/Search/TagsConverter.php
+++ src/Sulu/Bundle/TagBundle/Search/TagsConverter.php
@@ -12,6 +12,7 @@
 namespace Sulu\Bundle\TagBundle\Search;
 
 use Massive\Bundle\SearchBundle\Search\Converter\ConverterInterface;
+use Massive\Bundle\SearchBundle\Search\Field;
 use Sulu\Bundle\TagBundle\Tag\TagManagerInterface;
 
 /**
@@ -29,8 +30,44 @@ class TagsConverter implements ConverterInterface
         $this->tagManager = $tagManager;
     }
 
-    public function convert($value)
+    public function convert($value/*, Document $document = null*/)
     {
-        return $this->tagManager->resolveTagNames($value);
+        if (\count(\func_get_args()) < 2 || false === \func_get_arg(1) || null === \func_get_arg(1)) {
+            // Preserve backward compatibility
+            return $this->tagManager->resolveTagNames($value);
+        }
+
+        $resultValue = null;
+        $fields = [];
+
+        if (\is_string($value)) {
+            $tag = $this->tagManager->findByName($value);
+            $resultValue = $tag->getId();
+
+            $fields = [
+                new Field('id', $tag->getId()),
+                new Field('name', $tag->getName()),
+            ];
+        }
+
+        if (\is_array($value)) {
+            $ids = $this->tagManager->resolveTagNames($value);
+            $resultValue = $ids;
+            $tags = \array_combine($ids, $value);
+
+            if (false !== $tags) {
+                $index = 0;
+                foreach ($tags as $id => $tagName) {
+                    $fields[] = new Field($index . '#id', $id);
+                    $fields[] = new Field($index . '#name', $tagName);
+                    ++$index;
+                }
+            }
+        }
+
+        return [
+            'value' => $resultValue,
+            'fields' => $fields,
+        ];
     }
 }
