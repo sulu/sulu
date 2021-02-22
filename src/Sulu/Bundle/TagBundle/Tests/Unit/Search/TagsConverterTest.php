4af992fa69b5e8dd4faebd045c74e61df5c4a657
Luca Rath-Heel <luca@sulu.io>
2021-02-22 09:04:24 +0100

    Index embedded structures (#5831)
    * Index embedded structures
    
    * Add tests for converters

--- src/Sulu/Bundle/TagBundle/Tests/Unit/Search/TagsConverterTest.php
+++ src/Sulu/Bundle/TagBundle/Tests/Unit/Search/TagsConverterTest.php
@@ -11,7 +11,10 @@
 
 namespace Sulu\Bundle\TagBundle\Tests\Unit\Search;
 
+use Massive\Bundle\SearchBundle\Search\Document;
+use Massive\Bundle\SearchBundle\Search\Field;
 use PHPUnit\Framework\TestCase;
+use Sulu\Bundle\TagBundle\Entity\Tag;
 use Sulu\Bundle\TagBundle\Search\TagsConverter;
 use Sulu\Bundle\TagBundle\Tag\TagManager;
 
@@ -19,12 +22,100 @@ class TagsConverterTest extends TestCase
 {
     public function testConvert()
     {
-        $manager = $this->prophesize(TagManager::class);
-        $manager->resolveTagNames(['Tag1', 'Tag2', 'Tag3'])->willReturn([1, 2, 3]);
+        $tagManager = $this->prophesize(TagManager::class);
+        $tagsConverter = new TagsConverter($tagManager->reveal());
 
-        $converter = new TagsConverter($manager->reveal());
+        $tagManager->resolveTagNames(['Tag1', 'Tag2', 'Tag3'])->willReturn([1, 2, 3]);
 
-        $this->assertEquals([1, 2, 3], $converter->convert(['Tag1', 'Tag2', 'Tag3']));
-        $manager->resolveTagNames(['Tag1', 'Tag2', 'Tag3'])->shouldBeCalled();
+        $this->assertEquals([1, 2, 3], $tagsConverter->convert(['Tag1', 'Tag2', 'Tag3']));
+    }
+
+    public function testConvertNull()
+    {
+        $tagManager = $this->prophesize(TagManager::class);
+        $tagsConverter = new TagsConverter($tagManager->reveal());
+
+        $this->assertNull(
+            $tagsConverter->convert(null)
+        );
+    }
+
+    public function testConvertWithDocumentAndNull()
+    {
+        $tagManager = $this->prophesize(TagManager::class);
+        $tagsConverter = new TagsConverter($tagManager->reveal());
+
+        $this->assertNull(
+            $tagsConverter->convert(null, $this->prophesize(Document::class)->reveal())
+        );
+    }
+
+    public function testConvertWithDocumentAndStringValue()
+    {
+        $tagManager = $this->prophesize(TagManager::class);
+        $tagsConverter = new TagsConverter($tagManager->reveal());
+
+        $originalValue = 'Tag1';
+
+        $tag = new Tag();
+        $tag->setId(1);
+        $tag->setName($originalValue);
+        $tagManager->findByName($originalValue)->willReturn($tag);
+
+        $value = $tagsConverter->convert($originalValue, $this->prophesize(Document::class)->reveal());
+
+        $this->assertArrayHasKey('value', $value);
+        $this->assertSame($tag->getId(), $value['value']);
+
+        $this->assertArrayHasKey('fields', $value);
+        $this->assertCount(2, $value['fields']);
+
+        /** @var Field $idField */
+        $idField = $value['fields'][0];
+        $this->assertSame('id', $idField->getName());
+        $this->assertSame($tag->getId(), $idField->getValue());
+
+        /** @var Field $nameField */
+        $nameField = $value['fields'][1];
+        $this->assertSame('name', $nameField->getName());
+        $this->assertSame($tag->getName(), $nameField->getValue());
+    }
+
+    public function testConvertWithDocumentAndArrayValue()
+    {
+        $tagManager = $this->prophesize(TagManager::class);
+        $tagsConverter = new TagsConverter($tagManager->reveal());
+
+        $originalValue = ['Tag1', 'Tag2'];
+        $ids = [1, 2];
+        $tagManager->resolveTagNames($originalValue)->willReturn($ids);
+
+        $value = $tagsConverter->convert($originalValue, $this->prophesize(Document::class)->reveal());
+
+        $this->assertArrayHasKey('value', $value);
+        $this->assertSame($ids, $value['value']);
+
+        $this->assertArrayHasKey('fields', $value);
+        $this->assertCount(4, $value['fields']);
+
+        /** @var Field $firstIdField */
+        $firstIdField = $value['fields'][0];
+        $this->assertSame('0#id', $firstIdField->getName());
+        $this->assertSame($ids[0], $firstIdField->getValue());
+
+        /** @var Field $firstNameField */
+        $firstNameField = $value['fields'][1];
+        $this->assertSame('0#name', $firstNameField->getName());
+        $this->assertSame($originalValue[0], $firstNameField->getValue());
+
+        /** @var Field $secondIdField */
+        $secondIdField = $value['fields'][2];
+        $this->assertSame('1#id', $secondIdField->getName());
+        $this->assertSame($ids[1], $secondIdField->getValue());
+
+        /** @var Field $secondNameField */
+        $secondNameField = $value['fields'][3];
+        $this->assertSame('1#name', $secondNameField->getName());
+        $this->assertSame($originalValue[1], $secondNameField->getValue());
     }
 }
