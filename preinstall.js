5f70365f37cb8ac233d18a7439e02410c9cb13f9
Alexander Schranz <alexander@sulu.io>
2021-03-08 10:59:10 +0100

    Update symlink scripts and babel config

--- preinstall.js
+++ preinstall.js
@@ -1,5 +1,8 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 
+const path = require('path');
+
+
 if (process.env.npm_execpath.indexOf('npm') === -1) {
     throw new Error('\x1b[31mYou must use "npm install", yarn is not supported\x1b[0m');
 }
