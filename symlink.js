3e53a1aa71a902467466edf1fca79f2b0562f141
Alexander Schranz <alexander@sulu.io>
2021-03-08 10:59:10 +0100

    Update symlink scripts and babel config

--- symlink.js
+++ symlink.js
@@ -7,11 +7,18 @@ const arguments = process.argv.slice(2);
 
 const from = arguments.length > 1 ? arguments[0] : '../../vendor';
 const to = arguments.length > 2 ? arguments[1] : 'node_modules/@sulu/vendor';
+const ignoreCheck = arguments.length > 3 && arguments[2] ==='ignoreCheck';
 
-console.log('npx symlink-dir ' + from + ' ' + to);
-
-exec('npx symlink-dir ' + from + ' ' + to, (error) => {
-    if (error) {
-        console.error('Error occured while creating symlink: ' + error);
-    }
-});
+if (
+    ignoreCheck
+    || (
+        'admin' === path.basename(process.env.PWD)
+        && 'assets' === path.basename(path.dirname(process.env.PWD))
+    )
+) {
+    exec('npx symlink-dir ' + from + ' ' + to, (error) => {
+        if (error) {
+            console.error('Error occured while creating symlink: ' + error);
+        }
+    });
+}
