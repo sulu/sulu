2a29906be9881dd64e9d526e3f141ecfbb15d1bf
Niklas Natter <niklas@sulu.io>
2021-03-25 15:47:36 +0100

    Add test cases for EventLogBundle subscribers

---
+++ src/Sulu/Bundle/EventLogBundle/Tests/Unit/Application/Subscriber/DispatchSpecificDomainEventSubscriberTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\TagBundle\Tests\Unit\Application\Subscriber;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use Sulu\Bundle\EventLogBundle\Application\Subscriber\DispatchSpecificDomainEventSubscriber;
+use Sulu\Bundle\EventLogBundle\Tests\Application\Domain\Event\TestDomainEvent;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+class DispatchSpecificDomainEventSubscriberTest extends TestCase
+{
+    /**
+     * @var EventDispatcherInterface|ObjectProphecy
+     */
+    private $eventDispatcher;
+
+    public function setUp(): void
+    {
+        $this->eventDispatcher = $this->prophesize(EventDispatcherInterface::class);
+    }
+
+    public function testDispatchDomainEventWithSpecificEventName()
+    {
+        $subscriber = $this->createDispatchSpecificDomainEventSubscriber();
+
+        $event = new TestDomainEvent();
+        $this->eventDispatcher->dispatch($event, TestDomainEvent::class)
+            ->shouldBeCalled()
+            ->willReturn($event);
+
+        $subscriber->dispatchDomainEventWithSpecificEventName($event);
+    }
+
+    private function createDispatchSpecificDomainEventSubscriber(): DispatchSpecificDomainEventSubscriber
+    {
+        return new DispatchSpecificDomainEventSubscriber(
+            $this->eventDispatcher->reveal()
+        );
+    }
+}
---
+++ src/Sulu/Bundle/EventLogBundle/Tests/Unit/Application/Subscriber/StoreEventRecordSubscriberTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\TagBundle\Tests\Unit\Application\Subscriber;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use Sulu\Bundle\EventLogBundle\Application\Subscriber\StoreEventRecordSubscriber;
+use Sulu\Bundle\EventLogBundle\Domain\Event\DomainEvent;
+use Sulu\Bundle\EventLogBundle\Domain\Model\EventRecordInterface;
+use Sulu\Bundle\EventLogBundle\Domain\Repository\EventRecordRepositoryInterface;
+
+class StoreEventRecordSubscriberTest extends TestCase
+{
+    /**
+     * @var EventRecordRepositoryInterface|ObjectProphecy
+     */
+    private $eventRecordRepository;
+
+    public function setUp(): void
+    {
+        $this->eventRecordRepository = $this->prophesize(EventRecordRepositoryInterface::class);
+    }
+
+    public function testStoreEventRecord()
+    {
+        $subscriber = $this->createStoreEventRecordSubscriber();
+
+        $event = $this->prophesize(DomainEvent::class);
+        $eventRecord = $this->prophesize(EventRecordInterface::class);
+
+        $this->eventRecordRepository->createForDomainEvent($event->reveal())
+            ->willReturn($eventRecord->reveal());
+        $this->eventRecordRepository->add($eventRecord->reveal())->shouldBeCalled();
+        $this->eventRecordRepository->commit()->shouldBeCalled();
+
+        $subscriber->storeEventRecord($event->reveal());
+    }
+
+    private function createStoreEventRecordSubscriber(): StoreEventRecordSubscriber
+    {
+        return new StoreEventRecordSubscriber(
+            $this->eventRecordRepository->reveal()
+        );
+    }
+}
