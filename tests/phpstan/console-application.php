8abd3bda42c9e41bf8b5ee8bcfdd5a866739fbae
Niklas Natter <niklas@sulu.io>
2021-03-26 12:31:03 +0100

    Generate phpstan-baseline.neon with PHP8

--- tests/phpstan/console-application.php
+++ tests/phpstan/console-application.php
@@ -2,12 +2,21 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 use App\Kernel;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__, 2) . '/vendor/autoload.php';
-(new Dotenv())->bootEnv(dirname(__DIR__, 2) . '/.env');
+require \dirname(__DIR__, 2) . '/vendor/autoload.php';
+(new Dotenv())->bootEnv(\dirname(__DIR__, 2) . '/.env');
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG'], Kernel::CONTEXT_ADMIN);
 $kernel->boot();
