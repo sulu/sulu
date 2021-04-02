1efe8a5a249c504b269209a63ffefed7201a4b89
Alexander Schranz <alexander@sulu.io>
2021-04-02 14:24:39 +0200

    Fix single quote import

--- src/Sulu/Bundle/MediaBundle/Resources/js/containers/MediaCollection/MediaCollection.js
+++ src/Sulu/Bundle/MediaBundle/Resources/js/containers/MediaCollection/MediaCollection.js
@@ -6,7 +6,7 @@ import type {IObservableValue} from 'mobx'; // eslint-disable-line import/named
 import {observer} from 'mobx-react';
 import {Divider} from 'sulu-admin-bundle/components';
 import {List, ListStore} from 'sulu-admin-bundle/containers';
-import {translate} from "sulu-admin-bundle/utils/Translator";
+import {translate} from 'sulu-admin-bundle/utils/Translator';
 import CollectionStore from '../../stores/CollectionStore';
 import MultiMediaDropzone from '../MultiMediaDropzone';
 import type {OverlayType} from './types';
