3a5b9660700a21517f7ea971573a36689dc183f9
Niklas Natter <niklas@sulu.io>
2021-05-05 14:54:39 +0200

    Use PersistenceBundleTrait to resolve perisitence bundle entities

---
+++ src/Sulu/Bundle/PersistenceBundle/DependencyInjection/Compiler/ActivateResolveTargetEntityResolverPass.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\PersistenceBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ActivateResolveTargetEntityResolverPass implements CompilerPassInterface
+{
+    /**
+     * @return void
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition('doctrine.orm.listeners.resolve_target_entity')) {
+            throw new \RuntimeException('Cannot find Doctrine Target Entity Resolver Listener.');
+        }
+
+        $resolveTargetEntityListener = $container->findDefinition('doctrine.orm.listeners.resolve_target_entity');
+
+        // we need to make sure that the service is added as event subscriber when doctrine bundle is not doing it
+        // when the bundle dont get any config
+        if (!$resolveTargetEntityListener->hasTag('doctrine.event_subscriber')) {
+            $resolveTargetEntityListener->addTag('doctrine.event_subscriber');
+        }
+    }
+}
