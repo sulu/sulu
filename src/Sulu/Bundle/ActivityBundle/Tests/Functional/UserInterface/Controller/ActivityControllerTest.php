aa308f2df64f68ef3bfe8150b55b18eb887c6bbd
Luca Rath-Heel <luca@sulu.io>
2021-05-07 16:30:24 +0200

    Add general activity view (#5962)
    * Add api for event records
    
    * Add necessary transformers for activity list
    
    * Rename user fieldDescriptor to userFullName
    
    * Add icon mapping for activity types
    
    * Add tests and fix review

---
+++ src/Sulu/Bundle/ActivityBundle/Tests/Functional/UserInterface/Controller/ActivityControllerTest.php
@@ -0,0 +1,239 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\ActivityBundle\Tests\Functional\UserInterface\Controller;
+
+use Sulu\Bundle\ActivityBundle\Domain\Model\Activity;
+use Sulu\Bundle\AdminBundle\Admin\Admin;
+use Sulu\Bundle\SecurityBundle\Entity\Permission;
+use Sulu\Bundle\SecurityBundle\Entity\Role;
+use Sulu\Bundle\SecurityBundle\Entity\UserRole;
+use Sulu\Bundle\TestBundle\Kernel\SuluKernelBrowser;
+use Sulu\Bundle\TestBundle\Testing\SuluTestCase;
+use Sulu\Component\Security\Authorization\AccessControl\SecuredEntityInterface;
+use Sulu\Component\Security\Authorization\PermissionTypes;
+
+class ActivityControllerTest extends SuluTestCase
+{
+    const GRANTED_CONTEXT = 'sulu.context.granted';
+    const NOT_GRANTED_CONTEXT = 'sulu.context.not_granted';
+
+    /**
+     * @var SuluKernelBrowser
+     */
+    private $client;
+
+    public static function setUpBeforeClass(): void
+    {
+        static::purgeDatabase();
+
+        $accessControlManager = static::getContainer()->get('sulu_security.access_control_manager');
+
+        $role = static::setUpUserRole();
+
+        $count = 0;
+        foreach (['pages', 'other'] as $resourceKey) {
+            foreach ([null, 'en', 'de'] as $resourceLocale) {
+                foreach ([null, self::GRANTED_CONTEXT, self::NOT_GRANTED_CONTEXT] as $resourceSecurityContext) {
+                    foreach ([null, true, false] as $objectSecurity) {
+                        $resourceId = (string) ++$count;
+                        $resourceSecurityObjectType = null !== $objectSecurity ? SecuredEntityInterface::class : null;
+                        $resourceSecurityObjectId = null !== $objectSecurity ? $resourceId : null;
+
+                        static::createActivity(
+                            $resourceKey,
+                            $resourceId,
+                            $resourceLocale,
+                            $resourceSecurityContext,
+                            $resourceSecurityObjectType,
+                            $resourceSecurityObjectId
+                        );
+
+                        if (null !== $resourceSecurityObjectType && null !== $resourceSecurityObjectId) {
+                            $accessControlManager->setPermissions(
+                                $resourceSecurityObjectType,
+                                $resourceSecurityObjectId,
+                                [
+                                    $role->getId() => [PermissionTypes::VIEW => $objectSecurity],
+                                ]
+                            );
+                        }
+                    }
+                }
+            }
+        }
+
+        static::ensureKernelShutdown();
+    }
+
+    public function setUp(): void
+    {
+        /** @var SuluKernelBrowser $client */
+        $client = $this->createAuthenticatedClient();
+
+        $this->client = $client;
+    }
+
+    /**
+     * @return \Generator<mixed[]>
+     */
+    public function provideCgetAction(): \Generator
+    {
+        yield [
+            [],
+            24,
+        ];
+
+        yield [
+            ['locale' => 'en'],
+            16,
+        ];
+
+        yield [
+            ['resourceKey' => 'pages'],
+            12,
+        ];
+
+        yield [
+            ['resourceId' => 'anything'], // resourceId is only taken into account, if resourceKey is set
+            24,
+        ];
+
+        yield [
+            ['locale' => 'de', 'resourceKey' => 'other'],
+            8,
+        ];
+
+        yield [
+            ['resourceKey' => 'pages', 'resourceId' => 1],
+            1,
+        ];
+
+        yield [
+            ['resourceKey' => 'pages', 'resourceId' => 3], // Not granted object permission
+            0,
+        ];
+
+        yield [
+            ['resourceKey' => 'pages', 'resourceId' => 7], // Not granted context permission
+            0,
+        ];
+    }
+
+    /**
+     * @dataProvider provideCgetAction
+     *
+     * @param mixed[] $parameters
+     */
+    public function testCgetAction(array $parameters, int $expectedTotal): void
+    {
+        $urlParameters = \array_merge(
+            ['limit' => 0],
+            $parameters
+        );
+
+        $this->client->jsonRequest('GET', '/api/activities', $urlParameters);
+        $content = \json_decode((string) $this->client->getResponse()->getContent());
+
+        self::assertSame($expectedTotal, $content->total);
+    }
+
+    public function testCgetActionActivityText(): void
+    {
+        $this->client->jsonRequest('GET', '/api/activities', [
+            'resourceKey' => 'pages',
+            'resourceId' => 1,
+        ]);
+
+        $content = \json_decode((string) $this->client->getResponse()->getContent());
+
+        self::assertSame('sulu_activity.text.pages.created', $content->_embedded->activities[0]->text);
+    }
+
+    private static function createActivity(
+        string $resourceKey,
+        string $resourceId,
+        ?string $resourceLocale,
+        ?string $resourceSecurityContext,
+        ?string $resourceSecurityObjectType,
+        ?string $resourceSecurityObjectId
+    ): Activity {
+        $repository = static::getContainer()->get('sulu_activity.activity_repository.doctrine');
+        $testUser = static::getTestUser();
+
+        $activity = new Activity();
+        $activity->setType('created');
+        $activity->setContext(['foo' => 'bar']);
+        $activity->setPayload(['bar' => 'baz']);
+        $activity->setTimestamp(new \DateTimeImmutable());
+        $activity->setBatch('batch-1234');
+        $activity->setUser($testUser);
+        $activity->setResourceKey($resourceKey);
+        $activity->setResourceId($resourceId);
+        $activity->setResourceLocale($resourceLocale);
+        $activity->setResourceWebspaceKey('sulu-io');
+        $activity->setResourceTitle('Test Page 1234');
+        $activity->setResourceTitleLocale('en');
+        $activity->setResourceSecurityContext($resourceSecurityContext);
+        $activity->setResourceSecurityObjectType($resourceSecurityObjectType);
+        $activity->setResourceSecurityObjectId($resourceSecurityObjectId);
+
+        $repository->addAndCommit($activity);
+
+        return $activity;
+    }
+
+    private static function setUpUserRole(): Role
+    {
+        $entityManager = static::getEntityManager();
+        $testUser = static::getTestUser();
+
+        $role = new Role();
+        $role->setAnonymous(false);
+        $role->setKey('test');
+        $role->setName('Test');
+        $role->setSystem(Admin::SULU_ADMIN_SECURITY_SYSTEM);
+
+        $entityManager->persist($role);
+
+        $grantedPermission = new Permission();
+        $grantedPermission->setContext(static::GRANTED_CONTEXT);
+        $grantedPermission->setPermissions(127);
+        $grantedPermission->setRole($role);
+
+        $entityManager->persist($grantedPermission);
+
+        $role->addPermission($grantedPermission);
+
+        $notGrantedPermission = new Permission();
+        $notGrantedPermission->setContext(static::NOT_GRANTED_CONTEXT);
+        $notGrantedPermission->setPermissions(0);
+        $notGrantedPermission->setRole($role);
+
+        $entityManager->persist($notGrantedPermission);
+
+        $role->addPermission($notGrantedPermission);
+
+        $userRole = new UserRole();
+        $userRole->setRole($role);
+        $userRole->setLocale('["en"]');
+        $userRole->setUser($testUser);
+        $entityManager->persist($userRole);
+
+        $testUser->addUserRole($userRole);
+
+        $entityManager->flush();
+
+        return $role;
+    }
+}
---
+++ src/Sulu/Bundle/ActivityBundle/UserInterface/Controller/ActivityController.php
@@ -0,0 +1,472 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\ActivityBundle\UserInterface\Controller;
+
+use FOS\RestBundle\View\ViewHandlerInterface;
+use HandcraftedInTheAlps\RestRoutingBundle\Routing\ClassResourceInterface;
+use Sulu\Bundle\ActivityBundle\Domain\Model\ActivityInterface;
+use Sulu\Bundle\ActivityBundle\Infrastructure\Sulu\Admin\ActivityAdmin;
+use Sulu\Component\Rest\AbstractRestController;
+use Sulu\Component\Rest\ListBuilder\Doctrine\DoctrineListBuilder;
+use Sulu\Component\Rest\ListBuilder\Doctrine\DoctrineListBuilderFactoryInterface;
+use Sulu\Component\Rest\ListBuilder\Doctrine\FieldDescriptor\DoctrineConcatenationFieldDescriptor;
+use Sulu\Component\Rest\ListBuilder\Doctrine\FieldDescriptor\DoctrineFieldDescriptor;
+use Sulu\Component\Rest\ListBuilder\Doctrine\FieldDescriptor\DoctrineJoinDescriptor;
+use Sulu\Component\Rest\ListBuilder\FieldDescriptorInterface;
+use Sulu\Component\Rest\ListBuilder\ListBuilderInterface;
+use Sulu\Component\Rest\ListBuilder\ListRepresentation;
+use Sulu\Component\Rest\ListBuilder\Metadata\FieldDescriptorFactoryInterface;
+use Sulu\Component\Rest\RequestParametersTrait;
+use Sulu\Component\Rest\RestHelperInterface;
+use Sulu\Component\Security\Authentication\UserInterface;
+use Sulu\Component\Security\Authorization\PermissionTypes;
+use Sulu\Component\Security\Authorization\SecurityCheckerInterface;
+use Sulu\Component\Security\SecuredControllerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class ActivityController extends AbstractRestController implements ClassResourceInterface, SecuredControllerInterface
+{
+    use RequestParametersTrait;
+
+    /**
+     * @var DoctrineListBuilderFactoryInterface
+     */
+    private $listBuilderFactory;
+
+    /**
+     * @var FieldDescriptorFactoryInterface
+     */
+    private $fieldDescriptorFactory;
+
+    /**
+     * @var RestHelperInterface
+     */
+    private $restHelper;
+
+    /**
+     * @var SecurityCheckerInterface
+     */
+    private $securityChecker;
+
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * @var string
+     */
+    private $activityClass;
+
+    /**
+     * @var string
+     */
+    private $contactClass;
+
+    /**
+     * @var string
+     */
+    private $userClass;
+
+    /**
+     * @var array<string, int>
+     */
+    private $permissions;
+
+    /**
+     * @param array<string, int> $permissions Inject `sulu_security.permissions` parameter
+     */
+    public function __construct(
+        FieldDescriptorFactoryInterface $fieldDescriptorFactory,
+        DoctrineListBuilderFactoryInterface $listBuilderFactory,
+        RestHelperInterface $restHelper,
+        SecurityCheckerInterface $securityChecker,
+        TranslatorInterface $translator,
+        string $activityClass,
+        string $contactClass,
+        string $userClass,
+        array $permissions,
+        ViewHandlerInterface $viewHandler,
+        ?TokenStorageInterface $tokenStorage = null
+    ) {
+        parent::__construct($viewHandler, $tokenStorage);
+
+        $this->fieldDescriptorFactory = $fieldDescriptorFactory;
+        $this->listBuilderFactory = $listBuilderFactory;
+        $this->restHelper = $restHelper;
+        $this->securityChecker = $securityChecker;
+        $this->translator = $translator;
+        $this->activityClass = $activityClass;
+        $this->contactClass = $contactClass;
+        $this->userClass = $userClass;
+        $this->permissions = $permissions;
+    }
+
+    public function cgetAction(Request $request): Response
+    {
+        /** @var UserInterface $user */
+        $user = $this->getUser();
+
+        /** @var array<string, FieldDescriptorInterface> $configurationFieldDescriptors */
+        $configurationFieldDescriptors = $this->fieldDescriptorFactory->getFieldDescriptors('activities');
+
+        $fieldDescriptors = \array_merge(
+            $this->getRawDataFieldDescriptors(),
+            $configurationFieldDescriptors
+        );
+
+        /** @var DoctrineListBuilder $listBuilder */
+        $listBuilder = $this->listBuilderFactory->create($this->activityClass);
+        $this->restHelper->initializeListBuilder($listBuilder, $fieldDescriptors);
+        $listBuilder->setSelectFields($fieldDescriptors);
+        $listBuilder->sort($fieldDescriptors['timestamp'], ListBuilderInterface::SORTORDER_DESC);
+
+        $translationLocale = $user->getLocale();
+
+        /** @var string|null $resourceLocale */
+        $resourceLocale = $this->getRequestParameter($request, 'locale');
+        /** @var string|null $resourceId */
+        $resourceId = $this->getRequestParameter($request, 'resourceId');
+        /** @var string|null $resourceKey */
+        $resourceKey = $this->getRequestParameter($request, 'resourceKey');
+
+        $this->addResourceSecurityContextCondition($listBuilder, $fieldDescriptors, $user);
+        $this->addResourceObjectSecurityCondition($listBuilder, $fieldDescriptors, $user);
+
+        if (null !== $resourceLocale) {
+            $this->addResourceLocaleCondition($listBuilder, $fieldDescriptors, $resourceLocale);
+        }
+
+        if (null !== $resourceKey) {
+            $this->addResourceKeyCondition($listBuilder, $fieldDescriptors, $resourceKey);
+
+            if (null !== $resourceId) {
+                $this->addResourceIdCondition($listBuilder, $fieldDescriptors, $resourceId);
+            }
+        }
+
+        $activities = $listBuilder->execute();
+        $activities = \array_map(
+            function(array $activity) use ($translationLocale, $configurationFieldDescriptors) {
+                $text = $this->getActivityText($activity, $translationLocale);
+
+                return \array_filter(
+                    \array_merge(
+                        $activity,
+                        [
+                            'text' => $text,
+                        ]
+                    ),
+                    function(string $key) use ($configurationFieldDescriptors) {
+                        return \in_array($key, \array_keys($configurationFieldDescriptors), true);
+                    },
+                    \ARRAY_FILTER_USE_KEY
+                );
+            },
+            $activities
+        );
+
+        $listRepresentation = new ListRepresentation(
+            $activities,
+            ActivityInterface::RESOURCE_KEY,
+            'sulu_activity.get_activities',
+            $request->query->all(),
+            $listBuilder->getCurrentPage(),
+            $listBuilder->getLimit(),
+            $listBuilder->count()
+        );
+
+        return $this->handleView(
+            $this->view($listRepresentation, 200)
+        );
+    }
+
+    /**
+     * @param array<string, FieldDescriptorInterface> $fieldDescriptors
+     */
+    private function addResourceSecurityContextCondition(
+        DoctrineListBuilder $listBuilder,
+        array $fieldDescriptors,
+        UserInterface $user
+    ): void {
+        /** @var DoctrineFieldDescriptor $resourceSecurityContextFieldDescriptor */
+        $resourceSecurityContextFieldDescriptor = $fieldDescriptors['resourceSecurityContext'];
+        $listBuilder->addPermissionCheckField($resourceSecurityContextFieldDescriptor);
+
+        $securityContexts = [];
+        $viewPermission = $this->permissions[PermissionTypes::VIEW];
+
+        foreach ($user->getRoleObjects() as $role) {
+            foreach ($role->getPermissions() as $permission) {
+                if (($permission->getPermissions() & $viewPermission) === $viewPermission) {
+                    $securityContexts[] = $permission->getContext();
+                }
+            }
+        }
+
+        $securityContexts = \array_unique($securityContexts);
+
+        $listBuilder->addExpression(
+            $listBuilder->createOrExpression(
+                [
+                    $listBuilder->createInExpression(
+                        $fieldDescriptors['resourceSecurityContext'],
+                        $securityContexts
+                    ),
+                    $listBuilder->createWhereExpression(
+                        $fieldDescriptors['resourceSecurityContext'],
+                        null,
+                        ListBuilderInterface::WHERE_COMPARATOR_EQUAL
+                    ),
+                ]
+            )
+        );
+    }
+
+    /**
+     * @param array<string, FieldDescriptorInterface> $fieldDescriptors
+     */
+    private function addResourceObjectSecurityCondition(
+        DoctrineListBuilder $listBuilder,
+        array $fieldDescriptors,
+        UserInterface $user
+    ): void {
+        /** @var DoctrineFieldDescriptor $resourceSecurityObjectIdFieldDescriptor */
+        $resourceSecurityObjectIdFieldDescriptor = $fieldDescriptors['resourceSecurityObjectId'];
+        $listBuilder->addPermissionCheckField($resourceSecurityObjectIdFieldDescriptor);
+
+        /** @var DoctrineFieldDescriptor $resourceSecurityObjectTypeFieldDescriptor */
+        $resourceSecurityObjectTypeFieldDescriptor = $fieldDescriptors['resourceSecurityObjectType'];
+        $listBuilder->addPermissionCheckField($resourceSecurityObjectTypeFieldDescriptor);
+
+        $listBuilder->setPermissionCheckWithDynamicEntityClass(
+            $user,
+            PermissionTypes::VIEW,
+            'resourceSecurityObjectType',
+            'resourceSecurityObjectId'
+        );
+    }
+
+    /**
+     * @param array<string, FieldDescriptorInterface> $fieldDescriptors
+     */
+    private function addResourceLocaleCondition(
+        DoctrineListBuilder $listBuilder,
+        array $fieldDescriptors,
+        string $resourceLocale
+    ): void {
+        $listBuilder->addExpression(
+            $listBuilder->createOrExpression(
+                [
+                    $listBuilder->createWhereExpression(
+                        $fieldDescriptors['resourceLocale'],
+                        $resourceLocale,
+                        ListBuilderInterface::WHERE_COMPARATOR_EQUAL
+                    ),
+                    $listBuilder->createWhereExpression(
+                        $fieldDescriptors['resourceLocale'],
+                        null,
+                        ListBuilderInterface::WHERE_COMPARATOR_EQUAL
+                    ),
+                ]
+            )
+        );
+    }
+
+    /**
+     * @param array<string, FieldDescriptorInterface> $fieldDescriptors
+     */
+    private function addResourceKeyCondition(
+        DoctrineListBuilder $listBuilder,
+        array $fieldDescriptors,
+        string $resourceKey
+    ): void {
+        $listBuilder->where(
+            $fieldDescriptors['resourceKey'],
+            $resourceKey,
+            ListBuilderInterface::WHERE_COMPARATOR_EQUAL
+        );
+    }
+
+    /**
+     * @param array<string, FieldDescriptorInterface> $fieldDescriptors
+     */
+    private function addResourceIdCondition(
+        DoctrineListBuilder $listBuilder,
+        array $fieldDescriptors,
+        string $resourceId
+    ): void {
+        $listBuilder->where(
+            $fieldDescriptors['resourceId'],
+            $resourceId,
+            ListBuilderInterface::WHERE_COMPARATOR_EQUAL
+        );
+    }
+
+    /**
+     * @param array<string, mixed> $array
+     *
+     * @return array<string, mixed>
+     */
+    private function getTranslationParameters(array $array, string $prefix = '{', string $suffix = '}'): array
+    {
+        $translationParameters = [];
+        foreach ($array as $key => $value) {
+            if (\is_null($value)) {
+                $translationParameters[$prefix . $key . $suffix] = 'null';
+            } elseif (\is_bool($value)) {
+                $translationParameters[$prefix . $key . $suffix] = $value ? 'true' : 'false';
+            } elseif (\is_numeric($value)) {
+                $translationParameters[$prefix . $key . $suffix] = $value;
+            } elseif (\is_scalar($value)) {
+                $translationParameters[$prefix . $key . $suffix] = (string) $value;
+            } elseif (\is_array($value)) {
+                foreach ($this->getTranslationParameters($value, '', '') as $translationParameterKey => $translationParameterValue) {
+                    $translationParameters[$prefix . $key . '.' . $translationParameterKey . $suffix] = $translationParameterValue;
+                }
+            }
+        }
+
+        return $translationParameters;
+    }
+
+    /**
+     * @param array<string, mixed> $activity
+     */
+    private function getActivityText(array $activity, string $translationLocale): string
+    {
+        $translationParameters = $this->getTranslationParameters($activity);
+
+        $user = $activity['userFullName'] ?? $this->translator->trans('sulu_activity.someone', [], 'admin', $translationLocale);
+
+        $resourceTitleLocale = $activity['resourceTitleLocale'] ?? null;
+        $resourceTitle = '"' . $activity['resourceTitle'] . '"';
+
+        if (null !== $resourceTitleLocale && $translationLocale !== $resourceTitleLocale) {
+            $resourceTitle .= ' [' . \strtoupper($resourceTitleLocale) . ']';
+        }
+
+        $translationParameters['{userFullName}'] = $user;
+        $translationParameters['{resourceTitle}'] = $resourceTitle;
+
+        return $this->translator->trans(
+            \sprintf(
+                'sulu_activity.text.%s.%s',
+                $activity['resourceKey'],
+                $activity['type']
+            ),
+            $translationParameters,
+            'admin',
+            $translationLocale
+        );
+    }
+
+    /**
+     * @return array<string, FieldDescriptorInterface>
+     */
+    private function getRawDataFieldDescriptors(): array
+    {
+        $userJoins = [
+            $this->userClass => new DoctrineJoinDescriptor(
+                $this->userClass,
+                $this->activityClass . '.user'
+            ),
+            $this->contactClass => new DoctrineJoinDescriptor(
+                $this->contactClass,
+                $this->userClass . '.contact'
+            ),
+        ];
+
+        return [
+            'type' => $this->createFieldDescriptor('type'),
+            'context' => $this->createFieldDescriptor('context'),
+            'timestamp' => $this->createFieldDescriptor('timestamp'),
+            'batch' => $this->createFieldDescriptor('batch'),
+            'resourceKey' => $this->createFieldDescriptor('resourceKey'),
+            'resourceId' => $this->createFieldDescriptor('resourceId'),
+            'resourceLocale' => $this->createFieldDescriptor('resourceLocale'),
+            'resourceWebspaceKey' => $this->createFieldDescriptor('resourceWebspaceKey'),
+            'resourceTitle' => $this->createFieldDescriptor('resourceTitle'),
+            'resourceTitleLocale' => $this->createFieldDescriptor('resourceTitleLocale'),
+            'resourceSecurityContext' => $this->createFieldDescriptor('resourceSecurityContext'),
+            'resourceSecurityObjectType' => $this->createFieldDescriptor('resourceSecurityObjectType'),
+            'resourceSecurityObjectId' => $this->createFieldDescriptor('resourceSecurityObjectId'),
+            'userFullName' => $this->createConcatenationFieldDescriptor(
+                'userFullName',
+                [
+                    $this->createFieldDescriptor('firstName', null, $this->contactClass, $userJoins),
+                    $this->createFieldDescriptor('lastName', null, $this->contactClass, $userJoins),
+                ]
+            ),
+        ];
+    }
+
+    /**
+     * @param DoctrineJoinDescriptor[]|null $joins
+     */
+    private function createFieldDescriptor(
+        string $name,
+        ?string $fieldName = null,
+        ?string $entityName = null,
+        ?array $joins = null,
+        ?string $type = null
+    ): DoctrineFieldDescriptor {
+        $fieldName = $fieldName ?? $name;
+        $entityName = $entityName ?? $this->activityClass;
+        $joins = $joins ?? [];
+        $type = $type ?? 'string';
+
+        return new DoctrineFieldDescriptor(
+            $fieldName,
+            $name,
+            $entityName,
+            null,
+            $joins,
+            FieldDescriptorInterface::VISIBILITY_ALWAYS,
+            FieldDescriptorInterface::SEARCHABILITY_NEVER,
+            $type,
+            false
+        );
+    }
+
+    /**
+     * @param FieldDescriptorInterface[] $fieldDescriptors
+     */
+    private function createConcatenationFieldDescriptor(
+        string $name,
+        array $fieldDescriptors,
+        ?string $glue = null,
+        ?string $type = null
+    ): DoctrineConcatenationFieldDescriptor {
+        $glue = $glue ?? ' ';
+        $type = $type ?? 'string';
+
+        return new DoctrineConcatenationFieldDescriptor(
+            $fieldDescriptors,
+            $name,
+            null,
+            $glue,
+            FieldDescriptorInterface::VISIBILITY_ALWAYS,
+            FieldDescriptorInterface::SEARCHABILITY_NEVER,
+            $type,
+            false
+        );
+    }
+
+    public function getSecurityContext(): string
+    {
+        return ActivityAdmin::SECURITY_CONTEXT;
+    }
+}
