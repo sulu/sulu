87b1803a7a2a3fafaf62fb6c246beed6d9d00727
nnatter <niklas@sulu.io>
2021-05-11 10:27:04 +0200

    Update ajv package to new major version (#6009)

--- src/Sulu/Bundle/AdminBundle/Resources/js/utils/Ajv/createAjv.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/utils/Ajv/createAjv.js
@@ -1,13 +1,11 @@
 // @flow
 import Ajv, {Options} from 'ajv';
-import applyAjvKeywords from 'ajv-keywords';
 import applyAjvFormats from 'ajv-formats';
 import customFormats from './formats';
 
 const createAjv = (options: typeof Options = {allErrors: true, allowUnionTypes: true}) => {
     const ajv = new Ajv(options);
 
-    applyAjvKeywords(ajv);
     applyAjvFormats(ajv);
 
     Object.entries(customFormats).forEach(([name, format]) => {
