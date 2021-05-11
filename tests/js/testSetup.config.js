94266b491602dd0c83a06da515f200dff9e2757e
Niklas Natter <niklas@sulu.io>
2021-05-11 11:24:28 +0200

    Install @wojtekmaj/enzyme-adapter-react-17 package

--- tests/js/testSetup.config.js
+++ tests/js/testSetup.config.js
@@ -1,6 +1,6 @@
 // @flow
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({adapter: new Adapter()});
 
