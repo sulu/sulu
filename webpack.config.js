b0adaf68b3dbadb4bab7c5aff56b0d7878f35473
Alexander Schranz <alexander@sulu.io>
2021-06-17 11:13:17 +0200

    Remove module resolve and add symlinks also to loaders

--- webpack.config.js
+++ webpack.config.js
@@ -66,11 +66,10 @@ module.exports = (env, argv) => { // eslint-disable-line no-undef
                     'vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js'
                 ),
             },
-            symlinks: false,
-            modules: ['node_modules', nodeModulesPath],
+            symlinks: false, // @see https://github.com/sulu/sulu/pull/6117
         },
         resolveLoader: {
-            modules: ['node_modules', nodeModulesPath],
+            symlinks: false, // @see https://github.com/sulu/sulu/pull/6117
         },
         module: {
             rules: [
