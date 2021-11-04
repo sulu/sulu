ad50b4958a77a115b62ebce5c414642603856df4
Niklas Natter <niklas@sulu.io>
2021-11-04 12:18:37 +0100

    Add sulu-trash-bundle to package.json and index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import 'sulu-route-bundle';
 import 'sulu-search-bundle';
 import 'sulu-security-bundle';
 import 'sulu-snippet-bundle';
+import 'sulu-trash-bundle';
 import 'sulu-website-bundle';
 
 startAdmin();
