65fe3e519cb22053891595a67b4585fd657a31da
Luca Rath-Heel <luca@sulu.io>
2021-12-02 18:19:45 +0100

    Add PropertyMetadataMapper for number field type (#6310)

--- src/Sulu/Bundle/AdminBundle/Metadata/SchemaMetadata/NumberMetadata.php
+++ src/Sulu/Bundle/AdminBundle/Metadata/SchemaMetadata/NumberMetadata.php
@@ -13,10 +13,56 @@ namespace Sulu\Bundle\AdminBundle\Metadata\SchemaMetadata;
 
 class NumberMetadata implements SchemaMetadataInterface
 {
+    /**
+     * @var float|null
+     */
+    private $minimum;
+
+    /**
+     * @var float|null
+     */
+    private $maximum;
+
+    /**
+     * @var float|null
+     */
+    private $multipleOf;
+
+    public function __construct(
+        ?float $minimum = null,
+        ?float $maximum = null,
+        ?float $multipleOf = null
+    ) {
+        $this->minimum = $minimum;
+        $this->maximum = $maximum;
+        $this->multipleOf = $multipleOf;
+    }
+
     public function toJsonSchema(): array
     {
-        return [
-            'type' => 'number',
+        $jsonSchema = [
+            'type' => $this->multipleOf && $this->isInteger($this->multipleOf)
+                ? 'integer'
+                : 'number',
         ];
+
+        if (null !== $this->minimum) {
+            $jsonSchema['minimum'] = $this->minimum;
+        }
+
+        if (null !== $this->maximum) {
+            $jsonSchema['maximum'] = $this->maximum;
+        }
+
+        if (null !== $this->multipleOf) {
+            $jsonSchema['multipleOf'] = $this->multipleOf;
+        }
+
+        return $jsonSchema;
+    }
+
+    private function isInteger(float $number): bool
+    {
+        return 0.0 === \fmod($number, 1);
     }
 }
