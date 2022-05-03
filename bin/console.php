8cd3188acd17c468f7001cca6f1d2c9e888aa7ac
Alexander Schranz <alexander@sulu.io>
2022-05-03 22:55:15 +0200

    Update env files and add docker files for development

--- bin/console.php
+++ bin/console.php
@@ -12,17 +12,17 @@
 use App\Kernel;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 
-if (!is_file(dirname(__DIR__).'/vendor/autoload_runtime.php')) {
+if (!\is_file(\dirname(__DIR__) . '/vendor/autoload_runtime.php')) {
     throw new LogicException('Symfony Runtime is missing. Try running "composer require symfony/runtime".');
 }
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once \dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 if (!isset($suluContext)) {
     $suluContext = Kernel::CONTEXT_ADMIN;
 }
 
-return function (array $context) use ($suluContext) {
+return function(array $context) use ($suluContext) {
     $kernel = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG'], $suluContext);
 
     return new Application($kernel);
