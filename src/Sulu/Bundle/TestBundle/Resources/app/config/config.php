c0f5b548d9ec4567915fb6f37586e0b3e7d2d2fc
Alexander Schranz <alexander@sulu.io>
2022-05-10 22:09:50 +0200

    Add config bridge to symfony 6 security system for tests

--- src/Sulu/Bundle/TestBundle/Resources/app/config/config.php
+++ src/Sulu/Bundle/TestBundle/Resources/app/config/config.php
@@ -9,6 +9,8 @@
  * with this source code in the file LICENSE.
  */
 
+use Symfony\Component\HttpKernel\Kernel;
+
 $filesystem = new \Symfony\Component\Filesystem\Filesystem();
 
 $context = $container->getParameter('sulu.context');
@@ -21,4 +23,18 @@ $loader->import('context_' . $context . '.yml');
 
 if (\class_exists(\Swift_Mailer::class)) {
     $loader->import('swiftmailer.yml');
+}
+
+if ('admin' === $context) {
+    if (\version_compare(Kernel::VERSION, '6.0.0', '>=')) {
+        $loader->import('security-6.yml');
+    } else {
+        $loader->import('security-5-4.yml');
+    }
+}
+
+if (\version_compare(Kernel::VERSION, '6.0.0', '>=')) {
+    $loader->import('symfony-6.yml');
+} else {
+    $loader->import('symfony-5-4.yml');
 }
