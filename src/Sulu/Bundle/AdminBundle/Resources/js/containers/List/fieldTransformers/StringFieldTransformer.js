6c0ba36c9c62c89193d5e1dd7fa4499c0bff0e20
Thomas Zinnbauer <thomas.zinnbauer@ymc.ch>
2022-06-29 10:10:10 +0200

    Added title attrib and overflow css to StringFieldTransformer

--- src/Sulu/Bundle/AdminBundle/Resources/js/containers/List/fieldTransformers/StringFieldTransformer.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/containers/List/fieldTransformers/StringFieldTransformer.js
@@ -1,9 +1,11 @@
 // @flow
+import React from 'react';
 import type {Node} from 'react';
+import colorFieldTransformerStyles from './stringFieldTransformer.scss';
 import type {FieldTransformer} from '../types';
 
 export default class StringFieldTransformer implements FieldTransformer {
     transform(value: *): Node {
-        return value;
+        return <span title={value} className={colorFieldTransformerStyles.textBox}>{value}</span>;
     }
 }
