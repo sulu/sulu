e17c478018d011dd863d1a3ff9edbb0dab31726b
Thomas Zinnbauer <thomas.zinnbauer@ymc.ch>
2022-07-01 17:11:40 +0200

    Improved admin table cell text overflow handling (#6508)
    * added title attribute and added working text overflow for cell content css
    
    * revert first approach changes
    
    * Added title attrib and overflow css to StringFieldTransformer
    
    * Fix lint errors
    
    * Update snapshots and test cases
    
    Co-authored-by: Niklas Natter <niklas@sulu.io>

--- src/Sulu/Bundle/AdminBundle/Resources/js/containers/List/fieldTransformers/StringFieldTransformer.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/containers/List/fieldTransformers/StringFieldTransformer.js
@@ -1,9 +1,15 @@
 // @flow
+import React from 'react';
+import stringFieldTransformerStyles from './stringFieldTransformer.scss';
 import type {Node} from 'react';
 import type {FieldTransformer} from '../types';
 
 export default class StringFieldTransformer implements FieldTransformer {
     transform(value: *): Node {
-        return value;
+        if (!value) {
+            return null;
+        }
+
+        return <span className={stringFieldTransformerStyles.textBox} title={value}>{value}</span>;
     }
 }
