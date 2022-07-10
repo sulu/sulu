76885d62ea463f8e3af894f67a91a5edf9833e7a
Alexander Schranz <alexander@sulu.io>
2022-07-10 21:21:49 +0200

    Upgrade stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,8 +2,17 @@
 'use strict';
 
 module.exports = { // eslint-disable-line
-    'extends': 'stylelint-config-standard',
+    'extends': 'stylelint-config-standard-scss',
     'rules': {
+        'scss/dollar-variable-pattern': null,
+        'selector-class-pattern': null,
+        'scss/dollar-variable-empty-line-before': null,
+        'scss/at-import-partial-extension': null,
+        'shorthand-property-no-redundant-values': null,
+        'declaration-block-no-redundant-longhand-properties': null,
+        'value-keyword-case': ['lower', {
+            camelCaseSvgKeywords: true,
+        }],
         'indentation': 4,
         'max-line-length': 120,
         'no-descending-specificity': null,
