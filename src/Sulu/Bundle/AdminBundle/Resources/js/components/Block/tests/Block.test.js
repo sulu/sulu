4ebde578dc56fda00b6904d828681a8c89f2a458
simon.ostini <simon.ostini@massiveart.com>
2022-07-26 10:12:02 +0200

    added block tests

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Block/tests/Block.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Block/tests/Block.test.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import Block from '../Block';
 
 jest.mock('../../../utils/Translator', () => ({
@@ -23,4 +23,43 @@ test('Render an expanded block with multiple types', () => {
         </Block>);
 
     expect(container).toMatchSnapshot();
+});
+
+test('Render an block without dragHandle or collapse or expand button', () => {
+    const {container} = render(
+        <Block expanded={true}>
+            Some block content
+        </Block>
+    );
+    expect(container).toMatchSnapshot();
+});
+
+test('Render a collapsed block', () => {
+    const {container} = render(
+        <Block expanded={false} icons={['su-eye', 'su-people']} onCollapse={jest.fn()} onExpand={jest.fn()}>
+            Some block content
+        </Block>
+    );
+    expect(container).toMatchSnapshot();
+});
+
+test('Do not show type dropdown if only a single type is passed', () => {
+    const {container} = render(
+        <Block expanded={true} onCollapse={jest.fn()} onExpand={jest.fn()} types={{'type': 'Type'}}>
+            Some block content
+        </Block>
+    );
+
+    const element = container.querySelector('.select');
+    expect(element).not.toBeInTheDocument();
+});
+
+test('Do not show remove icon if no onRemove prop has been passed', () => {
+    render(
+        <Block expanded={true} onCollapse={jest.fn()} onExpand={jest.fn()} types={{'type': 'Type'}}>
+            Some block content
+        </Block>
+    );
+
+    expect(screen.queryByLabelText('su-trash-alt')).not.toBeInTheDocument();
 });
