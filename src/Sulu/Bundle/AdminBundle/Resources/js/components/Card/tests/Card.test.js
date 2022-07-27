a3e914a124e6042547329d5b2f1838e8aacb0963
simon.ostini <simon.ostini@massiveart.com>
2022-07-27 11:34:50 +0200

    migrated card tests

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Card/tests/Card.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Card/tests/Card.test.js
@@ -1,30 +1,34 @@
 // @flow
 import React from 'react';
-import {shallow, render} from 'enzyme';
+import {fireEvent, render, screen} from '@testing-library/react';
 import Card from '../Card';
 
 test('Render a card with its children', () => {
-    expect(render(<Card><h1>Content!</h1></Card>)).toMatchSnapshot();
+    const {container} = render(<Card><h1>Content!</h1></Card>);
+    expect(container).toMatchSnapshot();
 });
 
 test('Render a card with its children, edit and remove icon', () => {
-    expect(render(<Card onEdit={jest.fn()} onRemove={jest.fn()}>Content</Card>)).toMatchSnapshot();
+    const {container} = render(<Card onEdit={jest.fn()} onRemove={jest.fn()}>Content</Card>);
+    expect(container).toMatchSnapshot();
 });
 
 test('Call onEdit callback when edit icon is clicked', () => {
     const editSpy = jest.fn();
-    const card = shallow(<Card id={6} onEdit={editSpy}>Content</Card>);
+    render(<Card id={6} onEdit={editSpy}>Content</Card>);
+    const icon = screen.queryByLabelText('su-pen');
 
-    card.find('Icon[name="su-pen"]').simulate('click');
+    fireEvent.click(icon);
 
     expect(editSpy).toBeCalledWith(6);
 });
 
 test('Call onRemove callback when remove icon is clicked', () => {
     const removeSpy = jest.fn();
-    const card = shallow(<Card id={2} onRemove={removeSpy}>Content</Card>);
+    render(<Card id={2} onRemove={removeSpy}>Content</Card>);
+    const icon = screen.queryByLabelText('su-trash-alt');
 
-    card.find('Icon[name="su-trash-alt"]').simulate('click');
+    fireEvent.click(icon);
 
     expect(removeSpy).toBeCalledWith(2);
 });
