450f04293273571e9f0b116078230b2e98980d20
simon.ostini <simon.ostini@massiveart.com>
2022-07-28 16:10:20 +0200

    migrated chip component tests

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Chip/tests/Chip.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Chip/tests/Chip.test.js
@@ -1,34 +1,34 @@
 // @flow
 import React from 'react';
-import {render, shallow} from 'enzyme';
+import {fireEvent, render, screen} from '@testing-library/react';
 import Chip from '../Chip';
 
 test('Should render chip with children', () => {
-    expect(render(<Chip value={{}}>Name</Chip>)).toMatchSnapshot();
+    const {container} = render(<Chip value={{}}>Name</Chip>);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should render medium, primary chip with children', () => {
-    expect(render(<Chip size="medium" skin="primary" value={{}}>Name</Chip>)).toMatchSnapshot();
-});
-
-test('Should render chip with delete icon', () => {
-    expect(render(<Chip onDelete={jest.fn()} value={{}}>Name</Chip>)).toMatchSnapshot();
+    const {container} = render(<Chip size="medium" skin="primary" value={{}}>Name</Chip>);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should render chip without delete icon in disabled state', () => {
-    expect(render(<Chip disabled={true} onDelete={jest.fn()} value={{}}>Name</Chip>)).toMatchSnapshot();
+    const {container} = render(<Chip disabled={true} onDelete={jest.fn()} value={{}}>Name</Chip>);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should render chip as clickable', () => {
-    expect(render(<Chip onClick={jest.fn()} value={{}}>Name</Chip>)).toMatchSnapshot();
+    const {container} = render(<Chip onClick={jest.fn()} value={{}}>Name</Chip>);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should call onClick callback when the button is clicked', () => {
     const clickSpy = jest.fn();
     const value = {name: 'Test'};
-    const item = shallow(<Chip onClick={clickSpy} onDelete={jest.fn()} value={value}>Test</Chip>);
+    render(<Chip onClick={clickSpy} onDelete={jest.fn()} value={value}>Test</Chip>);
 
-    item.find('button').simulate('click');
+    fireEvent.click(screen.queryByText('Test'));
 
     expect(clickSpy).toBeCalledWith(value);
 });
@@ -36,9 +36,9 @@ test('Should call onClick callback when the button is clicked', () => {
 test('Should call onDelete callback when the times icon is clicked', () => {
     const deleteSpy = jest.fn();
     const value = {name: 'Test'};
-    const item = shallow(<Chip onDelete={deleteSpy} value={value}>Test</Chip>);
+    render(<Chip onDelete={deleteSpy} value={value}>Test</Chip>);
 
-    item.find('Icon').simulate('click');
+    fireEvent.click(screen.queryByLabelText('su-times'));
 
     expect(deleteSpy).toBeCalledWith(value);
 });
