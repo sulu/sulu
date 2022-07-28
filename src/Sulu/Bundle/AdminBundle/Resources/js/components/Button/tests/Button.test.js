544a6714d9ea4500e7cfdd5913d31c0193511858
Behemoth <54234287+b3h3m0th@users.noreply.github.com>
2022-07-28 09:55:10 +0200

    Migrate enzyme Button tests to React Testing Library (#6721)

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Button/tests/Button.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Button/tests/Button.test.js
@@ -1,72 +1,91 @@
 // @flow
 import React from 'react';
-import {mount, render, shallow} from 'enzyme';
+import {createEvent, fireEvent, render, screen} from '@testing-library/react';
 import Button from '../Button';
 
 test('Should render the button with icon', () => {
-    expect(render(<Button icon="su-plus">Add something</Button>)).toMatchSnapshot();
+    const {container} = render(<Button icon="su-plus">Add something</Button>);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should render with skin primary', () => {
-    expect(render(<Button skin="primary" />)).toMatchSnapshot();
+    const {container} = render(<Button skin="primary" />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should render with skin secondary', () => {
-    expect(render(<Button skin="secondary" />)).toMatchSnapshot();
+    const {container} = render(<Button skin="secondary" />);
+    expect(container).toMatchSnapshot();
 });
 
 test('should render disabled with skin secondary', () => {
-    expect(render(<Button disabled={true} skin="secondary" />)).toMatchSnapshot();
+    const {container} = render(<Button disabled={true} skin="secondary" />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should render with skin link', () => {
-    expect(render(<Button skin="link" />)).toMatchSnapshot();
+    const {container} = render(<Button skin="link" />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should render with skin text', () => {
-    expect(render(<Button skin="text" />)).toMatchSnapshot();
+    const {container} = render(<Button skin="text" />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should render with skin icon', () => {
-    expect(render(<Button skin="icon" />)).toMatchSnapshot();
+    const {container} = render(<Button skin="icon" />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should render with skin icon and text', () => {
-    expect(render(<Button skin="icon">Icon Text</Button>)).toMatchSnapshot();
+    const {container} = render(<Button skin="icon">Icon Text</Button>);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should render with skin icon and active', () => {
-    expect(render(<Button active={true} skin="icon" />)).toMatchSnapshot();
+    const {container} = render(<Button active={true} skin="icon" />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should render with skin icon and dropdown icon', () => {
-    expect(render(<Button showDropdownIcon={true} skin="icon" />)).toMatchSnapshot();
+    const {container} = render(<Button showDropdownIcon={true} skin="icon" />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should render with skin primary and dropdown icon', () => {
-    expect(render(<Button showDropdownIcon={true} skin="primary" />)).toMatchSnapshot();
+    const {container} = render( <Button showDropdownIcon={true} skin="primary" />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should render with skin secondary and dropdown icon', () => {
-    expect(render(<Button showDropdownIcon={true} skin="secondary" />)).toMatchSnapshot();
+    const {container} = render(<Button showDropdownIcon={true} skin="secondary" />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should render with skin link and dropdown icon', () => {
-    expect(render(<Button showDropdownIcon={true} skin="link" />)).toMatchSnapshot();
+    const {container} = render(<Button showDropdownIcon={true} skin="link" />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should call the callback on click', () => {
-    const preventDefaultSpy = jest.fn();
     const onClick = jest.fn();
-    const button = shallow(<Button onClick={onClick} skin="primary" />);
-    button.find('button').simulate('click', {preventDefault: preventDefaultSpy});
-    expect(preventDefaultSpy).toBeCalled();
+    render(<Button onClick={onClick} skin="primary" />);
+
+    const button = screen.queryByRole('button');
+    const onClickPreventDefault = createEvent.click(button);
+
+    fireEvent(button, onClickPreventDefault);
+
+    expect(onClickPreventDefault.defaultPrevented).toBe(true);
     expect(onClick).toBeCalled();
 });
 
 test('Should call the buttonRef callback correctly', () => {
     const buttonRefSpy = jest.fn();
-    const button = mount(<Button buttonRef={buttonRefSpy} />);
+    render(<Button buttonRef={buttonRefSpy} />);
 
-    expect(buttonRefSpy).toBeCalledWith(button.find('button').instance());
+    const button = screen.queryByRole('button');
+
+    expect(buttonRefSpy).toBeCalledWith(button);
 });
