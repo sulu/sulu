fa6476f802c548575ae98b527b098f2517b702a6
Simon Ostini <simonostini@gmail.com>
2022-07-29 10:19:23 +0200

    Migrate enzyme Checkbox & CheckboxGroup tests to React Testing Library (#6731)

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Checkbox/tests/CheckboxGroup.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Checkbox/tests/CheckboxGroup.test.js
@@ -1,33 +1,37 @@
 // @flow
 import React from 'react';
-import {mount, render} from 'enzyme';
+import {fireEvent, render, screen} from '@testing-library/react';
 import CheckboxGroup from '../CheckboxGroup';
 import Checkbox from '../Checkbox';
 
 test('The component should render', () => {
-    expect(render(
+    const {container} = render(
         <CheckboxGroup className="test" onChange={jest.fn()} values={['value-2', 'value-3']}>
             <Checkbox value="value-1">Value 1</Checkbox>
             <Checkbox value="value-2">Value 2</Checkbox>
             <Checkbox value="value-3">Value 3</Checkbox>
         </CheckboxGroup>
-    )).toMatchSnapshot();
+    );
+
+    expect(container).toMatchSnapshot();
 });
 
 test('The component should render disabled', () => {
-    expect(render(
+    const {container} = render(
         <CheckboxGroup disabled={true} onChange={jest.fn()} values={['value-2', 'value-3']}>
             <Checkbox value="value-1">Value 1</Checkbox>
             <Checkbox value="value-2">Value 2</Checkbox>
             <Checkbox value="value-3">Value 3</Checkbox>
         </CheckboxGroup>
-    )).toMatchSnapshot();
+    );
+
+    expect(container).toMatchSnapshot();
 });
 
 test('The component should call onChange handler when checkboxes are clicked', () => {
     const changeSpy = jest.fn();
 
-    const checkboxGroup = mount(
+    render(
         <CheckboxGroup onChange={changeSpy} values={['value-2', 'value-3']}>
             <Checkbox value="value-1">Value 1</Checkbox>
             <Checkbox value="value-2">Value 2</Checkbox>
@@ -37,9 +41,12 @@ test('The component should call onChange handler when checkboxes are clicked', (
 
     expect(changeSpy).not.toBeCalled();
 
-    checkboxGroup.find('Checkbox[value="value-1"] input').prop('onChange')({currentTarget: {checked: true}});
+    const checkbox1 = screen.getByDisplayValue('value-1');
+    const checkbox3 = screen.getByDisplayValue('value-3');
+
+    fireEvent.click(checkbox1);
     expect(changeSpy).toHaveBeenLastCalledWith(['value-2', 'value-3', 'value-1']);
 
-    checkboxGroup.find('Checkbox[value="value-3"] input').prop('onChange')({currentTarget: {checked: false}});
+    fireEvent.click(checkbox3);
     expect(changeSpy).toHaveBeenLastCalledWith(['value-2']);
 });
