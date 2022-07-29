790f872e175ca84d7b89447c70fe2a50f8dae3a4
simon.ostini <simonostini@gmail.com>
2022-07-29 10:35:26 +0200

    migrated tests

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/DropdownButton/tests/DropdownButton.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/DropdownButton/tests/DropdownButton.test.js
@@ -1,37 +1,45 @@
 // @flow
 import React from 'react';
-import {mount, render} from 'enzyme';
+import {fireEvent, render, screen} from '@testing-library/react';
 import DropdownButton from '../DropdownButton';
 
 test('Render dropdown button', () => {
-    expect(render(
+    const {container} = render(
         <DropdownButton icon="su-plus" label="Add">
             <DropdownButton.Item onClick={jest.fn()}>Option 1</DropdownButton.Item>
         </DropdownButton>
-    )).toMatchSnapshot();
+    );
+
+    expect(container).toMatchSnapshot();
 });
 
 test('Clicking dropdown items should call the corresponding callback', () => {
     const option1ClickSpy = jest.fn();
     const option2ClickSpy = jest.fn();
 
-    const dropdownButton = mount(
+    const {container, debug} = render(
         <DropdownButton icon="su-plus" label="Add">
             <DropdownButton.Item onClick={option1ClickSpy}>Option 1</DropdownButton.Item>
             <DropdownButton.Item onClick={option2ClickSpy}>Option 2</DropdownButton.Item>
         </DropdownButton>
     );
 
-    dropdownButton.find('Button').simulate('click');
-    dropdownButton.find('Action').at(0).simulate('click');
+    const dropdownButton = screen.queryByText('Add');
+    fireEvent.click(dropdownButton);
+
+    const option1 = screen.queryByText('Option 1');
+    fireEvent.click(option1);
+
     expect(option1ClickSpy).toBeCalled();
     expect(option2ClickSpy).not.toBeCalled();
 
     option1ClickSpy.mockReset();
     option2ClickSpy.mockReset();
 
-    dropdownButton.find('Button').simulate('click');
-    dropdownButton.find('Action').at(1).simulate('click');
+    fireEvent.click(dropdownButton);
+    const option2 = screen.queryByText('Option 2');
+    fireEvent.click(option2);
+
     expect(option1ClickSpy).not.toBeCalled();
     expect(option2ClickSpy).toBeCalled();
 });
