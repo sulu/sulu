fbeaf605372cd76e8efdd28d518fdaa0555b3628
simon.ostini <simonostini@gmail.com>
2022-07-29 10:36:31 +0200

    updated snapshots + rm enzyme duplicate test file

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/DropdownButton/tests/DropdownButton.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/DropdownButton/tests/DropdownButton.test.js
@@ -17,7 +17,7 @@ test('Clicking dropdown items should call the corresponding callback', () => {
     const option1ClickSpy = jest.fn();
     const option2ClickSpy = jest.fn();
 
-    const {container, debug} = render(
+    render(
         <DropdownButton icon="su-plus" label="Add">
             <DropdownButton.Item onClick={option1ClickSpy}>Option 1</DropdownButton.Item>
             <DropdownButton.Item onClick={option2ClickSpy}>Option 2</DropdownButton.Item>
