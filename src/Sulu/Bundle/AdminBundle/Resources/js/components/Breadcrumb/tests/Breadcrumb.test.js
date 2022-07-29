c16f3a9bc13175388630c2825419143b82ea6745
Simon Ostini <simonostini@gmail.com>
2022-07-29 10:32:48 +0200

    Migrate enzyme Breadcrumb tests to React Testing Library (#6725)

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Breadcrumb/tests/Breadcrumb.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Breadcrumb/tests/Breadcrumb.test.js
@@ -1,11 +1,11 @@
 // @flow
-import {mount, render} from 'enzyme';
+import {fireEvent, render, screen} from '@testing-library/react';
 import React from 'react';
 import Breadcrumb from '../Breadcrumb';
 
 test('Render a Breadcrumb', () => {
     const clickSpy = jest.fn();
-    const breadcrumb = render(
+    const {container} = render(
         <Breadcrumb onItemClick={clickSpy}>
             <Breadcrumb.Item>
                 Crumb 1
@@ -18,13 +18,13 @@ test('Render a Breadcrumb', () => {
             </Breadcrumb.Item>
         </Breadcrumb>
     );
-    expect(breadcrumb).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
 });
 
 test('Clicking on a clickable breadcrumb part should call a handler', () => {
     const clickSpy = jest.fn();
     const testValue = 2;
-    const breadcrumb = mount(
+    render(
         <Breadcrumb onItemClick={clickSpy}>
             <Breadcrumb.Item>
                 Crumb 1
@@ -38,6 +38,8 @@ test('Clicking on a clickable breadcrumb part should call a handler', () => {
         </Breadcrumb>
     );
 
-    breadcrumb.find('Item').at(1).simulate('click');
+    const item = screen.queryByText('Crumb 2');
+    fireEvent.click(item);
+
     expect(clickSpy).toHaveBeenCalledWith(testValue);
 });
