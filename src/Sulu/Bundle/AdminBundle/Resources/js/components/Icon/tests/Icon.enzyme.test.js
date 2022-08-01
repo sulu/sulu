a6cd47298a644664c02925965f5e2409a0b7dbf1
simon.ostini <simonostini@gmail.com>
2022-08-01 11:22:15 +0200

    added duplicate enzyme test file

---
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Icon/tests/Icon.enzyme.test.js
@@ -0,0 +1,61 @@
+// @flow
+import React from 'react';
+import {mount, render, shallow} from 'enzyme';
+import log from 'loglevel';
+import Icon from '../Icon';
+
+jest.mock('loglevel', () => ({
+    warn: jest.fn(),
+}));
+
+test('Icon should render', () => {
+    expect(render(<Icon name="su-save" />)).toMatchSnapshot();
+});
+
+test('Icon should not render with invalid icon', () => {
+    const icon = mount(<Icon name="xxx" />);
+    expect(icon.render()).toMatchSnapshot();
+    expect(log.warn).toHaveBeenCalled();
+});
+
+test('Icon should not render with empty string', () => {
+    const icon = mount(<Icon name="" />);
+    expect(icon.render()).toMatchSnapshot();
+    expect(log.warn).toHaveBeenCalled();
+});
+
+test('Icon should render with class names', () => {
+    expect(render(<Icon className="test" name="su-pen" />)).toMatchSnapshot();
+});
+
+test('Icon should render with onClick handler, role and tabindex', () => {
+    const onClickSpy = jest.fn();
+    expect(render(<Icon className="test" name="su-save" onClick={onClickSpy} />)).toMatchSnapshot();
+});
+
+test('Icon should call the callback on click', () => {
+    const onClick = jest.fn();
+    const stopPropagation = jest.fn();
+    const icon = shallow(<Icon className="test" name="su-pen" onClick={onClick} />);
+    icon.simulate('click', {stopPropagation});
+    expect(onClick).toBeCalled();
+    expect(stopPropagation).toBeCalled();
+});
+
+test('Icon should call the callback on when space is pressed', () => {
+    const onClick = jest.fn();
+    const stopPropagation = jest.fn();
+    const icon = shallow(<Icon className="test" name="su-pen" onClick={onClick} />);
+    icon.simulate('keypress', {key: ' ', stopPropagation});
+    expect(onClick).toBeCalled();
+    expect(stopPropagation).toBeCalled();
+});
+
+test('Icon should call the callback on when enter is pressed', () => {
+    const onClick = jest.fn();
+    const stopPropagation = jest.fn();
+    const icon = shallow(<Icon className="test" name="su-pen" onClick={onClick} />);
+    icon.simulate('keypress', {key: 'Enter', stopPropagation});
+    expect(onClick).toBeCalled();
+    expect(stopPropagation).toBeCalled();
+});
