6af87415f6c067e27ab44acbf16274f0165607ca
Simon Ostini <simonostini@gmail.com>
2022-08-02 22:00:11 +0200

    Migrate enzyme Backdrop tests to React Testing Library (#6758)

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Backdrop/tests/Backdrop.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Backdrop/tests/Backdrop.test.js
@@ -1,17 +1,19 @@
 // @flow
-import {render, shallow} from 'enzyme';
+import {fireEvent, render, screen} from '@testing-library/react';
 import React from 'react';
 import Backdrop from '../Backdrop';
 
 test('The component should render', () => {
-    expect(render(<Backdrop />)).toMatchSnapshot();
+    const {container} = render(<Backdrop />);
+    expect(container).toMatchSnapshot();
 });
 
 test('The component should call a function when clicked', () => {
     const onClickSpy = jest.fn();
-    const view = shallow(<Backdrop onClick={onClickSpy} />);
+    render(<Backdrop onClick={onClickSpy} />);
+    const backdrop = screen.queryByTestId('backdrop');
 
     expect(onClickSpy).toHaveBeenCalledTimes(0);
-    view.find('.backdrop').simulate('click');
+    fireEvent.click(backdrop);
     expect(onClickSpy).toHaveBeenCalledTimes(1);
 });
