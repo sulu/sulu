66cf4a6718ac64e436303898e17a508e7877add8
simon.ostini <simonostini@gmail.com>
2022-08-04 09:27:47 +0200

    WIP migrated tests

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Number/tests/Number.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Number/tests/Number.test.js
@@ -1,40 +1,44 @@
 // @flow
 import React from 'react';
-import {render, shallow} from 'enzyme';
+import {render, fireEvent, screen} from '@testing-library/react';
 import Number from '../Number';
 
 test('Number should render', () => {
-    expect(render(<Number onChange={jest.fn()} value={undefined} />)).toMatchSnapshot();
+    const {container} = render(<Number onChange={jest.fn()} value={undefined} />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Number should render when disabled', () => {
-    expect(render(<Number disabled={true} onChange={jest.fn()} value={8} />)).toMatchSnapshot();
+    const {container} = render(<Number disabled={true} onChange={jest.fn()} value={8} />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Number should call onChange with parsed value', () => {
     const onChange = jest.fn();
-    const number = shallow(<Number onChange={onChange} value={undefined} />);
+    render(<Number onChange={onChange} value={2} />);
 
-    const event = {};
-    number.find('Input').simulate('change', '10.2', event);
+    const input = screen.queryByDisplayValue(2);
+    fireEvent.keyDown(input, {target: {value: '10.2'}});
+
+    expect(input).toHaveValue(10.2);
 });
 
 test('Number should call onChange with undefined when value isn`t a float', () => {
     const onChange = jest.fn();
-    const number = shallow(<Number onChange={onChange} value={undefined} />);
+    render(<Number onChange={onChange} value={2} />);
 
-    const event = {};
-    number.find('Input').simulate('change', 'xxx', event);
+    const input = screen.queryByDisplayValue(2);
+    fireEvent.change(input, {target: {value: 'text'}});
 
-    expect(onChange).toBeCalledWith(undefined, event);
+    expect(onChange).toHaveBeenCalledWith(undefined, expect.anything());
 });
 
 test('Number should call onChange with undefined when value is undefined', () => {
     const onChange = jest.fn();
-    const number = shallow(<Number onChange={onChange} value={undefined} />);
+    render(<Number onChange={onChange} value={2} />);
 
-    const event = {};
-    number.find('Input').simulate('change', undefined, event);
+    const input = screen.queryByDisplayValue(2);
+    fireEvent.change(input, {target: {value: undefined}});
 
-    expect(onChange).toBeCalledWith(undefined, event);
+    expect(onChange).toBeCalledWith(undefined, expect.anything());
 });
