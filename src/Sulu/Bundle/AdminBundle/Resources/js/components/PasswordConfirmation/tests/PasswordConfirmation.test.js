8df960b02c3dad4ccbcdff7bcbccd6d4ca9154d0
Simon Ostini <simonostini@gmail.com>
2022-08-08 11:51:26 +0200

    Migrate enzyme PasswordConfirmation tests to React Testing Library (#6778)
    * Migrate enzyme PasswordConfirmation tests to React Testing Library
    
    * migrated passwordconfirmation tests
    
    * added snapshots

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/PasswordConfirmation/tests/PasswordConfirmation.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/PasswordConfirmation/tests/PasswordConfirmation.test.js
@@ -1,62 +1,68 @@
 // @flow
 import React from 'react';
-import {render, shallow} from 'enzyme';
+import {fireEvent, render, screen} from '@testing-library/react';
 import debounce from 'debounce';
 import PasswordConfirmation from '../PasswordConfirmation';
 
 jest.mock('debounce', () => jest.fn((value) => value));
 
 test('Should render two password fields and add a debounced function', () => {
-    expect(render(<PasswordConfirmation disabled={true} onChange={jest.fn()} />)).toMatchSnapshot();
+    const {container} = render(<PasswordConfirmation disabled={true} onChange={jest.fn()} />);
+    expect(container).toMatchSnapshot();
     expect(debounce).toBeCalledWith(expect.any(Function), 500);
 });
 
 test('Should render disabled input-components when disabled', () => {
-    expect(render(<PasswordConfirmation onChange={jest.fn()} />)).toMatchSnapshot();
+    const {container} = render(<PasswordConfirmation onChange={jest.fn()} />);
+    expect(container).toMatchSnapshot();
     expect(debounce).toBeCalledWith(expect.any(Function), 500);
 });
 
 test('Should only call onChange when both values match after the debounced time', () => {
     const changeSpy = jest.fn();
-    const passwordConfirmation = shallow(<PasswordConfirmation onChange={changeSpy} />);
+    render(<PasswordConfirmation onChange={changeSpy} />);
+
+    const inputs = screen.queryAllByDisplayValue('');
 
     expect(changeSpy).not.toBeCalled();
 
-    passwordConfirmation.find('Input').at(0).simulate('change', 'asdf');
-    passwordConfirmation.find('Input').at(1).simulate('change', 'jklö');
+    fireEvent.change(inputs[0], {target: {value: 'asdf'}});
+    fireEvent.change(inputs[1], {target: {value: 'jklö'}});
 
     expect(changeSpy).not.toBeCalled();
 
-    passwordConfirmation.find('Input').at(1).simulate('change', 'asdf');
-    passwordConfirmation.find('Input').at(1).simulate('blur');
+    fireEvent.change(inputs[1], {target: {value: 'asdf'}});
+    fireEvent.blur(inputs[1]);
+
     expect(changeSpy).toBeCalledWith('asdf');
 });
 
 test('Should mark the input fields as invalid if they do not match', () => {
     const changeSpy = jest.fn();
-    const passwordConfirmation = shallow(<PasswordConfirmation onChange={changeSpy} />);
+    const {container} = render(<PasswordConfirmation onChange={changeSpy} />);
+
+    const inputs = screen.queryAllByDisplayValue('');
 
     expect(changeSpy).not.toBeCalled();
 
-    passwordConfirmation.find('Input').at(0).simulate('change', 'asdf');
-    passwordConfirmation.find('Input').at(1).simulate('change', 'jklö');
+    fireEvent.change(inputs[0], {target: {value: 'asdf'}});
+    fireEvent.change(inputs[1], {target: {value: 'jklö'}});
+    fireEvent.blur(inputs[1]);
 
-    passwordConfirmation.find('Input').at(1).simulate('blur');
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    expect(container.querySelector('.error')).toBeInTheDocument();
 
-    expect(passwordConfirmation.find('Input').at(0).prop('valid')).toBe(false);
-    expect(passwordConfirmation.find('Input').at(1).prop('valid')).toBe(false);
+    fireEvent.change(inputs[1], {target: {value: 'asdf'}});
+    fireEvent.blur(inputs[1]);
 
-    passwordConfirmation.find('Input').at(1).simulate('change', 'asdf');
-    passwordConfirmation.find('Input').at(1).simulate('blur');
-
-    expect(passwordConfirmation.find('Input').at(0).prop('valid')).toBe(true);
-    expect(passwordConfirmation.find('Input').at(1).prop('valid')).toBe(true);
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    expect(container.querySelector('.error')).not.toBeInTheDocument();
 });
 
 test('Should mark the input fields as invalid if the valid prop is false', () => {
     const changeSpy = jest.fn();
-    const passwordConfirmation = shallow(<PasswordConfirmation onChange={changeSpy} valid={false} />);
+    const {container} = render(<PasswordConfirmation onChange={changeSpy} valid={false} />);
 
-    expect(passwordConfirmation.find('Input').at(0).prop('valid')).toBe(false);
-    expect(passwordConfirmation.find('Input').at(1).prop('valid')).toBe(false);
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    expect(container.querySelector('.error')).toBeInTheDocument();
 });
