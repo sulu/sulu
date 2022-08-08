ac9a91dfae39dc10a525845af94f03fcfd2be725
simon.ostini <simonostini@gmail.com>
2022-08-08 08:17:21 +0200

    migrated RadioGroup enzyme tests

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Radio/tests/RadioGroup.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Radio/tests/RadioGroup.test.js
@@ -1,65 +1,79 @@
 // @flow
-import {mount, render} from 'enzyme';
+import {fireEvent, render, screen} from '@testing-library/react';
 import React from 'react';
 import RadioGroup from '../RadioGroup';
 import Radio from '../Radio';
 
-jest.mock('../Radio', () => function Radio() {
-    return <p>My radio mock</p>;
-});
-
 test('The component should render', () => {
-    const group = render(
+    const {container} = render(
         <RadioGroup className="my-group" value="1">
             <Radio value="1" />
             <Radio value="2" />
             <Radio value="3" />
         </RadioGroup>
     );
-    expect(group).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
 });
 
 test('The component should check the correct radio', () => {
-    const group = mount(
+    render(
         <RadioGroup value="1">
             <Radio value="1" />
             <Radio value="2" />
             <Radio value="3" />
         </RadioGroup>
     );
-    const radios = group.find(Radio);
-    expect(radios.get(0).props.checked).toBe(true);
-    expect(radios.get(1).props.checked).toBe(false);
-    expect(radios.get(2).props.checked).toBe(false);
+
+    const radioGroup = [
+        screen.queryByDisplayValue('1'),
+        screen.queryByDisplayValue('2'),
+        screen.queryByDisplayValue('3'),
+    ];
+
+    expect(radioGroup[0]).toBeChecked();
+    expect(radioGroup[1]).not.toBeChecked();
+    expect(radioGroup[2]).not.toBeChecked();
 });
 
 test('The component should pass the disabled state to the radios', () => {
-    const group = mount(
+    render(
         <RadioGroup disabled={true} value="1">
             <Radio value="1" />
             <Radio value="2" />
             <Radio value="3" />
         </RadioGroup>
     );
-    const radios = group.find(Radio);
-    expect(radios.get(0).props.disabled).toBe(true);
-    expect(radios.get(1).props.disabled).toBe(true);
-    expect(radios.get(2).props.disabled).toBe(true);
+
+    const radioGroup = [
+        screen.queryByDisplayValue('1'),
+        screen.queryByDisplayValue('2'),
+        screen.queryByDisplayValue('3'),
+    ];
+
+    expect(radioGroup[0]).toBeDisabled();
+    expect(radioGroup[1]).toBeDisabled();
+    expect(radioGroup[2]).toBeDisabled();
 });
 
 test('The component should pass the change callback to the radios', () => {
     const onChange = jest.fn();
-    const group = mount(
-        <RadioGroup onChange={onChange} value="1">
+    render(
+        <RadioGroup onChange={onChange}>
             <Radio value="1" />
             <Radio value="2" />
             <Radio value="3" />
         </RadioGroup>
     );
-    const radios = group.find(Radio);
-    radios.get(0).props.onChange();
-    radios.get(1).props.onChange();
-    radios.get(2).props.onChange();
+
+    const radioGroup = [
+        screen.queryByDisplayValue('1'),
+        screen.queryByDisplayValue('2'),
+        screen.queryByDisplayValue('3'),
+    ];
+
+    fireEvent.click(radioGroup[0]);
+    fireEvent.click(radioGroup[1]);
+    fireEvent.click(radioGroup[2]);
 
     expect(onChange).toHaveBeenCalledTimes(3);
 });
