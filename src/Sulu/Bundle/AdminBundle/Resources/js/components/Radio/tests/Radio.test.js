7dee317e704ca6db8ccc46cfbfb94e5830789206
simon.ostini <simonostini@gmail.com>
2022-08-08 07:51:57 +0200

    migrated radio tests

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Radio/tests/Radio.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Radio/tests/Radio.test.js
@@ -18,34 +18,33 @@ test('The component should render in disabled state', () => {
     expect(container).toMatchSnapshot();
 });
 
-// test('The component pass the props correctly to the generic checkbox', () => {
-//     const {debug} = render(
-//         <Radio
-//             checked={true}
-//             disabled={true}
-//             name="my-name"
-//             value="my-value"
-//         >
-//             My label
-//         </Radio>
-//     );
-//     debug();
+test('The component pass the props correctly to the generic checkbox', () => {
+    render(
+        <Radio
+            checked={true}
+            disabled={true}
+            name="my-name"
+            value="my-value"
+        >
+            My label
+        </Radio>
+    );
 
-//     const checkbox = screen.queryByDisplayValue('my-value');
+    const checkbox = screen.queryByDisplayValue('my-value');
 
-//     // expect(checkbox).toHaveValue('my-value');
-//     expect(checkbox).toHaveAccessibleName('my-name');
-//     expect(checkbox).toBeChecked();
-//     expect(checkbox).toBeDisabled();
-//     expect(screen.getByText('My label')).toBeInTheDocument();
-// });
+    expect(checkbox.value).toEqual('my-value');
+    expect(checkbox).toHaveAttribute('name', 'my-name');
+    expect(screen.getByText('My label')).toBeInTheDocument();
+    expect(checkbox).toBeChecked();
+    expect(checkbox).toBeDisabled();
+});
 
 test('The component pass the the value to the change callback', () => {
     const onChange = jest.fn();
     render(<Radio onChange={onChange} value="my-value">My label</Radio>);
 
-    const input = screen.queryByDisplayValue('my-value');
-    fireEvent.click(input);
+    const checkbox = screen.queryByDisplayValue('my-value');
+    fireEvent.click(checkbox);
 
     expect(onChange).toHaveBeenCalledWith('my-value');
 });
