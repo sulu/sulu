dc0b07e8b239a479e06e85fe7dbde61438c1c161
Simon Ostini <simonostini@gmail.com>
2022-08-08 11:35:58 +0200

    Migrate enzyme Phone tests to React Testing Library (#6770)
    * Migrate enzyme Phone tests to React Testing Library
    
    * added iconclick test
    
    * added iconclick test
    
    * migrated tests
    
    * removed duplicate enzyme test file & fixed snapshots

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Phone/tests/Phone.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Phone/tests/Phone.test.js
@@ -1,80 +1,102 @@
 // @flow
 import React from 'react';
-import {render, mount} from 'enzyme';
+import {fireEvent, render, screen} from '@testing-library/react';
 import Phone from '../Phone';
 
 test('Phone should render', () => {
     const onChange = jest.fn();
-    expect(render(<Phone onChange={onChange} value={null} />)).toMatchSnapshot();
+    const {container} = render(<Phone onChange={onChange} value={null} />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Phone should render with placeholder', () => {
     const onChange = jest.fn();
-    expect(render(<Phone onChange={onChange} placeholder="My placeholder" value={null} />)).toMatchSnapshot();
+    const {container} = render(<Phone onChange={onChange} placeholder="My placeholder" value={null} />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Phone should render with value', () => {
     const onChange = jest.fn();
     const value = 'test@test.com';
-    expect(render(<Phone onChange={onChange} value={value} />)).toMatchSnapshot();
+    const {container} = render(<Phone onChange={onChange} value={value} />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Phone should render null value as empty string', () => {
     const onChange = jest.fn();
-    expect(render(<Phone onChange={onChange} value={null} />)).toMatchSnapshot();
+    const {container} = render(<Phone onChange={onChange} value={null} />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Phone should render error', () => {
     const onChange = jest.fn();
-    expect(render(<Phone onChange={onChange} valid={false} value={null} />)).toMatchSnapshot();
+    const {container} = render(<Phone onChange={onChange} valid={false} value={null} />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Phone should render when disabled', () => {
     const onChange = jest.fn();
-    expect(render(<Phone disabled={true} onChange={onChange} valid={false} value="‚+43245" />)).toMatchSnapshot();
+    const {container} = render(<Phone disabled={true} onChange={onChange} valid={false} value="‚+43245" />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Phone should trigger callbacks correctly', () => {
     const onChange = jest.fn();
     const onBlur = jest.fn();
-    const phone = mount(<Phone onBlur={onBlur} onChange={onChange} value={null} />);
+    render(<Phone onBlur={onBlur} onChange={onChange} value={null} />);
 
-    phone.find('Input').instance().props.onChange('+123', {target: {value: '+123'}});
-    phone.find('Input').instance().props.onBlur();
-    phone.update();
-    expect(onChange).toBeCalledWith('+123', {target: {value: '+123'}});
-    expect(onBlur).toBeCalled();
+    const input = screen.queryByRole('textbox');
 
-    expect(onBlur).toHaveBeenCalledTimes(1);
+    fireEvent.change(input, {target: {value: '+123'}});
+    fireEvent.blur(input);
+
+    expect(onChange).toBeCalledWith('+123', expect.anything());
     expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onBlur).toBeCalled();
+    expect(onBlur).toHaveBeenCalledTimes(1);
 });
 
 test('Phone should not set onIconClick when value is not set', () => {
+    const redirectSpy = jest.fn();
+    delete window.location;
+    window.location = {assign: redirectSpy};
+
     const onChange = jest.fn();
     const onBlur = jest.fn();
-    const phone = mount(<Phone onBlur={onBlur} onChange={onChange} value={null} />);
+    render(<Phone onBlur={onBlur} onChange={onChange} value={null} />);
 
-    expect(phone.find('Input').prop('onIconClick')).toBeUndefined();
+    const icon = screen.queryByLabelText('su-phone');
+    fireEvent.click(icon);
+
+    expect(redirectSpy).not.toHaveBeenCalled();
 });
 
 test('Phone should set onIconClick when value is set', () => {
+    const redirectSpy = jest.fn();
+    delete window.location;
+    window.location = {assign: redirectSpy};
+
     const onChange = jest.fn();
     const onBlur = jest.fn();
-    const phone = mount(<Phone onBlur={onBlur} onChange={onChange} value="+123" />);
+    render(<Phone onBlur={onBlur} onChange={onChange} value="+123" />);
 
-    expect(phone.find('Input').prop('onIconClick')).toBeInstanceOf(Function);
+    const icon = screen.queryByLabelText('su-phone');
+    fireEvent.click(icon);
+
+    expect(redirectSpy).toHaveBeenCalled();
 });
 
 test('Phone should set onIconClick when value is valid and window should be opened', () => {
+    const redirectSpy = jest.fn();
     delete window.location;
-    window.location = {assign: jest.fn()};
+    window.location = {assign: redirectSpy};
 
     const onChange = jest.fn();
     const onBlur = jest.fn();
-    const email = mount(<Phone onBlur={onBlur} onChange={onChange} value="+123" />);
+    render(<Phone onBlur={onBlur} onChange={onChange} value="+123" />);
 
-    const onIconClickFunction = email.find('Input').prop('onIconClick');
-    expect(onIconClickFunction).toBeInstanceOf(Function);
-    onIconClickFunction.call();
-    expect(window.location.assign).toBeCalledWith('tel:+123');
+    const icon = screen.queryByLabelText('su-phone');
+    fireEvent.click(icon);
+
+    expect(redirectSpy).toHaveBeenLastCalledWith('tel:+123');
 });
