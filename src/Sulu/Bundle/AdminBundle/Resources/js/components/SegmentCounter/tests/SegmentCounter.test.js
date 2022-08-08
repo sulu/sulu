e1d48f201e6dee8164f38aa3e961af3f1cd36d09
simon.ostini <simonostini@gmail.com>
2022-08-08 11:30:46 +0200

    Migrate enzyme SegmentCounter tests to React Testing Library

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/SegmentCounter/tests/SegmentCounter.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/SegmentCounter/tests/SegmentCounter.test.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import {render} from 'enzyme';
+import {render} from '@testing-library/react';
 import SegmentCounter from '../SegmentCounter';
 
 jest.mock('../../../utils/Translator', () => ({
@@ -8,13 +8,16 @@ jest.mock('../../../utils/Translator', () => ({
 }));
 
 test('Should show a positive count if nothing is passed', () => {
-    expect(render(<SegmentCounter delimiter="," max={20} value={undefined} />)).toMatchSnapshot();
+    const {container} = render(<SegmentCounter delimiter="," max={20} value={undefined} />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should show a positive count', () => {
-    expect(render(<SegmentCounter delimiter="," max={5} value="keyword1, keyword2, keyword3" />)).toMatchSnapshot();
+    const {container} = render(<SegmentCounter delimiter="," max={5} value="keyword1, keyword2, keyword3" />);
+    expect(container).toMatchSnapshot();
 });
 
 test('Should show a negative count', () => {
-    expect(render(<SegmentCounter delimiter="|" max={2} value="That|is|a|test" />)).toMatchSnapshot();
+    const {container} = render(<SegmentCounter delimiter="|" max={2} value="That|is|a|test" />);
+    expect(container).toMatchSnapshot();
 });
