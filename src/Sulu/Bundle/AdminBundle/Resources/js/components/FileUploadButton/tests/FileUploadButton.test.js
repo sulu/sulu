802b2f1f6e6a1d60eb490ef68e201f2fc457ccbd
simon.ostini <simonostini@gmail.com>
2022-08-09 11:24:46 +0200

    WIP fileupload update

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/FileUploadButton/tests/FileUploadButton.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/FileUploadButton/tests/FileUploadButton.test.js
@@ -1,46 +1,56 @@
 // @flow
 import React from 'react';
-import {fireEvent, render, screen, waitFor, act} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Dropzone from 'react-dropzone';
 import FileUploadButton from '../FileUploadButton';
 
 test('Render a FileUploadButton', () => {
-    const {container} = render(<FileUploadButton onUpload={jest.fn()}>Upload something!</FileUploadButton>);
+    const {container} = render(
+        <FileUploadButton onUpload={jest.fn()}>
+            Upload something!
+        </FileUploadButton>
+    );
     expect(container).toMatchSnapshot();
 });
 
 test('Render a disabled FileUploadButton', () => {
     const {container} = render(
-        <FileUploadButton disabled={true} onUpload={jest.fn()}>Upload something!</FileUploadButton>
+        <FileUploadButton disabled={true} onUpload={jest.fn()}>
+            Upload something!
+        </FileUploadButton>
     );
     expect(container).toMatchSnapshot();
 });
 
 test('Render a FileUploadButton with other skin and icon', () => {
     const {container} = render(
-        <FileUploadButton icon="su-image" onUpload={jest.fn()} skin="link">Upload something!</FileUploadButton>
+        <FileUploadButton
+            icon="su-image"
+            onUpload={jest.fn()}
+            skin="link"
+        >
+            Upload something!
+        </FileUploadButton>
     );
     expect(container).toMatchSnapshot();
 });
 
 test('Call onUpload callback when a file is uploaded', async() => {
     const uploadSpy = jest.fn();
-    const testFile = new File(['test-file'], 'test-file', {type: 'image/png'});
+    const file = new File(['hello'], 'hello.png', {type: 'image/png'});
 
-    const {container, debug} = render(<FileUploadButton onUpload={uploadSpy}>Upload something!</FileUploadButton>);
+    const {container, debug} = render(
+        <FileUploadButton onUpload={uploadSpy}>
+            Upload something!
+        </FileUploadButton>
+    );
     debug();
-    const dropzone = screen.queryByText('Upload something!');
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
     const input = container.querySelector('input');
 
-    await act(async() => {
-        await waitFor(() => {
-            userEvent.upload(input, testFile);
-        });
-    });
-
-    // fireEvent.drop(dropzone, testFile);
+    userEvent.upload(input, file);
 
     expect(uploadSpy).toBeCalledTimes(1);
-    expect(uploadSpy).toBeCalledWith(testFile);
+    expect(uploadSpy).toBeCalledWith(file);
 });
