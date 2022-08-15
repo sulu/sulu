74eba4859eab33705b2eeb9e6e8be4b239e1a76f
Simon Ostini <simonostini@gmail.com>
2022-08-15 16:39:17 +0200

    Migrate enzyme Select tests to React Testing Library (#6790)
    * Migrate enzyme Select tests to React Testing Library
    
    * fixed preventdefault test
    
    * migrated displayvalue enzyme tests
    
    * migrated option tests
    
    * migrated action tests
    
    * Await user-event interaction
    
    Co-authored-by: Niklas Natter <niklas@sulu.io>

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Select/tests/DisplayValue.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Select/tests/DisplayValue.test.js
@@ -1,62 +1,69 @@
 // @flow
-import {render, shallow} from 'enzyme';
+import {createEvent, fireEvent, render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
-import CroppedText from '../../CroppedText';
 import DisplayValue from '../DisplayValue';
 
 test('The component should render a CroppedText if value of children prop is a string', () => {
-    const displayValue = render(<DisplayValue onClick={jest.fn()}>My value</DisplayValue>);
-    expect(displayValue).toMatchSnapshot();
+    const {container} = render(<DisplayValue onClick={jest.fn()}>My value</DisplayValue>);
+    expect(container).toMatchSnapshot();
 });
 
 test('The component should directly render given children if value of children prop contains another component', () => {
-    const displayValue = render(<DisplayValue onClick={jest.fn()}>Some <b>bold</b> text</DisplayValue>);
-    expect(displayValue).toMatchSnapshot();
+    const {container} = render(<DisplayValue onClick={jest.fn()}>Some <b>bold</b> text</DisplayValue>);
+    expect(container).toMatchSnapshot();
 });
 
 test('The component should render with the flat skin', () => {
-    const displayValue = render(<DisplayValue onClick={jest.fn()} skin="flat">My value</DisplayValue>);
-    expect(displayValue).toMatchSnapshot();
+    const {container} = render(<DisplayValue onClick={jest.fn()} skin="flat">My value</DisplayValue>);
+    expect(container).toMatchSnapshot();
 });
 
 test('The component should render with the dark skin', () => {
-    const displayValue = render(<DisplayValue onClick={jest.fn()} skin="dark">My value</DisplayValue>);
-    expect(displayValue).toMatchSnapshot();
+    const {container} = render(<DisplayValue onClick={jest.fn()} skin="dark">My value</DisplayValue>);
+    expect(container).toMatchSnapshot();
 });
 
 test('The component should render with an icon', () => {
-    const displayValue = render(<DisplayValue icon="su-plus" onClick={jest.fn()}>My value</DisplayValue>);
-    expect(displayValue).toMatchSnapshot();
+    const {container} = render(<DisplayValue icon="su-plus" onClick={jest.fn()}>My value</DisplayValue>);
+    expect(container).toMatchSnapshot();
 });
 
 test('The component should render when disabled', () => {
-    const displayValue = render(<DisplayValue disabled={true} onClick={jest.fn()}>My value</DisplayValue>);
-    expect(displayValue).toMatchSnapshot();
+    const {container} = render(<DisplayValue disabled={true} onClick={jest.fn()}>My value</DisplayValue>);
+    expect(container).toMatchSnapshot();
 });
 
 test('A click on the component should fire the callback and prevent the default', () => {
     const clickSpy = jest.fn();
-    const preventDefaultSpy = jest.fn();
 
-    const displayValue = shallow(<DisplayValue onClick={clickSpy}>My value</DisplayValue>);
-    displayValue.simulate('click', {preventDefault: preventDefaultSpy});
+    render(<DisplayValue onClick={clickSpy}>My value</DisplayValue>);
+    const display = screen.queryByRole('button');
+    const event = createEvent.click(display);
+
+    // eslint-disable-next-line testing-library/prefer-user-event
+    fireEvent(display, event);
 
     expect(clickSpy).toBeCalled();
-    expect(preventDefaultSpy).toBeCalled();
+    expect(event.defaultPrevented).toBe(true);
 });
 
 test('A click on the component should not fire the callback when disabled', () => {
     const clickSpy = jest.fn();
 
-    const displayValue = shallow(<DisplayValue disabled={true} onClick={clickSpy}>My value</DisplayValue>);
-    displayValue.simulate('click', {preventDefault: jest.fn()});
+    render(<DisplayValue disabled={true} onClick={clickSpy}>My value</DisplayValue>);
+    const display = screen.queryByRole('button');
+
+    userEvent.click(display);
 
     expect(clickSpy).not.toBeCalled();
 });
 
 test('The component should use the CroppedText component to cut long texts', () => {
-    const displayValue = shallow(
+    const {container} = render(
         <DisplayValue onClick={jest.fn()}>This value should be wrapped in a CroppedText component</DisplayValue>
     );
-    expect(displayValue.find(CroppedText)).toHaveLength(1);
+
+    // eslint-disable-next-line testing-library/no-container
+    expect(container.querySelector('.croppedText')).toBeInTheDocument();
 });
