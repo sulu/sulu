7350c890b66762f7d39b55eaa4e04018339a38fc
Niklas Natter <niklas@sulu.io>
2022-08-23 14:04:01 +0200

    Fix migrated test cases

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/Tabs/tests/Tabs.enzyme.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/Tabs/tests/Tabs.enzyme.test.js
@@ -1,8 +1,7 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
-import {mount, render, shallow} from 'enzyme';
+import {shallow} from 'enzyme';
 import React from 'react';
 import debounce from 'debounce';
-import Badge from '../../Badge/Badge';
 import Tabs from '../Tabs.js';
 
 jest.mock('debounce', () => jest.fn((callback) => callback));
@@ -17,71 +16,6 @@ Object.defineProperty(window, 'getComputedStyle', {
         paddingLeft: 20.0,
         paddingRight: 20.0,
     }),
-});
-
-test('Render a Tabs component with type root', () => {
-    const changeSpy = jest.fn();
-
-    expect(render(
-        <Tabs onSelect={changeSpy} selectedIndex={null} type="root">
-            <Tabs.Tab>Tab 1</Tabs.Tab>
-            <Tabs.Tab>Tab 2</Tabs.Tab>
-            <Tabs.Tab>Tab 3</Tabs.Tab>
-        </Tabs>
-    )).toMatchSnapshot();
-});
-
-test('Render a Tabs component with type nested', () => {
-    const changeSpy = jest.fn();
-
-    expect(render(
-        <Tabs onSelect={changeSpy} selectedIndex={null} type="nested">
-            <Tabs.Tab>Tab 1</Tabs.Tab>
-            <Tabs.Tab>Tab 2</Tabs.Tab>
-            <Tabs.Tab>Tab 3</Tabs.Tab>
-        </Tabs>
-    )).toMatchSnapshot();
-});
-
-test('Render a Tabs component with type inline', () => {
-    const changeSpy = jest.fn();
-
-    expect(render(
-        <Tabs onSelect={changeSpy} selectedIndex={null} type="inline">
-            <Tabs.Tab>Tab 1</Tabs.Tab>
-            <Tabs.Tab>Tab 2</Tabs.Tab>
-            <Tabs.Tab>Tab 3</Tabs.Tab>
-        </Tabs>
-    )).toMatchSnapshot();
-});
-
-test('Render a Tabs component with a selected tab and a badge', () => {
-    const changeSpy = jest.fn();
-    const selectedTabIndex = 0;
-
-    expect(render(
-        <Tabs onSelect={changeSpy} selectedIndex={selectedTabIndex} type="root">
-            <Tabs.Tab>Tab 1</Tabs.Tab>
-            <Tabs.Tab>Tab 2</Tabs.Tab>
-            <Tabs.Tab badges={[<Badge key="badge1">1</Badge>, <Badge key="badge2">2</Badge>]}>Tab 3</Tabs.Tab>
-        </Tabs>
-    )).toMatchSnapshot();
-});
-
-test('Clicking on a Tab should call the onSelect handler', () => {
-    const changeSpy = jest.fn();
-    const selectedTabIndex = 0;
-
-    const tabs = mount(
-        <Tabs onSelect={changeSpy} selectedIndex={null}>
-            <Tabs.Tab>Tab 1</Tabs.Tab>
-            <Tabs.Tab>Tab 2</Tabs.Tab>
-            <Tabs.Tab>Tab 3</Tabs.Tab>
-        </Tabs>
-    );
-
-    tabs.find('.tab button').at(0).simulate('click');
-    expect(changeSpy).toHaveBeenCalledWith(selectedTabIndex);
 });
 
 test('Clicking on several non- and collapsed tabs', () => {
@@ -177,27 +111,4 @@ test('Clicking on several non- and collapsed tabs', () => {
 
     expect(tabs.instance().visibleTabIndices).toEqual([0, 1, 2, 3, 8]);
     expect(tabs.instance().collapsedTabIndices).toEqual([4, 5, 6, 7, 9]);
-});
-
-test('ResizeObserver.disconnect should be called before component unmount', () => {
-    const changeSpy = jest.fn();
-    const selectedTabIndex = 0;
-
-    const tabs = shallow(
-        <Tabs onSelect={changeSpy} selectedIndex={selectedTabIndex}>
-            <Tabs.Tab>Tab 1</Tabs.Tab>
-            <Tabs.Tab>Tab 2</Tabs.Tab>
-            <Tabs.Tab>Tab 3</Tabs.Tab>
-            <Tabs.Tab>Tab 4</Tabs.Tab>
-            <Tabs.Tab>Tab 5</Tabs.Tab>
-            <Tabs.Tab>Tab 6</Tabs.Tab>
-            <Tabs.Tab>Tab 7</Tabs.Tab>
-            <Tabs.Tab>Tab 8</Tabs.Tab>
-            <Tabs.Tab>Tab 9</Tabs.Tab>
-            <Tabs.Tab>Tab 10</Tabs.Tab>
-        </Tabs>
-    );
-
-    tabs.instance().componentWillUnmount();
-    expect(ResizeObserver.mock.instances[0].disconnect).toBeCalled();
 });
