89b871777a43fcf1429c03aecd908834630e3aa6
niklasnatter <niklas@sulu.io>
2022-08-23 16:24:00 +0200

    Migrate DatePicker tests from enzyme to react testing library (#6801)

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/DatePicker/tests/DatePicker.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/DatePicker/tests/DatePicker.test.js
@@ -1,8 +1,8 @@
 // @flow
 import React from 'react';
-import {mount} from 'enzyme';
+import {render, screen} from '@testing-library/react';
 import moment from 'moment-timezone';
-import ReactDatetime from 'react-datetime';
+import userEvent from '@testing-library/user-event';
 import DatePicker from '../DatePicker';
 
 beforeEach(() => {
@@ -14,115 +14,9 @@ beforeEach(() => {
 
 test('DatePicker should render', () => {
     const onChange = jest.fn();
-    const datePicker = mount(<DatePicker className="date-picker" onChange={onChange} value={null} />);
+    const {baseElement} = render(<DatePicker className="date-picker" onChange={onChange} value={undefined} />);
 
-    expect(datePicker.render()).toMatchSnapshot();
-
-    expect(datePicker.find(ReactDatetime).render()).toMatchSnapshot();
-});
-
-test('DatePicker should show disabled Input when disabled', () => {
-    const onChange = jest.fn();
-    const value = new Date('2017-05-23');
-    const datePicker = mount(<DatePicker disabled={true} onChange={onChange} value={value} />);
-
-    expect(datePicker.find('Input').prop('onIconClick')).toEqual(undefined);
-    expect(datePicker.find('Input').prop('disabled')).toEqual(true);
-});
-
-test('DatePicker should pass input to inputRef prop', () => {
-    const inputRefSpy = jest.fn();
-    const datePicker = mount(<DatePicker inputRef={inputRefSpy} onChange={jest.fn()} value={undefined} />);
-
-    expect(inputRefSpy).toBeCalledWith(datePicker.find('input').instance());
-});
-
-test('DatePicker should open overlay on icon-click', () => {
-    const onChange = jest.fn();
-    const datePicker = mount(<DatePicker onChange={onChange} value={null} />);
-
-    expect(datePicker.find(ReactDatetime).props().open).toBeFalsy();
-    datePicker.find('Icon').simulate('click');
-    expect(datePicker.find(ReactDatetime).props().open).toBeTruthy();
-});
-
-test('DatePicker should not open overlay on icon-click when disabled', () => {
-    const onChange = jest.fn();
-    const datePicker = mount(<DatePicker disabled={true} onChange={onChange} value={null} />);
-
-    expect(datePicker.find(ReactDatetime).props().open).toBeFalsy();
-    datePicker.find('Icon').simulate('click');
-    expect(datePicker.find(ReactDatetime).props().open).toBeFalsy();
-});
-
-test('DatePicker should render with placeholder', () => {
-    const onChange = jest.fn();
-    const datePicker = mount(<DatePicker onChange={onChange} placeholder="My placeholder" value={null} />);
-
-    expect(datePicker.find('Input').prop('placeholder')).toEqual('My placeholder');
-});
-
-test('DatePicker should render with value', () => {
-    const onChange = jest.fn();
-    const value = new Date('2017-05-23');
-    const datePicker = mount(<DatePicker onChange={onChange} value={value} />);
-
-    expect(datePicker.find(ReactDatetime).prop('value')).toEqual(value);
-    expect(datePicker.find('Input').prop('value')).toEqual('05/23/2017');
-});
-
-test('DatePicker should try to guess incomplete value using format on blur.', () => {
-    const onChangeCallback = jest.fn();
-    const options = {
-        dateFormat: false,
-        timeFormat: 'HH:mm',
-    };
-    const datePicker = mount(<DatePicker onChange={onChangeCallback} options={options} value={null} />);
-
-    const eventTarget = {
-        value: '9',
-    };
-
-    datePicker.find('input').at(0).simulate('change', {target: eventTarget, currentTarget: eventTarget});
-    datePicker.find('input').at(0).simulate('blur');
-
-    expect(onChangeCallback).toBeCalledWith(expect.any(Date));
-
-    const date = onChangeCallback.mock.calls[0][0];
-
-    const expectedMoment = moment('09:00', options.timeFormat);
-    expect(expectedMoment.isValid()).toBe(true);
-
-    const expectedDate = expectedMoment.toDate();
-    expect(date && date.getHours()).toBe(expectedDate.getHours());
-    expect(date && date.getMinutes()).toBe(expectedDate.getMinutes());
-});
-
-test('DatePicker should render null value as empty string', () => {
-    const onChange = jest.fn();
-    const datePicker = mount(<DatePicker onChange={onChange} value={null} />);
-
-    expect(datePicker.render()).toMatchSnapshot();
-});
-
-test('DatePicker should render date format only with month', () => {
-    const onChange = jest.fn();
-    const options = {
-        dateFormat: 'MMMM',
-    };
-    const datePicker = mount(<DatePicker onChange={onChange} options={options} value={null} />);
-
-    expect(datePicker.render()).toMatchSnapshot();
-});
-
-test('DatePicker should render date format only with year', () => {
-    const onChange = jest.fn();
-    const options = {
-        dateFormat: 'YYYY',
-    };
-    const datePicker = mount(<DatePicker onChange={onChange} options={options} value={null} />);
-
-    expect(datePicker.render()).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot();
 });
 
 test('DatePicker should render date picker with time picker', () => {
@@ -130,57 +24,173 @@ test('DatePicker should render date picker with time picker', () => {
     const options = {
         timeFormat: true,
     };
-    const datePicker = mount(<DatePicker onChange={onChange} options={options} value={null} />);
+    const {baseElement} = render(<DatePicker onChange={onChange} options={options} value={null} />);
 
-    expect(datePicker.render()).toMatchSnapshot();
-    expect(datePicker.find(ReactDatetime).render()).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot();
 });
 
-test('DatePicker should render error', () => {
+test('DatePicker should render null value as empty string', () => {
     const onChange = jest.fn();
-    const datePicker = mount(<DatePicker onChange={onChange} valid={false} value={null} />);
+    const {container} = render(<DatePicker onChange={onChange} value={null} />);
 
-    expect(datePicker.render()).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
 });
 
-test('DatePicker should render error when invalid value is set', () => {
+test('DatePicker should render date format only with month', () => {
+    const onChange = jest.fn();
+    const options = {
+        dateFormat: 'MMMM',
+    };
+    const {container} = render(<DatePicker onChange={onChange} options={options} value={null} />);
+
+    expect(container).toMatchSnapshot();
+});
+
+test('DatePicker should render date format only with year', () => {
     const onChange = jest.fn();
     const options = {
         dateFormat: 'YYYY',
     };
-    const datePicker = mount(<DatePicker onChange={onChange} options={options} value={null} />);
+    const {container} = render(<DatePicker onChange={onChange} options={options} value={null} />);
 
-    // check if showError is set correctly
-    datePicker.find('Input').instance().props.onChange('xxx', {target: {value: 'xxx'}});
-    datePicker.find('Input').instance().props.onBlur();
-    datePicker.update();
-    expect(datePicker.instance().showError).toBe(true);
-
-    // snapshot
-    expect(datePicker.render()).toMatchSnapshot();
-
-    // now add a valid value
-    datePicker.find('Input').instance().props.onChange('2018', {target: {value: '2018'}});
-    datePicker.find('Input').instance().props.onBlur();
-    datePicker.update();
-    expect(datePicker.instance().showError).toBe(false);
+    expect(container).toMatchSnapshot();
 });
 
-test('DatePicker should set class correctly when overlay was opened/closed', () => {
+test('DatePicker should render error', () => {
     const onChange = jest.fn();
-    const input = mount(<DatePicker onChange={onChange} value={null} />);
+    const {container} = render(<DatePicker onChange={onChange} valid={false} value={null} />);
+
+    expect(container).toMatchSnapshot();
+});
+
+test('DatePicker should show disabled Input when disabled', () => {
+    const onChange = jest.fn();
+    const value = new Date('2017-05-23');
+    render(<DatePicker disabled={true} onChange={onChange} value={value} />);
+
+    const input = screen.queryByDisplayValue('05/23/2017');
+    expect(input).toBeDisabled();
+});
+
+test('DatePicker should pass input to inputRef prop', () => {
+    const inputRefSpy = jest.fn();
+    const value = new Date('2017-05-23');
+    render(<DatePicker disabled={true} inputRef={inputRefSpy} onChange={jest.fn()} value={value} />);
+
+    const input = screen.queryByDisplayValue('05/23/2017');
+    expect(inputRefSpy).toBeCalledWith(input);
+});
+
+test('DatePicker should open overlay on icon-click', async() => {
+    const onChange = jest.fn();
+    const {baseElement} = render(<DatePicker onChange={onChange} value={undefined} />);
+
+    const overlay = baseElement.querySelector('.rdt');
+    expect(overlay).toBeInTheDocument();
+    expect(overlay).not.toHaveClass('rdtOpen');
+
+    const icon = screen.queryByLabelText('su-calendar');
+    await userEvent.click(icon);
+
+    expect(overlay).toHaveClass('rdtOpen');
+});
+
+test('DatePicker should not open overlay on icon-click when disabled', async() => {
+    const onChange = jest.fn();
+    const {baseElement} = render(<DatePicker disabled={true} onChange={onChange} value={undefined} />);
+
+    const overlay = baseElement.querySelector('.rdt');
+    expect(overlay).toBeInTheDocument();
+    expect(overlay).not.toHaveClass('rdtOpen');
+
+    const icon = screen.queryByLabelText('su-calendar');
+    await userEvent.click(icon);
+
+    expect(overlay).not.toHaveClass('rdtOpen');
+});
+
+test('DatePicker should render with placeholder', () => {
+    const onChange = jest.fn();
+    render(<DatePicker onChange={onChange} placeholder="My placeholder" value={null} />);
+
+    const input = screen.queryByPlaceholderText('My placeholder');
+    expect(input).toBeInTheDocument();
+});
+
+test('DatePicker should render with value', () => {
+    const onChange = jest.fn();
+    const value = new Date('2017-05-23');
+    render(<DatePicker onChange={onChange} value={value} />);
+
+    const input = screen.queryByDisplayValue('05/23/2017');
+    expect(input).toBeInTheDocument();
+});
+
+test('DatePicker should try to guess incomplete value using format on blur.', async() => {
+    const onChange = jest.fn();
+    const options = {
+        dateFormat: false,
+        timeFormat: 'HH:mm',
+    };
+    render(<DatePicker onChange={onChange} options={options} placeholder="My placeholder" value={null} />);
+
+    const input = screen.queryByPlaceholderText('My placeholder');
+    await userEvent.type(input, '9');
+    await userEvent.tab(); // tab away from input
+
+    expect(onChange).toBeCalledWith(expect.any(Date));
+    const newValue = onChange.mock.calls[0][0];
+
+    const expectedMoment = moment('09:00', options.timeFormat);
+    expect(expectedMoment.isValid()).toBe(true);
+
+    const expectedDate = expectedMoment.toDate();
+    expect(newValue && newValue.getHours()).toBe(expectedDate.getHours());
+    expect(newValue && newValue.getMinutes()).toBe(expectedDate.getMinutes());
+});
+
+test('DatePicker should render error when invalid value is set', async() => {
+    const onChange = jest.fn();
+    const options = {
+        dateFormat: 'YYYY',
+    };
+    render(<DatePicker onChange={onChange} options={options} placeholder="My placeholder" value={null} />);
+
+    // check if showError is set correctly
+    const input = screen.queryByPlaceholderText('My placeholder');
+    await userEvent.type(input, 'xxx');
+    await userEvent.tab(); // tab away from input
+
+    expect(input.parentElement).toHaveClass('error');
+
+    // now add a valid value
+    await userEvent.clear(input);
+    await userEvent.type(input, '2018');
+    await userEvent.tab(); // tab away from input
+
+    expect(input.parentElement).not.toHaveClass('error');
+});
+
+test('DatePicker should set class correctly when overlay was opened/closed', async() => {
+    const onChange = jest.fn();
+    const {baseElement} = render(<DatePicker onChange={onChange} placeholder="My placeholder" value={null} />);
 
     // overlay should be closed
-    expect(input.find('div.rdt').hasClass('rdtOpen')).toBe(false);
+    const overlay = baseElement.querySelector('.rdt');
+    expect(overlay).toBeInTheDocument();
+    expect(overlay).not.toHaveClass('rdtOpen');
 
     // open dialog and check if class is set
-    input.find('Input Icon span').simulate('click');
-    expect(input.find('div.rdt').hasClass('rdtOpen')).toBe(true);
+    const icon = screen.queryByLabelText('su-calendar');
+    await userEvent.click(icon);
+    expect(overlay).toHaveClass('rdtOpen');
 
     // choose a date and check if class was removed again
-    input.find('.rdtPicker tbody tr td').first().simulate('click');
-    expect(input.find('div.rdt').hasClass('rdtOpen')).toBe(false);
+    const dateCell = screen.queryAllByText('26');
+    await userEvent.click(dateCell[0]);
+    expect(overlay).not.toHaveClass('rdtOpen');
 
     // check if value is in input
-    expect(input.find('Input').prop('value')).toBe('03/26/2017');
+    const input = screen.queryByPlaceholderText('My placeholder');
+    expect(input).toHaveValue('03/26/2017');
 });
