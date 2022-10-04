85cbf4c8235b84eddacc9228931582fc5da40f47
Alexander Schranz <alexander@sulu.io>
2022-10-05 01:23:38 +0200

    Update phpstan baseline

--- src/Sulu/Component/Security/Tests/Unit/Authorization/SecurityCheckerTest.php
+++ src/Sulu/Component/Security/Tests/Unit/Authorization/SecurityCheckerTest.php
@@ -13,6 +13,7 @@ namespace Sulu\Component\Security\Tests\Unit\Authorization;
 
 use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
 use Sulu\Component\Security\Authorization\SecurityChecker;
 use Sulu\Component\Security\Authorization\SecurityCheckerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
@@ -27,12 +28,12 @@ class SecurityCheckerTest extends TestCase
     private $securityChecker;
 
     /**
-     * @var TokenStorageInterface
+     * @var ObjectProphecy<TokenStorageInterface>
      */
     private $tokenStorage;
 
     /**
-     * @var AuthorizationCheckerInterface
+     * @var ObjectProphecy<AuthorizationCheckerInterface>
      */
     private $authorizationChecker;
 
@@ -51,7 +52,7 @@ class SecurityCheckerTest extends TestCase
         );
     }
 
-    public function testIsGrantedContext()
+    public function testIsGrantedContext(): void
     {
         $this->authorizationChecker->isGranted(
             'view',
@@ -63,7 +64,7 @@ class SecurityCheckerTest extends TestCase
         $this->assertTrue($granted);
     }
 
-    public function testIsGrantedObject()
+    public function testIsGrantedObject(): void
     {
         $object = new \stdClass();
 
@@ -77,7 +78,7 @@ class SecurityCheckerTest extends TestCase
         $this->assertTrue($granted);
     }
 
-    public function testIsGrantedFalsyValue()
+    public function testIsGrantedFalsyValue(): void
     {
         $object = null;
 
@@ -85,12 +86,10 @@ class SecurityCheckerTest extends TestCase
         $this->assertTrue($this->securityChecker->checkPermission($object, 'view'));
     }
 
-    public function testIsGrantedFail()
+    public function testIsGrantedFail(): void
     {
-        $this->expectException(
-            AccessDeniedException::class,
-            'Permission "view" in security context "sulu.media.collection" not granted'
-        );
+        $this->expectException(AccessDeniedException::class);
+        $this->expectExceptionMessage('Permission "view" in security context "sulu.media.collection" not granted');
 
         $this->authorizationChecker->isGranted(
             'view',
@@ -100,12 +99,10 @@ class SecurityCheckerTest extends TestCase
         $this->securityChecker->checkPermission('sulu.media.collection', 'view');
     }
 
-    public function testIsGrantedWithoutToken()
+    public function testIsGrantedWithoutToken(): void
     {
-        $this->expectException(
-            AccessDeniedException::class,
-            'Permission "view" in security context "sulu.media.collection" not granted'
-        );
+        $this->expectException(AccessDeniedException::class);
+        $this->expectExceptionMessage('Permission "view" in security context "sulu.media.collection" not granted');
 
         $this->tokenStorage->getToken()->willReturn(null);
         $this->authorizationChecker->isGranted(Argument::any(), Argument::any())->willReturn(false);
@@ -113,7 +110,7 @@ class SecurityCheckerTest extends TestCase
         $this->assertFalse($this->securityChecker->checkPermission('sulu.media.collection', 'view'));
     }
 
-    public function testIsGrantedWithoutTokenAllowed()
+    public function testIsGrantedWithoutTokenAllowed(): void
     {
         $this->tokenStorage->getToken()->willReturn(null);
         $this->authorizationChecker->isGranted(Argument::any(), Argument::any())->willReturn(true);
