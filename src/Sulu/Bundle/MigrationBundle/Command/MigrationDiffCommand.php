59c689455b7655c13bf77a30a16a97be2329bd25
Alexander Schranz <alexander@sulu.io>
2022-11-22 21:31:53 +0100

    Fix migration namespace

--- src/Sulu/Bundle/MigrationBundle/Command/MigrationDiffCommand.php
+++ src/Sulu/Bundle/MigrationBundle/Command/MigrationDiffCommand.php
@@ -16,6 +16,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\Process\Process;
 
 /**
@@ -27,6 +28,11 @@ class MigrationDiffCommand extends Command
 {
     protected static $defaultName = 'sulu:migration:migration-diff';
 
+    /**
+     * This are configured in the ../Resources/docker/docker-compose.yml file
+     *
+     * @var array<string, string>
+     */
     private static $databases = [
         'MySQL 8' => 'mysql://root:ChangeMe@127.0.0.1:3306/su_myapp?serverVersion=8.0&charset=utf8mb4',
         'PostgreSQL 13' => 'postgresql://symfony:ChangeMe@127.0.0.1:5432/su_myapp?serverVersion=13&charset=utf8',
@@ -54,60 +60,78 @@ class MigrationDiffCommand extends Command
                 'SYMFONY_DOTENV_VARS' => false, // see https://symfony.com/doc/6.1/components/process.html#setting-environment-variables-for-processes
             ];
 
-            $createDatabaseCommand = new Process([
+            $command = new Process([
                 \PHP_BINARY,
                 $consolePath,
                 'doctrine:database:create',
+                '--no-interaction',
                 '--if-not-exists',
             ], null, $envs);
 
-            $createDatabaseCommand->run(function($type, $buffer) use ($ui) {
+            $command->run(function($type, $buffer) use ($ui) {
                 $ui->write($buffer);
             });
 
-            if ($exitCode = $createDatabaseCommand->getExitCode()) {
+            if ($exitCode = $command->getExitCode()) {
                 return $exitCode;
             }
 
             $ui->section('Run Migrations: ' . $database);
 
-            $createDatabaseCommand = new Process([
+            $command = new Process([
                 \PHP_BINARY,
                 $consolePath,
                 'doctrine:migrations:migrate',
-                '--allow-no-migration',
                 '--no-interaction',
+                '--allow-no-migration',
             ], null, $envs);
 
-            $createDatabaseCommand->run(function($type, $buffer) use ($ui) {
+            $command->run(function($type, $buffer) use ($ui) {
                 $ui->write($buffer);
             });
 
-            if ($exitCode = $createDatabaseCommand->getExitCode()) {
+            if ($exitCode = $command->getExitCode()) {
                 return $exitCode;
             }
 
             $ui->section('Create new Migration: ' . $database);
 
-            $createDatabaseCommand = new Process([
+            $command = new Process([
                 \PHP_BINARY,
                 $consolePath,
                 'doctrine:migrations:diff',
                 '--no-interaction',
+                '--allow-empty-diff',
                 '--check-database-platform',
                 'true',
-                '--allow-empty-diff',
+                '--namespace',
+                'Sulu\Bundle\MigrationBundle\Migrations',
             ], null, $envs);
 
-            $createDatabaseCommand->run(function($type, $buffer) use ($ui) {
+            $command->run(function($type, $buffer) use ($ui) {
                 $ui->write($buffer);
             });
 
-            if ($exitCode = $createDatabaseCommand->getExitCode()) {
+            if ($exitCode = $command->getExitCode()) {
                 return $exitCode;
             }
+
+            $this->replaceAbortWithSkip();
         }
 
         return 0;
+    }
+
+    private function replaceAbortWithSkip()
+    {
+        $finder = new Finder();
+        $finder->files()->in(dirname(__DIR__) . '/Migrations')->name('Version*.php');
+
+        foreach ($finder as $file) {
+            \file_put_contents(
+                $file->getPathname(),
+                str_replace('abortIf', 'skipIf', $file->getContents())
+            );
+        }
     }
 }
