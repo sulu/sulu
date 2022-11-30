7d6bd1928cd9bcc33c259786352c67014cb05e94
Alexander Schranz <alexander@sulu.io>
2022-11-30 01:13:20 +0100

    Update regex for check email

--- src/Sulu/Bundle/AdminBundle/Resources/js/utils/Email/validateEmail.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/utils/Email/validateEmail.js
@@ -4,5 +4,7 @@ export default function(value: ?string): boolean {
         return false;
     }
 
-    return /^(.+)@(.+)$/.test(value);
+    // used by webkit: https://github.com/WebKit/WebKit/blob/e205a844146bab749642d1b88974b706fe4d9e7e/Source/WebCore/html/EmailInputType.cpp#L38-L39
+    // and recommend by the WHATWG: https://html.spec.whatwg.org/#valid-e-mail-address
+    return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value);
 }
