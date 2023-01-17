45c1fec12b759123201df7d92abeebedc0ff8477
Jason Lang <jason.lang@massiveart.com>
2023-01-17 10:35:19 +0100

    Removed group by

--- src/Sulu/Bundle/ContactBundle/Controller/AccountController.php
+++ src/Sulu/Bundle/ContactBundle/Controller/AccountController.php
@@ -427,7 +427,6 @@ class AccountController extends AbstractRestController implements ClassResourceI
         if ('true' == $request->get('flat')) {
             $fieldDescriptors = $this->getFieldDescriptors();
             $listBuilder = $this->generateFlatListBuilder();
-            $listBuilder->addGroupBy($fieldDescriptors['id']);
             $this->restHelper->initializeListBuilder($listBuilder, $fieldDescriptors);
             $this->applyRequestParameters($request, $listBuilder);
 
