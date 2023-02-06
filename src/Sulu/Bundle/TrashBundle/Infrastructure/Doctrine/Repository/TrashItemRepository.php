6dcf96496d8aa42023946aa0a6301b2ec2513721
Jason Lang <raptorify@gmail.com>
2023-02-06 13:24:22 +0100

    Removed wrong data type

--- src/Sulu/Bundle/TrashBundle/Infrastructure/Doctrine/Repository/TrashItemRepository.php
+++ src/Sulu/Bundle/TrashBundle/Infrastructure/Doctrine/Repository/TrashItemRepository.php
@@ -119,7 +119,6 @@ final class TrashItemRepository implements TrashItemRepositoryInterface
             return null;
         }
 
-        /** @var UserInterface $user */
         $user = $this->security->getUser();
 
         if (!($user instanceof UserInterface)) {
