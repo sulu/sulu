7542d314ec07d8c5b34e90260bde9443e15cb83c
Alexander Schranz <alexander@sulu.io>
2023-06-27 16:10:51 +0200

    Adjust ReferenceTest

--- src/Sulu/Bundle/ReferenceBundle/Tests/Unit/Domain/Model/ReferenceTest.php
+++ src/Sulu/Bundle/ReferenceBundle/Tests/Unit/Domain/Model/ReferenceTest.php
@@ -12,6 +12,7 @@
 namespace Sulu\Bundle\ReferenceBundle\Tests\Unit\Domain\Model;
 
 use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
 use Sulu\Bundle\ReferenceBundle\Domain\Model\Reference;
 use Sulu\Bundle\TestBundle\Testing\SetGetPrivatePropertyTrait;
 
@@ -54,6 +55,13 @@ class ReferenceTest extends TestCase
         static::assertSame('321-123', $reference->getReferenceResourceId());
     }
 
+    public function testGetSetReferenceTitle(): void
+    {
+        $reference = new Reference();
+        $reference->setReferenceTitle('Title');
+        static::assertSame('Title', $reference->getReferenceTitle());
+    }
+
     public function testGetSetReferenceLocale(): void
     {
         $reference = new Reference();
@@ -92,5 +100,44 @@ class ReferenceTest extends TestCase
         static::assertSame(1, $reference->getReferenceLiveCount());
         $reference->increaseReferenceLiveCounter();
         static::assertSame(2, $reference->getReferenceLiveCount());
+    }
+
+    public function testEqualsTrue(): void
+    {
+        $uuid = Uuid::uuid4()->toString();
+        $reference1 = new Reference();
+        $reference1->setResourceKey('media');
+        $reference1->setResourceId('1');
+        $reference1->setReferenceLocale('en');
+        $reference1->setReferenceResourceKey('pages');
+        $reference1->setReferenceResourceId($uuid);
+        $reference1->setReferenceProperty('image');
+        $reference1->setReferenceViewAttributes(['webspace' => 'default']);
+        $reference2 = new Reference();
+        $reference2->setResourceKey('media');
+        $reference2->setResourceId('1');
+        $reference2->setReferenceLocale('en');
+        $reference2->setReferenceResourceKey('pages');
+        $reference2->setReferenceResourceId($uuid);
+        $reference2->setReferenceProperty('image');
+        $reference2->setReferenceViewAttributes(['webspace' => 'default']);
+
+        $this->assertTrue(
+            $reference1->equals($reference2)
+        );
+    }
+
+    public function testEqualsFalse(): void
+    {
+        $uuid = Uuid::uuid4()->toString();
+        $uuid2 = Uuid::uuid4()->toString();
+        $reference1 = new Reference();
+        $reference1->setReferenceResourceId($uuid);
+        $reference2 = new Reference();
+        $reference2->setReferenceResourceId($uuid2);
+
+        $this->assertFalse(
+            $reference1->equals($reference2)
+        );
     }
 }
