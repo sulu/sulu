d8bdd483bafa7f81e79305b587151b31505a6f50
Christian Stoller <christian.stoller@leonex.de>
2023-11-17 15:02:44 +0100

    Sort snippet areas in admin frontend by title instead of key
    Currently the snippet areas are sorted by their key. But this makes it harder to scan the list for the user because titles may have the wrong order.

--- src/Sulu/Component/Util/Tests/Unit/SortUtilsTest.php
+++ src/Sulu/Component/Util/Tests/Unit/SortUtilsTest.php
@@ -167,6 +167,56 @@ class SortUtilsTest extends TestCase
 
         SortUtils::multisort($collection, 'somefink');
     }
+
+    public function testSortLocaleAwareSimpleArray(): void
+    {
+        $array = ['D', 'A', 'Ê', 'E', 'Ä', 'M'];
+        $result = SortUtils::sortLocaleAware($array, 'de');
+
+        if (!\class_exists(\Collator::class)) {
+            $this->addWarning('Could not test sorting with \\Collator, because intl extension is not loaded');
+
+            $this->assertSame(['A', 'D', 'E', 'M', 'Ä', 'Ê'], $result);
+        } else {
+            $this->assertSame(['A', 'Ä', 'D', 'E', 'Ê', 'M'], $result);
+        }
+    }
+
+    public function testSortLocaleAwareDeepArray(): void
+    {
+        $array = [
+            ['value' => 'D'],
+            ['value' => 'A'],
+            ['value' => 'Ê'],
+            ['value' => 'E'],
+            ['value' => 'Ä'],
+            ['value' => 'M'],
+        ];
+
+        $result = SortUtils::sortLocaleAware($array, 'de', fn ($item) => $item['value']);
+
+        if (!\class_exists(\Collator::class)) {
+            $this->addWarning('Could not test sorting with \\Collator, because intl extension is not loaded');
+
+            $this->assertSame([
+                ['value' => 'A'],
+                ['value' => 'D'],
+                ['value' => 'E'],
+                ['value' => 'M'],
+                ['value' => 'Ä'],
+                ['value' => 'Ê'],
+            ], $result);
+        } else {
+            $this->assertSame([
+                ['value' => 'A'],
+                ['value' => 'Ä'],
+                ['value' => 'D'],
+                ['value' => 'E'],
+                ['value' => 'Ê'],
+                ['value' => 'M'],
+            ], $result);
+        }
+    }
 }
 
 class FoobarTestClass
