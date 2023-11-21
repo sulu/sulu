f7779d2fb8d7507203134af39e1341418e81503d
Alexander Schranz <alexander@sulu.io>
2023-11-22 00:49:10 +0100

    Add experimental support for bun

--- preinstall.js
+++ preinstall.js
@@ -1,6 +1,8 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 
-if (process.env.npm_execpath.indexOf('npm') === -1) {
+if (process.env.npm_execpath.indexOf('npm') === -1
+    && process.env.npm_execpath.indexOf('bun') === -1 // @experimental supported
+) {
     throw new Error('\x1b[31mYou must use "npm install", yarn is not supported\x1b[0m');
 }
 
