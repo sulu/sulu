5cffaf608b19e7c28f12f4906db8933a5afc921c
Prokyonn <daniel@sulu.io>
2024-01-24 00:32:37 +0100

    Add repository tests

--- src/Sulu/Bundle/ReferenceBundle/Tests/Functional/Infrastructure/Doctrine/Repository/ReferenceRepositoryTest.php
+++ src/Sulu/Bundle/ReferenceBundle/Tests/Functional/Infrastructure/Doctrine/Repository/ReferenceRepositoryTest.php
@@ -72,6 +72,31 @@ class ReferenceRepositoryTest extends SuluTestCase
         $this->assertSame($referenceId, $reference->getId());
     }
 
+    public function testFindFlatByNotExist(): void
+    {
+        /** @var \Generator $references */
+        $references = $this->referenceRepository->findFlatBy(filters: ['id' => 2147483647], fields: ['id', 'referenceResourceKey']);
+
+        self::assertNull($references->current());
+    }
+
+    public function testFindFlatByExist(): void
+    {
+        $reference1 = $this->createReference();
+        $this->referenceRepository->add($reference1);
+        $this->referenceRepository->flush();
+
+        $referenceId = $reference1->getId();
+
+        /** @var \Generator $references */
+        $references = $this->referenceRepository->findFlatBy(filters: ['id' => $referenceId], fields: ['id', 'referenceResourceKey']);
+
+        /** @var array{id: integer, referenceResourceKey: string} $reference */
+        $reference = $references->current();
+        $this->assertSame($referenceId, $reference['id']);
+        $this->assertSame('pages', $reference['referenceResourceKey']);
+    }
+
     public function testGetOneByNotExist(): void
     {
         $this->expectException(ReferenceNotFoundException::class);
@@ -91,6 +116,72 @@ class ReferenceRepositoryTest extends SuluTestCase
         $reference = $this->referenceRepository->getOneBy(['id' => $referenceId]);
 
         $this->assertSame($referenceId, $reference->getId());
+    }
+
+    /**
+     * @dataProvider filterProvider
+     */
+    public function testFindOneByFilter(string $filterKey): void
+    {
+        self::purgeDatabase();
+        $reference = $this->createReference();
+
+        $this->referenceRepository->add($reference);
+        $this->referenceRepository->flush();
+
+        $referenceId = $reference->getId();
+        /** @var array{id?: int} $filter */
+        $filter = [$filterKey => self::getPrivateProperty($reference, $filterKey)];
+        $reference = $this->referenceRepository->findOneBy($filter);
+
+        $this->assertNotNull($reference);
+        $this->assertSame($referenceId, $reference->getId());
+    }
+
+    /**
+     * @dataProvider filterProvider
+     */
+    public function testGetOneByFilter(string $filterKey): void
+    {
+        self::purgeDatabase();
+        $reference = $this->createReference();
+
+        $this->referenceRepository->add($reference);
+        $this->referenceRepository->flush();
+
+        $referenceId = $reference->getId();
+
+        /** @var array{id?: int} $filter */
+        $filter = [$filterKey => self::getPrivateProperty($reference, $filterKey)];
+        $reference = $this->referenceRepository->getOneBy($filter);
+
+        $this->assertNotNull($reference);
+        $this->assertSame($referenceId, $reference->getId());
+    }
+
+    /**
+     * @dataProvider filterProvider
+     */
+    public function testFindFlatFilter(string $filterKey): void
+    {
+        self::purgeDatabase();
+        $reference = $this->createReference();
+
+        $this->referenceRepository->add($reference);
+        $this->referenceRepository->flush();
+
+        $referenceId = $reference->getId();
+
+        /** @var array{id?: int} $filters */
+        $filters = [$filterKey => self::getPrivateProperty($reference, $filterKey)];
+
+        /** @var \Generator $references */
+        $references = $this->referenceRepository->findFlatBy(filters: $filters, fields: ['id']);
+
+        $this->assertNotNull($references);
+        /** @var array{id: int} $reference */
+        $reference = $references->current();
+        $this->assertSame($referenceId, $reference['id']);
     }
 
     public function testAddAndRemove(): void
@@ -154,6 +245,77 @@ class ReferenceRepositoryTest extends SuluTestCase
         $this->assertNull($this->referenceRepository->findOneBy(['id' => $reference4Id]));
     }
 
+    public function testCount(): void
+    {
+        static::purgeDatabase();
+
+        $referenceResourceKey = 'article';
+        $referenceResourceId = Uuid::uuid4()->toString();
+        $referenceLocale = 'de';
+
+        $reference1 = $this->createReference();
+        $reference2 = $this->createReference();
+        $reference3 = $this->createReference();
+        $reference4 = $this->createReference();
+        static::setPrivateProperty($reference2, 'referenceResourceKey', $referenceResourceKey);
+        static::setPrivateProperty($reference3, 'referenceResourceId', $referenceResourceId);
+        static::setPrivateProperty($reference3, 'referenceLocale', $referenceLocale);
+        static::setPrivateProperty($reference4, 'referenceResourceKey', $referenceResourceKey);
+        static::setPrivateProperty($reference4, 'referenceResourceId', $referenceResourceId);
+        static::setPrivateProperty($reference4, 'referenceLocale', $referenceLocale);
+
+        $this->referenceRepository->add($reference1);
+        $this->referenceRepository->add($reference2);
+        $this->referenceRepository->add($reference3);
+        $this->referenceRepository->add($reference4);
+
+        $this->referenceRepository->flush();
+        $reference1Id = $reference1->getId();
+        $reference2Id = $reference2->getId();
+        $reference3Id = $reference3->getId();
+        $reference4Id = $reference4->getId();
+
+        $this->assertSame(4, $this->referenceRepository->count());
+        $this->assertSame(2, $this->referenceRepository->count(['referenceResourceKey' => $referenceResourceKey]));
+        $this->assertSame(2, $this->referenceRepository->count(['referenceResourceId' => $referenceResourceId]));
+        $this->assertSame(2, $this->referenceRepository->count(['referenceLocale' => $referenceLocale]));
+        $this->assertSame(1, $this->referenceRepository->count(['id' => $reference1Id]));
+        $this->assertSame(1, $this->referenceRepository->count(['id' => $reference2Id]));
+        $this->assertSame(1, $this->referenceRepository->count(['id' => $reference3Id]));
+        $this->assertSame(1, $this->referenceRepository->count(['id' => $reference4Id]));
+    }
+
+    public function testCountDistinctFields(): void
+    {
+        static::purgeDatabase();
+
+        $referenceResourceKey = 'article';
+        $referenceResourceId = Uuid::uuid4()->toString();
+        $referenceLocale = 'de';
+
+        $reference1 = $this->createReference();
+        $reference2 = $this->createReference();
+        $reference3 = $this->createReference();
+        $reference4 = $this->createReference();
+        static::setPrivateProperty($reference2, 'referenceResourceKey', $referenceResourceKey);
+        static::setPrivateProperty($reference3, 'referenceResourceId', $referenceResourceId);
+        static::setPrivateProperty($reference3, 'referenceLocale', $referenceLocale);
+        static::setPrivateProperty($reference4, 'referenceResourceKey', $referenceResourceKey);
+        static::setPrivateProperty($reference4, 'referenceResourceId', $referenceResourceId);
+        static::setPrivateProperty($reference4, 'referenceLocale', $referenceLocale);
+
+        $this->referenceRepository->add($reference1);
+        $this->referenceRepository->add($reference2);
+        $this->referenceRepository->add($reference3);
+        $this->referenceRepository->add($reference4);
+
+        $this->referenceRepository->flush();
+
+        $this->assertSame(4, $this->referenceRepository->count());
+        $this->assertSame(1, $this->referenceRepository->count(['referenceResourceKey' => $referenceResourceKey], ['referenceResourceKey']));
+        $this->assertSame(2, $this->referenceRepository->count(['referenceResourceKey' => $referenceResourceKey], ['referenceLocale']));
+    }
+
     private function createReference(): ReferenceInterface
     {
         $reference = $this->referenceRepository->create(
@@ -169,5 +331,24 @@ class ReferenceRepositoryTest extends SuluTestCase
         );
 
         return $reference;
+    }
+
+    /**
+     * @return array<int, array<int, string>>
+     */
+    public function filterProvider(): array
+    {
+        return [
+            ['id'],
+            ['resourceKey'],
+            ['resourceId'],
+            ['referenceResourceKey'],
+            ['referenceResourceId'],
+            ['referenceLocale'],
+            ['referenceTitle'],
+            ['referenceContext'],
+            ['referenceProperty'],
+            ['referenceRouterAttributes'],
+        ];
     }
 }
