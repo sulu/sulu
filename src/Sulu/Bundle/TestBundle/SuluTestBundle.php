04b7c4017bebac24ccc178bb63319b08e56baa3b
Alexander Schranz <alexander@sulu.io>
2024-02-05 10:22:34 +0100

    Add final also to test bundle

--- src/Sulu/Bundle/TestBundle/SuluTestBundle.php
+++ src/Sulu/Bundle/TestBundle/SuluTestBundle.php
@@ -15,6 +15,9 @@ use Sulu\Bundle\TestBundle\DependencyInjection\Compiler\ReplaceTestClientPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * @final
+ */
 class SuluTestBundle extends Bundle
 {
     /**
