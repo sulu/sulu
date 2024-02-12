357c8aa5e9af0943a85bf8f2115696491c35f51f
Alexander Schranz <alexander@sulu.io>
2024-02-12 10:49:15 +0100

    Fix code style

--- src/Sulu/Component/DocumentManager/EventDispatcher/CallListenersTrait.php
+++ src/Sulu/Component/DocumentManager/EventDispatcher/CallListenersTrait.php
@@ -1,8 +1,17 @@
 <?php
 
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Sulu\Component\DocumentManager\EventDispatcher;
 
-if (\class_exists( 'Symfony\Component\EventDispatcher\Event')) { // indicates its Symfony 4.4 or lower
+if (\class_exists('Symfony\Component\EventDispatcher\Event')) { // indicates its Symfony 4.4 or lower
     /**
      * @internal
      */
