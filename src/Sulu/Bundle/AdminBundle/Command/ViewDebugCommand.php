2366c48b8227a8f467d8a9fe458e8e34880fe5b7
Oliver Kossin <oliver.kossin@qossmic.com>
2024-02-16 10:58:19 +0100

    Run CS-Fixer

--- src/Sulu/Bundle/AdminBundle/Command/ViewDebugCommand.php
+++ src/Sulu/Bundle/AdminBundle/Command/ViewDebugCommand.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Sulu\Bundle\AdminBundle\Command;
 
 use Sulu\Bundle\AdminBundle\Admin\View\DropdownToolbarAction;
@@ -26,8 +35,7 @@ class ViewDebugCommand extends Command
 {
     public function __construct(
         private ViewRegistry $viewRegistry,
-    )
-    {
+    ) {
         parent::__construct();
     }
 
@@ -154,5 +162,4 @@ class ViewDebugCommand extends Command
     {
         return $tableRows;
     }
-
 }
