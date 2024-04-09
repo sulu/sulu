ce6e0c11b4963b6a189fcfb8dd5585b72c6668a4
Saulius Vaičeliūnas <vaiceliunas@inbox.lt>
2024-04-09 19:25:35 +0300

    fix: adjustments by comments

--- src/Sulu/Bundle/PreviewBundle/Infrastructure/Symfony/DependencyInjection/Compiler/RegisterPreviewWebspaceClassPass.php
+++ src/Sulu/Bundle/PreviewBundle/Infrastructure/Symfony/DependencyInjection/Compiler/RegisterPreviewWebspaceClassPass.php
@@ -1,10 +1,22 @@
 <?php
 
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Sulu\Bundle\PreviewBundle\Infrastructure\Symfony\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
+/**
+ * @internal
+ */
 class RegisterPreviewWebspaceClassPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
