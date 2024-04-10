114eeaff4aed9de89ffda0a881c1dd0c526ac907
Alexander Schranz <alexander@sulu.io>
2024-04-10 12:15:00 +0200

    Update phpstan baseline to remove latest not longer ignored errors

--- src/Sulu/Bundle/PreviewBundle/Preview/Renderer/PreviewKernel.php
+++ src/Sulu/Bundle/PreviewBundle/Preview/Renderer/PreviewKernel.php
@@ -124,6 +124,9 @@ class PreviewKernel extends Kernel
         );
     }
 
+    /**
+     * @return void
+     */
     protected function build(ContainerBuilder $container)
     {
         $container->addCompilerPass(new RegisterPreviewWebspaceClassPass());
