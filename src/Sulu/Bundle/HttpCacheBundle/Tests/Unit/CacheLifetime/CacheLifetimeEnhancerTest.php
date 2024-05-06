31d84c559429ac5529dfca0cd8578fd09f18c5a5
mamazu <14860264+mamazu@users.noreply.github.com>
2024-05-06 12:34:41 +0200

    Fix correct return self in the CacheManagerTest  (#7400)

--- src/Sulu/Bundle/HttpCacheBundle/Tests/Unit/CacheLifetime/CacheLifetimeEnhancerTest.php
+++ src/Sulu/Bundle/HttpCacheBundle/Tests/Unit/CacheLifetime/CacheLifetimeEnhancerTest.php
@@ -122,7 +122,7 @@ class CacheLifetimeEnhancerTest extends TestCase
             $this->response->setMaxAge($this->maxAge)->shouldBeCalled()->willReturn($this->response->reveal());
             $this->response->setSharedMaxAge($this->sharedMaxAge)->shouldBeCalled()->willReturn($this->response->reveal());
         } else {
-            $this->responseHeaderBag->set(Argument::cetera())->shouldNotBeCalled()->willReturn($this->response->reveal());
+            $this->responseHeaderBag->set(Argument::cetera())->shouldNotBeCalled();
             $this->response->setPublic()->shouldNotBeCalled()->willReturn($this->response->reveal());
             $this->response->setMaxAge(Argument::any())->shouldNotBeCalled()->willReturn($this->response->reveal());
             $this->response->setSharedMaxAge(Argument::any())->shouldNotBeCalled()->willReturn($this->response->reveal());
