e3095c48b5be933aa4b6b55e4e02a75cc21b6e18
Alexander Schranz <alexander@sulu.io>
2024-07-05 16:02:26 +0200

    Fix Activity ID Generator

--- src/Sulu/Bundle/ActivityBundle/Infrastructure/Doctrine/Repository/ActivityRepository.php
+++ src/Sulu/Bundle/ActivityBundle/Infrastructure/Doctrine/Repository/ActivityRepository.php
@@ -103,7 +103,12 @@ class ActivityRepository implements ActivityRepositoryInterface
 
         // set value of id explicitly if class has a pre-insert identity-generator to be compatible with postgresql
         if (!$classMetadata->idGenerator->isPostInsertGenerator()) {
-            $queryBuilder->setValue($classMetadata->getColumnName('id'), $classMetadata->idGenerator->generate($this->entityManager, $activity));
+            $queryBuilder->setValue(
+                $classMetadata->getColumnName('id'),
+                \method_exists($classMetadata->idGenerator, 'generate')
+                    ? $classMetadata->idGenerator->generate($this->entityManager, $activity)
+                    : $classMetadata->idGenerator->generateId($this->entityManager, $activity)
+            );
         }
 
         return $queryBuilder;
