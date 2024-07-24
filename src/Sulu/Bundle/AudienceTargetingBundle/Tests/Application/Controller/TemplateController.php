4da4a717f7af2555f96789a99f6f80644182d7c7
Alexander Schranz <alexander@sulu.io>
2024-07-24 09:49:11 +0200

    Fix TemplateController linting

--- src/Sulu/Bundle/AudienceTargetingBundle/Tests/Application/Controller/TemplateController.php
+++ src/Sulu/Bundle/AudienceTargetingBundle/Tests/Application/Controller/TemplateController.php
@@ -12,17 +12,45 @@
 namespace Sulu\Bundle\AudienceTargetingBundle\Tests\Application\Controller;
 
 use Sulu\Bundle\HttpCacheBundle\Cache\SuluHttpCache;
-use Symfony\Bundle\FrameworkBundle\Controller\TemplateController as SymfonyTemplateController;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
-class TemplateController extends SymfonyTemplateController
+/**
+ * @internal
+ */
+class TemplateController
 {
+    private Environment $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param array<string, mixed> $context
+     */
     public function templateAction(string $template, ?int $maxAge = null, ?int $sharedAge = null, ?bool $private = null, array $context = [], int $statusCode = 200, ?int $reverseProxyTtl = null): Response
     {
-        $response = parent::templateAction($template, $maxAge, $sharedAge, $private, $context, $statusCode);
+        $content = $this->twig->render($template, $context);
+        $response = new Response($content, $statusCode);
+
+        if ($maxAge) {
+            $response->setMaxAge($maxAge);
+        }
+
+        if (null !== $sharedAge) {
+            $response->setSharedMaxAge($sharedAge);
+        }
+
+        if ($private) {
+            $response->setPrivate();
+        } elseif (false === $private || (null === $private && (null !== $maxAge || null !== $sharedAge))) {
+            $response->setPublic();
+        }
 
         if (null !== $reverseProxyTtl) {
-            $response->headers->set(SuluHttpCache::HEADER_REVERSE_PROXY_TTL, $reverseProxyTtl);
+            $response->headers->set(SuluHttpCache::HEADER_REVERSE_PROXY_TTL, (string) $reverseProxyTtl);
         }
 
         return $response;
