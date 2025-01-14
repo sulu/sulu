fde0d3447fde0bde4ab5a03e7f728b3f388f1a4a
mamazu <14860264+mamazu@users.noreply.github.com>
2025-01-14 12:07:53 +0100

    Adding more classes

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -61,6 +61,9 @@ $config
         'Sulu/internal_class' => [
             'base_type' => [
                 'CompilerPassInterface',
+                'FormMetadataVisitorInterface',
+                'TypedFormMetadataVisitorInterface',
+                'MetadataProviderInterface',
             ],
         ]
     ])
