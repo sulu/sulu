704b03ef1c8f3bdbb0889eac5b56be67ae3e86f5
Alexander Schranz <alexander@sulu.io>
2025-02-21 13:33:18 +0100

    Make the whole implementation final

--- packages/content/src/Application/PropertyResolver/PropertyResolverProvider.php
+++ packages/content/src/Application/PropertyResolver/PropertyResolverProvider.php
@@ -16,6 +16,11 @@ namespace Sulu\Content\Application\PropertyResolver;
 use Sulu\Content\Application\PropertyResolver\Resolver\PropertyResolverInterface;
 
 /**
+ * @internal The constructor of this class maybe change in future releases to add new features or improve performance.
+ *           Use this service via the dependency injection container via the PropertyResolverProviderInterface only.
+ *           It is still fine to use this class to create a new instance for mock-less unit tests,
+ *           but no backwards compatibility promise can be given for the constructor.
+ *
  * If you need to override this service use service decoration via the provided interface: https://symfony.com/doc/6.4/service_container/service_decoration.html.
  */
 final class PropertyResolverProvider implements PropertyResolverProviderInterface
@@ -26,8 +31,6 @@ final class PropertyResolverProvider implements PropertyResolverProviderInterfac
     private array $propertyResolvers;
 
     /**
-     * @internal The constructor of this class maybe change in future releases. Use this service via the dependency injection container only.
-     *
      * @param iterable<PropertyResolverInterface> $propertyResolvers
      */
     public function __construct(iterable $propertyResolvers)
