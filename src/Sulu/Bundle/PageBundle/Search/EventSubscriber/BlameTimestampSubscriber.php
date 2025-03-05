8bfee27a1adeaaa90fe65cc1d2a7bf3b5cfd7d1f
Alexander Schranz <alexander@sulu.io>
2025-03-05 09:55:26 +0100

    Revert unnecessary changes for providing DateTimeInterface support
    Code reformats and cleanups should be done in own PRs

--- src/Sulu/Bundle/PageBundle/Search/EventSubscriber/BlameTimestampSubscriber.php
+++ src/Sulu/Bundle/PageBundle/Search/EventSubscriber/BlameTimestampSubscriber.php
@@ -100,22 +100,32 @@ class BlameTimestampSubscriber implements EventSubscriberInterface
         ?\DateTimeInterface $created = null,
         ?\DateTimeInterface $changed = null,
     ): void {
-        $document->addField($this->factory->createField('created', $created?->format('c'), 'string'));
-        $document->addField($this->factory->createField('changed', $changed?->format('c'), 'string'));
+        $document->addField(
+            $this->factory->createField('created', $created ? $created->format('c') : null, 'string')
+        );
+
+        $document->addField(
+            $this->factory->createField('changed', $changed ? $changed->format('c') : null, 'string')
+        );
     }
 
     /**
      * Map the creator and changer to the document.
      */
-    private function mapCreatorAndChanger(
-        Document $document,
-        ?UserInterface $creator = null,
-        ?UserInterface $changer = null,
-    ): void {
-        $document->addField($this->factory->createField('changer', $changer?->getUserIdentifier(), 'string'));
-        $document->addField($this->factory->createField('changer_id', $changer?->getId(), 'string'));
+    private function mapCreatorAndChanger(Document $document, ?UserInterface $creator = null, ?UserInterface $changer = null)
+    {
+        $document->addField(
+            $this->factory->createField('changer', $changer ? $changer->getUserIdentifier() : null, 'string')
+        );
+        $document->addField(
+            $this->factory->createField('changer_id', $changer ? $changer->getId() : null, 'string')
+        );
 
-        $document->addField($this->factory->createField('creator', $creator?->getUserIdentifier(), 'string'));
-        $document->addField($this->factory->createField('creator_id', $creator?->getId(), 'string'));
+        $document->addField(
+            $this->factory->createField('creator', $creator ? $creator->getUserIdentifier() : null, 'string')
+        );
+        $document->addField(
+            $this->factory->createField('creator_id', $creator ? $creator->getId() : null, 'string')
+        );
     }
 }
