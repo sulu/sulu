6d9aaad49965a651bd3c7e441ce4a27b2b150ceb
patrick <patrick@sulu.io>
2025-05-13 13:44:33 +0200

    add mimetype fallback if thumbnail isn't available
    add check for single media selection
    
    test fixes
    
    linter fixes
    
    more linter fixes
    
    add / adjust tests
    
    code style
    
    remove data-testid
    
    update baseline
    
    linter fix

--- src/Sulu/Bundle/MediaBundle/Resources/js/containers/FieldBlocks/tests/blockPreviewTransformers/MediaSelectionBlockPreviewTransformer.test.js
+++ src/Sulu/Bundle/MediaBundle/Resources/js/containers/FieldBlocks/tests/blockPreviewTransformers/MediaSelectionBlockPreviewTransformer.test.js
@@ -1,4 +1,6 @@
 // @flow
+import {render} from '@testing-library/react';
+import {runInAction} from 'mobx';
 import MediaSelectionBlockPreviewTransformer
     from '../../blockPreviewTransformers/MediaSelectionBlockPreviewTransformer';
 
@@ -6,11 +8,23 @@ const MEDIA_URL = '/admin/media/redirect/media/:id';
 
 test('Render a single image if an id is given', () => {
     const mediaSelectionBlockPreviewTransformer = new MediaSelectionBlockPreviewTransformer(MEDIA_URL);
+    // eslint-disable-next-line no-undef
+    global.fetch = jest.fn(() =>
+        Promise.resolve({
+            json: () => Promise.resolve(),
+        })
+    );
     expect(mediaSelectionBlockPreviewTransformer.transform({ids: [3, 7]})).toMatchSnapshot();
 });
 
 test('Render only eight images if more are given if an id is given', () => {
     const mediaSelectionBlockPreviewTransformer = new MediaSelectionBlockPreviewTransformer(MEDIA_URL);
+    // eslint-disable-next-line no-undef
+    global.fetch = jest.fn(() =>
+        Promise.resolve({
+            json: () => Promise.resolve({status: 200}),
+        })
+    );
     expect(mediaSelectionBlockPreviewTransformer.transform({ids: [3, 7, 9, 11, 13, 2, 1, 4, 5]})).toMatchSnapshot();
 });
 
@@ -22,4 +36,24 @@ test('Render nothing if no id is given', () => {
 test('Render nothing if a wrong type of value is given', () => {
     const mediaSelectionBlockPreviewTransformer = new MediaSelectionBlockPreviewTransformer(MEDIA_URL);
     expect(mediaSelectionBlockPreviewTransformer.transform('')).toMatchSnapshot();
+});
+
+test('Render MimeTypeIndicator if image isn\'t available', async() => {
+    const mediaSelectionBlockPreviewTransformer = new MediaSelectionBlockPreviewTransformer(MEDIA_URL);
+    // eslint-disable-next-line no-undef
+    global.fetch = jest.fn(() =>
+        Promise.resolve({
+            ok: false,
+            status: 404,
+            json: () => Promise.resolve({}),
+        })
+    );
+
+    const {container, rerender} = render(mediaSelectionBlockPreviewTransformer.transform({ids: [1, 2, 3]}));
+    await new Promise((resolve) => setTimeout(resolve));
+    runInAction(() => { });
+    rerender(mediaSelectionBlockPreviewTransformer.transform({ids: [1, 2, 3]}));
+
+    //eslint-disable-next-line testing-library/no-container
+    expect(container.querySelectorAll('span')).toHaveLength(3);
 });
--- src/Sulu/Bundle/MediaBundle/Resources/js/containers/FieldBlocks/tests/blockPreviewTransformers/SingleMediaSelectionBlockPreviewTransformer.test.js
+++ src/Sulu/Bundle/MediaBundle/Resources/js/containers/FieldBlocks/tests/blockPreviewTransformers/SingleMediaSelectionBlockPreviewTransformer.test.js
@@ -1,4 +1,6 @@
 // @flow
+import {render} from '@testing-library/react';
+import {runInAction} from 'mobx';
 import SingleMediaSelectionBlockPreviewTransformer
     from '../../blockPreviewTransformers/SingleMediaSelectionBlockPreviewTransformer';
 
@@ -6,15 +8,53 @@ const MEDIA_URL = '/admin/media/redirect/media/:id';
 
 test('Render a single image if an id is given', () => {
     const singleMediaSelectionBlockPreviewTransformer = new SingleMediaSelectionBlockPreviewTransformer(MEDIA_URL);
+    // eslint-disable-next-line no-undef
+    global.fetch = jest.fn(() =>
+        Promise.resolve({
+            json: () => Promise.resolve({}),
+        })
+    );
     expect(singleMediaSelectionBlockPreviewTransformer.transform({id: 5})).toMatchSnapshot();
 });
 
 test('Render nothing if no id is given', () => {
     const singleMediaSelectionBlockPreviewTransformer = new SingleMediaSelectionBlockPreviewTransformer(MEDIA_URL);
+    // eslint-disable-next-line no-undef
+    global.fetch = jest.fn(() =>
+        Promise.resolve({
+            json: () => Promise.resolve({}),
+        })
+    );
     expect(singleMediaSelectionBlockPreviewTransformer.transform({})).toMatchSnapshot();
 });
 
 test('Render nothing if a wrong type of value is given', () => {
     const singleMediaSelectionBlockPreviewTransformer = new SingleMediaSelectionBlockPreviewTransformer(MEDIA_URL);
+    // eslint-disable-next-line no-undef
+    global.fetch = jest.fn(() =>
+        Promise.resolve({
+            json: () => Promise.resolve({}),
+        })
+    );
     expect(singleMediaSelectionBlockPreviewTransformer.transform('')).toMatchSnapshot();
+});
+
+test('Render MimeTypeIndicator if image isn\'t available', async() => {
+    const singleMediaSelectionBlockPreviewTransformer = new SingleMediaSelectionBlockPreviewTransformer(MEDIA_URL);
+    // eslint-disable-next-line no-undef
+    global.fetch = jest.fn(() =>
+        Promise.resolve({
+            ok: false,
+            status: 404,
+            json: () => Promise.resolve({}),
+        })
+    );
+
+    const {container, rerender} = render(singleMediaSelectionBlockPreviewTransformer.transform({id: 123}));
+    await new Promise((resolve) => setTimeout(resolve));
+    runInAction(() => { });
+    rerender(singleMediaSelectionBlockPreviewTransformer.transform({id: 123}));
+
+    //eslint-disable-next-line testing-library/no-container
+    expect(container.querySelector('.mimeTypeIndicator')).toMatchSnapshot();
 });
