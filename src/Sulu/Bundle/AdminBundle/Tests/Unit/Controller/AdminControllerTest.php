f34c56653b376339473dd4c1965bfd9bae033ea0
Alexander Schranz <alexander@sulu.io>
2025-06-10 14:09:56 +0200

    Fix backmerge issues with routing changes

--- src/Sulu/Bundle/AdminBundle/Tests/Unit/Controller/AdminControllerTest.php
+++ src/Sulu/Bundle/AdminBundle/Tests/Unit/Controller/AdminControllerTest.php
@@ -251,7 +251,6 @@ class AdminControllerTest extends TestCase
         $this->urlGenerator->generate('sulu_preview.update')->willReturn('/preview/update');
         $this->urlGenerator->generate('sulu_preview.update-context')->willReturn('/preview/update-context');
         $this->urlGenerator->generate('sulu_preview.stop')->willReturn('/preview/stop');
-        $this->urlGenerator->generate('sulu_security.cget_security-contexts')->willReturn('/api/security-contexts');
         $this->urlGenerator->generate('sulu_website.cache.remove')->willReturn('/admin/website/cache');
         $this->urlGenerator->generate('sulu_media.redirect', ['id' => ':id'])->willReturn('/media/redirect');
 
