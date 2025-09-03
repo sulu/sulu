8ac9eee9750597510e4f4da154dae3c91e0ea5d8
Alexander Schranz <alexander@sulu.io>
2025-09-03 13:51:12 +0200

    Add Test for AdminConfig endpoint

---
+++ packages/page/tests/Functional/Infrastructure/Sulu/Admin/AdminConfigControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Sulu\Page\Tests\Functional\Infrastructure\JMS\Serializer;
+
+use Sulu\Bundle\TestBundle\Testing\AssertSnapshotTrait;
+use Sulu\Bundle\TestBundle\Testing\SuluTestCase;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+
+class AdminConfigControllerTest extends SuluTestCase
+{
+    use AssertSnapshotTrait;
+
+    private KernelBrowser $client;
+
+    public function setUp(): void
+    {
+        $this->client = static::createAuthenticatedClient();
+    }
+
+    public function testWebspaceResponse(): void
+    {
+        $this->client->request('GET', '/admin/config');
+
+        $response = $this->client->getResponse();
+
+        $data = \json_decode($response->getContent() ?: '', true, 512, \JSON_THROW_ON_ERROR);
+        $this->assertIsArray($data);
+        $this->assertArrayHasKey('sulu_page', $data);
+
+        $suluPageData = $data['sulu_page'];
+
+        $this->assertSnapshot('sulu_page_admin_config.json', \json_encode($suluPageData, \JSON_THROW_ON_ERROR | \JSON_PRETTY_PRINT));
+    }
+}
