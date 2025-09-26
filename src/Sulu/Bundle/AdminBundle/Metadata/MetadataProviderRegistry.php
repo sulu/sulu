906c098d53361c5e3177b1a47eae1cbbc226f4c5
mamazu <14860264+mamazu@users.noreply.github.com>
2025-09-26 13:00:01 +0200

    Deprecate the `AddMetadataProviderPass` (#8038)

--- src/Sulu/Bundle/AdminBundle/Metadata/MetadataProviderRegistry.php
+++ src/Sulu/Bundle/AdminBundle/Metadata/MetadataProviderRegistry.php
@@ -11,14 +11,36 @@
 
 namespace Sulu\Bundle\AdminBundle\Metadata;
 
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
 use Sulu\Bundle\AdminBundle\Exception\MetadataProviderNotFoundException;
 
 class MetadataProviderRegistry
 {
     private $metadataProviders = [];
 
+    public function __construct(
+        private ?ContainerInterface $container = null,
+    ) {
+    }
+
+    /**
+     * @throws MetadataProviderNotFoundException
+     * @throws ContainerExceptionInterface
+     */
     public function getMetadataProvider(string $type): MetadataProviderInterface
     {
+        if (null !== $this->container) {
+            if (!$this->container->has($type)) {
+                throw new MetadataProviderNotFoundException($type);
+            }
+
+            /** @var MetadataProviderInterface $metadataProvider */
+            $metadataProvider = $this->container->get($type);
+
+            return $metadataProvider;
+        }
+
         if (!\array_key_exists($type, $this->metadataProviders)) {
             throw new MetadataProviderNotFoundException($type);
         }
@@ -26,6 +48,9 @@ class MetadataProviderRegistry
         return $this->metadataProviders[$type];
     }
 
+    /**
+     * @deprecated since Sulu 2.6 use the constructor instead
+     */
     public function addMetadataProvider(string $type, MetadataProviderInterface $metadataProvider)
     {
         $this->metadataProviders[$type] = $metadataProvider;
