f9e1a90cdbbd63c40e3f46711a495f7f2ba5ca6e
mamazu <14860264+mamazu@users.noreply.github.com>
2025-11-03 15:33:23 +0100

    Deprecating 'ImageTransformationCompilerPass'

--- src/Sulu/Bundle/MediaBundle/Media/ImageConverter/TransformationPool.php
+++ src/Sulu/Bundle/MediaBundle/Media/ImageConverter/TransformationPool.php
@@ -11,6 +11,7 @@
 
 namespace Sulu\Bundle\MediaBundle\Media\ImageConverter;
 
+use Psr\Container\ContainerInterface;
 use Sulu\Bundle\MediaBundle\Media\ImageConverter\Transformation\TransformationInterface;
 
 /**
@@ -23,8 +24,14 @@ class TransformationPool implements TransformationPoolInterface
      */
     private $transformations = [];
 
+    public function __construct(private ContainerInterface $container)
+    {
+    }
+
     /**
      * @param string $alias
+     *
+     * @deprecated Use the constructor instead
      */
     public function add(TransformationInterface $transformation, $alias)
     {
@@ -40,6 +47,12 @@ class TransformationPool implements TransformationPoolInterface
      */
     public function get($name)
     {
+        try {
+            return $this->container->get($name);
+        } catch (\Throwable) {
+            // do nothing and try the old logic.
+        }
+
         if (\array_key_exists($name, $this->transformations)) {
             return $this->transformations[$name];
         }
