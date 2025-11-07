8f67ada55d88339634479a2a148b50bf67fa27bd
Manuel Bertrams <manuel@bertrams-media.de>
2025-11-07 22:49:10 +0100

    SelectPropertyMetadataMapper

---
+++ src/Sulu/Bundle/AdminBundle/Metadata/SchemaMetadata/SelectPropertyMetadataMapper.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sulu\Bundle\AdminBundle\Metadata\SchemaMetadata;
+
+use Sulu\Component\Content\Metadata\PropertyMetadata as ContentPropertyMetadata;
+
+class SelectPropertyMetadataMapper implements PropertyMetadataMapperInterface
+{
+    public function mapPropertyMetadata(ContentPropertyMetadata $propertyMetadata): PropertyMetadata
+    {
+        $mandatory = $propertyMetadata->isRequired();
+
+        $selectMetadata = new AnyOfsMetadata([
+            new StringMetadata(),
+            new NumberMetadata(),
+        ]);
+
+        if (!$mandatory) {
+            $selectMetadata = new AnyOfsMetadata([
+                new NullMetadata(),
+                $selectMetadata,
+            ]);
+        }
+
+        return new PropertyMetadata($propertyMetadata->getName(), $mandatory, $selectMetadata);
+    }
+}
