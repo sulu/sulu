c4fe3f956513b115eeb5f5dfb7274d2a72773e72
mamazu <14860264+mamazu@users.noreply.github.com>
2025-11-07 14:25:04 +0100

    Use `tagged_iterator`s for the markup listener (#7403)

--- src/Sulu/Bundle/MarkupBundle/SuluMarkupBundle.php
+++ src/Sulu/Bundle/MarkupBundle/SuluMarkupBundle.php
@@ -11,7 +11,6 @@
 
 namespace Sulu\Bundle\MarkupBundle;
 
-use Sulu\Bundle\MarkupBundle\DependencyInjection\CompilerPass\ParserCompilerPass;
 use Sulu\Bundle\MarkupBundle\DependencyInjection\CompilerPass\TagCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -30,7 +29,6 @@ class SuluMarkupBundle extends Bundle
     {
         parent::build($container);
 
-        $container->addCompilerPass(new ParserCompilerPass());
         $container->addCompilerPass(new TagCompilerPass());
     }
 }
