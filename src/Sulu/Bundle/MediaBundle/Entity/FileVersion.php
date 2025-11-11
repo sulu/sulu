48d3d64aa7e1ecd1f51e9e43fd9c02d1504d642a
matthiasnoback <matthiasnoback@gmail.com>
2025-11-11 10:54:47 +0100

    Deal with absent default meta

--- src/Sulu/Bundle/MediaBundle/Entity/FileVersion.php
+++ src/Sulu/Bundle/MediaBundle/Entity/FileVersion.php
@@ -549,7 +549,7 @@ class FileVersion implements AuditableInterface
             $this->id = null;
             /** @var FileVersionMeta[] $newMetaList */
             $newMetaList = [];
-            $defaultMetaLocale = $this->getDefaultMeta()->getLocale();
+            $defaultMetaLocale = $this->getDefaultMeta()?->getLocale();
             /** @var FileVersionContentLanguage[] $newContentLanguageList */
             $newContentLanguageList = [];
             /** @var FileVersionPublishLanguage[] $newPublishLanguageList */
