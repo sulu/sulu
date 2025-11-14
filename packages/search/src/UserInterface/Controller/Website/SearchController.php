1d3989c9f0e780177f03d0d376b91fd1bbb97f18
Martin Lagler <martin@sulu.io>
2025-11-14 10:50:53 +0100

    Update website SearchController

--- packages/search/src/UserInterface/Controller/Website/SearchController.php
+++ packages/search/src/UserInterface/Controller/Website/SearchController.php
@@ -17,13 +17,12 @@ use CmsIg\Seal\EngineInterface;
 use CmsIg\Seal\Search\Condition\Condition;
 use Sulu\Bundle\WebsiteBundle\Resolver\TemplateAttributeResolverInterface;
 use Sulu\Component\Webspace\Analyzer\RequestAnalyzerInterface;
-use Sulu\Content\UserInterface\Controller\Website\ContentController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Twig\Environment;
 
-class SearchController extends ContentController
+class SearchController
 {
     public function __construct(
         private readonly EngineInterface $engine,
@@ -36,7 +35,6 @@ class SearchController extends ContentController
     public function queryAction(Request $request): Response
     {
         $query = $request->query->get('q', '');
-        $requestFormat = $request->getRequestFormat() ?? 'html';
 
         $locale = $this->requestAnalyzer->getCurrentLocalization()->getLocale();
         $webspace = $this->requestAnalyzer->getWebspace();
@@ -68,7 +66,7 @@ class SearchController extends ContentController
 
         $parameters = $this->templateAttributeResolver->resolve($parameters);
 
-        $response = new Response($this->renderSuluView($template, $requestFormat, $parameters, false, false));
+        $response = new Response($this->twig->render($template, $parameters));
 
         // we need to set the content type ourselves here
         // else symfony will use the accept header of the client and the page could be cached with false content-type
