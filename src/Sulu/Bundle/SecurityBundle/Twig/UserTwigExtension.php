c5920efd8faaed5bf829467453084adf8fbcff42
mamazu <14860264+mamazu@users.noreply.github.com>
2025-11-20 17:17:14 +0100

    Removing doctrine cache

--- src/Sulu/Bundle/SecurityBundle/Twig/UserTwigExtension.php
+++ src/Sulu/Bundle/SecurityBundle/Twig/UserTwigExtension.php
@@ -14,6 +14,7 @@ namespace Sulu\Bundle\SecurityBundle\Twig;
 use Doctrine\Common\Cache\Cache;
 use Sulu\Bundle\SecurityBundle\Entity\User;
 use Sulu\Bundle\SecurityBundle\Entity\UserRepository;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -22,7 +23,7 @@ use Twig\TwigFunction;
  */
 class UserTwigExtension extends AbstractExtension
 {
-    public function __construct(private Cache $cache, private UserRepository $userRepository)
+    public function __construct(private ArrayAdapter $cache, private UserRepository $userRepository)
     {
     }
 
@@ -32,7 +33,7 @@ class UserTwigExtension extends AbstractExtension
     public function getFunctions()
     {
         return [
-            new TwigFunction('sulu_resolve_user', [$this, 'resolveUserFunction']),
+            new TwigFunction('sulu_resolve_user', $this->resolveUserFunction(...)),
         ];
     }
 
@@ -43,19 +44,10 @@ class UserTwigExtension extends AbstractExtension
      *
      * @return User
      */
-    public function resolveUserFunction($id)
+    public function resolveUserFunction(string $id)
     {
-        if ($this->cache->contains($id)) {
-            return $this->cache->fetch($id);
-        }
-
-        $user = $this->userRepository->findUserById($id);
-        if (null === $user) {
-            return;
-        }
-
-        $this->cache->save($id, $user);
-
-        return $user;
+        return $this->cache->get($id, function() use ($id) {
+            return $this->userRepository->findUserById($id);
+        });
     }
 }
