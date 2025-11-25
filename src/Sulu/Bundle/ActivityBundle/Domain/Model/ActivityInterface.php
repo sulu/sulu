c7b169745ac775ce306bd0aa9acaac1efdbda753
Prokyonn <daniel@sulu.io>
2025-11-25 08:49:41 +0100

    Add native types to ActivityBundle entities and interfaces

--- src/Sulu/Bundle/ActivityBundle/Domain/Model/ActivityInterface.php
+++ src/Sulu/Bundle/ActivityBundle/Domain/Model/ActivityInterface.php
@@ -20,7 +20,7 @@ interface ActivityInterface
 
     public function getType(): string;
 
-    public function setType(string $type): ActivityInterface;
+    public function setType(string $type): static;
 
     /**
      * @return mixed[]
@@ -30,7 +30,7 @@ interface ActivityInterface
     /**
      * @param mixed[] $context
      */
-    public function setContext(array $context): ActivityInterface;
+    public function setContext(array $context): static;
 
     /**
      * @return mixed[]|null
@@ -40,53 +40,53 @@ interface ActivityInterface
     /**
      * @param mixed[]|null $payload
      */
-    public function setPayload(?array $payload): ActivityInterface;
+    public function setPayload(?array $payload): static;
 
     public function getTimestamp(): \DateTimeImmutable;
 
-    public function setTimestamp(\DateTimeImmutable $timestamp): ActivityInterface;
+    public function setTimestamp(\DateTimeImmutable $timestamp): static;
 
     public function getBatch(): ?string;
 
-    public function setBatch(?string $batch): ActivityInterface;
+    public function setBatch(?string $batch): static;
 
     public function getUser(): ?UserInterface;
 
-    public function setUser(?UserInterface $user): ActivityInterface;
+    public function setUser(?UserInterface $user): static;
 
     public function getResourceKey(): string;
 
-    public function setResourceKey(string $resourceKey): ActivityInterface;
+    public function setResourceKey(string $resourceKey): static;
 
     public function getResourceId(): string;
 
-    public function setResourceId(string $resourceId): ActivityInterface;
+    public function setResourceId(string $resourceId): static;
 
     public function getResourceLocale(): ?string;
 
-    public function setResourceLocale(?string $resourceLocale): ActivityInterface;
+    public function setResourceLocale(?string $resourceLocale): static;
 
     public function getResourceWebspaceKey(): ?string;
 
-    public function setResourceWebspaceKey(?string $resourceWebspaceKey): ActivityInterface;
+    public function setResourceWebspaceKey(?string $resourceWebspaceKey): static;
 
     public function getResourceTitle(): ?string;
 
-    public function setResourceTitle(?string $resourceTitle): ActivityInterface;
+    public function setResourceTitle(?string $resourceTitle): static;
 
     public function getResourceTitleLocale(): ?string;
 
-    public function setResourceTitleLocale(?string $resourceTitleLocale): ActivityInterface;
+    public function setResourceTitleLocale(?string $resourceTitleLocale): static;
 
     public function getResourceSecurityContext(): ?string;
 
-    public function setResourceSecurityContext(?string $resourceSecurityContext): ActivityInterface;
+    public function setResourceSecurityContext(?string $resourceSecurityContext): static;
 
     public function getResourceSecurityObjectType(): ?string;
 
-    public function setResourceSecurityObjectType(?string $resourceSecurityObjectType): ActivityInterface;
+    public function setResourceSecurityObjectType(?string $resourceSecurityObjectType): static;
 
     public function getResourceSecurityObjectId(): ?string;
 
-    public function setResourceSecurityObjectId(?string $resourceSecurityObjectId): ActivityInterface;
+    public function setResourceSecurityObjectId(?string $resourceSecurityObjectId): static;
 }
