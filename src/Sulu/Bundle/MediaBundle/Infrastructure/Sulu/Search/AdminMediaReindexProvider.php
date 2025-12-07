b441b1a90e97221218ecf51a6d99f4662af31eb2
Alexander Schranz <alexander@sulu.io>
2025-12-07 18:30:47 +0100

    Fix more setParameters calls

--- src/Sulu/Bundle/MediaBundle/Infrastructure/Sulu/Search/AdminMediaReindexProvider.php
+++ src/Sulu/Bundle/MediaBundle/Infrastructure/Sulu/Search/AdminMediaReindexProvider.php
@@ -117,7 +117,10 @@ final class AdminMediaReindexProvider implements ReindexProviderInterface
             }
 
             $qb->where(\implode(' OR ', $conditions));
-            $qb->setParameters($parameters);
+
+            foreach ($parameters as $parameterKey => $parameterValue) {
+                $qb->setParameter($parameterKey, $parameterValue);
+            }
         }
 
         /** @var iterable<Media> */
