7d61816e7cf2fe97315a8379b4b475e2e6866e83
mamazu <14860264+mamazu@users.noreply.github.com>
2026-01-15 11:11:21 +0100

    Using a function to add directories to the cache

--- src/Sulu/Bundle/AdminBundle/DependencyInjection/Compiler/FormMetadataCachePass.php
+++ src/Sulu/Bundle/AdminBundle/DependencyInjection/Compiler/FormMetadataCachePass.php
@@ -22,11 +22,28 @@ class FormMetadataCachePass implements CompilerPassInterface
     public function process(ContainerBuilder $container): void
     {
         foreach ($container->getParameter('sulu_admin.forms.directories') as $directory) {
+            $this->addDirectory($directory, $container);
+        }
+        foreach ($container->getParameter('sulu_admin.lists.directories') as $directory) {
+            $this->addDirectory($directory, $container);
+        }
+
+        $this->addDirectory($container->getParameter('sulu_core.webspace.config_dir'), $container);
+
+        // Adding templates to the cache
+        foreach ($container->getParameter('sulu_admin.templates.configuration') as $configuration) {
+            foreach ($configuration['directories'] as $directory) {
+                $this->addDirectory($directory, $container);
+            }
+        }
+    }
+
+    private function addDirectory(string $directory, ContainerBuilder $container): void
+    {
+        // Resolving container parameters
+        $directory = \preg_replace_callback('#%([^%]+)%#', fn ($match) => $container->getParameter($match[1]), $directory);
+        if (\file_exists($directory)) {
             $container->addResource(new DirectoryResource($directory, '/\.xml$/'));
         }
-        foreach ($container->getParameter('sulu_admin.list.directories') as $directory) {
-            $container->addResource(new DirectoryResource($directory, '/\.xml$/'));
-        }
-        $container->addResource(new DirectoryResource($container->getParameter('sulu_core.webspace.config_dir'), '/\.xml$/'));
     }
 }
