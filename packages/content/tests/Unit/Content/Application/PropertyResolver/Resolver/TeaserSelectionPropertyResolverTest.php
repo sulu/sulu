10ea01947ff917cbc92881563dac6f1c31bdd3cb
Prokyonn <daniel.mathis@sector8.eu>
2026-01-15 16:12:32 +0100

    Fix teaser presentAs for 3.0 (#8528)

--- packages/content/tests/Unit/Content/Application/PropertyResolver/Resolver/TeaserSelectionPropertyResolverTest.php
+++ packages/content/tests/Unit/Content/Application/PropertyResolver/Resolver/TeaserSelectionPropertyResolverTest.php
@@ -15,7 +15,6 @@ namespace Sulu\Content\Tests\Unit\Content\Application\PropertyResolver\Resolver;
 
 use PHPUnit\Framework\TestCase;
 use Sulu\Bundle\AdminBundle\Teaser\Teaser;
-use Sulu\Content\Application\ContentResolver\Value\ContentView;
 use Sulu\Content\Application\ContentResolver\Value\ResolvableResource;
 use Sulu\Content\Application\PropertyResolver\Resolver\TeaserSelectionPropertyResolver;
 
@@ -33,7 +32,7 @@ class TeaserSelectionPropertyResolverTest extends TestCase
         $contentView = $this->resolver->resolve([], 'en');
 
         $this->assertSame([], $contentView->getContent());
-        $this->assertSame(['presentsAs' => null], $contentView->getView());
+        $this->assertSame(['presentAs' => null, 'items' => []], $contentView->getView());
     }
 
     public function testResolveWrongData(): void
@@ -41,7 +40,7 @@ class TeaserSelectionPropertyResolverTest extends TestCase
         $contentView = $this->resolver->resolve(['source' => 1], 'en');
 
         $this->assertSame([], $contentView->getContent());
-        $this->assertSame(['presentsAs' => null], $contentView->getView());
+        $this->assertSame(['presentAs' => null, 'items' => []], $contentView->getView());
     }
 
     public function testResolveParams(): void
@@ -49,13 +48,13 @@ class TeaserSelectionPropertyResolverTest extends TestCase
         $contentView = $this->resolver->resolve([], 'en', ['custom' => 'params']);
 
         $this->assertSame([], $contentView->getContent());
-        $this->assertSame(['presentsAs' => null, 'custom' => 'params'], $contentView->getView());
+        $this->assertSame(['presentAs' => null, 'items' => [], 'custom' => 'params'], $contentView->getView());
     }
 
     public function testResolveData(): void
     {
         $data = [
-            'presentsAs' => 'two-columns',
+            'presentAs' => 'two-columns',
             'items' => [
                 ['id' => '123', 'type' => 'article'],
                 ['id' => '456', 'type' => 'page'],
@@ -68,23 +67,22 @@ class TeaserSelectionPropertyResolverTest extends TestCase
         $this->assertIsArray($content);
         $this->assertCount(2, $content);
 
-        $innerContentView1 = $content[0];
-        $this->assertInstanceOf(ContentView::class, $innerContentView1);
-        $resolvableResource = $innerContentView1->getContent();
-        $this->assertInstanceOf(ResolvableResource::class, $resolvableResource);
-        $this->assertSame('article::123', $resolvableResource->getId());
-        $this->assertSame('teaser', $resolvableResource->getResourceLoaderKey());
-        $this->assertSame(['id' => '123', 'type' => 'article'], $innerContentView1->getView());
+        $resolvable1 = $content[0];
+        $this->assertInstanceOf(ResolvableResource::class, $resolvable1);
+        $this->assertSame('article::123', $resolvable1->getId());
+        $this->assertSame('teaser', $resolvable1->getResourceLoaderKey());
 
-        $innerContentView2 = $content[1];
-        $this->assertInstanceOf(ContentView::class, $innerContentView2);
-        $resolvableResource = $innerContentView2->getContent();
-        $this->assertInstanceOf(ResolvableResource::class, $resolvableResource);
-        $this->assertSame('page::456', $resolvableResource->getId());
-        $this->assertSame('teaser', $resolvableResource->getResourceLoaderKey());
-        $this->assertSame(['id' => '456', 'type' => 'page'], $innerContentView2->getView());
+        $resolvable2 = $content[1];
+        $this->assertInstanceOf(ResolvableResource::class, $resolvable2);
+        $this->assertSame('page::456', $resolvable2->getId());
+        $this->assertSame('teaser', $resolvable2->getResourceLoaderKey());
 
-        $this->assertSame(['presentsAs' => 'two-columns'], $contentView->getView());
+        $view = $contentView->getView();
+        $this->assertSame('two-columns', $view['presentAs']);
+        $this->assertIsArray($view['items']);
+        $this->assertCount(2, $view['items']);
+        $this->assertSame(['id' => '123', 'type' => 'article'], $view['items'][0]);
+        $this->assertSame(['id' => '456', 'type' => 'page'], $view['items'][1]);
     }
 
     public function testResolveCustomResourceLoader(): void
@@ -101,9 +99,7 @@ class TeaserSelectionPropertyResolverTest extends TestCase
         $this->assertIsArray($content);
         $this->assertCount(1, $content);
 
-        $innerContent = $content[0];
-        $this->assertInstanceOf(ContentView::class, $innerContent);
-        $resolvable = $innerContent->getContent();
+        $resolvable = $content[0];
         $this->assertInstanceOf(ResolvableResource::class, $resolvable);
         $this->assertSame('article::123', $resolvable->getId());
         $this->assertSame('custom_teaser', $resolvable->getResourceLoaderKey());
@@ -129,9 +125,7 @@ class TeaserSelectionPropertyResolverTest extends TestCase
         $this->assertIsArray($content);
         $this->assertCount(1, $content);
 
-        $innerContent = $content[0];
-        $this->assertInstanceOf(ContentView::class, $innerContent);
-        $resolvable = $innerContent->getContent();
+        $resolvable = $content[0];
         $this->assertInstanceOf(ResolvableResource::class, $resolvable);
         $this->assertSame('article::123', $resolvable->getId());
         $this->assertSame('teaser', $resolvable->getResourceLoaderKey());
