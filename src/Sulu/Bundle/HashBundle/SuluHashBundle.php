f377f6d9ab8858f865085bb15c67eaeabee1306c
mamazu <14860264+mamazu@users.noreply.github.com>
2026-01-19 14:18:21 +0100

    Migrating HashBundle to service php definition
    # Conflicts:
    #	src/Sulu/Bundle/HashBundle/DependencyInjection/SuluHashExtension.php
    #	src/Sulu/Bundle/HashBundle/SuluHashBundle.php

--- src/Sulu/Bundle/HashBundle/SuluHashBundle.php
+++ src/Sulu/Bundle/HashBundle/SuluHashBundle.php
@@ -11,8 +11,35 @@
 
 namespace Sulu\Bundle\HashBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Sulu\Component\Hash\AuditableHasher;
+use Sulu\Component\Hash\RequestHashChecker;
+use Sulu\Component\Hash\Serializer\Subscriber\HashSerializeEventSubscriber;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class SuluHashBundle extends Bundle
+final class SuluHashBundle extends AbstractBundle
 {
+    /**
+     * @param array<string, mixed> $config
+     */
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $services = $container->services();
+
+        $services->set('sulu_hash.auditable_hasher', AuditableHasher::class);
+
+        $services->set('sulu_hash.event_subscriber.serializer', HashSerializeEventSubscriber::class)
+            ->args([
+                new Reference('sulu_hash.auditable_hasher'),
+            ])
+            ->tag('jms_serializer.event_subscriber');
+
+        $services->set('sulu_hash.request_hash_checker', RequestHashChecker::class)
+            ->public()
+            ->args([
+                new Reference('sulu_hash.auditable_hasher'),
+            ]);
+    }
 }
