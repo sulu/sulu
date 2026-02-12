c6d73dccbfa8c22ba2720c79e67f2b0d05ca6425
Alexander Schranz <alexander@sulu.io>
2026-02-12 17:42:12 +0100

    Rename PageSmartContentFiltersVisitor to WebspaceSmartContentFiltersVisitor

--- packages/page/src/Infrastructure/Symfony/HttpKernel/SuluPageBundle.php
+++ packages/page/src/Infrastructure/Symfony/HttpKernel/SuluPageBundle.php
@@ -63,8 +63,8 @@ use Sulu\Page\Infrastructure\Sulu\Content\PropertyResolver\PageSelectionProperty
 use Sulu\Page\Infrastructure\Sulu\Content\PropertyResolver\PageTreeRoutePropertyResolver;
 use Sulu\Page\Infrastructure\Sulu\Content\PropertyResolver\SinglePageSelectionPropertyResolver;
 use Sulu\Page\Infrastructure\Sulu\Content\ResourceLoader\PageResourceLoader;
-use Sulu\Page\Infrastructure\Sulu\Content\Visitor\PageSmartContentFiltersVisitor;
 use Sulu\Page\Infrastructure\Sulu\Content\Visitor\SegmentSmartContentFiltersVisitor;
+use Sulu\Page\Infrastructure\Sulu\Content\Visitor\WebspaceSmartContentFiltersVisitor;
 use Sulu\Page\Infrastructure\Sulu\HttpCache\EventSubscriber\PageCacheInvalidationSubscriber;
 use Sulu\Page\Infrastructure\Sulu\Reference\PageReferenceRefresher;
 use Sulu\Page\Infrastructure\Sulu\Route\PageWebspaceRouteGenerator;
@@ -456,8 +456,8 @@ final class SuluPageBundle extends AbstractBundle
             ])
             ->tag('sulu_content.smart_content_provider', ['type' => PageInterface::RESOURCE_KEY]);
 
-        $services->set('sulu_page.page_smart_content_filters_visitor_webspace')
-            ->class(PageSmartContentFiltersVisitor::class)
+        $services->set('sulu_page.webspace_smart_content_filters_visitor_webspace')
+            ->class(WebspaceSmartContentFiltersVisitor::class)
             ->args([
                 new Reference('sulu_core.webspace.request_analyzer'),
                 new Reference('request_stack'),
