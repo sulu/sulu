ba189af4899b95fea5373c5f11fb7d810fc6d63c
mamazu <14860264+mamazu@users.noreply.github.com>
2026-02-14 14:52:39 +0100

    Using dataprovider and removing useless tests

--- src/Sulu/Bundle/AdminBundle/Resources/js/components/AutofocusableContent/tests/AutofocusableContent.test.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/components/AutofocusableContent/tests/AutofocusableContent.test.js
@@ -1,69 +1,43 @@
 // @flow
 import {render, screen} from '@testing-library/react';
-import React from 'react';
+import React, {Fragment} from 'react';
 import AutofocusableContent from '../AutofocusableContent';
 
 jest.mock('../../../utils/DOM/afterElementsRendered', () => jest.fn((callback) => callback()));
 
-test('The component should focus the first input when it mounts', () => {
-    render(
-        <AutofocusableContent>
-            <input data-testid="first-input" type="text" />
-            <input data-testid="second-input" type="text" />
-        </AutofocusableContent>
-    );
-
-    const firstInput = screen.getByTestId('first-input');
-    expect(firstInput).toHaveFocus();
-});
-
-test('The component should focus the first focusable element (textarea) when it comes before other inputs', () => {
-    render(
-        <AutofocusableContent>
-            <textarea data-testid="first-field" />
-            <input data-testid="second-input" type="text" />
-        </AutofocusableContent>
-    );
-
-    const firstField = screen.getByTestId('first-field');
-    expect(firstField).toHaveFocus();
-});
-
-test('The component should focus the first focusable element (select) when it is the only focusable', () => {
-    render(
-        <AutofocusableContent>
-            <select data-testid="first-select">
-                <option value="a">A</option>
-            </select>
-        </AutofocusableContent>
-    );
-
-    const firstSelect = screen.getByTestId('first-select');
-    expect(firstSelect).toHaveFocus();
-});
-
-test('The component should not focus hidden or disabled inputs', () => {
-    render(
-        <AutofocusableContent>
+test.each([
+    [
+        'the first input when multiple inputs',
+        <Fragment>
+            <input data-testid="focused-element" type="text" />
+            <input type="text" />
+        </Fragment>,
+    ],
+    [
+        'the textarea when it comes before other inputs',
+        <Fragment>
+            <textarea data-testid="focused-element" />
+            <input type="text" />
+        </Fragment>,
+    ],
+    [
+        'the select when it is the only focusable',
+        <select data-testid="focused-element">
+            <option value="a">A</option>
+        </select>,
+    ],
+    [
+        'the first focusable input, skipping hidden and disabled',
+        <Fragment>
             <input data-testid="hidden-input" type="hidden" />
             <input data-testid="disabled-input" type="text" disabled />
-            <input data-testid="focusable-input" type="text" />
-        </AutofocusableContent>
-    );
+            <input data-testid="focused-element" type="text" />
+        </Fragment>,
+    ],
+])('The component should focus %s when it mounts', (description, children) => {
+    render(<AutofocusableContent>{children}</AutofocusableContent>);
 
-    const focusableInput = screen.getByTestId('focusable-input');
-    expect(focusableInput).toHaveFocus();
-});
-
-test('The component should render children inside an article with the given className', () => {
-    render(
-        <AutofocusableContent className="my-article-class">
-            <span data-testid="child">Content</span>
-        </AutofocusableContent>
-    );
-
-    const child = screen.getByTestId('child');
-    expect(child.closest('article')).toHaveClass('my-article-class');
+    expect(screen.getByTestId('focused-element')).toHaveFocus();
 });
 
 test('The component should not throw when there is no focusable element', () => {
