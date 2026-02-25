053c9eec4bf4c47fc7cca8bd13e419a6c1fefdd7
patrick <patrick@sulu.io>
2026-02-25 13:37:39 +0100

    add check for http_cache

---
+++ src/Sulu/Bundle/HttpCacheBundle/DependencyInjection/SuluHttpCacheCompilerPass.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\HttpCacheBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class SuluHttpCacheCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        if ($container->hasDefinition('http_cache') && $container->getDefinition('http_cache')->isPublic()) {
+            throw new \InvalidArgumentException('Enabling the Symfony Http Cache is not compatible with Sulu! Cache is handled in index.php!');
+        }
+    }
+}
