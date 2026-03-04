f16472ef6117c85ee8265a0b0426db7c0de1ea14
Steeven <steeventhomas64@gmail.com>
2026-03-04 11:57:07 +0100

    fix: revert leftover force-delete references in DeleteToolbarAction and SuluCoreExtension
    Remove EXCEPTION_CODE_MEDIA_NOT_FOUND import and 410 dialog handling from
    DeleteToolbarAction.js, remove associated tests, and remove the
    MediaNotFoundExceptionInterface mapping from SuluCoreExtension.php.
    These were missed during the refactor commit.

--- src/Sulu/Bundle/CoreBundle/DependencyInjection/SuluCoreExtension.php
+++ src/Sulu/Bundle/CoreBundle/DependencyInjection/SuluCoreExtension.php
@@ -19,7 +19,6 @@ use Sulu\Component\Content\Types\Block\BlockVisitorInterface;
 use Sulu\Component\Rest\Csv\ObjectNotSupportedException;
 use Sulu\Component\Rest\Exception\InsufficientDescendantPermissionsException;
 use Sulu\Component\Rest\Exception\InvalidHashException;
-use Sulu\Component\Rest\Exception\MediaNotFoundExceptionInterface;
 use Sulu\Component\Rest\Exception\MissingParameterException;
 use Sulu\Component\Rest\Exception\ReferencingResourcesFoundExceptionInterface;
 use Sulu\Component\Rest\Exception\RemoveDependantResourcesFoundExceptionInterface;
@@ -100,7 +99,6 @@ class SuluCoreExtension extends Extension implements PrependExtensionInterface
                             RemoveDependantResourcesFoundExceptionInterface::class => 409,
                             InsufficientDescendantPermissionsException::class => 403,
                             ReferencingResourcesFoundExceptionInterface::class => 409,
-                            MediaNotFoundExceptionInterface::class => 410,
                         ],
                         'exception_listener' => false,
                         'serialize_exceptions' => false,
