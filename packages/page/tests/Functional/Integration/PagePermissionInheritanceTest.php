9e75d9447fbf91086ccfc9f547a3dbeb08fcd417
Prokyonn <daniel@sulu.io>
2026-04-21 17:29:02 +0200

    Fix permission inheritance overlay on pages
    # Conflicts:
    #	packages/page/tests/Functional/Integration/PagePermissionInheritanceTest.php

--- packages/page/tests/Functional/Integration/PagePermissionInheritanceTest.php
+++ packages/page/tests/Functional/Integration/PagePermissionInheritanceTest.php
@@ -20,6 +20,7 @@ use Sulu\Bundle\TestBundle\Testing\SuluTestCase;
 use Sulu\Component\Security\Authorization\AccessControl\AccessControlManagerInterface;
 use Sulu\Page\Domain\Model\Page;
 use Sulu\Page\Tests\Traits\CreatePageTrait;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 
 /**
  * The integration test should have no impact on the coverage so we set it to coversNothing.
@@ -29,7 +30,104 @@ class PagePermissionInheritanceTest extends SuluTestCase
 {
     use CreatePageTrait;
 
-    public function testChildPageInheritsParentPermissionsOnCreation(): void
+    protected KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        $this->client = $this->createAuthenticatedClient(
+            [],
+            ['CONTENT_TYPE' => 'application/json', 'HTTP_ACCEPT' => 'application/json'],
+        );
+    }
+
+    public function testGetPageReturnsHasSubFalseForLeafPage(): void
+    {
+        self::purgeDatabase();
+
+        $homepage = $this->createPage([
+            'en' => [
+                'live' => [
+                    'template' => 'default',
+                    'title' => 'Homepage',
+                    'url' => '/',
+                ],
+            ],
+        ]);
+
+        $leafPage = $this->createPage([
+            'en' => [
+                'live' => [
+                    'template' => 'default',
+                    'title' => 'Leaf Page',
+                    'url' => '/leaf',
+                    'parentId' => $homepage->getUuid(),
+                ],
+            ],
+        ]);
+
+        self::ensureKernelShutdown();
+
+        $this->client->request('GET', '/admin/api/pages/' . $leafPage->getUuid() . '?locale=en');
+        $response = $this->client->getResponse();
+
+        $this->assertHttpStatusCode(200, $response);
+        /** @var array<string, mixed> $content */
+        $content = \json_decode((string) $response->getContent(), true);
+
+        $this->assertArrayHasKey('hasSub', $content);
+        $this->assertFalse($content['hasSub']);
+    }
+
+    public function testGetPageReturnsHasSubTrueForPageWithChildren(): void
+    {
+        self::purgeDatabase();
+
+        $homepage = $this->createPage([
+            'en' => [
+                'live' => [
+                    'template' => 'default',
+                    'title' => 'Homepage',
+                    'url' => '/',
+                ],
+            ],
+        ]);
+
+        $parentPage = $this->createPage([
+            'en' => [
+                'live' => [
+                    'template' => 'default',
+                    'title' => 'Parent Page',
+                    'url' => '/parent',
+                    'parentId' => $homepage->getUuid(),
+                ],
+            ],
+        ]);
+
+        $this->createPage([
+            'en' => [
+                'live' => [
+                    'template' => 'default',
+                    'title' => 'Child Page',
+                    'url' => '/parent/child',
+                    'parentId' => $parentPage->getUuid(),
+                ],
+            ],
+        ]);
+
+        self::ensureKernelShutdown();
+
+        $this->client->request('GET', '/admin/api/pages/' . $parentPage->getUuid() . '?locale=en');
+        $response = $this->client->getResponse();
+
+        $this->assertHttpStatusCode(200, $response);
+        /** @var array<string, mixed> $content */
+        $content = \json_decode((string) $response->getContent(), true);
+
+        $this->assertArrayHasKey('hasSub', $content);
+        $this->assertTrue($content['hasSub']);
+    }
+
+    public function testPermissionInheritanceSetsPermissionsOnChildPages(): void
     {
         self::purgeDatabase();
 
@@ -104,6 +202,16 @@ class PagePermissionInheritanceTest extends SuluTestCase
         $accessControlManager = self::getContainer()->get('sulu_security.access_control_manager');
         $childPermissions = $accessControlManager->getPermissions(Page::class, $childPage->getUuid());
 
+        // Must not throw AccessControlDescendantProviderNotFoundException for Page type
+        $accessControlManager->setPermissions(
+            Page::class,
+            $parentPage->getUuid(),
+            [$roleId => ['view' => true, 'add' => true, 'edit' => true, 'delete' => true, 'live' => true, 'security' => true]],
+            true,
+        );
+
+        $childPermissions = $accessControlManager->getPermissions(Page::class, $parentPage->getUuid());
+        $this->assertArrayHasKey($roleId, $childPermissions);
         $this->assertSame($expectedPermissions, $childPermissions);
     }
 }
