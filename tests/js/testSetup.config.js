498c35441d19ae964aad03334b03b25d418a93d6
Prokyonn <daniel@sulu.io>
2026-04-29 15:03:56 +0200

    Fix node linting issues after dependency update

--- tests/js/testSetup.config.js
+++ tests/js/testSetup.config.js
@@ -1,4 +1,6 @@
 // @flow
+// eslint-disable-next-line import/no-nodejs-modules
+import {TextDecoder, TextEncoder} from 'util';
 import 'core-js/features/string/replace-all';
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
@@ -6,6 +8,16 @@ import {isObservableArray, toJS} from 'mobx';
 import '@testing-library/jest-dom';
 
 Enzyme.configure({adapter: new Adapter()});
+
+Object.defineProperty(window, 'TextDecoder', {
+    writable: true,
+    value: TextDecoder,
+});
+
+Object.defineProperty(window, 'TextEncoder', {
+    writable: true,
+    value: TextEncoder,
+});
 
 function mobxAwareEqualityTester(a, b, customTesters) {
     const isAObservable = isObservableArray(a);
