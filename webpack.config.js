34223489a7415a5f661e7053fe1a96c1eebfb7b2
Alexander Schranz <alexander@sulu.io>
2026-05-26 08:57:14 +0200

    Fix warning

--- webpack.config.js
+++ webpack.config.js
@@ -99,7 +99,7 @@ module.exports = (env, argv) => { // eslint-disable-line no-undef
                         options: {
                             cacheDirectory: true,
                             cacheCompression: false,
-                            compact: true,
+                            compact: argv.mode === 'production',
                         },
                     },
                 },
