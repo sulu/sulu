c2f34454dff6f34ff12938a88e9daa2e56071111
Martin Lagler <martin@sulu.io>
2026-06-10 15:16:22 +0200

    Update StringFieldTransformer condition

--- src/Sulu/Bundle/AdminBundle/Resources/js/containers/List/fieldTransformers/StringFieldTransformer.js
+++ src/Sulu/Bundle/AdminBundle/Resources/js/containers/List/fieldTransformers/StringFieldTransformer.js
@@ -6,7 +6,7 @@ import type {FieldTransformer} from '../types';
 
 export default class StringFieldTransformer implements FieldTransformer {
     transform(value: *): Node {
-        if (!value) {
+        if (value == null || value === '') {
             return null;
         }
 
