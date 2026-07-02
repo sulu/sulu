485abe73ef97177df94791bf40370b544aeb5a00
Prokyonn <daniel@sulu.io>
2026-07-02 17:31:40 +0200

    Fix linting issues

--- src/Sulu/Component/Rest/RequestParametersTrait.php
+++ src/Sulu/Component/Rest/RequestParametersTrait.php
@@ -58,12 +58,13 @@ trait RequestParametersTrait
      */
     protected function getBooleanRequestParameter($request, $name, $force = false, $default = null)
     {
+        /** @var mixed $value */
         $value = $this->getRequestParameter($request, $name, $force, $default);
         if ('true' === $value || true === $value) {
             $value = true;
         } elseif ('false' === $value || false === $value) {
             $value = false;
-        } elseif ($force && true !== $value && false !== $value) {
+        } elseif ($force) {
             throw new ParameterDataTypeException(\get_class($this), $name);
         } else {
             $value = $default;
