afc685ee6e2a49d95528abbfce4763dd3198dd33
Dirk <155744902+rhp-zellerd@users.noreply.github.com>
2026-07-07 09:43:20 +0200

    Normalize image format extension to lowercase
    Convert image format extension to lowercase for consistency.

--- src/Sulu/Bundle/MediaBundle/Media/FormatManager/FormatManager.php
+++ src/Sulu/Bundle/MediaBundle/Media/FormatManager/FormatManager.php
@@ -79,7 +79,7 @@ class FormatManager implements FormatManagerInterface
                 throw new ImageProxyInvalidUrl(\sprintf('No `extension` was found in the url "%s".', $fileName));
             }
 
-            $imageFormat = $info['extension'];
+            $imageFormat = strtolower($info['extension']);
 
             $media = $this->mediaRepository->findMediaByIdForRendering($id, $formatKey, $version);
 
