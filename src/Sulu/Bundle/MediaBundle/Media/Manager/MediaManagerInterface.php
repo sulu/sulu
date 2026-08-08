123edbd14e3efa5b85527a84d346640fe1749b22
mamazu <14860264+mamazu@users.noreply.github.com>
2026-08-08 16:57:13 +0200

    Adding download count test in MediaController

--- src/Sulu/Bundle/MediaBundle/Media/Manager/MediaManagerInterface.php
+++ src/Sulu/Bundle/MediaBundle/Media/Manager/MediaManagerInterface.php
@@ -89,6 +89,8 @@ interface MediaManagerInterface
      * Deletes a media with a given id.
      *
      * @param int $id the id of the category to delete
+     *
+     * @return void
      */
     public function delete($id, $checkSecurity = false);
 
@@ -110,6 +112,8 @@ interface MediaManagerInterface
      * Increase the download counter of a fileVersion.
      *
      * @param int $fileVersionId
+     *
+     * @return void
      */
     public function increaseDownloadCounter($fileVersionId);
 
