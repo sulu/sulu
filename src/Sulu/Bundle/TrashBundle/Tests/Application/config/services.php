b2a60ff931fae127e5785f1b92f6f8d3e8f8e06e
Alexander Schranz <alexander@sulu.io>
2026-08-12 08:40:14 +0200

    Migrate TrashBundle test services.xml to services.php (#9003)
    Prepare TrashBundle tests for Symfony 8 compatibility

---
+++ src/Sulu/Bundle/TrashBundle/Tests/Application/config/services.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Sulu\Bundle\TrashBundle\Tests\Application\Trash\TestTrashItemHandler;
+use Symfony\Component\DependencyInjection\Reference;
+
+return static function(ContainerConfigurator $container) {
+    $services = $container->services();
+
+    $services->set('test_trash_item_handler', TestTrashItemHandler::class)
+        ->args([
+            new Reference('sulu_trash.trash_item_repository'),
+        ])
+        ->tag('sulu_trash.store_trash_item_handler')
+        ->tag('sulu_trash.restore_trash_item_handler')
+        ->tag('sulu_trash.restore_configuration_provider');
+};
