9386430878df81b2c9c873f51dbfc1b5786ce61f
Martin Lagler <martin@sulu.io>
2026-09-14 10:16:48 +0200

    Fix phpstan errors

--- src/Sulu/Bundle/TrashBundle/Tests/Functional/UserInterface/Controller/Admin/TrashItemControllerTest.php
+++ src/Sulu/Bundle/TrashBundle/Tests/Functional/UserInterface/Controller/Admin/TrashItemControllerTest.php
@@ -127,7 +127,7 @@ class TrashItemControllerTest extends SuluTestCase
                     $resourceSecurityObjectId
                 );
 
-                if (null !== $resourceSecurityObjectType && null !== $resourceSecurityObjectId) {
+                if (null !== $resourceSecurityObjectType) {
                     $accessControlManager->setPermissions(
                         $resourceSecurityObjectType,
                         $resourceSecurityObjectId,
